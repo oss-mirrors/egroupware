a403a8e3025468de7a1efc7940ca894a590f8508
jpkh <jpkh@923e3c2e-ae13-0410-ba18-ced7211e33de>
2000-08-31 09:43:43 +0000

    Admin link fixed to point to new dir Initial CVS import
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@144 923e3c2e-ae13-0410-ba18-ced7211e33de

---
+++ forum/admin/index.php
@@ -0,0 +1,27 @@
+<?php
+  /**************************************************************************\
+  * phpGroupWare - Forums                                                    *
+  * http://www.phpgroupware.org                                              *
+  * Written by Jani Hirvinen <jpkh@shadownet.com>                            *
+  * --------------------------------------------                             *
+  *  This program is free software; you can redistribute it and/or modify it *
+  *  under the terms of the GNU General Public License as published by the   *
+  *  Free Software Foundation; either version 2 of the License, or (at your  *
+  *  option) any later version.                                              *
+  \**************************************************************************/
+
+  $phpgw_flags["currentapp"] = "forum";
+  include("../header.inc.php");
+
+?>
+
+
+
+
+
+
+
+<?
+  include($phpgw_info["server"]["api_dir"] . "/footer.inc.php");
+?>
+
