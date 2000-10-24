99305628cd9b8d4e67eb03c5b734039af312ec9c
skeeter <skeeter@923e3c2e-ae13-0410-ba18-ced7211e33de>
2000-10-24 02:04:55 +0000

    applied patch #102089 - scrollbars for addressbook when composing email
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@492 923e3c2e-ae13-0410-ba18-ced7211e33de

--- email/compose.php
+++ email/compose.php
@@ -88,7 +88,8 @@
     self.name="first_Window";
     function addressbook()
     {
-       Window1=window.open('<?php echo $phpgw->link("addressbook.php","query="); ?>+document.doit.to.value',"Search","width=500,height=300","toolbar=yes,resizable=yes");
+<!--   Window1=window.open('<?php echo $phpgw->link("addressbook.php","query="); ?>+document.doit.to.value',"Search","width=500,height=300","toolbar=yes,resizable=yes");  -->
+       Window1=window.open('<?php echo $phpgw->link("addressbook.php","query="); ?>+document.doit.to.value',"Search","width=500,height=300,toolbar=yes,scrollbars=yes,resizable=yes");
     }
 
     function attach_window(url)
