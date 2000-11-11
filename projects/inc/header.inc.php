2db5a047aebd0be62bad2ae5d207c46f261d8cb3
bettina <bettina@923e3c2e-ae13-0410-ba18-ced7211e33de>
2000-11-11 19:47:36 +0000

    update app-navigation
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@626 923e3c2e-ae13-0410-ba18-ced7211e33de

--- projects/inc/header.inc.php
+++ projects/inc/header.inc.php
@@ -11,80 +11,32 @@
   *  Free Software Foundation; either version 2 of the License, or (at your  *
   *  option) any later version.                                              *
   \**************************************************************************/
-
-       $phpgw->db->query("select app_name from applications where (app_enabled > 0  and app_name='projects')");
-       $lid = $phpgw_info["user"]["userid"];
        
-       while ($phpgw->db->next_record()); 
-       
-       if (gettype($id) == "integer") {
-          $phpgw->db->query("select account_permissions from accounts where account_id='$lid'");
-       } else {
-          $phpgw->db->query("select account_permissions from accounts where account_lid='$lid'");
-       }
-       $phpgw->db->next_record();
-
-       $pl = explode(":",$phpgw->db->f("account_permissions"));
-
-       for ($i=0; $i<count($pl); $i++) {
-          if ($enabled_apps[$pl[$i]]) {
-             $enabled_apps[$pl[$i]] = 2;
-          }
-       }
-       
-       // This is to prevent things from being loaded twice
-       if ($phpgw_info["user"]["userid"] == $lid) {
-          $group_list = $this->groups;
-       } else {
-          $group_list = $this->read_groups($lid);
-       }
-
-       for ($k=0; $k<count($group_list); $k++) {
-          $phpgw->db->query("select group_apps from groups where group_id='"
-				. "$group_list[$i]'");
-          $phpgw->db->next_record();
-
-          $gp = explode(",",$phpgw->db->f("group_apps"));
-          for ($i=1,$j=0;$i<count($gp)-1;$i++,$j++) {
-             $apps[$gp[$i]] = 2;
-          }
-       }
+      $lid = $phpgw_info["user"]["userid"];                                                                                                                                                                                                                                                                                                                            
+      
       $t = new Template($phpgw_info["server"]["app_tpl"]);
       $t->set_file(array("projects_header" => "header.tpl"));
       $phpgw->db->query("select group_id from groups where group_name = 'projectAdmin'");
-
-      $admin_info = lang("Projects");
+      $admin_info = lang("projects");
+      
       if ($phpgw->db->next_record()) {
          $group_id = $phpgw->db->f("group_id");
 	 $phpgw->db->query("select account_lid from accounts where account_groups like '%,$group_id%' and account_lid='$lid'");
          $phpgw->db->next_record();
 	 if($phpgw->db->f("account_lid") == $lid)
-            $admin_info = lang("Projects")."&nbsp;&nbsp;&nbsp;".lang("Admin");
+            $admin_info = lang("projects")."&nbsp;&nbsp;&nbsp;".lang("admin");
 	 }
       $t->set_var("admin_info", "<td bgcolor=\"" . $phpgw_info["theme"]["th_bg"] 
                         . "\" align=\"left\"><b>" . $admin_info . "</b>");
 
- /*  if($apps["projectbilling"]==True)
-     $t->set_var("link_billing","<a href=\"" . $phpgw->link("../projectbilling/") . "\">" . lang("Projectbilling") ."</a>");
-     else
-     $t->set_var("link_billing","");
+     
+     
 
-     if($apps["projecthours"]==True)
-     $t->set_var("link_hours","<a href=\"" . $phpgw->link("../projecthours/") . "\">" . lang("Projecthours") ."</a>");
-     else
-    $t->set_var("link_hours","");
+     $t->set_var("link_billing","<a href=\"" . $phpgw->link("../projectbilling/") . "\">" . lang("projectbilling") ."</a>");
+     $t->set_var("link_hours","<a href=\"" . $phpgw->link("../projecthours/") . "\">" . lang("projecthours") ."</a>");
+     $t->set_var("link_statistics","<a href=\"" . $phpgw->link("../projectstatistics/") . "\">" . lang("projectstatistics") ."</a>");
+     $t->set_var("link_delivery","<a href=\"" . $phpgw->link("../projectdelivery/") . "\">" . lang("projectdelivery") ."</a>");                                                                                 
 
-    if($apps["projectstatistics"]==True)
-    $t->set_var("link_statistics","<a href=\"" . $phpgw->link("../projectstatistics/") . "\">" . lang("Projectstatistics") ."</a>");
-    else
-    $t->set_var("link_statistics","");
-
-    if($apps["projects"]==True)
-    $t->set_var("link_projects","<a href=\"" . $phpgw->link("../projects/") . "\">" . lang("Projects") ."</a>");
-    else
-    $t->set_var("link_projects","");
- */
-
-$t->pparse("out","projects_header");
+     $t->pparse("out","projects_header");
  
 ?>
--- projects/templates/default/header.tpl
+++ projects/templates/default/header.tpl
@@ -3,5 +3,24 @@
 <table border="0" width=100%>
 <tr>
 {admin_info}
+</td>
 </tr>
+<tr>
+ <td>
+
+{link_hours}
+&nbsp;
+&nbsp;
+{link_statistics}
+&nbsp;
+&nbsp;
+
+{link_billing}
+&nbsp;
+&nbsp;
+
+{link_delivery}
+
+  </font>
+</td></tr>
 </table>
