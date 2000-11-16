93cf2df8cfcb2f76543c578a7216a376ca973e77
lkneschke <lkneschke@923e3c2e-ae13-0410-ba18-ced7211e33de>
2000-11-16 22:03:49 +0000

    added "enable_nextmatchs_class" => True
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@703 923e3c2e-ae13-0410-ba18-ced7211e33de

--- email/preferences.php
+++ email/preferences.php
@@ -12,7 +12,9 @@
   /* $Id$ */
 
   $phpgw_info["flags"] = array("currentapp" => "email", "enable_message_class" => True, 
-                                "noheader" => True, "nonavbar" => True);
+                                "noheader" => True, "nonavbar" => True,
+				"enable_nextmatchs_class" => True);
+
   include("../header.inc.php");
 
   if ($submit) {
