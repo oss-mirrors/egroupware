232263706dec63e963992c20cf22b5ef7f0da8dd
seek3r <seek3r@923e3c2e-ae13-0410-ba18-ced7211e33de>
2000-11-20 07:55:30 +0000

    making the user manual into a phpgw app
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@771 923e3c2e-ae13-0410-ba18-ced7211e33de

--- manual/index.php
+++ manual/index.php
@@ -1,5 +1,18 @@
-<html>
-<body bgcolor="white">
+<?php
+  /**************************************************************************\
+  * phpGroupWare module (File Manager)                                       *
+  * http://www.phpgroupware.org                                              *
+  * Written by Dan Kuykendall <dan@kuykendall.org>                           *
+  * --------------------------------------------                             *
+  *  This program is free software; you can redistribute it and/or modify it *
+  *  under the terms of the GNU General Public License as published by the   *
+  *  Free Software Foundation; either version 2 of the License, or (at your  *
+  *  option) any later version.                                              *
+  \**************************************************************************/
+
+  $phpgw_info["flags"] = array("currentapp" => "manual");
+  include("../header.inc.php");
+?>
 <table cellpadding="10" width=20%>
 <td>
 <font face="Arial, Helvetica, san-serif" size="2">
@@ -10,6 +23,4 @@
 ?>
 </td>
 </table>
-</body>
-</html>
-
+<?php include($phpgw_info["server"]["api_dir"] . "/footer.inc.php"); ?>
