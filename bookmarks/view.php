35555815f5c9f88bc4e92345050e8c146057c418
jengo <jengo@923e3c2e-ae13-0410-ba18-ced7211e33de>
2000-11-25 06:57:16 +0000

    Added file for new view
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@885 923e3c2e-ae13-0410-ba18-ced7211e33de

---
+++ bookmarks/view.php
@@ -0,0 +1,18 @@
+<?php 
+  /**************************************************************************\
+  * phpGroupWare - Bookmarks                                                 *
+  * http://www.phpgroupware.org                                              *
+  * --------------------------------------------                             *
+  *  This program is free software; you can redistribute it and/or modify it *
+  *  under the terms of the GNU General Public License as published by the   *
+  *  Free Software Foundation; either version 2 of the License, or (at your  *
+  *  option) any later version.                                              *
+  \**************************************************************************/
+
+  /* $Id$ */
+
+  $phpgw_info["flags"]["currentapp"] = "bookmarks";
+  $phpgw_info["flags"]["enable_nextmatchs_class"] = True;
+  include("../header.inc.php");
+?>
+  <center>Not avaiable yet</center>
