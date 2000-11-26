8937162602f65b37a521c7cb8a53b34ab80467e0
jengo <jengo@923e3c2e-ae13-0410-ba18-ced7211e33de>
2000-11-26 09:33:37 +0000

    Working on view for bookmarks
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@914 923e3c2e-ae13-0410-ba18-ced7211e33de

--- bookmarks/view.php
+++ bookmarks/view.php
@@ -14,5 +14,52 @@
   $phpgw_info["flags"]["currentapp"] = "bookmarks";
   $phpgw_info["flags"]["enable_nextmatchs_class"] = True;
   include("../header.inc.php");
+  
+  $phpgw->template->set_file(array("body"     => "maintain.body.tpl",
+                                   "standard" => "common.standard.tpl"
+                            ));
+  set_standard("create", &$phpgw->template);
+  
+  $phpgw->db->query("select * from bookmarks where username='" . $phpgw_info["user"]["account_id"]
+                  . "'",__LINE__,__FILE__);
+  $phpgw->db->next_record();
+
+  $ts = explode(",",$bm_timestamps_raw);
+  $f_ts[0] = $phpgw->common->show_date($ts[0]);
+
+  if ($ts[1]) {
+     $f_ts[1] = $phpgw->common->show_date($ts[1]);
+  } else {
+     $f_ts[1] = lang("Never");
+  }
+
+  if ($ts[2]) {
+     $f_ts[2] = $phpgw->common->show_date($ts[2]);
+  } else {
+     $f_ts[2] = lang("Never");
+  }
+
+  $phpgw->template->set_var(array(
+    DEFAULT_URL            => $default_url,
+    DEFAULT_NAME           => htmlspecialchars(stripslashes($default_name)),
+    DEFAULT_DESC           => htmlspecialchars(stripslashes($default_desc)),
+    DEFAULT_KEYW           => htmlspecialchars(stripslashes($default_keyw)),
+    DEFAULT_CATEGORY       => $default_category,
+    DEFAULT_SUBCATEGORY    => $default_subcategory,
+    DEFAULT_RATING         => $default_rating,
+    RATING_SELECT          => $rating_select,
+    CATEGORY_SELECT        => $category_select,
+    SUBCATEGORY_SELECT     => $subcategory_select,
+    RATING_SELECT          => $rating_select,
+    DEFAULT_PUBLIC         => $default_public
+                                        ADDED              => $f_ts[0],
+                                        VISTED             => $f_ts[1],
+                                        UPDATED            => $f_ts[2],
+                                        ADDED_VALUE        => $ts[0],
+                                        VISTED_VALUE       => $ts[1],
+                                        TOTAL_VISTS        => $total_vists,
+
+  ));
+
+  include($phpgw_info["server"]["server_root"] . "/bookmarks/inc/footer.inc.php");
 ?>
-  <center>Not avaiable yet</center>
