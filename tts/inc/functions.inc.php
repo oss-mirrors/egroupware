ff202d9a3c02f47ec46e34529726c00ea2e3c010
skeeter <skeeter@923e3c2e-ae13-0410-ba18-ced7211e33de>
2000-11-27 11:47:38 +0000

    Fix for TTS with Update by non-email user
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@930 923e3c2e-ae13-0410-ba18-ced7211e33de

--- tts/inc/functions.inc.php
+++ tts/inc/functions.inc.php
@@ -71,7 +71,13 @@ function mail_ticket($ticket_id) {
   } else {
     $to = $toarray[0];
   }
-  
+
+  if($phpgw_info["user"]["apps"]["email"]) {
+    $phpgw->preferences->read_preferences("email");
+  } else {
+    $phpgw_info["user"]["preferences"]["email"]["address"] = $phpgw_info["user"]["account_lid"]."@".$phpgw_info["server"]["mail_suffix"];
+  }
+
   $rc = $phpgw->send->msg("email", $to, $subject, stripslashes($body), "", $cc, $bcc);
   if (!$rc) {
     echo "Your message could <B>not</B> be sent!<BR>\n";
