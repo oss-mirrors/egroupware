269959b87d1c9214f01688335b899105ccb43a45
skeeter <skeeter@923e3c2e-ae13-0410-ba18-ced7211e33de>
2000-12-20 04:28:37 +0000

    fixes for preparation for 0.9.8
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@1174 923e3c2e-ae13-0410-ba18-ced7211e33de

--- headlines/inc/hook_preferences.inc.php
+++ headlines/inc/hook_preferences.inc.php
@@ -9,7 +9,7 @@
   *  Free Software Foundation; either version 2 of the License, or (at your  *
   *  option) any later version.                                              *
   \**************************************************************************/
-  /* $ Id $ */
+  /* $Id$ */
 {
   echo "<p>";
   $imgfile = $phpgw->common->get_image_dir($appname)."/".$appname.".gif";
