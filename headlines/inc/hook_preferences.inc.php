b27089d5cb43808207eeeb774e1b301a3fb7e0b0
skeeter <skeeter@923e3c2e-ae13-0410-ba18-ced7211e33de>
2000-12-20 03:21:52 +0000

    update hook
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@1171 923e3c2e-ae13-0410-ba18-ced7211e33de

--- headlines/inc/hook_preferences.inc.php
+++ headlines/inc/hook_preferences.inc.php
@@ -1,17 +1,29 @@
 <?php
+  /**************************************************************************\
+  * phpGroupWare                                                             *
+  * http://www.phpgroupware.org                                              *
+  * Written by Joseph Engo <jengo@phpgroupware.org>                          *
+  * --------------------------------------------                             *
+  *  This program is free software; you can redistribute it and/or modify it *
+  *  under the terms of the GNU General Public License as published by the   *
+  *  Free Software Foundation; either version 2 of the License, or (at your  *
+  *  option) any later version.                                              *
+  \**************************************************************************/
+  /* $ Id $ */
 {
-  $img = "/" . $appname . "/images/" . $appname .".gif";
-  if (file_exists($phpgw_info["server"]["server_root"].$img)) {
-    $img = $phpgw_info["server"]["webserver_url"].$img;
+  echo "<p>";
+  $imgfile = $phpgw->common->get_image_dir("headlines")."/".$appname.".gif";
+  if (file_exists($imgfile)) {
+    $imgpath = $phpgw->common->get_image_path("headlines")."/".$appname.".gif";
   } else {
-    $img = "/" . $appname . "/images/navbar.gif";
-    if (file_exists($phpgw_info["server"]["server_root"].$img)) {
-      $img=$phpgw_info["server"]["webserver_url"].$img;
+    $imgfile = $phpgw->common->get_image_dir("headlines")."/navbar.gif";
+    if (file_exists($imgfile)) {
+      $imgpath = $phpgw->common->get_image_path("headlines")."/navbar.gif";
     } else {
-    $img = "";
+      $imgpath = "";
     }
   }
-  section_start("Headlines",$img);
+  section_start("Headlines",$imgpath);
 
   $pg = $phpgw->link($phpgw_info["server"]["webserver_url"]."/headlines/preferences.php");
   echo "<A href=".$pg.">Select headlines to display</A>";
