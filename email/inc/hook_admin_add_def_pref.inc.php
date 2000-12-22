49faee3478d5f3ebb85a0b1c8fd80f9d76706568
skeeter <skeeter@923e3c2e-ae13-0410-ba18-ced7211e33de>
2000-12-22 21:18:35 +0000

    new hook file for preferences
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@1200 923e3c2e-ae13-0410-ba18-ced7211e33de

---
+++ email/inc/hook_admin_add_def_pref.inc.php
@@ -0,0 +1,6 @@
+<?php
+  $phpgw->preferences->add_newuser("email","mainscreen_showmail","True");
+  $phpgw->preferences->add_newuser("email","use_trash_folder","False");
+  $phpgw->preferences->add_newuser("email","default_sorting","old_new");
+  $phpgw->preferences->add_newuser("email","email_sig","");
+?>
