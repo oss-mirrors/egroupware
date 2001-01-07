c2e001f0118b63bb42209062c996a3c8c5cd8f0d
skeeter <skeeter@923e3c2e-ae13-0410-ba18-ced7211e33de>
2001-01-07 21:48:25 +0000

    minor fixes for reading out of bounds email
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@1455 923e3c2e-ae13-0410-ba18-ced7211e33de

--- email/inc/hook_home.inc.php
+++ email/inc/hook_home.inc.php
@@ -34,8 +34,8 @@
     $title = '<a href="'.$phpgw->link($phpgw_info["server"]["webserver_url"]."/email/index.php").'">EMail'.($str ? ' - '.$str : '').'</a>';
     $portalbox = new linkbox($title,$phpgw_info["theme"]["navbar_bg"],$phpgw_info["theme"]["bg_color"],$phpgw_info["theme"]["bg_color"]);
     $portalbox->setvar('width',600);
-    if($nummsg >= 5) { $check_msgs = 4; } else { $check_msgs = $nummsg; }
-    for($i=$nummsg - $check_msgs,$j=0;$i<=$nummsg;$i++,$j++) {
+    if($nummsg >= 5) { $check_msgs = 5; } else { $check_msgs = $nummsg; }
+    for($i=$nummsg - $check_msgs + 1,$j=0;$i<=$nummsg;$i++,$j++) {
       $msg = $phpgw->msg->header($mailbox,$i);
       $subject = !$msg->Subject ? '['.lang("no subject").']' : substr($msg->Subject,0,65).' ...';
       $portalbox->data[$j] = array(decode_header_string($subject),$phpgw->link($phpgw_info["server"]["webserver_url"]."/email/message.php","folder=".urlencode($folder)."&msgnum=".$i));
