db9d53888e35de933c7501c1a8f92efd36da1077
skeeter <skeeter@923e3c2e-ae13-0410-ba18-ced7211e33de>
2001-01-07 20:47:29 +0000

    uses mailbox instead of mbox
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@1452 923e3c2e-ae13-0410-ba18-ced7211e33de

--- email/inc/hook_home.inc.php
+++ email/inc/hook_home.inc.php
@@ -1,4 +1,5 @@
 <?php
+ /* $Id$ */
   $d1 = strtolower(substr($phpgw_info["server"]["app_inc"],0,3));
   if($d1 == "htt" || $d1 == "ftp" ) {
     echo "Failed attempt to break in via an old Security Hole!<br>\n";
@@ -11,13 +12,12 @@
   if ($phpgw_info["user"]["preferences"]["email"]["mainscreen_showmail"]) {
     include($phpgw_info["server"]["app_inc"] . "/functions.inc.php");
     echo "<!-- Mailox info -->\n";
-    $mbox = $phpgw->msg->login();
-    if (! $mbox) {
-      echo "Mail error: can not open connection to mail server";
-      $phpgw->common->phpgw_exit();
-    }
+//    if (! $mbox) {
+//      echo "Mail error: can not open connection to mail server";
+//      $phpgw->common->phpgw_exit();
+//    }
 
-  	$mailbox_status = $phpgw->msg->status($mbox,"{" . $phpgw_info["user"]["preferences"]["email"]["mail_server"] . ":" . $phpgw_info["user"]["preferences"]["email"]["mail_port"] . "}INBOX",SA_UNSEEN);
+  	$mailbox_status = $phpgw->msg->status($mailbox,"{" . $phpgw_info["user"]["preferences"]["email"]["mail_server"] . ":" . $phpgw_info["user"]["preferences"]["email"]["mail_port"] . "}INBOX",SA_UNSEEN);
   	$str = '';
     if ($mailbox_status->unseen == 1) {
 //      echo "<tr><td><A href=\"" . $phpgw->link("email/index.php") . "\"> "
@@ -29,14 +29,14 @@
 //	 . lang("You have x new messages!",$mailbox_status->unseen) . "</A></td></tr>";
 	  $str .= lang("You have x new messages!",$mailbox_status->unseen);
     }
-    $nummsg = $phpgw->msg->num_msg($mbox);
+    $nummsg = $phpgw->msg->num_msg($mailbox);
     include($phpgw_info["server"]["api_inc"].'/phpgw_utilities_portalbox.inc.php');
     $title = '<a href="'.$phpgw->link($phpgw_info["server"]["webserver_url"]."/email/index.php").'">EMail'.($str ? ' - '.$str : '').'</a>';
     $portalbox = new linkbox($title,$phpgw_info["theme"]["navbar_bg"],$phpgw_info["theme"]["bg_color"],$phpgw_info["theme"]["bg_color"]);
-//    $portalbox->data[0] = array($str,$phpgw->link("email/index.php"));
-    for($i=$nummsg - 4,$j=0;$i<=$nummsg;$i++,$j++) {
-      $msg = $phpgw->msg->header($mbox,$i);
-      $subject = !$msg->Subject ? "[".lang("no subject")."]" : substr($msg->Subject,0,35).' ...';
+    if($nummsg >= 5) { $check_msgs = 4; } else { $check_msgs = $nummsg; }
+    for($i=$nummsg - $check_msgs,$j=0;$i<=$nummsg;$i++,$j++) {
+      $msg = $phpgw->msg->header($mailbox,$i);
+      $subject = !$msg->Subject ? '['.lang("no subject").']' : substr($msg->Subject,0,35).' ...';
       $portalbox->data[$j] = array(decode_header_string($subject),$phpgw->link($phpgw_info["server"]["webserver_url"]."/email/message.php","folder=".urlencode($folder)."&msgnum=".$i));
     }
     echo $portalbox->draw();
