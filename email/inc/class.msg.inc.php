0dc4fc0222ce3263907aedade00ea8e6bc093dea
lkneschke <lkneschke@923e3c2e-ae13-0410-ba18-ced7211e33de>
2001-02-06 16:24:29 +0000

    introduced PHPGW_INCLUDE_ROOT
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@1964 923e3c2e-ae13-0410-ba18-ced7211e33de

--- email/inc/class.msg.inc.php
+++ email/inc/class.msg.inc.php
@@ -1,1 +1,1 @@
-<?php include($phpgw_info["server"]["include_root"]."/email/inc/class.msg_".$phpgw_info["user"]["preferences"]["email"]["mail_server_type"].".inc.php"); ?>
+<?php include(PHPGW_INCLUDE_ROOT."/email/inc/class.msg_".$phpgw_info["user"]["preferences"]["email"]["mail_server_type"].".inc.php"); ?>
