0bbcbfb894c8a5aa8eaa8be746d9b2d6f1e88316
jengo <jengo@923e3c2e-ae13-0410-ba18-ced7211e33de>
2001-03-12 14:21:41 +0000

    Cleaned up some of the code in create
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@2452 923e3c2e-ae13-0410-ba18-ced7211e33de

--- bookmarks/create.php
+++ bookmarks/create.php
@@ -1,120 +1,56 @@
 <?php
-  /**************************************************************************\
-  * phpGroupWare - Bookmarks                                                 *
-  * http://www.phpgroupware.org                                              *
-  * Based on Bookmarker Copyright (C) 1998  Padraic Renaghan                 *
-  *                     http://www.renaghan.com/bookmarker                   *
-  * --------------------------------------------                             *
-  *  This program is free software; you can redistribute it and/or modify it *
-  *  under the terms of the GNU General Public License as published by the   *
-  *  Free Software Foundation; either version 2 of the License, or (at your  *
-  *  option) any later version.                                              *
-  \**************************************************************************/
+	/**************************************************************************\
+	* phpGroupWare - Bookmarks                                                 *
+	* http://www.phpgroupware.org                                              *
+	* Based on Bookmarker Copyright (C) 1998  Padraic Renaghan                 *
+	*                     http://www.renaghan.com/bookmarker                   *
+	* --------------------------------------------                             *
+	*  This program is free software; you can redistribute it and/or modify it *
+	*  under the terms of the GNU General Public License as published by the   *
+	*  Free Software Foundation; either version 2 of the License, or (at your  *
+	*  option) any later version.                                              *
+	\**************************************************************************/
 
-  /* $Id$ */
+	/* $Id$ */
 
-  $phpgw_info["flags"]["currentapp"] = "bookmarks";
-  $phpgw_info["flags"]["enable_nextmatchs_class"] = True;
-  $phpgw_info["flags"]["enable_categories_class"]  = True;
-  include("../header.inc.php");
+	$phpgw_info['flags'] = array(
+		'currentapp'              => 'bookmarks',
+		'enable_nextmatchs_class' = True,
+		'enable_categories_class' = True
+	);
+	include('../header.inc.php');
   
-  $phpgw->sbox = createobject("phpgwapi.sbox");
+	$phpgw->sbox = createobject('phpgwapi.sbox');
 
-  $phpgw->template->set_file(array("common"             => "common.tpl",
-//                                 "standard"           => "common.standard.tpl",
-                                   "possible_dup"       => "create.possible_dup.tpl",
-                                   "possible_dup_lines" => "create.possible_dup.line.tpl",
-                                   "body"               => "form.tpl"
-                            ));
-  app_header(&$phpgw->template);
+	$phpgw->template->set_file(array(
+		'common'             => 'common.tpl',
+		'possible_dup'       => 'create.possible_dup.tpl',
+		'possible_dup_lines' => 'create.possible_dup.line.tpl',
+		'body'               => 'form.tpl'
+	));
 
-//  set_standard("create", &$phpgw->template);
+	app_header(&$phpgw->template);
 
-  // if browser is MSIE, then need to add this bit
-  // of javascript to the page so that MSIE correctly
-  // brings quik-mark and mail-this-link popups to the front.
-  if (check_browser() == "MSIE") {
-     $phpgw->template->parse(MSIE_JS, "msie_js");
-  }
 
-  // initialize variable that holds id of newly created bookmark
-  $id = 0;
-  $bmark = new bmark;
+	// if browser is MSIE, then need to add this bit
+	// of javascript to the page so that MSIE correctly
+	// brings quik-mark and mail-this-link popups to the front.
+	if (check_browser() == 'MSIE')
+	{
+		$phpgw->template->parse(MSIE_JS, 'msie_js');
+	}
 
-## Check if there was a submission
-/*while (is_array($HTTP_POST_VARS) && list($key, $val) = each($HTTP_POST_VARS)) {
-  switch ($key) {
-
-  ## Create a new bookmark
-  case "bk_create_x":
-  case "bk_create": */
+	// initialize variable that holds id of newly created bookmark
+	$id = 0;
+	$bmark = new bmark;
 
 	if ($create)
 	{
 		$bmark->add(&$id,$url,$name,$desc,$keyw,$bookmarks_category,$bookmarks_subcategory,$bookmarks_rating, $access,$groups);
 	}
-/*    break;
 
-  default:
-    break;
- }
-	} */
-
-# if dislpaying b/c of error, show previous data
-if ($url > "") {
-  $default_url = $url;
-# otherwise default from URL if available
-} elseif ($curl > "") {
-  $default_url = $curl;
-# otherwise just default to http://
-} else {
-  $default_url = "http://";
-}
-
-# if dislpaying b/c of error, show previous data
-if ($name > "") {
-  $default_name = $name;
-# otherwise default from URL if available
-} elseif ($ctitle > "") {
-  $default_name = $ctitle;
-}
-
-# if dislpaying b/c of error, show previous data
-if ($desc > "") {
-  $default_desc = $desc;
-# otherwise default from URL if available
-} elseif ($ctitle > "") {
-  $default_desc = $ctitle;
-}
-
-# if dislpaying b/c of error, show previous data
-if ($keyw > "") {
-  $default_keyw = $keyw;
-}
-
-# if dislpaying b/c of error, show previous data
-if ($category > 0) {
-  $default_category = $category;
-} else {
-  $default_category = 0;
-}
-
-# if dislpaying b/c of error, show previous data
-if ($subcategory > 0) {
-  $default_subcategory = $subcategory;
-} else {
-  $default_subcategory = 0;
-}
-
-# if dislpaying b/c of error, show previous data
-if ($rating > 0) {
-  $default_rating = $rating;
-} else {
-  $default_rating = 0;
-}
-
-## Check to see if any existing bookmarks are a "close match".
-## don't do this check after a save.
+	// Check to see if any existing bookmarks are a "close match".
+	// don't do this check after a save.
 /*if ($default_url != "http://") {
    $db_dup   = $phpgw->db;
 
@@ -143,34 +79,21 @@ if ($rating > 0) {
    }
 } */
 
-# only default the public checkbox the first time the
-# page is shown. after that, show the value that the
-# user chose.
-/*if ($id == 0) {
-  if ($auth->auth["default_public"] == "Y") {
-    $default_public = "CHECKED";
-  }
-} else {
-  if ($public == "on") {
-    $default_public = "CHECKED";
-  }
-} */
+	$phpgw->template->set_var('lang_header',lang('Create new bookmark'));
 
-  $phpgw->template->set_var("lang_header",lang("Create new bookmark"));
-
-  $phpgw->template->set_var("input_category",'<select name="bookmarks_category">'
+	$phpgw->template->set_var('input_category','<select name="bookmarks_category">'
                                            . '<option value="0">--</option>'
-                                           . $phpgw->categories->formated_list("select","mains")
+                                           . $phpgw->categories->formated_list('select','mains')
                                            . '</select>');
 
-  $phpgw->template->set_var("input_subcategory",'<select name="bookmarks_subcategory">'
+	$phpgw->template->set_var('input_subcategory','<select name="bookmarks_subcategory">'
                                               . '<option value="0">--</option>'
-                                              . $phpgw->categories->formated_list("select","subs")
+                                              . $phpgw->categories->formated_list('select','subs')
                                               . '</select>');
 
-  $phpgw->template->set_var("input_rating",'<select name="bookmarks_rating">'
+	$phpgw->template->set_var('input_rating','<select name="bookmarks_rating">'
                                          . ' <option value="0">--</option>'
-                                         . ' <option value="1">1 - ' . lang("Lowest") . '</option>'
+                                         . ' <option value="1">1 - ' . lang('Lowest') . '</option>'
                                          . ' <option value="2">2</option>'
                                          . ' <option value="3">3</option>'
                                          . ' <option value="4">4</option>'
@@ -179,41 +102,35 @@ if ($rating > 0) {
                                          . ' <option value="7">7</option>'
                                          . ' <option value="8">8</option>'
                                          . ' <option value="9">9</option>'
-                                         . ' <option value="10">10 - ' . lang("Highest") . '</option>'
+                                         . ' <option value="10">10 - ' . lang('Highest') . '</option>'
                                          . '</select>');
 
-  $phpgw->template->set_var("th_bg",$phpgw_info["theme"]["th_bg"]);
+	$phpgw->template->set_var('th_bg',$phpgw_info['theme']['th_bg']);
 
-  $phpgw->template->set_var("form_action",$phpgw->link('/bookmarks/create.php','create=True'));
-  $phpgw->template->set_var("lang_url",lang("URL"));
-  $phpgw->template->set_var("lang_name",lang("Name"));
-  $phpgw->template->set_var("lang_desc",lang("Description"));
-  $phpgw->template->set_var("lang_keywords",lang("Keywords"));
-  $phpgw->template->set_var("lang_access",lang("Access"));
-  $phpgw->template->set_var("lang_category",lang("Category"));
-  $phpgw->template->set_var("lang_subcategory",lang("Sub Category"));
-  $phpgw->template->set_var("lang_rating",lang("Rating"));
+	$phpgw->template->set_var('form_action',$phpgw->link('/bookmarks/create.php','create=True'));
+	$phpgw->template->set_var('lang_url',lang('URL'));
+	$phpgw->template->set_var('lang_name',lang('Name'));
+	$phpgw->template->set_var('lang_desc',lang('Description'));
+	$phpgw->template->set_var('lang_keywords',lang('Keywords'));
+	$phpgw->template->set_var('lang_access',lang('Access'));
+	$phpgw->template->set_var('lang_category',lang('Category'));
+	$phpgw->template->set_var('lang_subcategory',lang('Sub Category'));
+	$phpgw->template->set_var('lang_rating',lang('Rating'));
 
-  $phpgw->template->set_var("form_info_","");
+	$phpgw->template->set_var('input_url','<input name="url" size="60" maxlength="255" value="' . ($url?$url:'http://') . '">');
+	$phpgw->template->set_var('input_name','<input name="name" size="60" maxlength="255" value="' . $name . '">');
 
-  if ($phpgw_bm_url) {
-     $phpgw->template->set_var("input_url",'<input name="url" size="60" maxlength="255" value="' . $phpgw_bm_url . '">');
-     $phpgw->template->set_var("input_name",'<input name="name" size="60" maxlength="255" value="' . $phpgw_bm_title . '">');
-  } else {
-     $phpgw->template->set_var("input_url",'<input name="url" size="60" maxlength="255" value="http://">');
-     $phpgw->template->set_var("input_name",'<input name="name" size="60" maxlength="255">');
-  }
-  $phpgw->template->set_var("input_desc",'<textarea name="desc" rows="3" cols="60" wrap="virtual"></textarea>');
-  $phpgw->template->set_var("input_keywords",'<input type="text" name="keyw" size="60" maxlength="255">');
+	$phpgw->template->set_var('input_desc','<textarea name="desc" rows="3" cols="60" wrap="virtual">' . $desc . '</textarea>');
+	$phpgw->template->set_var('input_keywords','<input type="text" name="keyw" size="60" maxlength="255" value="' . $keyw . '">');
 
-	$phpgw->template->set_var("input_access",$phpgw->sbox->getAccessList("access") . "&nbsp;"
-                                         . $phpgw->sbox->getGroups($phpgw->accounts->memberships($phpgw_info["user"]["account_id"]),-1,"groups"));
+	$phpgw->template->set_var('input_access',$phpgw->sbox->getAccessList('access') . "&nbsp;"
+                                         . $phpgw->sbox->getGroups($phpgw->accounts->memberships($phpgw_info['user']['account_id']),-1,'groups'));
 
-  $phpgw->template->set_var("delete_link","");
-  $phpgw->template->set_var("cancel_link","");
-  $phpgw->template->set_var("form_link",'<input type="image" name="bk_create" title="'
-                                      . lang("Create bookmark") . '" src="' . $phpgw_info["server"]["app_images"]
+	$phpgw->template->set_var('delete_link','');
+	$phpgw->template->set_var('cancel_link','');
+	$phpgw->template->set_var('form_link','<input type="image" name="bk_create" title="'
+                                      . lang('Create bookmark') . '" src="' . $phpgw_info['server']['app_images']
                                       . '/save.gif" border="0" width="24" height="24">');
 
-  $phpgw->common->phpgw_footer();
+	$phpgw->common->phpgw_footer();
 ?>
