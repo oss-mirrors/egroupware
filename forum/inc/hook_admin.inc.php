c0d616d236cd70dc26ddaadcabb74b155a5fa37f
milosch <milosch@923e3c2e-ae13-0410-ba18-ced7211e33de>
2001-04-02 20:05:01 +0000

    Restructure admin
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@3041 923e3c2e-ae13-0410-ba18-ced7211e33de

--- forum/inc/hook_admin.inc.php
+++ forum/inc/hook_admin.inc.php
@@ -1,18 +1,37 @@
 <?php
-  echo "<p>\n";                                                                                                                                                                        
-  $imgfile = $phpgw->common->get_image_dir("forum")."/" . $appname .".gif";                                                                                                            
-  if (file_exists($imgfile)) {                                                                                                                                                         
-    $imgpath = $phpgw->common->get_image_path("forum")."/" . $appname .".gif";                                                                                                         
-  } else {                                                                                                                                                                             
-    $imgfile = $phpgw->common->get_image_dir("forum")."/navbar.gif";                                                                                                                   
-    if (file_exists($imgfile)) {                                                                                                                                                       
-      $imgpath = $phpgw->common->get_image_path("forum")."/navbar.gif";                                                                                                                
-    } else {                                                                                                                                                                           
-      $imgpath = "";                                                                                                                                                                   
-    }                                                                                                                                                                                  
-  }
-  section_start("Forum",$imgpath);
-  echo '<a href="' . $phpgw->link('/forum/admin/index.php') . '">';
-  echo lang('Change Forum settings') . '</a>';
-  section_end();
+  /**************************************************************************\
+  * phpGroupWare                                                             *
+  * http://www.phpgroupware.org                                              *
+  * Written by Joseph Engo <jengo@phpgroupware.org>                          *
+  * --------------------------------------------                             *
+  *  This program is free software; you can redistribute it and/or modify it *
+  *  under the terms of the GNU General Public License as published by the   *
+  *  Free Software Foundation; either version 2 of the License, or (at your  *
+  *  option) any later version.                                              *
+  \**************************************************************************/
+  /* $Id$ */
+{
+
+	echo "<p>\n";
+	$imgfile = $phpgw->common->get_image_dir($appname) . SEP . $appname .".gif";
+	if (file_exists($imgfile)) {
+		$imgpath = $phpgw->common->get_image_path($appname) . SEP . $appname .".gif"; 
+	}
+	else
+	{
+		$imgfile = $phpgw->common->get_image_dir($appname) . SEP . "navbar.gif";
+		if (file_exists($imgfile)) {
+			$imgpath = $phpgw->common->get_image_path($appname) . SEP . "/navbar.gif";
+		}
+		else
+		{
+			$imgpath = "";
+		}
+	}
+
+	section_start(ucfirst($appname),$imgpath);
+	echo '<a href="'.$phpgw->link('/forum/preference_index.php').'">';
+	echo lang('Change Forum settings') . '</a>';
+	section_end();
+}
 ?>
--- forum/index.php
+++ forum/index.php
@@ -10,38 +10,43 @@
   *  option) any later version.                                              *
   \**************************************************************************/
 
-  if (! $sessionid)
-    Header("Location: ../login.php");
+	if (! $sessionid) Header("Location: ../login.php");
 
-  $phpgw_info["flags"] = array("currentapp" => "forum", "enable_nextmatchs_class" => True);
-  include("../header.inc.php");
-
+	$phpgw_info["flags"] = array(
+		"currentapp" => "forum",
+		"enable_nextmatchs_class" => True
+	);
+	include("../header.inc.php");
 ?>
 
 <p>
 <table border="0" width="100%">
  <tr>
-<?   echo "<td bgcolor=" . $phpgw_info["theme"]["th_bg"] . " align='left'>" . lang("Forums") ."</td></tr>"; ?>
+<?php echo "<td bgcolor=" . $phpgw_info["theme"]["th_bg"] . " align='left'>" . lang("Forums") ."</td></tr>"; ?>
  <tr>
   <td align="left" width="50%" valign="top">
    <?php
-      echo "<center>";
-      echo '<table border="0" width="80%">';
+	echo "<center>";
+	echo '<table border="0" width="80%">';
 
-      //  Pull all the categories from the table f_categories and display them
-      $phpgw->db->query("select * from f_categories");
+	//  Pull all the categories from the table f_categories and display them
+	$phpgw->db->query("select * from f_categories");
 
-      while($phpgw->db->next_record()) {
-        $tr_color = $phpgw->nextmatchs->alternate_row_color($tr_color);
-        echo "<tr bgcolor=".$tr_color."><td><a href=" . $phpgw->link("/forum/forums.php","cat=" . $phpgw->db->f("id")) .">". $phpgw->db->f("name") . "</a></td><td align=left valign=top>" . $phpgw->db->f("descr") . "</td></tr>\n";
-      }
+	while($phpgw->db->next_record()) {
+		$tr_color = $phpgw->nextmatchs->alternate_row_color($tr_color);
+		echo "<tr bgcolor=".$tr_color."><td><a href="
+		. $phpgw->link("/forum/forums.php","cat="
+		. $phpgw->db->f("id")) .">". $phpgw->db->f("name")
+		. "</a></td><td align=left valign=top>"
+		. $phpgw->db->f("descr") . "</td></tr>\n";
+	}
 
-     echo "</table>";
-     echo "</center>";
-   ?>
+	echo "</table>";
+	echo "</center>";
+?>
   </td>
 </table>
 
 <?php
-  $phpgw->common->phpgw_footer();
+	$phpgw->common->phpgw_footer();
 ?>
