8b8c9d881dd9829a5c827ff578447e0c21bfbc83
bettina <bettina@923e3c2e-ae13-0410-ba18-ced7211e33de>
2001-04-02 16:08:13 +0000

    update addressbook
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@3037 923e3c2e-ae13-0410-ba18-ced7211e33de

--- email/addressbook.php
+++ email/addressbook.php
@@ -11,104 +11,113 @@
   \**************************************************************************/
   /* $Id$ */
 
-	$phpgw_info["flags"] = array(
-		"noheader" => True, 
-		"nonavbar" => True, 
-		"currentapp" => "email", 
-		"enable_nextmatchs_class" => True
-	);
+    $phpgw_info["flags"] = array('noheader' => True,
+				 'nonavbar' => True,
+				'currentapp' => 'email',
+		    'enable_nextmatchs_class' => True);
 
-	include("../header.inc.php");
+    include('../header.inc.php');
 
-	$t = CreateObject('phpgwapi.Template',$phpgw->common->get_tpl_dir('email'));
-	$t->set_file(array(
-		'addressbook_list_t' => 'addressbook.tpl',
-		'addressbook_list' => 'addressbook.tpl'
-	));
-	$t->set_block('addressbook_list_t','addressbook_list','list');
+    $t = CreateObject('phpgwapi.Template',$phpgw->common->get_tpl_dir('email'));
+    $t->set_file(array('addressbook_list_t' => 'addressbook.tpl',
+			'addressbook_list' => 'addressbook.tpl'));
+    $t->set_block('addressbook_list_t','addressbook_list','list');
 
-	$d = CreateObject('phpgwapi.contacts');
+    $d = CreateObject('phpgwapi.contacts');
+    $c = CreateObject('phpgwapi.categories');
+    $c->app_name = 'addressbook';
 
-	$charset = $phpgw->translation->translate("charset");                                                                                                    
-	$t->set_var('charset',$charset);
-	$t->set_var('title',$phpgw_info["site_title"]);
-	$t->set_var('bg_color',$phpgw_info["theme"]["bg_color"]);
-	$t->set_var('lang_addressbook_action',lang('Address book'));
-	$t->set_var('font',$phpgw_info["theme"]["font"]);  
-	$t->set_var('lang_search',lang('Search'));
-	$t->set_var('searchurl',$phpgw->link("/".$phpgw_info['flags']['currentapp']."/addressbook.php"));
+    $charset = $phpgw->translation->translate("charset");                                                                                                    
+    $t->set_var('charset',$charset);
+    $t->set_var('title',$phpgw_info["site_title"]);
+    $t->set_var('bg_color',$phpgw_info["theme"]["bg_color"]);
+    $t->set_var('lang_addressbook_action',lang('Address book'));
+    $t->set_var('font',$phpgw_info["theme"]["font"]);  
+    $t->set_var('lang_search',lang('Search'));
+    $t->set_var('searchurl',$phpgw->link("/".$phpgw_info['flags']['currentapp']."/addressbook.php"));
+    $t->set_var('lang_select_cats',lang('Select category'));
 
     if (! $start) { $start = 0; }
   
-    if ($filter == "none") { $filter = ""; }
-    if ($filter != "" ) { $filter = "access=$filter"; }
+    if (!$cat_id) {
+        if ($filter == 'none') { $qfilter  = 'tid=n'; }
+        elseif ($filter == 'private') { $qfilter  = 'tid=n,owner='.$phpgw_info["user"]["account_id"]; }
+        else { $qfilter = 'tid=n,owner='.$filter; }
+    }
+    else {
+        if ($filter == 'none') { $qfilter  = 'tid=n,cat_id='.$cat_id; }
+        elseif ($filter == 'private') { $qfilter  = 'tid=n,owner='.$phpgw_info["user"]["account_id"].',cat_id='.$cat_id; }
+        else { $qfilter = 'tid=n,owner='.$filter.'cat_id='.$cat_id; }
+    }
 
-	if($phpgw_info["user"]["preferences"]["common"]["maxmatchs"] && $phpgw_info["user"]["preferences"]["common"]["maxmatchs"] > 0) {
-		$offset = $phpgw_info["user"]["preferences"]["common"]["maxmatchs"];
-	}
+    if($phpgw_info["user"]["preferences"]["common"]["maxmatchs"] && $phpgw_info["user"]["preferences"]["common"]["maxmatchs"] > 0) {
+	$offset = $phpgw_info["user"]["preferences"]["common"]["maxmatchs"];
+    }
     else { $offset = 15; }
 
     $account_id = $phpgw_info['user']['account_id'];
 
-	$cols = array(
-		'n_given'     => 'n_given',
-		'n_family'    => 'n_family',
-		'd_email'     => 'd_email',
-		'd_emailtype' => 'd_emailtype'
-	);
+    $cols = array('n_given'     => 'n_given',
+		  'n_family'    => 'n_family',
+		  'd_email'     => 'd_email',
+		  'd_emailtype' => 'd_emailtype');
 
-	$entries = $d->read($start,$offset,$cols,$query,$filter,$sort,$order,$account_id);
+    $entries = $d->read($start,$offset,$cols,$query,$qfilter,$sort,$order,$account_id);
 
-	//--------------------------------- nextmatch --------------------------------------------
-	$left = $phpgw->nextmatchs->left('/'.$phpgw_info['flags']['currentapp'].'/addressbook.php',$start,$d->total_records,"&order=$order&filter=$filter&sort=$sort&query=$query");
-	$right = $phpgw->nextmatchs->right('/'.$phpgw_info['flags']['currentapp'].'/addressbook.php',$start,$d->total_records,"&order=$order&filter=$filter&sort=$sort&query=$query");
-	$t->set_var('left',$left);
-	$t->set_var('right',$right);
+//------------------------------------------- nextmatch --------------------------------------------
 
-	if ($d->total_records > $phpgw_info["user"]["preferences"]["common"]["maxmatchs"]) {
-		$lang_showing=lang("showing x - x of x",($start + 1),($start + $phpgw_info["user"]["preferences"]["common"]["maxmatchs"]),$d->total_records);
-	} else {
-		$lang_showing=lang("showing x",$d->total_records);
-	}
-	$t->set_var('lang_showing',$lang_showing);
-	// ------------------------------ end nextmatch ------------------------------------------
+    $left = $phpgw->nextmatchs->left('/'.$phpgw_info['flags']['currentapp'].'/addressbook.php',$start,$d->total_records,"&order=$order&filter=$filter&sort=$sort&query=$query");
+    $right = $phpgw->nextmatchs->right('/'.$phpgw_info['flags']['currentapp'].'/addressbook.php',$start,$d->total_records,"&order=$order&filter=$filter&sort=$sort&query=$query");
+    $t->set_var('left',$left);
+    $t->set_var('right',$right);
+
+    if ($d->total_records > $phpgw_info["user"]["preferences"]["common"]["maxmatchs"]) {
+	$t->set_var('lang_showing',lang("showing x - x of x",($start + 1),($start + $phpgw_info["user"]["preferences"]["common"]["maxmatchs"]),$d->total_records));
+    } 
+    else { $t->set_var('lang_showing',lang("showing x",$d->total_records)); }
+
+// --------------------------------------- end nextmatch ------------------------------------------
 
 	// ------------------- list header variable template-declaration -----------------------
-	$t->set_var('th_bg',$phpgw_info["theme"]["th_bg"]);
-	$t->set_var('sort_firstname',$phpgw->nextmatchs->show_sort_order($sort,'n_given',$order,'/'.$phpgw_info['flags']['currentapp'].'/addressbook.php',lang('Firstname')));
-	$t->set_var('sort_lastname',$phpgw->nextmatchs->show_sort_order($sort,'n_family',$order,'/'.$phpgw_info['flags']['currentapp'].'/addressbook.php',lang('Lastname')));
-	$t->set_var('sort_etype',$phpgw->nextmatchs->show_sort_order($sort,'d_emailtype',$order,'/'.$phpgw_info['flags']['currentapp'].'/addressbook.php',lang('Email type')));
-	$t->set_var('lang_email',lang('Select the email address'));
+
+    $t->set_var('th_bg',$phpgw_info["theme"]["th_bg"]);
+    $t->set_var('sort_firstname',$phpgw->nextmatchs->show_sort_order($sort,'n_given',$order,'/'.$phpgw_info['flags']['currentapp'].'/addressbook.php',lang('Firstname')));
+    $t->set_var('sort_lastname',$phpgw->nextmatchs->show_sort_order($sort,'n_family',$order,'/'.$phpgw_info['flags']['currentapp'].'/addressbook.php',lang('Lastname')));
+    $t->set_var('sort_etype',lang('Email type'));
+    $t->set_var('lang_email',lang('Select email address'));
+    $t->set_var('cats_action',$phpgw->link('/'.$phpgw_info['flags']['currentapp'].'/addressbook.php',"sort=$sort&order=$order&filter=$filter&start=$start&query=$query&cat_id=$cat_id"));
+    $t->set_var('cats_list',$c->formated_list('select','all',$cat_id,'True'));
+    $t->set_var('lang_select',lang('Select'));
+
 	// --------------------------- end header declaration ----------------------------------
 
-	for ($i=0;$i<count($entries);$i++) {
-		$tr_color = $phpgw->nextmatchs->alternate_row_color($tr_color);
-		$t->set_var(tr_color,$tr_color);
-		$firstname = $entries[$i]['n_given'];
-		if (!$firstname) { $firstname = "&nbsp;"; }
-		$lastname = $entries[$i]['n_family'];
-		if (!$lastname) { $lastname = "&nbsp;"; }
-		$etype = $entries[$i]['d_emailtype'];
-		$id    = $entries[$i]['id'];
-		$email = $entries[$i]['d_email'];
+    for ($i=0;$i<count($entries);$i++) {
+	$tr_color = $phpgw->nextmatchs->alternate_row_color($tr_color);
+	$t->set_var(tr_color,$tr_color);
+	$firstname = $entries[$i]['n_given'];
+	if (!$firstname) { $firstname = "&nbsp;"; }
+	$lastname = $entries[$i]['n_family'];
+	if (!$lastname) { $lastname = "&nbsp;"; }
+	$etype = $entries[$i]['d_emailtype'];
+	$id    = $entries[$i]['id'];
+	$email = $entries[$i]['d_email'];
 
-	// ---------------- template declaration for list records --------------------------
-		$t->set_var(array(
-			"etype" => $etype,
-			"firstname" => $firstname,
-			"lastname" => $lastname
-		));
+// --------------------- template declaration for list records --------------------------
 
-		$t->set_var('id',$id);
-		$t->set_var('email',$email);
+	$t->set_var(array('etype' => $etype,
+			'firstname' => $firstname,
+			'lastname' => $lastname));
 
-		$t->parse('list','addressbook_list',True);
-	}
-	// --------- end record declaration ----------------
+	$t->set_var('id',$id);
+	$t->set_var('email',$email);
 
-	$t->set_var('lang_done',lang('Done'));
-	$t->parse('out','addressbook_list_t',True);
-	$t->p('out');
+	$t->parse('list','addressbook_list',True);
+    }
+// --------------------------- end record declaration ---------------------------
 
-	$phpgw->common->phpgw_exit();
+    $t->set_var('lang_done',lang('Done'));
+    $t->parse('out','addressbook_list_t',True);
+    $t->p('out');
+
+    $phpgw->common->phpgw_exit();
 ?>
--- email/templates/default/addressbook.tpl
+++ email/templates/default/addressbook.tpl
@@ -4,12 +4,12 @@
 <head>
 <title>{title}</title>
 <meta http-equiv="content-type" content="text/html; charset={charset}">
-<STYLE type="text/css">                                                                                                                                                                                         
-   A {text-decoration:none;}                                                                                                                                                    
+<STYLE type="text/css">
+   A {text-decoration:none;}
    <!--
-   A:link {text-decoration:none;}                                                                                                                            
-   A:visted {text-decoration:none;}                                                                                                                          
-   A:active {text-decoration:none;}                                                                                                                          
+   A:link {text-decoration:none;}
+   A:visted {text-decoration:none;}
+   A:active {text-decoration:none;}
    body {margin-top: 0px; margin-right: 0px; margin-left: 0px;}
    td {text-decoration:none;}
    tr {text-decoration:none;}
@@ -25,43 +25,48 @@
    }
   else {
    opener.document.doit.to.value +=","+thisform.elements[0].value;
-  }  
-}   
-function ExchangeCc(thisform)                                                                                                                               
-   {                                                                                                                                                           
-     if (opener.document.doit.cc.value=='') {                                                                                                                  
-        opener.document.doit.cc.value=thisform.elements[0].value;                                                                                              
-     } else {                                                                                                                                                  
-        opener.document.doit.cc.value+=","+thisform.elements[0].value;                                                                                         
-     }                                                                                                                                                         
+  }
+}
+function ExchangeCc(thisform)
+   {
+     if (opener.document.doit.cc.value=='') {
+        opener.document.doit.cc.value=thisform.elements[0].value;
+     } else {
+        opener.document.doit.cc.value+=","+thisform.elements[0].value;
+     }
    }
 </script>
 </head>
-<body bgcolor="{bg_color}">   
+<body bgcolor="{bg_color}">
 <center>
 <p><font face="{font}"><b>{lang_addressbook_action}</b></font><br>
 <hr noshade width="98%" align="center" size="1">
 
-<table border="0" width="100%" cellspacing="2" cellpadding="2">
- <tr>
-  <td colspan="4" align="left">
-   <table border="0" width="100%">
+<table border="0" width="100%">
+    <tr>
+    <td width="33%" align="left">
+    <form action="{cats_action}" name="form" method="POST">
+    <select name="cat_id" onChange="this.form.submit();"><option value="">{lang_select_cats}</option>{cats_list}</select>
+    <noscript>&nbsp;<input type="submit" name="submit" value="{lang_submit}"></noscript></form></td>
+    <td width="33%" align="center">{lang_showing}</td>
+    <td width="33%" align="right">
+    <form method="POST" action="{search_action}">
+    <input type="text" name="query">&nbsp;<input type="submit" name="search" value="{lang_search}">
+    </form></td>
+    </tr>
+    <tr>
+    <td colspan="4">
+    <table border="0" width="100%">
     <tr>
     {left}
-    <td align="center">{lang_showing}</td>
+    <td>&nbsp;</td>
     {right}
     </tr>
-   </table>
-   </td>
-  </tr>
- <tr>
-  <td>&nbsp;</td>
-  <td colspan="4" align=right>
-  <form method="post" action="{searchurl}">
-  <input type="text" name="query">&nbsp;<input type="submit" name="search" value="{lang_search}">
-  </form></td>
- </tr>
-
+    </table>
+    </td>
+    </tr>
+</table>
+<table border="0" width="100%" cellpadding="2" cellspacing="2">
     <tr bgcolor="{th_bg}">
       <td width="20%" bgcolor="{th_bg}" align=center><font face="{font}">{sort_firstname}</font></td>
       <td width="20%" bgcolor="{th_bg}" align=center><font face="{font}">{sort_lastname}</font></td>
@@ -78,14 +83,14 @@ function ExchangeCc(thisform)
         <td align="center"><font face="{font}"><input type="text" size="25" name="email" value="{email}">
 	<input type="button" size="25" name="button" value="To" onClick="ExchangeTo(this.form);">
         <input type="button" size="25" name="button" value="Cc" onClick="ExchangeCc(this.form);"></font></td>
-      </form>    
+      </form>
       </tr>
 <!-- END addressbook_list -->
 
   </table>
   <table cellpadding="2" cellspacing="2">
-      <tr> 
-    <form>  
+      <tr>
+    <form>
     <td><font face="{font}"><input type="button" name="done" value="{lang_done}" onClick="window.close()"></font>
       </form>
       </td>
