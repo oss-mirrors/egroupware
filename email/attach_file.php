aec77f556b1d8f78fb55a9eb87452be046bf00f0
skeeter <skeeter@923e3c2e-ae13-0410-ba18-ced7211e33de>
2001-04-03 02:23:10 +0000

    Fix for bug #410947
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@3058 923e3c2e-ae13-0410-ba18-ced7211e33de

--- email/attach_file.php
+++ email/attach_file.php
@@ -11,65 +11,70 @@
   *  option) any later version.                                              *
   \**************************************************************************/
 
-  /* $Id$ */
+	/* $Id$ */
 
-  $phpgw_info["flags"] = array("currentapp" => "email", "enable_network_class" => True, 
-                                "noheader" => True, "nonavbar" => True);
-  include("../header.inc.php");
+	$phpgw_flags = Array(
+		'currentapp'	=>	'email',
+		'enable_network_class'	=> True,
+		'noheader'	=> True,
+		'nonavbar'	=> True
+	);
+	
+	$phpgw_info['flags'] = $phpgw_flags;
   
-  echo '<body bgcolor="' . $phpgw_info["theme"]["bg_color"] . '">';
+	include('../header.inc.php');
+  
+	echo '<body bgcolor="' . $phpgw_info['theme']['bg_color'] . '">';
 
-  // Some on the methods where borrowed from
-  // Squirrelmail <Luke Ehresman> http://www.squirrelmail.org
+	// Some on the methods where borrowed from
+	// Squirrelmail <Luke Ehresman> http://www.squirrelmail.org
 
-  $sep = $phpgw->common->filesystem_separator();
+	$uploaddir = $phpgw_info['server']['temp_dir'] . SEP . $phpgw_info['user']['sessionid'] . SEP;
 
-  $uploaddir = $phpgw_info["server"]["temp_dir"] . $sep . $phpgw_info["user"]["sessionid"] . $sep;
+	if ($action == 'Delete')
+	{
+		for ($i=0; $i<count($delete); $i++)
+		{
+			unlink($uploaddir . $delete[$i]);
+			unlink($uploaddir . $delete[$i] . '.info');
+		}
+	}
 
-  if ($action == "Delete") {
-     for ($i=0; $i<count($delete); $i++) {
+	if ($action == 'Attach File')
+	{
+		srand((double)microtime()*1000000);
+		$random_number = rand(100000000,999999999);
+		$newfilename = md5($uploadedfile.', '.$uploadedfile_name.', '.$phpgw_info['user']['sessionid'].time().getenv('REMOTE_ADDR').$random_number);
 
+		copy($uploadedfile, $uploaddir . $newfilename);
+		$ftp = fopen($uploaddir . $newfilename . '.info','w');
+		fputs($ftp,$uploadedfile_type."\n".$uploadedfile_name."\n");
+		fclose($ftp);
+	}
 
-        unlink($uploaddir . $delete[$i]);
-        unlink($uploaddir . $delete[$i] . ".info");
-     }
-  }
-
-  if ($action == "Attach File") {
-     srand((double)microtime()*1000000);
-     $random_number = rand(100000000,999999999);
-     $newfilename = md5("$uploadedfile, $uploadedfile_name, " . $phpgw_info["user"]["sessionid"]
-		      . time() . getenv("REMOTE_ADDR") . $random_number );
-
-     copy($uploadedfile, $uploaddir . $newfilename);
-     $ftp = fopen($uploaddir . $newfilename . ".info","w");
-      fputs($ftp,"$uploadedfile_type\n$uploadedfile_name\n");
-     fclose($ftp);
-  }
-
-  if (! file_exists($phpgw_info["server"]["temp_dir"] . $sep . $phpgw_info["user"]["sessionid"]))
-     mkdir($phpgw_info["server"]["temp_dir"] . $sep . $phpgw_info["user"]["sessionid"],0700);
-
+	if (!file_exists($phpgw_info['server']['temp_dir'] . SEP . $phpgw_info['user']['sessionid']))
+	{
+		mkdir($phpgw_info['server']['temp_dir'] . SEP . $phpgw_info['user']['sessionid'],0700);
+	}
   ?>
-    <form ENCTYPE="multipart/form-data" method="POST" action="<?php echo $phpgw->link("attach_file.php")?>">
+    <form ENCTYPE="multipart/form-data" method="POST" action="<?php echo $phpgw->link('/email/attach_file.php')?>">
       <table border=0>
       <tr> <td>Attach file:</td> </tr>
       <tr> <td>Current attachments:</td> </tr>
       <?php
-        $dh = opendir($phpgw_info["server"]["temp_dir"] . $sep . $phpgw_info["user"]["sessionid"]);
+        $dh = opendir($phpgw_info['server']['temp_dir'] . SEP . $phpgw_info['user']['sessionid']);
         while ($file = readdir($dh)) {
-          if ($file != "." && $file != ".." && ereg("\.info",$file)) {
+          if ($file != '.' && $file != '..' && ereg("\.info",$file)) {
              $file_info = file($uploaddir . $file);
-             echo "<tr><td><input type=checkbox name=\"delete[]\" "
-		. "value=\"" . substr($file,0,-5) . "\">$file_info[1]</tr></td>\n";
+             echo '<tr><td><input type="checkbox" name="delete[]" value="'.substr($file,0,-5).'">'.$file_info[1].'</tr></td>'."\n";
              $totalfiles++;
           }
         }
         closedir($dh);
         if ($totalfiles == 0)
-           echo "<tr></td>None</td></tr>\n";
+           echo '<tr></td>None</td></tr>'."\n";
         else
-           echo "<tr><td><input type=\"submit\" name=\"action\" value=\"Delete\"></td></tr>\n";
+           echo '<tr><td><input type="submit" name="action" value="Delete"></td></tr>'."\n";
       ?>
       <tr>
        <td>File: <input type="file" name="uploadedfile"></td>
@@ -81,5 +86,5 @@
       </table>
      </form>
 <?php
-//$phpgw->common->phpgw_footer();
+	$phpgw->common->phpgw_exit();
 ?>
