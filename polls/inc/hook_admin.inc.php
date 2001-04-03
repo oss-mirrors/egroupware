b297ce9d990b6ffbd9fb14e826bb0e04a96a0b7a
milosch <milosch@923e3c2e-ae13-0410-ba18-ced7211e33de>
2001-04-03 10:35:19 +0000

    Fix spelling
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@3069 923e3c2e-ae13-0410-ba18-ced7211e33de

--- polls/inc/hook_admin.inc.php
+++ polls/inc/hook_admin.inc.php
@@ -3,7 +3,7 @@
   section_start($appname,$phpgw_info['server']['webserver_url'] . '/polls/templates/'
               . $phpgw_info['server']['template_set'] . '/images/navbar.gif');
 
-  echo '<a href="' . $phpgw->link('/polls/admin.php') . '">' . lang('Votting booth admin') . '</a>';
+  echo '<a href="' . $phpgw->link('/polls/admin.php') . '">' . lang('Voting booth admin') . '</a>';
 
   section_end(); 
 }
