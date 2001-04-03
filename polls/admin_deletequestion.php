422f98d1eadb5c9247a85fd682d94e1175aaead3
milosch <milosch@923e3c2e-ae13-0410-ba18-ced7211e33de>
2001-04-03 10:29:05 +0000

    How long has this been gone?
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@3068 923e3c2e-ae13-0410-ba18-ced7211e33de

---
+++ polls/admin_deletequestion.php
@@ -0,0 +1,35 @@
+<?php
+  /**************************************************************************\
+  * phpGroupWare - Polls                                                     *
+  * http://www.phpgroupware.org                                              *
+  * --------------------------------------------                             *
+  *  This program is free software; you can redistribute it and/or modify it *
+  *  under the terms of the GNU General Public License as published by the   *
+  *  Free Software Foundation; either version 2 of the License, or (at your  *
+  *  option) any later version.                                              *
+  \**************************************************************************/
+
+  /* $Id$ */
+
+	$phpgw_info["flags"] = array(
+		"currentapp"   => "polls",
+		"enable_nextmatchs_class" => True,
+		"admin_header" => True
+	);
+
+	include("../header.inc.php");
+
+	if ($confirm)
+	{
+		$phpgw->db->query("delete from phpgw_polls_desc where poll_id='$poll_id'");
+	}
+	else
+	{
+		echo '<p><br><table border="0" width="40%" align="center"><tr><td align="center" colspan="center">';
+		echo lang("Are you sure want to delete this question ?") . "</td></tr>";
+		echo '<tr><td align="left"><a href="' . $phpgw->link("/polls/admin.php","show=answers") . '">' . lang("No") . '</td>';
+		echo '    <td align="right"><a href="' . $phpgw->link("/polls/admin_deletequestion.php","poll_id=$poll_id&confirm=True") . '">' . lang("Yes") . '</td></tr>';
+		echo '</table>';
+	}
+	$phpgw->common->phpgw_footer();
+?>
