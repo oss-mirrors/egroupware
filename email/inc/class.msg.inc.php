47dfa11f4c5cf899df841075a3d97a1ee4bff8e4
milosch <milosch@923e3c2e-ae13-0410-ba18-ced7211e33de>
2001-04-20 17:48:20 +0000

    Move msg_common from email functions to its own class, include by msg.inc
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@3302 923e3c2e-ae13-0410-ba18-ced7211e33de

--- email/inc/class.msg.inc.php
+++ email/inc/class.msg.inc.php
@@ -1,1 +1,4 @@
-<?php include(PHPGW_INCLUDE_ROOT."/email/inc/class.msg_".$phpgw_info["user"]["preferences"]["email"]["mail_server_type"].".inc.php"); ?>
+<?php
+	include(PHPGW_INCLUDE_ROOT."/email/inc/class.msg_common.inc.php");
+	include(PHPGW_INCLUDE_ROOT."/email/inc/class.msg_".$phpgw_info["user"]["preferences"]["email"]["mail_server_type"].".inc.php");
+?>
