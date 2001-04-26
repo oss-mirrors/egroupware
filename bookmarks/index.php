cb4a0dd6a77cbc3f2951822bb371f5fead36d631
jengo <jengo@923e3c2e-ae13-0410-ba18-ced7211e33de>
2001-04-26 19:20:13 +0000

    Changed to forward users back to where they where if they wonder of into another app
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@3405 923e3c2e-ae13-0410-ba18-ced7211e33de

--- bookmarks/index.php
+++ bookmarks/index.php
@@ -1,19 +1,17 @@
 <?php
-  /**************************************************************************\
-  * phpGroupWare - Bookmarks                                                 *
-  * http://www.phpgroupware.org                                              *
-  * Based on Bookmarker Copyright (C) 1998  Padraic Renaghan                 *
-  *                     http://www.renaghan.com/bookmarker                   *
-  * --------------------------------------------                             *
-  *  This program is free software; you can redistribute it and/or modify it *
-  *  under the terms of the GNU General Public License as published by the   *
-  *  Free Software Foundation; either version 2 of the License, or (at your  *
-  *  option) any later version.                                              *
-  \**************************************************************************/
+	/**************************************************************************\
+	* phpGroupWare - Bookmarks                                                 *
+	* http://www.phpgroupware.org                                              *
+	* Based on Bookmarker Copyright (C) 1998  Padraic Renaghan                 *
+	*                     http://www.renaghan.com/bookmarker                   *
+	* --------------------------------------------                             *
+	*  This program is free software; you can redistribute it and/or modify it *
+	*  under the terms of the GNU General Public License as published by the   *
+	*  Free Software Foundation; either version 2 of the License, or (at your  *
+	*  option) any later version.                                              *
+	\**************************************************************************/
 
-  /* $Id$ */
-
-  // Note: This file won't be here for too long
+	/* $Id$ */
 
 	$phpgw_info['flags'] = array(
 		'currentapp' => 'bookmarks',
@@ -22,5 +20,15 @@
 	);
 
 	include('../header.inc.php');
-	Header('Location: ' . $phpgw->link('/bookmarks/list.php'));
+	$phpgw->bookmarks = createobject('bookmarks.bookmarks');
+	$location_info    = $phpgw->bookmarks->read_session_data();
+
+	if (is_array($location_info) && $location_info['returnto'])
+	{
+		Header('Location: ' . $phpgw->link('/bookmarks/' . $location_info['returnto']));	
+	}
+	else
+	{
+		Header('Location: ' . $phpgw->link('/bookmarks/list.php'));
+	}
 ?>
