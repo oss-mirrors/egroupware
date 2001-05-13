1fcada0a0084d21ab1a4706978ff2d655dc6ad01
milosch <milosch@923e3c2e-ae13-0410-ba18-ced7211e33de>
2001-05-13 16:28:25 +0000

    Update templates
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@3702 923e3c2e-ae13-0410-ba18-ced7211e33de

--- polls/admin_viewquestion.php
+++ polls/admin_viewquestion.php
@@ -11,38 +11,45 @@
 
   /* $Id$ */
 
-  $phpgw_info["flags"] = array("currentapp"   => "polls", "enable_nextmatchs_class" => True,
-                               "admin_header" => True);
-  include("../header.inc.php");
+	$phpgw_info['flags'] = array(
+		'currentapp'   => 'polls',
+		'enable_nextmatchs_class' => True,
+		'admin_header' => True
+	);
+	include('../header.inc.php');
 
-  $phpgw->template->set_file(array("form"   => "admin_form.tpl",
-                                   "row"    => "admin_form_row_2.tpl"
-                                  ));
+	$p = CreateObject('phpgwapi.Template',PHPGW_APP_TPL);
+	$p->set_file(array('admin' => 'admin_form.tpl'));
+	$p->set_block('admin','form','form');
+	$p->set_block('admin','row','row');
 
-  $phpgw->db->query("select poll_title from phpgw_polls_desc where poll_id='$poll_id'");
-  $phpgw->db->next_record();
-  $poll_title = $phpgw->db->f("poll_title");
+	$phpgw->db->query("select poll_title from phpgw_polls_desc where poll_id='$poll_id'");
+	$phpgw->db->next_record();
+	$poll_title = $phpgw->db->f('poll_title');
 
-  $phpgw->template->set_var("message","");
-  $phpgw->template->set_var("header_message",lang("View poll"));
-  $phpgw->template->set_var("td_message","&nbsp;");
-  $phpgw->template->set_var("th_bg",$phpgw_info["theme"]["th_bg"]);
-  $phpgw->template->set_var("form_action",$phpgw->link("/polls/admin_editquestion.php"));
-  $phpgw->template->set_var("form_button_1",'<input type="submit" name="submit" value="' . lang("Edit") . '">');
-  $phpgw->template->set_var("form_button_2",'</form><form method="POST" action="' . $phpgw->link("/polls/admin_deletequestion.php","poll_id=$poll_id") . '"><input type="submit" name="submit" value="' . lang("Delete") . '">');
+	$p->set_var('message','');
+	$p->set_var('header_message',lang('View poll'));
+	$p->set_var('td_message','&nbsp;');
+	$p->set_var('th_bg',$phpgw_info['theme']['th_bg']);
+	$p->set_var('form_action',$phpgw->link('/polls/admin_editquestion.php'));
+	$p->set_var('form_button_1','<input type="submit" name="submit" value="' . lang('Edit') . '">');
+	$p->set_var('form_button_2','</form><form method="POST" action="'
+		 $phpgw->link("/polls/admin_deletequestion.php","poll_id=$poll_id") . '"><input type="submit" name="submit" value="' . lang('Delete') . '">');
 
-  add_template_row($phpgw->template,lang("Poll question"),$phpgw->strip_html($poll_title));
+	add_template_row($p,lang('Poll question'),$phpgw->strip_html($poll_title));
 
-  $phpgw->db->query("select * from phpgw_polls_data where poll_id='$poll_id'");
-  while ($phpgw->db->next_record()) {
-     if (! $title_shown) {
-        $title = lang("Answers");
-        $title_shown = True;
-     }
-     add_template_row($phpgw->template,$title,$phpgw->strip_html($phpgw->db->f("option_text")));
-     $title = "&nbsp;";
-  }
+	$phpgw->db->query("select * from phpgw_polls_data where poll_id='$poll_id'");
+	while ($phpgw->db->next_record())
+	{
+		if (! $title_shown)
+		{
+			$title = lang('Answers');
+			$title_shown = True;
+		}
+		add_template_row($p,$title,$phpgw->strip_html($phpgw->db->f('option_text')));
+		$title = '&nbsp;';
+	}
 
-  $phpgw->template->pparse("out","form");
-  $phpgw->common->phpgw_footer();
+	$phpgw->template->pparse('out','form');
+	$phpgw->common->phpgw_footer();
 ?>
--- polls/templates/default/admin_form_row.tpl
+++
@@ -1,5 +0,0 @@
-<!-- BEGIN row -->
-   <tr bgcolor="{tr_color}">
-    <td colspan="2">{td_1}</td>
-   </tr>
-<!-- END row -->
