d5d4d226310e6d3a1c9c5a49805aae6f035aaa98
jengo <jengo@923e3c2e-ae13-0410-ba18-ced7211e33de>
2001-05-15 22:08:40 +0000

    Fixed formating
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@3759 923e3c2e-ae13-0410-ba18-ced7211e33de

--- comic/inc/hook_preferences.inc.php
+++ comic/inc/hook_preferences.inc.php
@@ -1,40 +1,40 @@
 <?php
-  /**************************************************************************\
-  * phpGroupWare - Comic Prefs                                               *
-  * http://www.phpgroupware.org                                              *
-  * --------------------------------------------                             *
-  *  This program is free software; you can redistribute it and/or modify it *
-  *  under the terms of the GNU General Public License as published by the   *
-  *  Free Software Foundation; either version 2 of the License, or (at your  *
-  *  option) any later version.                                              *
-  \**************************************************************************/
-/* $Id$ */
+	/**************************************************************************\
+	* phpGroupWare - Comic Prefs                                               *
+	* http://www.phpgroupware.org                                              *
+	* --------------------------------------------                             *
+	*  This program is free software; you can redistribute it and/or modify it *
+	*  under the terms of the GNU General Public License as published by the   *
+	*  Free Software Foundation; either version 2 of the License, or (at your  *
+	*  option) any later version.                                              *
+	\**************************************************************************/
 
-{
-	echo "<p>\n";
-
-	$imgfile = $phpgw->common->get_image_dir("comic")."/" . $appname .".gif";
-	if (file_exists($imgfile))
+	/* $Id$ */
+   
 	{
-		$imgpath = $phpgw->common->get_image_path("comic")."/" . $appname .".gif";
-	}
-	else
-	{
-		$imgfile = $phpgw->common->get_image_dir("comic")."/navbar.gif";
+		echo "<p>\n";
+		$imgfile = $phpgw->common->get_image_dir('comic') . '/' . $appname . '.gif';
 		if (file_exists($imgfile))
 		{
-			$imgpath = $phpgw->common->get_image_path("comic")."/navbar.gif";
+			$imgpath = $phpgw->common->get_image_path('comic') . '/' . $appname . '.gif';
 		}
 		else
 		{
-			$imgpath = "";
+			$imgfile = $phpgw->common->get_image_dir('comic') . '/navbar.gif';
+			if (file_exists($imgfile))
+			{
+				$imgpath = $phpgw->common->get_image_path('comic') . '/navbar.gif';
+			}
+			else
+			{
+				$imgpath = '';
+			}
 		}
-	}
 
-	section_start("Daily Comics",$imgpath);
+		section_start('Daily Comics',$imgpath);
 
-	section_item($phpgw->link('/comic/preferences.php'),lang("Preferences"));
+		section_item($phpgw->link('/comic/preferences.php'),lang('Preferences'));
 
-	section_end(); 
+		section_end(); 
 }
 ?>
