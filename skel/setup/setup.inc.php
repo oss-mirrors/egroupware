525de5061a250b27b1410cf1c5fd68084d2be1e7
seek3r <seek3r@923e3c2e-ae13-0410-ba18-ced7211e33de>
2001-05-23 17:14:55 +0000

    updating to new format that supports depends
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@3910 923e3c2e-ae13-0410-ba18-ced7211e33de

--- skel/setup/setup.inc.php
+++ skel/setup/setup.inc.php
@@ -1,9 +1,19 @@
 <?php
-	$setup_info['skel']['name'] = 'Skeleton';
-	$setup_info['skel']['version'] = '0.0.1.000';
+	/* Basic information about this app */
+	$setup_info['skel']['name']      = 'skel';
+	$setup_info['skel']['title']     = 'Skeleton';
+	$setup_info['skel']['version']   = '0.0.1.000';
 	$setup_info['skel']['app_order'] = 8;
-	$setup_info['skel']['tables'] = "";
-	$hooks = Array();
-	$hooks_string = implode (',', $hooks);
-	$setup_info['notes']['skel'] = $hooks_string;
+	
+	/* The tables this app creates */
+	$setup_info['skel']['tables']    = Array('skel');
+
+	/* The hooks this app includes, needed for hooks registration */
+	$setup_info['skel']['hooks'][] = 'preferences';
+
+	/* Dependacies for this app to work */
+	$setup_info['skel']['depends'][] = array(
+			 'appname' => 'phpgwapi',
+			 'versions' => Array('0.9.10', '0.9.11' , '0.9.12')
+		);
 ?>
