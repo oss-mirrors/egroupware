fa344062e13f6006954276f476d26f9e33fcfa3e
milosch <milosch@923e3c2e-ae13-0410-ba18-ced7211e33de>
2001-05-29 14:15:53 +0000

    setup.inc changes to work with setup3
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@4020 923e3c2e-ae13-0410-ba18-ced7211e33de

--- email/setup/setup.inc.php
+++ email/setup/setup.inc.php
@@ -1,9 +1,17 @@
 <?php
-	$setup_info['email']['name'] = 'email';
-	$setup_info['email']['version'] = '0.9.11';
+	$setup_info['email']['name']      = 'email';
+	$setup_info['email']['title']     = 'Email';
+	$setup_info['email']['version']   = '0.9.11';
 	$setup_info['email']['app_order'] = '2';
-	$setup_info['email']['tables'] = "";
-	$hooks = Array();
-	$hooks_string = implode (',', $hooks);
-	$setup_info['email']['hooks'] = $hooks_string;
+	$setup_info['email']['tables']    = "";
+
+	/* The hooks this app includes, needed for hooks registration */
+	$setup_info['email']['hooks'][] = 'preferences';
+	$setup_info['email']['hooks'][] = 'admin';
+
+	/* Dependacies for this app to work */
+	$setup_info['email']['depends'][] = array(
+		 'appname' => 'phpgwapi',
+		 'versions' => Array('0.9.10', '0.9.11' , '0.9.12')
+	);
 ?>
