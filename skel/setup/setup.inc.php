2c82b6c065c3da751f7ec9b2f0f112a12f6214b3
seek3r <seek3r@923e3c2e-ae13-0410-ba18-ced7211e33de>
2001-05-30 17:48:20 +0000

    updating for the new dev versioning
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@4046 923e3c2e-ae13-0410-ba18-ced7211e33de

--- skel/setup/setup.inc.php
+++ skel/setup/setup.inc.php
@@ -14,10 +14,10 @@
 	/* Dependacies for this app to work */
 	$setup_info['skel']['depends'][] = array(
 			 'appname' => 'phpgwapi',
-			 'versions' => Array('0.9.10', '0.9.11' , '0.9.12')
+			 'versions' => Array('0.9.10', '0.9.11' , '0.9.12', '0.9.13')
 		);
 	$setup_info['skel']['depends'][] = array(
 			 'appname' => 'email',
-			 'versions' => Array('0.9.10', '0.9.11' , '0.9.12')
+			 'versions' => Array('0.9.10', '0.9.11' , '0.9.12', '0.9.13')
 		);
 ?>
