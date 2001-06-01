3cabc92aa93b34e993ed3428dd1f8323f4d383d4
milosch <milosch@923e3c2e-ae13-0410-ba18-ced7211e33de>
2001-06-01 07:33:07 +0000

    Add a skel table spec
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@4092 923e3c2e-ae13-0410-ba18-ced7211e33de

---
+++ skel/setup/tables_current.inc.php
@@ -0,0 +1,30 @@
+<?php
+  /**************************************************************************\
+  * phpGroupWare                                                             *
+  * http://www.phpgroupware.org                                              *
+  * --------------------------------------------                             *
+  *  This program is free software; you can redistribute it and/or modify it *
+  *  under the terms of the GNU General Public License as published by the   *
+  *  Free Software Foundation; either version 2 of the License, or (at your  *
+  *  option) any later version.                                              *
+  \**************************************************************************/
+
+  /* $Id$ */
+
+	$phpgw_baseline = array(
+		'skel' => array(
+			'fd' => array(
+				'skel_id' => array('type' => 'auto', 'nullable' => false),
+				'skel_owner' => array('type' => 'varchar', 'precision' => 25),
+				'skel_access' => array('type' => 'varchar', 'precision' => 10),
+				'skel_cat' => array('type' => 'int', 'precision' => 4),
+				'skel_des' => array('type' => 'text'),
+				'skel_pri' => array('type' => 'int', 'precision' => 4),
+			),
+			'pk' => array('skel_id'),
+			'fk' => array(),
+			'ix' => array(),
+			'uc' => array()
+		)
+	);
+?>
