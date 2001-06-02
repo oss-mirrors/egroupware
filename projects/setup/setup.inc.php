beeedaa1c633dd7efdb8250fa11c229a655705f0
bettina <bettina@923e3c2e-ae13-0410-ba18-ced7211e33de>
2001-06-02 02:45:48 +0000

    setup
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@4106 923e3c2e-ae13-0410-ba18-ced7211e33de

---
+++ projects/setup/setup.inc.php
@@ -0,0 +1,41 @@
+<?php
+  /*************************************************************************\
+  * phpGroupWare Setup - Projects                                           *
+  * http://www.phpgroupware.org                                             *
+  * --------------------------------------------                            *
+  * This program is free software; you can redistribute it and/or modify it *
+  * under the terms of the GNU General Public License as published by the   *
+  * Free Software Foundation; either version 2 of the License, or (at your  *
+  * option) any later version.                                              *
+  \*************************************************************************/
+  /* $Id$ */
+
+	$setup_info['projects']['name']      = 'projects';
+	$setup_info['projects']['title']     = 'Projects';
+	$setup_info['projects']['version']   = '0.8.4.006';
+	$setup_info['projects']['app_order'] = 14;
+
+	$setup_info['projects']['tables'] = array(
+		'phpgw_p_projects',
+		'phpgw_p_activities',
+		'phpgw_p_projectactivities',
+		'phpgw_p_hours',
+		'phpgw_p_projectmembers',
+		'phpgw_p_invoice',
+		'phpgw_p_invoicepos',
+		'phpgw_p_delivery',
+		'phpgw_p_deliverypos'
+	);
+
+/* The hooks this app includes, needed for hooks registration */
+
+	$setup_info['projects']['hooks'][] = 'preferences';
+	$setup_info['projects']['hooks'][] = 'admin';
+
+/* Dependacies for this app to work */
+
+	$setup_info['projects']['depends'][] = array(
+		 'appname' => 'phpgwapi',
+		 'versions' => Array('0.9.10', '0.9.11' , '0.9.12', '0.9.13')
+	);
+?>
