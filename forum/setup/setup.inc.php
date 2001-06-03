b0f2177d19132551097debba88773698dc738a4a
jengo <jengo@923e3c2e-ae13-0410-ba18-ced7211e33de>
2001-06-03 19:34:07 +0000

    Added the setup files for the new setup app
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@4120 923e3c2e-ae13-0410-ba18-ced7211e33de

---
+++ forum/setup/setup.inc.php
@@ -0,0 +1,33 @@
+<?php
+	/**************************************************************************\
+	* phpGroupWare - Forum                                                      *
+	* http://www.phpgroupware.org                                              *
+	* --------------------------------------------                             *
+	*  This program is free software; you can redistribute it and/or modify it *
+	*  under the terms of the GNU General Public License as published by the   *
+	*  Free Software Foundation; either version 2 of the License, or (at your  *
+	*  option) any later version.                                              *
+	\**************************************************************************/
+
+	/* $Id$ */
+
+	/* Basic information about this app */
+	$setup_info['forum']['name']      = 'forum';
+	$setup_info['forum']['title']     = 'forum';
+	$setup_info['forum']['version']   = '0.8.1';
+	$setup_info['forum']['app_order'] = 4;
+
+	/* The tables this app creates */
+	$setup_info['forum']['tables']    = array(
+		'f_body',
+		'f_categories',
+		'f_forums',
+		'f_threads'
+	);
+
+	/* Dependencies for this app to work */
+	$setup_info['forum']['depends'][] = array(
+		 'appname' => 'phpgwapi',
+		 'versions' => Array('0.9.10', '0.9.11' , '0.9.12', '0.9.13')
+	);
+?>
---
+++ forum/setup/tables_current.inc.php
@@ -0,0 +1,81 @@
+<?php
+  /**************************************************************************\
+  * phpGroupWare - Setup                                                     *
+  * http://www.phpgroupware.org                                              *
+  * --------------------------------------------                             *
+  *  This program is free software; you can redistribute it and/or modify it *
+  *  under the terms of the GNU General Public License as published by the   *
+  *  Free Software Foundation; either version 2 of the License, or (at your  *
+  *  option) any later version.                                              *
+  \**************************************************************************/
+
+  /**************************************************************************\
+  * This file should be generated for you. It should never be edited by hand *
+  \**************************************************************************/
+
+  /* $Id$ */
+
+  // table array for forum
+	$phpgw_baseline = array(
+		'f_body' => array(
+			'fd' => array(
+				'id' => array('type' => 'auto','nullable' => False),
+				'cat_id' => array('type' => 'int', 'precision' => 8,'nullable' => False),
+				'for_id' => array('type' => 'int', 'precision' => 8,'nullable' => False),
+				'message' => array('type' => 'blob','nullable' => False),
+			),
+			'pk' => array('id'),
+			'fk' => array(),
+			'ix' => array(),
+			'uc' => array()
+		),
+		'f_categories' => array(
+			'fd' => array(
+				'id' => array('type' => 'auto','nullable' => False),
+				'name' => array('type' => 'varchar', 'precision' => 8,'nullable' => False),
+				'descr' => array('type' => 'varchar', 'precision' => 8,'nullable' => False),
+			),
+			'pk' => array('id'),
+			'fk' => array(),
+			'ix' => array(),
+			'uc' => array()
+		),
+		'f_forums' => array(
+			'fd' => array(
+				'id' => array('type' => 'auto','nullable' => False),
+				'name' => array('type' => 'varchar', 'precision' => 8,'nullable' => False),
+				'perm' => array('type' => 'int', 'precision' => 2,'nullable' => False),
+				'groups' => array('type' => 'varchar', 'precision' => 8,'nullable' => False),
+				'descr' => array('type' => 'varchar', 'precision' => 8,'nullable' => False),
+				'cat_id' => array('type' => 'int', 'precision' => 8,'nullable' => False),
+			),
+			'pk' => array('id'),
+			'fk' => array(),
+			'ix' => array(),
+			'uc' => array()
+		),
+		'f_threads' => array(
+			'fd' => array(
+				'id' => array('type' => 'auto','nullable' => False),
+				'postdate' => array('type' => 'timestamp','nullable' => False,'default' => '0000-00-00 00:00:00'),
+				'main' => array('type' => 'int', 'precision' => 8,'nullable' => False),
+				'parent' => array('type' => 'int', 'precision' => 8,'nullable' => False),
+				'cat_id' => array('type' => 'int', 'precision' => 8,'nullable' => False),
+				'for_id' => array('type' => 'int', 'precision' => 8,'nullable' => False),
+				'author' => array('type' => 'varchar', 'precision' => 8,'nullable' => False),
+				'subject' => array('type' => 'varchar', 'precision' => 8,'nullable' => False),
+				'email' => array('type' => 'varchar', 'precision' => 8,'nullable' => False),
+				'host' => array('type' => 'varchar', 'precision' => 8,'nullable' => False),
+				'stat' => array('type' => 'int', 'precision' => 2,'nullable' => False),
+				'thread' => array('type' => 'int', 'precision' => 8,'nullable' => False),
+				'depth' => array('type' => 'int', 'precision' => 8,'nullable' => False),
+				'pos' => array('type' => 'int', 'precision' => 8,'nullable' => False),
+				'n_replies' => array('type' => 'int', 'precision' => 8,'nullable' => False),
+			),
+			'pk' => array('id'),
+			'fk' => array(),
+			'ix' => array('postdate'),
+			'uc' => array()
+		),
+	);
+?>
