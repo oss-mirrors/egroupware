14cbf59cc9798d9d4e4202fe03397c9f733cdbb0
jengo <jengo@923e3c2e-ae13-0410-ba18-ced7211e33de>
2001-06-03 19:46:02 +0000

    Added new setup files for the new setup app
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@4121 923e3c2e-ae13-0410-ba18-ced7211e33de

---
+++ polls/setup/setup.inc.php
@@ -0,0 +1,33 @@
+<?php
+	/**************************************************************************\
+	* phpGroupWare - Polls                                                     *
+	* http://www.phpgroupware.org                                              *
+	* --------------------------------------------                             *
+	*  This program is free software; you can redistribute it and/or modify it *
+	*  under the terms of the GNU General Public License as published by the   *
+	*  Free Software Foundation; either version 2 of the License, or (at your  *
+	*  option) any later version.                                              *
+	\**************************************************************************/
+
+	/* $Id$ */
+
+	/* Basic information about this app */
+	$setup_info['polls']['name']      = 'polls';
+	$setup_info['polls']['title']     = 'Polls';
+	$setup_info['polls']['version']   = '0.8.1';
+	$setup_info['polls']['app_order'] = 15;
+
+	/* The tables this app creates */
+	$setup_info['polls']['tables']    = array(
+		'phpgw_polls_data',
+		'phpgw_polls_desc',
+		'phpgw_polls_user',
+		'phpgw_polls_settings'
+	);
+
+	/* Dependencies for this app to work */
+	$setup_info['polls']['depends'][] = array(
+		 'appname' => 'phpgwapi',
+		 'versions' => Array('0.9.10','0.9.11','0.9.12','0.9.13')
+	);
+?>
