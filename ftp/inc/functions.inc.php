8d5bdf89912a22709ea9569a2af3c9605076520a
milosch <milosch@923e3c2e-ae13-0410-ba18-ced7211e33de>
2001-06-05 17:51:10 +0000

    add comments header to file to keep revision #
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@4179 923e3c2e-ae13-0410-ba18-ced7211e33de

--- ftp/inc/functions.inc.php
+++ ftp/inc/functions.inc.php
@@ -1,4 +1,17 @@
 <?php
+   /**************************************************************************\
+   * phpGroupWare - Ftp Module                                                *
+   * http://www.phpgroupware.org                                              *
+   * Written by Scott Moser <smoser@brickies.net>                             *
+   * --------------------------------------------                             *
+   *  This program is free software; you can redistribute it and/or modify it *
+   *  under the terms of the GNU General Public License as published by the   *
+   *  Free Software Foundation; either version 2 of the License, or (at your  *
+   *  option) any later version.                                              *
+   \**************************************************************************/
+
+	/* $Id$ */
+
    function createLink($string) {
       global $phpgw;
       return $phpgw->link($string);
