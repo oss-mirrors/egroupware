4be29275026377cf5c643cb2cd5fc0a2a8306f3d
angles <angles@923e3c2e-ae13-0410-ba18-ced7211e33de>
2001-06-05 07:44:39 +0000

    Experimental- fix multiple attachment problem, add sig before attachments, fix Message-Boundary placement
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@4172 923e3c2e-ae13-0410-ba18-ced7211e33de

--- email/send_message.php
+++ email/send_message.php
@@ -24,27 +24,67 @@
 	$phpgw_info['flags'] = $phpgw_flags;
 	include('../header.inc.php');
 
+	$num_expected = 0;
+	$total_files = 0;
+
 	$upload_dir = $phpgw_info['server']['temp_dir'].SEP.$phpgw_info['user']['sessionid'];
 
 	if (file_exists($upload_dir))
 	{
 		@set_time_limit(0);
 		$dh = opendir($upload_dir);
+		// how many attachments do we have?
 		while ($file = readdir($dh))
 		{
-			if ($file != '.' && $file != '..')
+			if (($file != '.')
+			&& ($file != '..')
+			&& (ereg("\.info",$file)))
+			{
+				$num_expected++;
+			}
+		}
+		closedir($dh);
+
+		// if there are attachments then we need to add the sig to the body BEFORE the attachments
+		if (($num_expected > 0)
+		&& ($phpgw_info['user']['preferences']['email']['email_sig'])
+		&& ($attach_sig))
+		{
+			//$body .= "\n-----\n".$phpgw_info['user']['preferences']['email']['email_sig'];
+			$user_sig = $phpgw_info['user']['preferences']['email']['email_sig'];
+			// obsoleted: ereg_replace should not be needed after 0.9.13
+			$user_sig = ereg_replace('&quot;', '"', $user_sig);
+			$user_sig = ereg_replace('&#039;', '\'', $user_sig);
+			$body .= "\n-----\n" .$user_sig;
+		}
+
+		$dh = opendir($upload_dir);
+		// encode attachments and add to the email
+		while ($file = readdir($dh))
+		{
+			if (($file != '.')
+			&& ($file != '..'))
 			{
 				if (! ereg("\.info",$file))
 				{
 					$total_files++;
 					$size = filesize($upload_dir.SEP.$file);
 
-					$fd = fopen ($upload_dir.SEP.$file.'.info','rb');
-					while (!feof ($fd))
+					$info_file = $upload_dir.SEP.$file.'.info';
+					$file_info = file($info_file);
+					$content_type = trim($file_info[0]);
+					$content_name = trim($file_info[1]);
+					
+					// what boundry do we use
+					if ($total_files >= $num_expected)
 					{
-						$file_info[] = chop(fgets($fd, 4096));
+						$mess_boundary = '--Message-Boundary--';
 					}
-					fclose ($fd);
+					else
+					{
+						$mess_boundary = '';
+					}
+					//echo 'tot: '.$total_files .' expext: '.$num_expected;
 
 					set_magic_quotes_runtime(0); 
 					$fh = fopen($upload_dir.SEP.$file,'rb');
@@ -54,10 +94,11 @@
 					set_magic_quotes_runtime(get_magic_quotes_gpc());
 
 					$body .= "\n\n".'--Message-Boundary'."\n"
-						. 'Content-type: '.$file_info[0].'; name="'.$file_info[1].'"'."\n"
+						. 'Content-type: '.$content_type.'; name="'.$content_name.'"'."\n"
 						. 'Content-Transfer-Encoding: BASE64'."\n"
-						. 'Content-disposition: attachment; filename="'.$file_info[1].'"'."\n\n"
-						. $encoded_attach.'--Message-Boundary--'."\n";
+						. 'Content-disposition: attachment; filename="'.$content_name.'"'."\n\n"
+						//. $encoded_attach."$message_boundary"."\n";
+						. $encoded_attach .$mess_boundary ."\n";
 					unlink($upload_dir.SEP.$file);
 
 					unlink($upload_dir.SEP.$file.'.info');
@@ -66,6 +107,19 @@
 		} 		// while dirread
 		rmdir($upload_dir);
 	}		// if dir
+
+	// if there are NO attachments then add the sig to the body here
+	if (($num_expected == 0)
+	&& ($phpgw_info['user']['preferences']['email']['email_sig'])
+	&& ($attach_sig))
+	{
+		//$body .= "\n-----\n".$phpgw_info['user']['preferences']['email']['email_sig'];
+		$user_sig = $phpgw_info['user']['preferences']['email']['email_sig'];
+		// obsoleted: ereg_replace should not be needed after 0.9.13
+		$user_sig = ereg_replace('&quot;', '"', $user_sig);
+		$user_sig = ereg_replace('&#039;', '\'', $user_sig);
+		$body .= "\n-----\n" .$user_sig;
+	}
 
 	$rc = $phpgw->send->msg('email', $to, $subject, stripslashes($body), '', $cc, $bcc);
 	if ($rc)
