98f21e21c18108306fe09e581b72bac1e29331c4
milosch <milosch@923e3c2e-ae13-0410-ba18-ced7211e33de>
2001-06-07 15:06:58 +0000

    New install should work for this
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@4227 923e3c2e-ae13-0410-ba18-ced7211e33de

---
+++ tts/setup/setup.inc.php
@@ -0,0 +1,37 @@
+<?php
+	/**************************************************************************\
+	* phpGroupWare - Addressbook                                               *
+	* http://www.phpgroupware.org                                              *
+	* --------------------------------------------                             *
+	*  This program is free software; you can redistribute it and/or modify it *
+	*  under the terms of the GNU General Public License as published by the   *
+	*  Free Software Foundation; either version 2 of the License, or (at your  *
+	*  option) any later version.                                              *
+	\**************************************************************************/
+
+	/* $Id$ */
+
+	/* Basic information about this app */
+	$setup_info['tts']['name']      = 'tts';
+	$setup_info['tts']['title']     = 'Trouble Ticket System';
+	$setup_info['tts']['version']   = '0.0.1';
+	$setup_info['tts']['app_order'] = 99;
+	$setup_info['tts']['enable']    = 1;
+
+	/* The tables this app creates */
+	$setup_info['tts']['tables']    = array(
+		'ticket',
+		'category',
+		'department'
+	);
+
+	/* The hooks this app includes, needed for hooks registration */
+	$setup_info['tts']['hooks'][] = 'preferences';
+	$setup_info['tts']['hooks'][] = 'admin';
+
+	/* Dependencies for this app to work */
+	$setup_info['tts']['depends'][] = array(
+		 'appname' => 'phpgwapi',
+		 'versions' => Array('0.9.10', '0.9.11' , '0.9.12', '0.9.13')
+	);
+?>
