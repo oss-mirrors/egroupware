bc754fbce8e93265468b13efe97dbad8c885b165
milosch <milosch@923e3c2e-ae13-0410-ba18-ced7211e33de>
2001-06-07 10:55:50 +0000

    Formatting
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@4221 923e3c2e-ae13-0410-ba18-ced7211e33de

--- email/inc/hook_notifywindow.inc.php
+++ email/inc/hook_notifywindow.inc.php
@@ -11,55 +11,54 @@
 
 	/* $Id$ */
 
-	$d1 = strtolower(substr($phpgw_info["server"]["app_inc"],0,3));
-	if($d1 == "htt" || $d1 == "ftp" ) 
+	$d1 = strtolower(substr($phpgw_info['server']['app_inc'],0,3));
+	if($d1 == 'htt' || $d1 == 'ftp' ) 
 	{
 		echo 'error:Failed attempt to break in via an old Security Hole!'.chr(13);
 		$phpgw->common->phpgw_exit();
 	}
 	unset($d1);
 
-	$tmp_app_inc = $phpgw_info["server"]["app_inc"];
-	$phpgw_info["server"]["app_inc"] = $phpgw_info["server"]["server_root"]."/email/inc";
+	$phpgw_info['server']['app_inc'] = PHPGW_SERVER_ROOT . SEP . 'email' . SEP . 'inc';
 
-	if ($phpgw_info["user"]["preferences"]["email"]["mainscreen_showmail"] &&
-		 (isset($phpgw_info["user"]["apps"]["email"]) && $phpgw_info["user"]["apps"]["email"]))
+	if ($phpgw_info['user']['preferences']['email']['mainscreen_showmail'] &&
+		 (isset($phpgw_info['user']['apps']['email']) && $phpgw_info['user']['apps']['email']))
 	{
-		include($phpgw_info["server"]["app_inc"] . "/functions.inc.php");
+		include($phpgw_info['server']['app_inc'] . SEP . 'functions.inc.php');
 
-		$mailbox_status = $phpgw->msg->status($mailbox,"{" . $phpgw_info["user"]["preferences"]["email"]["mail_server"] . ":" . $phpgw_info["user"]["preferences"]["email"]["mail_port"] . "}INBOX",SA_UNSEEN);
+		$mailbox_status = $phpgw->msg->status($mailbox,'{' . $phpgw_info['user']['preferences']['email']['mail_server'] . ':' . $phpgw_info['user']['preferences']['email']['mail_port'] . '}INBOX',SA_UNSEEN);
 		$nummsg = intval($phpgw->msg->num_msg($mailbox));
 		$str = '';
 		
-		if ($phpgw_info["user"]["preferences"]["email"]["mail_server_type"] == "imap") 
+		if ($phpgw_info['user']['preferences']['email']['mail_server_type'] == 'imap')
 		{
 			if ($mailbox_status->unseen == 1) 
 			{
-				$str .= lang("You have 1 new message!");
+				$str .= lang('You have 1 new message!');
 			}
 			if ($mailbox_status->unseen > 1) 
 			{
-				$str .= lang("You have x new messages!",$mailbox_status->unseen);
+				$str .= lang('You have x new messages!',$mailbox_status->unseen);
 			}
 			if ($mailbox_status->unseen == 0) 
 			{
-				$str .= lang("You have no new messages");
+				$str .= lang('You have no new messages');
 			}
 		}
 		else
 		{
 			if ($nummsg > 0) 
 			{
-				$str .= lang("You have messages!");
+				$str .= lang('You have messages!');
 			}
 			elseif ($nummsg == 0) 
 			{
-				$str .= lang("You have no new messages");
+				$str .= lang('You have no new messages');
 			}
 		}
 		if ($str != '')
 		{
-			echo "\n".'<tr><td align="left"><!-- Mailbox info -->'."\n";
+			echo "\n" . '<tr><td align="left"><!-- Mailbox info -->' . "\n";
 /*			echo '<script language="JavaScript">'.chr(13).chr(10);
 			echo '<!-- Activate Cloaking Device'.chr(13).chr(10);
 			echo '	funtion CheckEmail()'.chr(13).chr(10);
@@ -74,5 +73,5 @@
 		}
 	}
 
-	$phpgw_info["server"]["app_inc"] = $tmp_app_inc;
+	$phpgw_info['server']['app_inc'] = $tmp_app_inc;
 ?>
