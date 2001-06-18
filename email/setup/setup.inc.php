20f103e557a40ba964a077e80a22133acf609a5f
milosch <milosch@923e3c2e-ae13-0410-ba18-ced7211e33de>
2001-06-18 22:43:12 +0000

    Update to current api version
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@4485 923e3c2e-ae13-0410-ba18-ced7211e33de

--- email/setup/setup.inc.php
+++ email/setup/setup.inc.php
@@ -13,7 +13,7 @@
 
 	$setup_info['email']['name']      = 'email';
 	$setup_info['email']['title']     = 'Email';
-	$setup_info['email']['version']   = '0.9.11';
+	$setup_info['email']['version']   = '0.9.13.002';
 	$setup_info['email']['app_order'] = '2';
 	$setup_info['email']['enable']    = 1;
 	$setup_info['email']['tables']    = '';
