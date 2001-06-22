f7287ffea9da9bdedfa33ee46746d87e6564b2f1
sameon <sameon@923e3c2e-ae13-0410-ba18-ced7211e33de>
2001-06-22 09:07:24 +0000

    Changing to template
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@4538 923e3c2e-ae13-0410-ba18-ced7211e33de

--- forum/post.php
+++ forum/post.php
@@ -1,112 +1,132 @@
 <?php
-  /**************************************************************************\
-  * phpGroupWare - Forums                                                    *
-  * http://www.phpgroupware.org                                              *
-  * Written by Jani Hirvinen <jpkh@shadownet.com>                            *
-  * --------------------------------------------                             *
-  *  This program is free software; you can redistribute it and/or modify it *
-  *  under the terms of the GNU General Public License as published by the   *
-  *  Free Software Foundation; either version 2 of the License, or (at your  *
-  *  option) any later version.                                              *
-  \**************************************************************************/
 
-	$phpgw_info['flags'] = array(
-		'currentapp' => 'forum',
-		'enable_nextmatchs_class' => True
-	);
 
-	if($action)
-	{
-		$phpgw_info['flags']['noheader'] = True;
-		$phpgw_info['flags']['nonavbar'] = True;
-	}
-	include('../header.inc.php');
+  $phpgw_info["flags"] = array("currentapp" => "forum", "enable_nextmatchs_class" => True);
+  if($action) {
+    $phpgw_info["flags"]["noheader"] = True;
+    $phpgw_info["flags"]["nonavbar"] = True;
+  }
+  include("../header.inc.php");
 
-	if($action == 'post')
-	{
-		$host = getenv('REMOTE_ADDR');
-		if(!$host) getenv('REMOTE_HOST');
+if($action == "post") {
 
-		$stat = 0;
+ $host = getenv('REMOTE_ADDR');
+ if(!$host) getenv('REMOTE_HOST');
 
-		$phpgw->db->query("select max(id) from f_body");
-		$phpgw->db->next_record();
-		$next_f_body_id = $phpgw->db->f("0") + 1;
+ $stat = 0;
 
-		$phpgw->db->query("select max(id) from f_threads");
-		$phpgw->db->next_record();
-		$next_f_threads_id = $phpgw->db->f("0") + 1;
+ $phpgw->db->query("select max(id) from f_body");
+ $phpgw->db->next_record();
+ $next_f_body_id = $phpgw->db->f("0") + 1;
 
-		//print "$next_f_threads_id <br> $next_f_body_id";
+ $phpgw->db->query("select max(id) from f_threads");
+ $phpgw->db->next_record();
+ $next_f_threads_id = $phpgw->db->f("0") + 1;
 
-		$dattim = date("Y-m-d H:i:s",time());
+//print "$next_f_threads_id <br> $next_f_body_id";
+$datetime = $phpgw->common->show_date(time(),"Y-m-d h:i:s");
 
-		$phpgw->db->query("insert into f_threads (postdate,pos,thread,depth,main,parent,cat_id,for_id,author,subject,email,host,stat) VALUES (
-			'$dattim',0,$next_f_body_id,0,$next_f_body_id,-1,$cat,$for,'$author','$subject','$email','$host',$stat)"
-		);
+ $phpgw->db->query("insert into f_threads (postdate,pos,thread,depth,main,parent,cat_id,for_id,author,subject,email,host,stat) VALUES (
+  '$datetime',
+  0,
+  $next_f_body_id,
+  0,
+  $next_f_body_id,
+  -1,
+  $cat,
+  $for,
+  '$author',
+  '$subject',
+  '$email',
+  '$host',
+  $stat)");
 
-		$phpgw->db->query("insert into f_body (cat_id,for_id,message) VALUES (
-			$cat,$for,'$message')"
-		);
+  $phpgw->db->query("insert into f_body (cat_id,for_id,message) VALUES (
+  $cat,
+  $for,
+  '$message')");
+  
 
-		Header("Location: ". $phpgw->link("/forum/threads.php","cat=".$cat."&for=".$for."&col=".$col));
-		$phpgw->common->phpgw_exit();
-	}	
-?>
-<p>
-<table border="1" width="100%">
- <tr>
-<?php
-	$phpgw->db->query("select * from f_categories where id = $cat");
-	$phpgw->db->next_record();
-	$category = $phpgw->db->f('name');
+  Header("Location: ". $phpgw->link("/forum/threads.php","cat=".$cat."&for=".$for."&col=".$col));
+  $phpgw->common->phpgw_exit();
 
-	$phpgw->db->query("select * from f_forums where id = $for");
-	$phpgw->db->next_record();
-	$forums = $phpgw->db->f('name');
+}
 
-	$catfor = "cat=" . $cat . "&for=" . $for;
+$phpgw->template->set_file('POST','post.body.tpl');
 
-	echo '<td bgcolor="' . $phpgw_info['theme']['th_bg'] . '" align="left"><font size=+1><a href="' . $phpgw->link('/forum/index.php') .'">' . lang('Forums');
-	echo '</a> : <a href="' . $phpgw->link('/forum/forums.php','cat=' . $cat) . '">' . $category . '</a> : ';
-	echo '<a href="' . $phpgw->link('/forum/threads.php',$catfor . '&col=' . $col) . '">' . $forums . '</a></font></td></tr>';
+ 
+ $phpgw->db->query("select * from f_categories where id = $cat");
+ $phpgw->db->next_record();
+ $category = $phpgw->db->f("name");
 
-	echo '<tr>';
-	echo '<td align="left" width="50%" valign="top">';
+ $phpgw->db->query("select * from f_forums where id = $for");
+ $phpgw->db->next_record();
+ $forums = $phpgw->db->f("name");
 
-	echo '<font size=-1>';
-	echo '[ <a href="' . $phpgw->link('/forum/post.php',$catfor . '&type=new') . '">' . lang('New Topic') . '</a>';
-	if(!$col) { echo ' | <a href="' . $phpgw->link('/forum/threads.php',$catfor . '&col=1') . '">' . lang('View Threads') . '</a>'; }
-	else { echo ' | <a href="' . $phpgw->link('/forum/threads.php',$catfor . '&col=0') . '">' . lang('Collapse Threads') . '</a>'; }
-	# This file doesn't exist yet
-	#echo " | <a href=" . $phpgw->link("/forum/search.php","$catfor") . ">" . lang("Search") . "</a>";
-	echo " ]\n</font><br><br>\n";
+ $catfor = "cat=" . $cat . "&for=" . $for;
 
-	echo "<center>\n";
-	echo "<form method=post action=\"".$phpgw->link("/forum/post.php")."\">\n";
-	echo '<input type="hidden" name="cat"  value="' . $cat . '">' . "\n";
-	echo '<input type="hidden" name="for"  value="' . $for . '">' . "\n";
-	echo '<input type="hidden" name="type" value="' . $type . '">' . "\n";
-	if($col) { echo '<input type="hidden" name="col" value="' . $col . '">' . "\n"; }
-	echo '<input type="hidden" name="action" value="post">' . "\n";
+$phpgw->template->set_var(array(
+	BGROUND 		=> $phpgw_info["theme"]["th_bg"],
+	LANG_TOPIC 		=> lang("Topic"),
+	LANG_AUTHOR 		=> lang("Author"),
+	LANG_REPLIES 		=> lang("Replies"),
+	LANG_LATREP 		=> lang("Latest Reply"),
+	LANG_MAIN 		=> lang("Forum"),
+	LANG_NEWTOPIC 		=> lang("New Topic"),
+	LANG_CATEGORY 		=> $category,
+	LANG_FORUM 		=> $forums,
+	LANG_SEARCH		=> lang("Search"),
+	LANG_POST  		=> lang("Post"),
+	FORUM_LINK 		=> $phpgw->link("/forum/forums.php","cat=" . $cat),
+	MAIN_LINK 		=> $phpgw->link("/forum/index.php"),
+	POST_LINK		=> $phpgw->link("/forum/post.php","$catfor&type=new"),
+	THREADS_LINK		=> $phpgw->link("/forum/threads.php","$catfor&col=$col"),
+	SEARCH_LINK		=> $phpgw->link("/forum/search.php","$catfor"),
+	POST_ACTION		=> $phpgw->link("/forum/post.php"),
+	CAT			=> $cat,
+	FORU			=> $for,
+	TYPE			=> $type,
+	ACTION			=> 'post'
+				));	
 
-	echo ' <table border="0" width="80%" bgcolor="' . $phpgw_info['theme']['table_bg'] . '">';
 
-	$name = $phpgw_info['user']['firstname'] . ' ' . $phpgw_info['user']['lastname'];
-	$email = $phpgw_info['user']['email_address'];
 
-	echo ' <tr><th colspan="3" bgcolor="' . $phpgw_info['theme']['th_bg'] . '">' . lang('New Topic') . '</th></tr>';
-	echo ' <tr><td>' . lang('Your Name') . ':</td><td><input type="text" name="author" size="32" maxlength="49" value="' . $name . '"></td><td></td></tr>';
-	echo ' <tr><td>' . lang('Your Email') . ':</td><td><input type="text" name="email" size="32" maxlength="49" value="' . $email . '"></td><td></td></tr>';
-	echo ' <tr><td>' . lang('Subject') . ':</td><td><input type="text" name="subject" size="32" maxlength="49"></td><td></td></tr>';
-	echo ' <tr><td colspan="3"><center><textarea rows="20" cols="50" name="message"></textarea>';
-	echo ' <tr><td colspan="2"><input type="checkbox" name="repmail"> ' . lang('Email replies to this thread, to the address above') . '</td>';
-	echo '  <td align="right"><input type="submit" value="' . lang('Submit') . '"></td></tr>';
-?>
-	</table>
-	</center>
-  </td>
-</table>
-<?php
-	$phpgw->common->phpgw_footer();
+
+ if(!$col) 
+ {
+  $phpgw->template->set_var(array(
+  	THREADS_LINK  => $phpgw->link("/forum/threads.php","$catfor&col=1"),
+  	LANG_THREADS  => lang("Normal View")
+  	));
+ }
+ if($col) 
+ {
+  $phpgw->template->set_var(array(
+  	THREADS_LINK  	=> $phpgw->link("/forum/threads.php","$catfor&col=0"),
+  	LANG_THREADS 	=> lang("View Collapse"),
+  	COL	  	=> $col
+  	));
+	
+}
+
+
+
+ $name = $phpgw_info["user"]["firstname"] . " " . $phpgw_info["user"]["lastname"];
+ $email = $phpgw_info["user"]["email_address"];
+
+ $phpgw->template->set_var(array(
+  	LANG_NAME 	 	=> lang("Your Name"),
+  	LANG_EMAIL  		=> lang("Your Email"),
+  	LANG_SUBJECT		=> lang("Subject"),
+  	LANG_REPLY		=> lang("Email replies to this thread, to the address above"),
+  	LANG_SUBMIT		=> lang("Submit"),
+  	LANG_MESSAGE		=> lang("Message"),
+  	NAME			=> $name,
+  	EMAIL			=> $email,
+  	SUBJECT			=> $subject,
+  	));
+
+
+$phpgw->template->pfp('Out','POST');
+  $phpgw->common->phpgw_footer();
 ?>
--- forum/threads.php
+++ forum/threads.php
@@ -1,82 +1,126 @@
-<?php
-  /**************************************************************************\
-  * phpGroupWare - Forums                                                    *
-  * http://www.phpgroupware.org                                              *
-  * Written by Jani Hirvinen <jpkh@shadownet.com>                            *
-  * --------------------------------------------                             *
-  *  This program is free software; you can redistribute it and/or modify it *
-  *  under the terms of the GNU General Public License as published by the   *
-  *  Free Software Foundation; either version 2 of the License, or (at your  *
-  *  option) any later version.                                              *
-  \**************************************************************************/
+<?
 
-	$phpgw_info['flags'] = array(
-		'currentapp' => 'forum',
-		'enable_nextmatchs_class' => True
-	);
-	include('../header.inc.php');
-?>
 
-<p>
-<table border="0" width="100%">
- <tr>
-<?php
-	$phpgw->db->query("select * from f_categories where id = $cat");
-	$phpgw->db->next_record();
-	$category = $phpgw->db->f('name');
+$phpgw_info["flags"] = array("currentapp" => "forum", "enable_nextmatchs_class" => True);
+include("../header.inc.php");
 
-	$phpgw->db->query("select * from f_forums where id = $for");
-	$phpgw->db->next_record();
-	$forums = $phpgw->db->f('name');
 
-	$catfor = 'cat=' . $cat . '&for=' . $for;
+$phpgw->db->query("select * from f_categories where id = $cat");
+$phpgw->db->next_record();
+$category = $phpgw->db->f("name");
 
-	echo '<td bgcolor="' . $phpgw_info['theme']['th_bg'] . '" align="left"><font size=+1><a href="' . $phpgw->link('/forum/index.php') .'">' . lang('Forums');
-	echo '</a> : <a href="' . $phpgw->link('/forum/forums.php','cat=' . $cat) . '">' . $category . '</a> : ' . $forums . '</font></td></tr>';
+$phpgw->db->query("select * from f_forums where id = $for");
+$phpgw->db->next_record();
+$forums = $phpgw->db->f("name");
+ 
+$catfor = "cat=" . $cat . "&for=" . $for;
 
-	echo '<tr>';
-	echo '<td align="left" width="50%" valign="top">';
+$phpgw->template->set_var(array(
+	BGROUND 		=> $phpgw_info["theme"]["th_bg"],
+	LANG_TOPIC 		=> lang("Topic"),
+	LANG_AUTHOR 		=> lang("Author"),
+	LANG_REPLIES 		=> lang("Replies"),
+	LANG_LATREP 		=> lang("Latest Reply"),
+	LANG_MAIN 		=> lang("Forums"),
+	LANG_NEWTOPIC 		=> lang("New Topic"),
+	LANG_CATEGORY 		=> $category,
+	LANG_FORUM 		=> $forum,
+	FORUM_LINK 		=> $phpgw->link("/forum/forums.php","cat=" . $cat),
+	MAIN_LINK 		=> $phpgw->link("/forum/index.php"),
+	POST_LINK		=> $phpgw->link("/forum/post.php","$catfor&type=new&col=$col"),
+				));	
 
-	include('./inc/bar.inc.php');
+			 
+if(!$col) {
+echo "run this";
+$phpgw->template->set_file('COLLAPSE','collapse.threads.tpl');
 
-	echo '<center>';
-	echo ' <table border="0" width="80%">';
+$phpgw->template->set_block('COLLAPSE','CollapseThreads','CollapseT');
+$phpgw->db->query("select * from f_threads where cat_id=$cat and for_id=$for and parent = -1  order by postdate DESC");
 
-	// Collapsed view
-	// Something wrong with this section when it goes here the query for $msgid fails
-	if(!$col)
-	{
-		echo '<tr bgcolor="' . $phpgw_info['theme']['th_bg'] . '" align="left">';
-		echo '<th width="40%">' .lang('Topic') .'</th>';
-		echo '<th>' . lang('Author') . '</th>';
-		echo '<th>' . lang('Replies') . '</th>';
-		echo '<th>' . lang('Latest Reply') . '</th>';
-		echo '</tr>';
 
-		$phpgw->db->query("select * from f_threads where cat_id=$cat and for_id=$for and parent = -1 order by postdate DESC");
-		show_topics($cat,$for);
+//for viewing the collapse threads
 
-		// Threaded view  ...... I hate these darn threads, and this gotta redo soon
-	}
-	else
-	{
-		echo '<tr bgcolor="' . $phpgw_info['theme']['th_bg'] . '" align=left>';
-		echo '<th width="40%">' .lang('Topic') .'</th>';
-		echo '<th>' . lang('Author') . '</th>';
-		echo '<th>' . lang('Date') . '</th>';
-		echo '</tr>';
-		echo '<tr>';
+//$phpgw->db->query("select * from f_threads where cat_id=$cat and for_id=$for and parent = -1  order by postdate DESC");
 
-		$phpgw->db->query("select * from f_threads where cat_id = $cat and for_id = $for order by thread DESC, postdate, depth");
-		showthread($cat);
-	}
-	echo '</table>';
-	if(!$phpgw->db->num_rows()) { echo '<b>' . lang('No messages available!') . '</b>'; }
+while($phpgw->db->next_record()) {
+$tr_color = $phpgw->nextmatchs->alternate_row_color($tr_color);
+$subject = $phpgw->db->f("subject");
+      if (! $subject) {
+         $subject = "[ No subject ]";
+      } //end if
 
-	echo '</center>';
-?>
-  </td>
-</table>
-<?php
-	$phpgw->common->phpgw_footer();
+
+$phpgw->template->set_var(array(
+	COLOR => $tr_color,
+	TOPIC => $subject,
+	AUTHOR => $phpgw->db->f("author"),
+	REPLIES => $phpgw->db->f("n_replies") ,
+	LATESTREPLY => $phpgw->db->f("postdate")
+	));
+	
+$phpgw->template->parse('CollapseT','CollapseThreads',true);
+			
+	} //end while
+$phpgw->template->set_var(array(
+	THREADS_LINK => $phpgw->link("/forum/threads.php","$catfor&col=1"),
+	READ_LINK => $phpgw->link("/forum/read.php","cat=$cat&for=$for&msg=$msg" . $phpgw->db->f("id")),
+	LANG_THREAD => lang("View Threads")
+	));
+$phpgw->template->parse("Out",'COLLAPSE');	
+$phpgw->template->p("Out");			
+
+}//end if 				 
+
+//For viewing the normal view
+if($col){
+
+$phpgw->template->set_file('NORMAL','normal.threads.tpl');
+$phpgw->template->set_block('NORMAL','NormalThreads','NormalT');
+
+			 
+$phpgw->db->query("select * from f_threads where cat_id = $cat and for_id = $for order by thread DESC, postdate, depth");
+
+while($phpgw->db->next_record()) {
+$tr_color = $phpgw->nextmatchs->alternate_row_color($tr_color);
+
+$move = "";
+      for($tmp = 1;$tmp <= $phpgw->db->f("depth"); $tmp++)
+          $move .= "&nbsp;&nbsp;";
+          
+
+$subject = $phpgw->db->f("subject");
+      if (! $subject) {
+         $subject = lang("No Subject");
+      }
+
+        	
+
+$phpgw->template->set_var(array(
+	COLOR 		=> $tr_color,
+	TOPIC 		=> $subject,
+	AUTHOR 		=> $phpgw->db->f("author"),
+	REPLIES 	=> $phpgw->db->f("n_replies") ,
+	LATESTREPLY 	=> $phpgw->db->f("postdate"),
+	DEPTH 		=> $move
+	));
+	
+$phpgw->template->parse('NormalT','NormalThreads',true);
+
+			
+	} //end while
+
+$phpgw->template->set_var(array(
+	THREADS_LINK 	=> $phpgw->link("/forum/threads.php","$catfor&col=0"),
+	READ_LINK 	=> $phpgw->link("/forum/read.php","cat=$cat&for=$for&pos=$pos&col=1&msg=" . $phpgw->db->f("id")),
+	LANG_THREAD  	=> lang("Collapse Threads")
+	));
+
+$phpgw->template->parse('Out','NORMAL');	
+$phpgw->template->p('Out');	
+
+
+}//end else
+
+$phpgw->common->phpgw_footer();
 ?>
