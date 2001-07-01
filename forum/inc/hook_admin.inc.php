3bbbc4476505c418e80ae28034040954ad284ee0
jengo <jengo@923e3c2e-ae13-0410-ba18-ced7211e33de>
2001-07-01 23:47:32 +0000

    Fixed duplicate ?> and did some reformating of the file
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@4811 923e3c2e-ae13-0410-ba18-ced7211e33de

--- forum/inc/hook_admin.inc.php
+++ forum/inc/hook_admin.inc.php
@@ -1,30 +1,23 @@
 <?php
-  /**************************************************************************\
-  * phpGroupWare                                                             *
-  * http://www.phpgroupware.org                                              *
-  * --------------------------------------------                             *
-  *  This program is free software; you can redistribute it and/or modify it *
-  *  under the terms of the GNU General Public License as published by the   *
-  *  Free Software Foundation; either version 2 of the License, or (at your  *
-  *  option) any later version.                                              *
-  \**************************************************************************/
+	/**************************************************************************\
+	* phpGroupWare                                                             *
+	* http://www.phpgroupware.org                                              *
+	* --------------------------------------------                             *
+	*  This program is free software; you can redistribute it and/or modify it *
+	*  under the terms of the GNU General Public License as published by the   *
+	*  Free Software Foundation; either version 2 of the License, or (at your  *
+	*  option) any later version.                                              *
+	\**************************************************************************/
 
-  /* $Id$ */
-{
-// Only Modify the $file and $title variables.....
-$title = 'forum';
+	/* $Id$ */
 
-        $title = $appname;
-        $file = Array(
-                'Forum Administration'    =>
-$phpgw->link('/forum/admin/index.php','appname=' .
-$appname)
-        );
-//Do not modify below this line
-        display_section($appname,$title,$file);
-}
-?>
+	// Only Modify the $file and $title variables.....
+	$title = 'forum';
 
-
+	$file = Array(
+		'Forum Administration' => $phpgw->link('/forum/admin/index.php','appname=forum')
+	);
+	//Do not modify below this line
+	display_section($appname,$title,$file);
 
 ?>
