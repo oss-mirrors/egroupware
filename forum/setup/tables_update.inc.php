5a8123dcc3e922d0b61483251af6bde34e9ed195
jengo <jengo@923e3c2e-ae13-0410-ba18-ced7211e33de>
2001-07-04 04:11:17 +0000

    Renamed the tables to use the standard phpgw_ prefix
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@4861 923e3c2e-ae13-0410-ba18-ced7211e33de

---
+++ forum/setup/tables_update.inc.php
@@ -0,0 +1,27 @@
+<?php
+	/**************************************************************************\
+	* phpGroupWare - Setup                                                     *
+	* http://www.phpgroupware.org                                              *
+	* --------------------------------------------                             *
+	*  This program is free software; you can redistribute it and/or modify it *
+	*  under the terms of the GNU General Public License as published by the   *
+	*  Free Software Foundation; either version 2 of the License, or (at your  *
+	*  option) any later version.                                              *
+	\**************************************************************************/
+
+	/* $Id$ */
+
+	$test[] = '0.9.13';
+	function forum_upgrade0_9_13()
+	{
+		global $setup_info, $phpgw_setup;
+
+		$phpgw_setup->oProc->RenameTable('f_body','phpgw_forum_body');
+		$phpgw_setup->oProc->RenameTable('f_categories','phpgw_forum_categories');
+		$phpgw_setup->oProc->RenameTable('f_forums','phpgw_forum_forums');
+		$phpgw_setup->oProc->RenameTable('f_threads','phpgw_forum_threads');
+
+		$setup_info['forum']['currentver'] = '0.9.13.001';
+		return $setup_info['forum']['currentver'];
+	}
+?>
