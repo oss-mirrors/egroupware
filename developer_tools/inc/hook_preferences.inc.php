6a65518fdea0e1429f4185015e1974254c18f5d6
jengo <jengo@923e3c2e-ae13-0410-ba18-ced7211e33de>
2001-07-14 10:14:19 +0000

    Forgot to add the hook and added some new templates
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@5075 923e3c2e-ae13-0410-ba18-ced7211e33de

---
+++ developer_tools/inc/hook_preferences.inc.php
@@ -0,0 +1,21 @@
+<?php
+	/**************************************************************************\
+	* phpGroupWare                                                             *
+	* http://www.phpgroupware.org                                              *
+	* Written by Joseph Engo <jengo@phpgroupware.org>                          *
+	* --------------------------------------------                             *
+	*  This program is free software; you can redistribute it and/or modify it *
+	*  under the terms of the GNU General Public License as published by the   *
+	*  Free Software Foundation; either version 2 of the License, or (at your  *
+	*  option) any later version.                                              *
+	\**************************************************************************/
+
+	/* $Id$ */
+
+	$file  = array(
+		'Categories' => $phpgw->link('/preferences/categories.php','cats_app=developer_tools&global_cats=True'),
+		'SF Project tracker preferences' => $phpgw->link('/index.php','menuaction=developer_tools.uisf_project_tracker.preferences')
+	);
+
+	display_section('developer_tools','Developer Tools',$file);
+?>
---
+++ developer_tools/templates/default/changelog_form.tpl
@@ -0,0 +1,24 @@
+<!-- BEGIN form -->
+<form action="{form_action}" method="POST">
+ <table border="0" width="60%" align="center">
+  <tr>
+   <td colspan="2">&nbsp;</td>
+  </tr>
+  
+  <tr>
+   <td>{lang_project}</td>
+   <td>{value_project}</td>
+  </tr>
+ 
+  <tr>
+   <td>{lang_version}</td>
+   <td>{value_version}</td>
+  </tr>
+ 
+  <tr>
+   <td>{lang_change}</td>
+   <td>{value_change}</td>
+  </tr>
+ </table>
+</form>
+<!-- END form -->
