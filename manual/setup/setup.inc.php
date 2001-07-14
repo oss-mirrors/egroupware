ea6c466ea6beb1fc2e23d945b2e8ab092b57ba7e
milosch <milosch@923e3c2e-ae13-0410-ba18-ced7211e33de>
2001-07-14 01:59:00 +0000

    add new setup.inc.php, delete old details.inc.php
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@5058 923e3c2e-ae13-0410-ba18-ced7211e33de

---
+++ manual/setup/setup.inc.php
@@ -0,0 +1,26 @@
+<?php
+	/**************************************************************************\
+	* phpGroupWare - Manual                                                    *
+	* http://www.phpgroupware.org                                              *
+	* --------------------------------------------                             *
+	*  This program is free software; you can redistribute it and/or modify it *
+	*  under the terms of the GNU General Public License as published by the   *
+	*  Free Software Foundation; either version 2 of the License, or (at your  *
+	*  option) any later version.                                              *
+	\**************************************************************************/
+
+	/* $Id$ */
+
+	/* Basic information about this app */
+	$setup_info['manual']['name']      = 'manual';
+	$setup_info['manual']['title']     = 'User Manual';
+	$setup_info['manual']['version']   = '0.9.13.002';
+	$setup_info['manual']['app_order'] = 5;
+	$setup_info['manual']['enable']    = 1;
+
+	/* Dependencies for this app to work */
+	$setup_info['manual']['depends'][] = array(
+		 'appname' => 'phpgwapi',
+		 'versions' => Array('0.9.13', '0.9.14')
+	);
+?>
