d43e5af7dd69c16f3fccc0d5c7ad82760b2827dd
jengo <jengo@923e3c2e-ae13-0410-ba18-ced7211e33de>
2001-07-14 01:32:27 +0000

    - Fixed banners - Converted DOS format to Unix format
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@5054 923e3c2e-ae13-0410-ba18-ced7211e33de

--- forum/preference_deletecategory.php
+++ forum/preference_deletecategory.php
@@ -1,14 +1,16 @@
 <?php
-  /**************************************************************************\
-  * phpGroupWare - Forums                                                    *
-  * http://www.phpgroupware.org                                              *
-  * Written by Jani Hirvinen <jpkh@shadownet.com>                            *
-  * --------------------------------------------                             *
-  *  This program is free software; you can redistribute it and/or modify it *
-  *  under the terms of the GNU General Public License as published by the   *
-  *  Free Software Foundation; either version 2 of the License, or (at your  *
-  *  option) any later version.                                              *
-  \**************************************************************************/
+	/*****************************************************************************\
+	* phpGroupWare - Forums                                                       *
+	* http://www.phpgroupware.org                                                 *
+	* Written by Jani Hirvinen <jpkh@shadownet.com>                               *
+	* -------------------------------------------                                 *
+	*  This program is free software; you	can redistribute it and/or modify it   *
+	*  under the terms of	the GNU	General	Public License as published by the  *
+	*  Free Software Foundation; either version 2	of the License,	or (at your *
+	*  option) any later version.                                                 *
+	\*****************************************************************************/
+
+	/* $Id$ */
 
   $phpgw_info["flags"] = array("currentapp" => "forum", "enable_nextmatchs_class" => True);
   if($confirm || ! $cat_id) {
