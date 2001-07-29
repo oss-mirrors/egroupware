3c47e27ed2804919883daf65a34eec2cf1a564b7
milosch <milosch@923e3c2e-ae13-0410-ba18-ced7211e33de>
2001-07-29 00:00:57 +0000

    This should work anonymously for now
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@5243 923e3c2e-ae13-0410-ba18-ced7211e33de

--- soap/index.php
+++ soap/index.php
@@ -1,8 +1,8 @@
 <?php
 /**************************************************************************\
-* phpGroupWare - addressbook                                               *
+* phpGroupWare - SOAP Server                                               *
 * http://www.phpgroupware.org                                              *
-* Written by Joseph Engo <jengo@phpgroupware.org>                          *
+* Written by Miles Lott <milosch@phpgroupware.org>                         *
 * --------------------------------------------                             *
 *  This program is free software; you can redistribute it and/or modify it *
 *  under the terms of the GNU General Public License as published by the   *
@@ -11,6 +11,8 @@
 \**************************************************************************/
 
 /* $Id$ */
+	$login  = 'anonymous';
+	$passwd = 'anonymous1';
 
 	$phpgw_info['flags'] = array(
 		'disable_Template_class' => True,
@@ -20,48 +22,49 @@
 	);
 
 	include('../header.inc.php');
+	$sessionid = $phpgw->session->create($login,$passwd);
 
 	$soapTypes = array(
-		"i4" => 1,
-		"int" => 1,
-		"boolean" => 1,
-		"string" => 1,
-		"double" => 1,
-		"float" => 1,
-		"dateTime" => 1,
-		"timeInstant" => 1,
-		"dateTime" => 1,
-		"base64Binary" => 1,
-		"base64" => 1,
-		"array" => 2,
-		"Array" => 2,
-		"SOAPStruct" => 3,
-		"ur-type" => 2
+		'i4'           => 1,
+		'int'          => 1,
+		'boolean'      => 1,
+		'string'       => 1,
+		'double'       => 1,
+		'float'        => 1,
+		'dateTime'     => 1,
+		'timeInstant'  => 1,
+		'dateTime'     => 1,
+		'base64Binary' => 1,
+		'base64'       => 1,
+		'array'        => 2,
+		'Array'        => 2,
+		'SOAPStruct'   => 3,
+		'ur-type'      => 2
 	);
 
 	$typemap = array(
-		"http://soapinterop.org/xsd" => array("SOAPStruct"),
-		"http://schemas.xmlsoap.org/soap/encoding/" => array("base64"),
-		"http://www.w3.org/1999/XMLSchema" => array_keys($soapTypes)
+		'http://soapinterop.org/xsd' => array('SOAPStruct'),
+		'http://schemas.xmlsoap.org/soap/encoding/' => array('base64'),
+		'http://www.w3.org/1999/XMLSchema' => array_keys($soapTypes)
 	);
 
 	$namespaces = array(
-		"http://schemas.xmlsoap.org/soap/envelope/" => "SOAP-ENV",
-		"http://www.w3.org/1999/XMLSchema-instance" => "xsi",
-		"http://www.w3.org/1999/XMLSchema" => "xsd",
-		"http://schemas.xmlsoap.org/soap/encoding/" => "SOAP-ENC",
-		"http://soapinterop.org/xsd"=>"si"
+		'http://schemas.xmlsoap.org/soap/envelope/' => 'SOAP-ENV',
+		'http://www.w3.org/1999/XMLSchema-instance' => 'xsi',
+		'http://www.w3.org/1999/XMLSchema' => 'xsd',
+		'http://schemas.xmlsoap.org/soap/encoding/' => 'SOAP-ENC',
+		'http://soapinterop.org/xsd' =>'si'
 	);
 
-	$xmlEntities = array("quot" => '"',
-		"amp" => "&",
-		"lt" => "<",
-		"gt" => ">",
-		"apos" => "'"
+	$xmlEntities = array(
+		'quot' => '"',
+		'amp'  => "&",
+		'lt'   => "<",
+		'gt'   => ">",
+		'apos' => "'"
 	);
 
-	$soap_defencoding = "UTF-8";
-
+	$soap_defencoding = 'UTF-8';
 
 	$server = CreateObject('phpgwapi.soap_server');
 	/* _debug_array($server);exit; */
