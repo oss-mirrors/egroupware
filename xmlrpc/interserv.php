6e4ba4561b5e30c0e818e93b0dd0e17b0b23de0a
milosch <milosch@923e3c2e-ae13-0410-ba18-ced7211e33de>
2001-08-15 15:00:24 +0000

    Add banner
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@5526 923e3c2e-ae13-0410-ba18-ced7211e33de

--- xmlrpc/interserv.php
+++ xmlrpc/interserv.php
@@ -1,14 +1,23 @@
 <?php
-	/* Method borrowed partially from:
-	  http://sourceforge.net/tracker/index.php?func=detail&aid=427359&group_id=23199&atid=377731
-	*/
+	/**************************************************************************\
+	* phpGroupWare - Interserver XML-RPC/SOAP Test app                         *
+	* http://www.phpgroupware.org                                              *
+	* This file written by Miles Lott <milosch@phpgroupware.org                *
+	* --------------------------------------------                             *
+	*  This program is free software; you can redistribute it and/or modify it *
+	*  under the terms of the GNU General Public License as published by the   *
+	*  Free Software Foundation; either version 2 of the License, or (at your  *
+	*  option) any later version.                                              *
+	\**************************************************************************/
+
+	/* $Id$ */
+
 	$phpgw_info['flags'] = array(
 		'currentapp' => 'login',
 		'disable_Template_class' => True
 	);
 
 	include('../header.inc.php');
-	/* call an xmlrpc method on a remote http server */ 
 
 	$is = CreateObject('phpgwapi.interserver',$server_id);
 
