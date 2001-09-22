87c1993001eca8cd7c263d039eb1fe07b4bc2090
milosch <milosch@923e3c2e-ae13-0410-ba18-ced7211e33de>
2001-09-22 02:21:47 +0000

    Renaming classes
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@6026 923e3c2e-ae13-0410-ba18-ced7211e33de

---
+++ messenger/inc/class.bomessenger.inc.php
@@ -0,0 +1,403 @@
+<?php
+	/**************************************************************************\
+	* phpGroupWare - Messenger                                                 *
+	* http://www.phpgroupware.org                                              *
+	* This application written by Joseph Engo <jengo@phpgroupware.org>         *
+	* --------------------------------------------                             *
+	* Funding for this program was provided by http://www.checkwithmom.com     *
+	* --------------------------------------------                             *
+	*  This program is free software; you can redistribute it and/or modify it *
+	*  under the terms of the GNU General Public License as published by the   *
+	*  Free Software Foundation; either version 2 of the License, or (at your  *
+	*  option) any later version.                                              *
+	\**************************************************************************/
+
+	/* $Id$ */
+
+	class bomessenger
+	{
+		var $so;
+		var $public_functions = array(
+			'delete_message'      => True,
+			'send_message'        => True,
+			'send_global_message' => True,
+			'reply'               => True,
+			'forward'             => True,
+			'list_methods'        => True
+		);
+		var $soap_functions = array();
+
+		function bomessenger()
+		{
+			$this->so = createobject('messenger.somessenger');
+		}
+
+		function send_global_message($data='')
+		{
+			if(is_array($data))
+			{
+				$message = $data['message'];
+				$send    = $data['send'];
+				$cancel  = $data['cancel'];
+			}
+			else
+			{
+				$message = $GLOBALS['HTTP_POST_VARS']['message'];
+				$send    = $GLOBALS['HTTP_POST_VARS']['send'];
+				$cancel  = $GLOBALS['HTTP_POST_VARS']['cancel'];
+			}
+
+			if (! $GLOBALS['phpgw']->acl->check('run',1,'admin') || $cancel)
+			{
+				Header('Location: ' . $GLOBALS['phpgw']->link('/index.php','menuaction=messenger.uimessenger.inbox'));
+				return False;
+			}
+
+			if (! $message['subject'])
+			{
+				$errors[] = lang('You must enter a subject');
+			}
+
+			if (! $message['content'])
+			{
+				$errors[] = lang("You didn't enter anything for the message");
+			}
+
+			if (is_array($errors))
+			{
+				ExecMethod('messenger.uimessenger.compose',$errors);
+				//$this->ui->compose($errors);
+			}
+			else
+			{
+				$account_info = $GLOBALS['phpgw']->accounts->get_list('accounts');
+
+				$this->so->db->transaction_begin();
+				while (list(,$account) = each($account_info))
+				{
+					$message['to'] = $account['account_lid'];
+					$this->so->send_message($message,True);
+
+				}
+				$this->so->db->transaction_commit();
+				Header('Location: ' . $GLOBALS['phpgw']->link('/index.php','menuaction=messenger.uimessenger.inbox'));
+			}
+		}
+
+		function check_for_missing_fields($message)
+		{
+			$acctid = $GLOBALS['phpgw']->accounts->name2id($message['to']);
+
+			if (!$acctid)
+			{
+				if ($message['to'])
+				{
+					$errors[] = lang("I can't find the username %1 on the system",$message['to']);
+				}
+				else
+				{
+					$errors[] = lang('You must enter the username this message is for');
+				}
+			}
+
+			$acct = createobject('phpgwapi.accounts',$GLOBALS['phpgw']->accounts->name2id($message['to']));
+			$acct->read_repository();
+			if ($acct->is_expired() && $GLOBALS['phpgw']->accounts->name2id($message['to']))
+			{
+				$errors[] = lang("Sorry, %1's account is not currently active",$message['to']);
+			}
+
+			if (! $message['subject'])
+			{
+				$errors[] = lang('You must enter a subject');
+			}
+
+			if (! $message['content'])
+			{
+				$errors[] = lang("You didn't enter anything for the message");
+			}
+			return $errors;		
+		}
+
+		function send_message($data='')
+		{
+			if(is_array($data))
+			{
+				$message = $data['message'];
+				$send    = $data['send'];
+				$cancel  = $data['cancel'];
+			}
+			else
+			{
+				$message = $GLOBALS['HTTP_POST_VARS']['message'];
+				$send    = $GLOBALS['HTTP_POST_VARS']['send'];
+				$cancel  = $GLOBALS['HTTP_POST_VARS']['cancel'];
+			}
+
+			if ($cancel)
+			{
+				Header('Location: ' . $GLOBALS['phpgw']->link('/index.php','menuaction=messenger.uimessenger.inbox'));
+				return False;
+			}
+
+			$errors = $this->check_for_missing_fields($message);
+
+			if (is_array($errors))
+			{
+				ExecMethod('messenger.uimessenger.compose',$errors);
+				//$this->ui->compose($errors);
+			}
+			else
+			{
+				$this->so->send_message($message);
+				Header('Location: ' . $GLOBALS['phpgw']->link('/index.php','menuaction=messenger.uimessenger.inbox'));
+			}
+		}
+
+		function read_inbox($start,$order,$sort)
+		{
+			$messages = $this->so->read_inbox($start,$order,$sort);
+
+			while (is_array($messages) && list(,$message) = each($messages))
+			{
+				if ($message['from'] == -1)
+				{
+					$cached['-1']       = -1;
+					$cached_names['-1'] = lang('Global Message');
+				}
+
+				// Cache our results, so we don't query the same account multiable times
+				if (! $cached[$message['from']])
+				{
+					$acct = createobject('phpgwapi.accounts',$message['from']);
+					$acct->read_repository();
+					$cached[$message['from']]       = $message['from'];
+					$cached_names[$message['from']] = $GLOBALS['phpgw']->common->display_fullname($acct->data['account_lid'],$acct->data['firstname'],$acct->data['lastname']);
+				}
+
+				/*
+				** N - New
+				** R - Replied
+				** O - Old (read)
+				** F - Forwarded
+				*/
+				if ($message['status'] == 'N')
+				{
+					$message['subject'] = '<b>' . $message['subject'] . '</b>';
+					$message['status'] = '&nbsp';
+					$message['date'] = '<b>' . $GLOBALS['phpgw']->common->show_date($message['date']) . '</b>';
+					$message['from'] = '<b>' . $cached_names[$message['from']] . '</b>';
+				}
+				else
+				{
+					$message['date'] = $GLOBALS['phpgw']->common->show_date($message['date']);
+					$message['from'] = $cached_names[$message['from']];
+				}
+
+				if ($message['status'] == 'O')
+				{
+					$message['status'] = '&nbsp;';
+				}
+
+				$_messages[] = array(
+					'id'      => $message['id'],
+					'from'    => $message['from'],
+					'status'  => $message['status'],
+					'date'    => $message['date'],
+					'subject' => $message['subject']
+				);
+			}
+			return $_messages;
+		}
+
+		function read_message($message_id)
+		{
+			$message = $this->so->read_message($message_id);
+
+			$message['date'] = $GLOBALS['phpgw']->common->show_date($message['date']);
+
+			if ($message['from'] == -1)
+			{
+				$message['from']           = lang('Global Message');
+				$message['global_message'] = True;
+			}
+			else
+			{
+				$acct = createobject('phpgwapi.accounts',$message['from']);
+				$acct->read_repository();
+				$message['from'] = $GLOBALS['phpgw']->common->display_fullname($acct->data['account_lid'],$acct->data['firstname'],$acct->data['lastname']);
+			}
+
+			return $message;
+		}
+
+		function read_message_for_reply($message_id,$type,$n_message='')
+		{
+			if(!$n_message)
+			{
+				$n_message = $GLOBALS['HTTP_POST_VARS']['n_message'];
+			}
+
+			$message = $this->so->read_message($message_id);
+
+			$acct = createobject('phpgwapi.accounts',$message['from']);
+			$acct->read_repository();
+
+			if (! $n_message['content'])
+			{
+				$content_array = explode("\n",$message['content']);
+
+				$new_content_array[] = ' ';
+				$new_content_array[] = '> ' . $GLOBALS['phpgw']->common->display_fullname($acct->data['account_lid'],$acct->data['firstname'],$acct->data['lastname']) . ' wrote:';
+				$new_content_array[] = '>';
+				while (list(,$line) = each($content_array))
+				{
+					$new_content_array[] = '> ' . $line;
+				}
+				$message['content'] = implode("\n",$new_content_array);
+			}
+
+			$message['subject'] = $type . ': ' . $message['subject'];
+			$message['from']    = $acct->data['account_lid'];
+
+			return $message;
+		}
+
+		function delete_message($messages='')
+		{
+			if(!$messages)
+			{
+				$messages = $GLOBALS['HTTP_POST_VARS']['messages'] ? $GLOBALS['HTTP_POST_VARS']['messages'] : $GLOBALS['HTTP_GET_VARS']['messages'];
+			}
+
+			if (! is_array($messages))
+			{
+				Header('Location: ' . $GLOBALS['phpgw']->link('/index.php','menuaction=messenger.uimessenger.inbox'));
+				return False;
+			}
+			$this->so->db->transaction_begin();
+			while (list(,$message_id) = each($messages))
+			{
+				$this->so->delete_message($message_id);
+			}
+			$this->so->db->transaction_commit();
+			Header('Location: ' . $GLOBALS['phpgw']->link('/index.php','menuaction=messenger.uimessenger.inbox'));
+		}
+
+		function reply($message_id='',$n_message='')
+		{
+			if(!$message_id)
+			{
+				$message_id = $GLOBALS['HTTP_POST_VARS']['message_id'];
+				$n_message  = $GLOBALS['HTTP_POST_VARS']['n_message'];
+			}
+
+			$errors = $this->check_for_missing_fields($n_message);
+			if (is_array($errors))
+			{
+				ExecMethod('messenger.uimessenger.reply',array($errors,$n_message));
+				//$this->ui->reply($errors, $n_message);
+			}
+			else
+			{
+				$this->so->send_message($n_message);
+				$this->so->update_message_status('R',$message_id);
+				Header('Location: ' . $GLOBALS['phpgw']->link('/index.php','menuaction=messenger.uimessenger.inbox'));
+			}
+		}
+
+		function forward($message_id='',$n_message='')
+		{
+			if(!$message_id)
+			{
+				$message_id = $GLOBALS['HTTP_POST_VARS']['message_id'];
+				$n_message  = $GLOBALS['HTTP_POST_VARS']['n_message'];
+			}
+
+			$errors = $this->check_for_missing_fields($n_message);
+
+			if (is_array($errors))
+			{
+				ExecMethod('messenger.uimessenger.forward',array($errors,$n_message));
+				//$this->ui->forward($errors, $n_message);
+			}
+			else
+			{
+				$this->so->send_message($n_message);
+				$this->so->update_message_status('F',$message_id);
+				Header('Location: ' . $GLOBALS['phpgw']->link('/index.php','menuaction=messenger.uimessenger.inbox'));
+			}
+		}
+
+		function total_messages($extra_where_clause = '')
+		{
+			return $this->so->total_messages($extra_where_clause);
+		}
+
+		function list_methods($_type='xmlrpc')
+		{
+			/*
+			  This handles introspection or discovery by the logged in client,
+			  in which case the input might be an array.  The server always calls
+			  this function to fill the server dispatch map using a string.
+			*/
+			if (is_array($_type))
+			{
+				$_type = $_type['type'] ? $_type['type'] : $_type[0];
+			}
+			switch($_type)
+			{
+				case 'xmlrpc':
+					$xml_functions = array(
+						'delete_message' => array(
+							'function'  => 'delete_message',
+							'signature' => array(array(xmlrpcStruct,xmlrpcStruct)),
+							'docstring' => lang('Delete a message.')
+						),
+						'read_message' => array(
+							'function'  => 'read_message',
+							'signature' => array(array(xmlrpcStruct,xmlrpcString)),
+							'docstring' => lang('Read a single message.')
+						),
+						'read_inbox' => array(
+							'function'  => 'read_inbox',
+							'signature' => array(array(xmlrpcStruct,xmlrpcString,xmlrpcString,xmlrpcString)),
+							'docstring' => lang('Read a list of messages.')
+						),
+						'send_message' => array(
+							'function'  => 'send_message',
+							'signature' => array(array(xmlrpcStruct,xmlrpcStruct)),
+							'docstring' => lang('Send a message to a single recipient.')
+						),
+						'send_global_message' => array(
+							'function'  => 'send_global_message',
+							'signature' => array(array(xmlrpcStruct,xmlrpcStruct)),
+							'docstring' => lang('Send a global message.'),
+						),
+						'reply' => array(
+							'function'  => 'reply',
+							'signature' => array(array(xmlrpcInt,xmlrpcInt)),
+							'docstring' => lang('Reply to a received message.')
+						),
+						'forward' => array(
+							'function'  => 'forward',
+							'signature' => array(array(xmlrpcStruct,xmlrpcStruct)),
+							'docstring' => lang('Forward a message to another user.')
+						),
+						'list_methods' => array(
+							'function'  => 'list_methods',
+							'signature' => array(array(xmlrpcStruct,xmlrpcString)),
+							'docstring' => lang('Read this list of methods.')
+						)
+					);
+					return $xml_functions;
+					break;
+				case 'soap':
+					return $this->soap_functions;
+					break;
+				default:
+					return array();
+					break;
+			}
+		}
+	}
---
+++ messenger/inc/class.uimessenger.inc.php
@@ -0,0 +1,329 @@
+<?php
+	/**************************************************************************\
+	* phpGroupWare - Messenger                                                 *
+	* http://www.phpgroupware.org                                              *
+	* This application written by Joseph Engo <jengo@phpgroupware.org>         *
+	* --------------------------------------------                             *
+	* Funding for this program was provided by http://www.checkwithmom.com     *
+	* --------------------------------------------                             *
+	*  This program is free software; you can redistribute it and/or modify it *
+	*  under the terms of the GNU General Public License as published by the   *
+	*  Free Software Foundation; either version 2 of the License, or (at your  *
+	*  option) any later version.                                              *
+	\**************************************************************************/
+
+	/* $Id$ */
+
+	class uimessenger
+	{
+		var $bo;
+		var $template;
+		var $public_functions = array(
+			'inbox'          => True,
+			'compose'        => True,
+			'compose_global' => True,
+			'read_message'   => True,
+			'reply'          => True,
+			'forward'        => True
+		);
+
+		function uimessenger()
+		{
+			$this->template   = $GLOBALS['phpgw']->template;
+			$this->bo         = CreateObject('messenger.bomessenger');
+			$this->nextmatchs = createobject('phpgwapi.nextmatchs');
+		}
+
+		function display_headers($extras = '')
+		{
+			$this->template->set_file(array(
+				'_header' => 'header.tpl'
+			));
+			$this->template->set_block('_header','global_header');
+			$this->template->set_var('lang_inbox','<a href="' . $GLOBALS['phpgw']->link('/index.php','menuaction=messenger.uimessenger.inbox') . '">' . lang('Inbox') . '</a>');
+			$this->template->set_var('lang_compose','<a href="' . $GLOBALS['phpgw']->link('/index.php','menuaction=messenger.uimessenger.compose') . '">' . lang('Compose') . '</a>');
+
+			if ($extras['nextmatchs_left'])
+			{
+				$this->template->set_var('nextmatchs_left',$extras['nextmatchs_left']);
+			}
+
+			if ($extras['nextmatchs_right'])
+			{
+				$this->template->set_var('nextmatchs_right',$extras['nextmatchs_right']);
+			}
+
+			$this->template->fp('app_header','global_header');
+
+			$GLOBALS['phpgw']->common->phpgw_header();
+			echo parse_navbar();
+		}
+
+		function set_common_langs()
+		{
+			$this->template->set_var('th_bg',$GLOBALS['phpgw_info']['theme']['th_bg']);
+			$this->template->set_var('row_on',$GLOBALS['phpgw_info']['theme']['row_on']);
+			$this->template->set_var('row_off',$GLOBALS['phpgw_info']['theme']['row_off']);
+
+			$this->template->set_var('lang_to',lang('Send message to'));
+			$this->template->set_var('lang_from',lang('Message from'));
+			$this->template->set_var('lang_subject',lang('Subject'));
+			$this->template->set_var('lang_content',lang('Message'));
+			$this->template->set_var('lang_date',lang('Date'));
+		}
+
+		function inbox()
+		{
+			$start = $GLOBALS['HTTP_GET_VARS']['start'] ? $GLOBALS['HTTP_GET_VARS']['start'] : $GLOBALS['HTTP_POST_VARS']['start'];
+			$order = $GLOBALS['HTTP_GET_VARS']['order'] ? $GLOBALS['HTTP_GET_VARS']['order'] : $GLOBALS['HTTP_POST_VARS']['order'];
+			$sort  = $GLOBALS['HTTP_GET_VARS']['sort']  ? $GLOBALS['HTTP_GET_VARS']['sort']  : $GLOBALS['HTTP_POST_VARS']['sort'];
+			$total = $this->bo->total_messages();
+
+			$extra_menuaction = '&menuaction=messenger.uimessenger.inbox';
+			$extra_header_info['nextmatchs_left']  = $this->nextmatchs->left('/index.php',$start,$total,$extra_menuaction);
+			$extra_header_info['nextmatchs_right'] = $this->nextmatchs->right('/index.php',$start,$total,$extra_menuaction);
+
+			$this->display_headers($extra_header_info);
+
+			$this->template->set_file(array(
+				'_inbox' => 'inbox.tpl'
+			));
+			$this->template->set_block('_inbox','list');
+			$this->template->set_block('_inbox','row');
+			$this->template->set_block('_inbox','row_empty');
+
+			$this->set_common_langs();
+			$this->template->set_var('sort_date','<a href="' . $this->nextmatchs->show_sort_order($sort,'message_date',$order,'/index.php','','&menuaction=messenger.uimessenger.inbox',False) . '" class="topsort">' . lang('Date') . '</a>');
+			$this->template->set_var('sort_subject','<a href="' . $this->nextmatchs->show_sort_order($sort,'message_subject',$order,'/index.php','','&menuaction=messenger.uimessenger.inbox',False) . '" class="topsort">' . lang('Subject') . '</a>');
+			$this->template->set_var('sort_from','<a href="' . $this->nextmatchs->show_sort_order($sort,'message_from',$order,'/index.php','','&menuaction=messenger.uimessenger.inbox',False) . '" class="topsort">' . lang('From') . '</a>');
+
+			$messages = $this->bo->read_inbox($start,$order,$sort);
+
+			while (is_array($messages) && list(,$message) = each($messages))
+			{
+				$this->template->set_var('row_status',$message['status']);
+				$this->template->set_var('row_from',$message['from']);
+				$this->template->set_var('row_date',$message['date']);
+				$this->template->set_var('row_subject','<a href="' . $GLOBALS['phpgw']->link('/index.php','menuaction=messenger.uimessenger.read_message&message_id=' . $message['id']) . '">' . $message['subject'] . '</a>');
+				$this->template->set_var('row_status',$message['status']);
+				$this->template->set_var('row_checkbox','<input type="checkbox" name="messages[]" value="' . $message['id'] . '">');
+
+				$this->template->fp('rows','row',True);
+			}
+
+			if (! is_array($messages))
+			{
+				$this->template->set_var('lang_empty',lang('You have no messages'));
+				$this->template->fp('rows','row_empty',True);				
+			}
+			else
+			{
+				$this->template->set_var('form_action',$GLOBALS['phpgw']->link('/index.php','menuaction=messenger.bomessenger.delete_message'));
+				$this->template->set_var('button_delete','<input type="image" src="' . PHPGW_IMAGES . '/delete.gif" name="delete" title="' . lang('Delete selected') . '" border="0">');
+			}
+
+			$this->template->pfp('out','list');
+		}
+
+		function set_compose_read_blocks()
+		{
+			$this->template->set_file(array(
+				'_form' => 'form.tpl'
+			));
+
+			$this->template->set_block('_form','form');
+			$this->template->set_block('_form','form_to');
+			$this->template->set_block('_form','form_date');
+			$this->template->set_block('_form','form_from');
+			$this->template->set_block('_form','form_buttons');
+			$this->template->set_block('_form','form_read_buttons');
+			$this->template->set_block('_form','form_read_buttons_for_global');
+		}
+
+		function compose_global($errors = '')
+		{
+			global $message;
+
+			if (! $GLOBALS['phpgw']->acl->check('run',1,'admin'))
+			{
+				$this->inbox();
+				return False;
+			}
+
+			$this->display_headers();
+			$this->set_compose_read_blocks();
+
+			if (is_array($errors))
+			{
+				$this->template->set_var('errors',$GLOBALS['phpgw']->common->error_list($errors));
+			}
+
+			$this->set_common_langs();
+			$this->template->set_var('header_message',lang('Compose global message'));
+
+			$this->template->set_var('form_action',$GLOBALS['phpgw']->link('/index.php','menuaction=messenger.bomessenger.send_global_message'));
+			$this->template->set_var('value_subject','<input name="message[subject]" value="' . $message['subject'] . '">');
+			$this->template->set_var('value_content','<textarea name="message[content]" rows="20" wrap="hard" cols="76">' . $message['content'] . '</textarea>');
+
+			$this->template->set_var('button_send','<input type="submit" name="send" value="' . lang('Send') . '">');
+			$this->template->set_var('button_cancel','<input type="submit" name="cancel" value="' . lang('Cancel') . '">');
+
+			$this->template->fp('buttons','form_buttons');
+			$this->template->pfp('out','form');
+		}
+
+		function compose($errors = '')
+		{
+			$message = $GLOBALS['HTTP_POST_VARS']['message'];
+
+			$this->display_headers();
+			$this->set_compose_read_blocks();
+
+			if (is_array($errors))
+			{
+				$this->template->set_var('errors',$GLOBALS['phpgw']->common->error_list($errors));
+			}
+
+			$this->set_common_langs();
+			$this->template->set_var('header_message',lang('Compose message'));
+
+			$this->template->set_var('form_action',$GLOBALS['phpgw']->link('/index.php','menuaction=messenger.bomessenger.send_message'));
+			$this->template->set_var('value_to','<input name="message[to]" value="' . $message['to'] . '" size="30">');
+			$this->template->set_var('value_subject','<input name="message[subject]" value="' . $message['subject'] . '" size="30">');
+			$this->template->set_var('value_content','<textarea name="message[content]" rows="20" wrap="hard" cols="76">' . $message['content'] . '</textarea>');
+
+			$this->template->set_var('button_send','<input type="submit" name="send" value="' . lang('Send') . '">');
+			$this->template->set_var('button_cancel','<input type="submit" name="cancel" value="' . lang('Cancel') . '">');
+
+			$this->template->fp('to','form_to');
+			$this->template->fp('buttons','form_buttons');
+			$this->template->pfp('out','form');
+		}
+
+		function read_message()
+		{
+			$message_id = $GLOBALS['HTTP_GET_VARS']['message_id'] ? $GLOBALS['HTTP_GET_VARS']['message_id'] : $GLOBALS['HTTP_POST_VARS']['message_id'];
+			$message = $this->bo->read_message($message_id);
+
+			$this->display_headers();
+			$this->set_compose_read_blocks();
+			$this->set_common_langs();
+
+			$this->template->set_var('header_message',lang('Read message'));
+
+			$this->template->set_var('value_from',$message['from']);
+			$this->template->set_var('value_subject',$message['subject']);
+			$this->template->set_var('value_date',$message['date']);
+			$this->template->set_var('value_content','<pre>' . $GLOBALS['phpgw']->strip_html($message['content']) . '</pre>');
+
+			$this->template->set_var('link_delete','<a href="'
+					. $GLOBALS['phpgw']->link('/index.php','menuaction=messenger.bomessenger.delete_message&messages%5B%5D=' . $message['id'])
+					. '">' . lang('Delete') . '</a>');
+
+			$this->template->set_var('link_reply','<a href="'
+					. $GLOBALS['phpgw']->link('/index.php','menuaction=messenger.uimessenger.reply&message_id=' . $message['id'])
+					. '">' . lang('Reply') . '</a>');
+
+			$this->template->set_var('link_forward','<a href="'
+					. $GLOBALS['phpgw']->link('/index.php','menuaction=messenger.uimessenger.forward&message_id=' . $message['id'])
+					. '">' . lang('Forward') . '</a>');
+
+			switch($message['status'])
+			{
+				case 'N': $this->template->set_var('value_status',lang('New'));		break;
+				case 'R': $this->template->set_var('value_status',lang('Replied'));	break;
+				case 'F': $this->template->set_var('value_status',lang('Forwarded'));	break;
+			}
+
+			if ($message['global_message'])
+			{
+				$this->template->fp('read_buttons','form_read_buttons_for_global');
+			}
+			else
+			{
+				$this->template->fp('read_buttons','form_read_buttons');			
+			}
+
+			$this->template->fp('date','form_date');
+			$this->template->fp('from','form_from');
+			$this->template->pfp('out','form');
+		}
+
+		function reply($errors = '', $message = '')
+		{
+			$message_id = $GLOBALS['HTTP_GET_VARS']['message_id'] ? $GLOBALS['HTTP_GET_VARS']['message_id'] : $GLOBALS['HTTP_POST_VARS']['message_id'];
+
+			if(is_array($errors))
+			{
+				$errors  = $errors['errors'];
+				$message = $errors['message'];
+			}
+
+			if (!$message)
+			{
+				$message = $this->bo->read_message_for_reply($message_id,'RE');
+			}
+
+			$this->display_headers();
+			$this->set_compose_read_blocks();
+			$this->set_common_langs();
+
+			if (is_array($errors))
+			{
+				$this->template->set_var('errors',$GLOBALS['phpgw']->common->error_list($errors));
+			}
+
+			$this->template->set_var('header_message',lang('Reply to a message'));
+
+			$this->template->set_var('form_action',$GLOBALS['phpgw']->link('/index.php','menuaction=messenger.bomessenger.reply&message_id=' . $message['id']));
+			$this->template->set_var('value_to','<input name="n_message[to]" value="' . $message['from'] . '" size="30">');
+			$this->template->set_var('value_subject','<input name="n_message[subject]" value="' . stripslashes($message['subject']) . '" size="30">');
+			$this->template->set_var('value_content','<textarea name="n_message[content]" rows="20" wrap="hard" cols="76">' . stripslashes($message['content']) . '</textarea>');
+
+			$this->template->set_var('button_send','<input type="submit" name="send" value="' . lang('Send') . '">');
+			$this->template->set_var('button_cancel','<input type="submit" name="cancel" value="' . lang('Cancel') . '">');
+
+			$this->template->fp('to','form_to');
+			$this->template->fp('buttons','form_buttons');
+			$this->template->pfp('out','form');
+		}
+
+		function forward($errors = '', $message = '')
+		{
+			$message_id = $GLOBALS['HTTP_GET_VARS']['message_id'] ? $GLOBALS['HTTP_GET_VARS']['message_id'] : $GLOBALS['HTTP_POST_VARS']['message_id'];
+
+			if(is_array($errors))
+			{
+				$errors  = $errors['errors'];
+				$message = $errors['message'];
+			}
+
+			if (!$message)
+			{
+				$message = $this->bo->read_message_for_reply($message_id,'FW');
+			}
+
+			$this->display_headers();
+			$this->set_compose_read_blocks();
+			$this->set_common_langs();
+
+			if (is_array($errors))
+			{
+				$this->template->set_var('errors',$GLOBALS['phpgw']->common->error_list($errors));
+			}
+
+			$this->template->set_var('header_message',lang('Forward a message'));
+
+			$this->template->set_var('form_action',$GLOBALS['phpgw']->link('/index.php','menuaction=messenger.bomessenger.forward&message_id=' . $message['id']));
+			$this->template->set_var('value_to','<input name="n_message[to]" value="' . $message['from'] . '" size="30">');
+			$this->template->set_var('value_subject','<input name="n_message[subject]" value="' . stripslashes($message['subject']) . '" size="30">');
+			$this->template->set_var('value_content','<textarea name="n_message[content]" rows="20" wrap="hard" cols="76">' . stripslashes($message['content']) . '</textarea>');
+
+			$this->template->set_var('button_send','<input type="submit" name="send" value="' . lang('Send') . '">');
+			$this->template->set_var('button_cancel','<input type="submit" name="cancel" value="' . lang('Cancel') . '">');
+
+			$this->template->fp('to','form_to');
+			$this->template->fp('buttons','form_buttons');
+			$this->template->pfp('out','form');
+		}
+	}
