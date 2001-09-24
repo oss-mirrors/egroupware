6fb5be7f7f9535f8b62fc007868e04ecd2b77f64
milosch <milosch@923e3c2e-ae13-0410-ba18-ced7211e33de>
2001-09-24 16:05:12 +0000

    GLOBALS and format changes
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@6058 923e3c2e-ae13-0410-ba18-ced7211e33de

--- tts/newticket.php
+++ tts/newticket.php
@@ -9,128 +9,132 @@
   *  option) any later version.                                              *
   \**************************************************************************/
 
-
   /* $Id$ */
 
+	$submit = $HTTP_POST_VARS['submit'];
 	if ($submit)
 	{
-		$phpgw_info["flags"] = array("noheader" => True, "nonavbar" => True);
+		$GLOBALS['phpgw_info']['flags'] = array('noheader' => True, 'nonavbar' => True);
 	}
 
-	$phpgw_info["flags"]["currentapp"] = "tts";
-	$phpgw_info["flags"]["enable_send_class"]       = True;
-	$phpgw_info["flags"]["enable_config_class"]       = True;
+	$GLOBALS['phpgw_info']['flags']['currentapp'] = 'tts';
+	$GLOBALS['phpgw_info']['flags']['enable_send_class']   = True;
+	$GLOBALS['phpgw_info']['flags']['enable_config_class'] = True;
 	
-	include("../header.inc.php");
+	include('../header.inc.php');
 
 	$account_selected = array();
 	$entry_selected = array();
 	$priority_selected = array();
 	$priority_comment = array();
 
-
 	if (! $submit)
 	{
-
-	$phpgw->preferences->read_repository();
-	if ($phpgw_info['user']['preferences']['tts']['groupdefault']) { $entry_selected[$phpgw_info['user']['preferences']['tts']['groupdefault']]=" selected"; };
-	if ($phpgw_info['user']['preferences']['tts']['assigntodefault']) { $account_selected[$phpgw_info['user']['preferences']['tts']['assigntodefault']]=" selected"; };
-	if ($phpgw_info['user']['preferences']['tts']['prioritydefault']) { $priority_selected[$phpgw_info['user']['preferences']['tts']['prioritydefault']]=" selected"; };
-
-	$p = CreateObject('phpgwapi.Template',PHPGW_APP_TPL);
-	//  echo PHPGW_APP_TPL;
-	$p->set_file(array(
-		'newticket'   => 'newticket.tpl'
-    	));
-	
-	$p->set_block('newticket', 'tts_new_lstassignto','tts_new_lstassignto');
-	$p->set_block('newticket', 'tts_new_lstcategory','tts_new_lstcategory');
-	$p->set_block('newticket', 'tts_select_options','tts_select_options');
-	
-	$p->set_unknowns('remove');
-	$p->set_var('tts_newticket_link', $phpgw->link("/tts/newticket.php"));
-	$p->set_var('tts_bgcolor',$theme["th_bg"] );
-	$p->set_var('tts_textcolor', $theme["th_text"] );
-	$p->set_var('tts_lang_addnewticket', lang("Add new ticket"));
-	$p->set_var('tts_lang_group', lang("Group"));
-	$p->set_var('tts_lang_subject', lang("Subject") );
-	$p->set_var('tts_lang_nosubject', lang("No subject"));
-	$p->set_var('tts_lang_details', lang("Detail"));
-	$p->set_var('tts_lang_priority', lang("Priority"));
-	$p->set_var('tts_lang_lowest', lang("Lowest"));
-	$p->set_var('tts_lang_medium', lang("Medium"));
-	$p->set_var('tts_lang_highest', lang("Highest"));
-	$p->set_var('tts_lang_addticket', lang("Add Ticket"));
-	$p->set_var('tts_lang_clearform', lang("Clear Form"));
-
-	$groups = CreateObject('phpgwapi.accounts');
-	$group_list = $groups->get_list('groups');
-	while (list($key,$entry) = each($group_list))
-	{
-		$p->set_var('tts_account_lid', $entry['account_lid']);
-		$p->set_var('tts_account_name', $entry['account_lid']);
-		$p->set_var('tts_categoryselected', $entry_selected[$entry['account_lid']]);
-		$p->parse('tts_new_lstcategories','tts_new_lstcategory',true);
-	}
-            
-	$p->set_var('tts_lang_assignto', lang("assign to"));
-	$accounts = CreateObject('phpgwapi.accounts',$group_id);
-	$account_list = $accounts->get_list('accounts');
-
-	$p->set_var('tts_account_lid', "none" );
-	$p->set_var('tts_account_name', lang("none"));
-	$p->parse('tts_new_lstassigntos','tts_new_lstassignto',false);
-		
-	while (list($key,$entry) = each($account_list))
-	{
-		if ($entry['account_lid'])
+		$GLOBALS['phpgw']->preferences->read_repository();
+		if ($GLOBALS['phpgw_info']['user']['preferences']['tts']['groupdefault'])
 		{
-    			$p->set_var('tts_account_lid', $entry['account_lid']);
-    			$p->set_var('tts_account_name', $entry['account_lid']);
-			$p->set_var('tts_assignedtoselected', $account_selected[$entry['account_lid']]);
+			$entry_selected[$GLOBALS['phpgw_info']['user']['preferences']['tts']['groupdefault']]=' selected';
 		}
-		$p->parse('tts_new_lstassigntos','tts_new_lstassignto',true);
-	}
+		if ($GLOBALS['phpgw_info']['user']['preferences']['tts']['assigntodefault'])
+		{
+			$account_selected[$GLOBALS['phpgw_info']['user']['preferences']['tts']['assigntodefault']]=' selected';
+		}
+		if ($GLOBALS['phpgw_info']['user']['preferences']['tts']['prioritydefault'])
+		{
+			$priority_selected[$GLOBALS['phpgw_info']['user']['preferences']['tts']['prioritydefault']]=' selected';
+		}
 
-	// Choose the correct priority to display
-	// $prority_selected[$phpgw->db->f("t_priority")] = " selected";
-	$priority_comment[1]=" - ".lang("Lowest"); 
-	$priority_comment[5]=" - ".lang("Medium"); 
-	$priority_comment[10]=" - ".lang("Highest"); 
-    	for ($i=1; $i<=10; $i++) {
-	    $p->set_var('tts_optionname', $i.$priority_comment[$i]);
-	    $p->set_var('tts_optionvalue', $i);
-	    $p->set_var('tts_optionselected', $priority_selected[$i]);
-	    $p->parse('tts_priority_options','tts_select_options',true);
-	}
+		$GLOBALS['phpgw']->template->set_file(array(
+			'newticket'   => 'newticket.tpl'
+		));
 
-	$p->set_var('tts_select_options','');
-	$p->set_var('tts_new_lstcategory','');
-	$p->set_var('tts_new_lstassignto','');
+		$GLOBALS['phpgw']->template->set_block('newticket', 'tts_new_lstassignto','tts_new_lstassignto');
+		$GLOBALS['phpgw']->template->set_block('newticket', 'tts_new_lstcategory','tts_new_lstcategory');
+		$GLOBALS['phpgw']->template->set_block('newticket', 'tts_select_options','tts_select_options');
 	
-	$p->pparse('out', 'newticket');
+		$GLOBALS['phpgw']->template->set_unknowns('remove');
+		$GLOBALS['phpgw']->template->set_var('tts_newticket_link', $GLOBALS['phpgw']->link('/tts/newticket.php'));
+		$GLOBALS['phpgw']->template->set_var('tts_bgcolor',$theme['th_bg'] );
+		$GLOBALS['phpgw']->template->set_var('tts_textcolor', $theme['th_text'] );
+		$GLOBALS['phpgw']->template->set_var('tts_lang_addnewticket', lang('Add new ticket'));
+		$GLOBALS['phpgw']->template->set_var('tts_lang_group', lang('Group'));
+		$GLOBALS['phpgw']->template->set_var('tts_lang_subject', lang('Subject') );
+		$GLOBALS['phpgw']->template->set_var('tts_lang_nosubject', lang('No subject'));
+		$GLOBALS['phpgw']->template->set_var('tts_lang_details', lang('Detail'));
+		$GLOBALS['phpgw']->template->set_var('tts_lang_priority', lang('Priority'));
+		$GLOBALS['phpgw']->template->set_var('tts_lang_lowest', lang('Lowest'));
+		$GLOBALS['phpgw']->template->set_var('tts_lang_medium', lang('Medium'));
+		$GLOBALS['phpgw']->template->set_var('tts_lang_highest', lang('Highest'));
+		$GLOBALS['phpgw']->template->set_var('tts_lang_addticket', lang('Add Ticket'));
+		$GLOBALS['phpgw']->template->set_var('tts_lang_clearform', lang('Clear Form'));
+
+		$groups = CreateObject('phpgwapi.accounts');
+		$group_list = $groups->get_list('groups');
+		while (list($key,$entry) = each($group_list))
+		{
+			$GLOBALS['phpgw']->template->set_var('tts_account_lid', $entry['account_lid']);
+			$GLOBALS['phpgw']->template->set_var('tts_account_name', $entry['account_lid']);
+			$GLOBALS['phpgw']->template->set_var('tts_categoryselected', $entry_selected[$entry['account_lid']]);
+			$GLOBALS['phpgw']->template->parse('tts_new_lstcategories','tts_new_lstcategory',true);
+		}
+
+		$GLOBALS['phpgw']->template->set_var('tts_lang_assignto', lang('assign to'));
+		$accounts = CreateObject('phpgwapi.accounts',$group_id);
+		$account_list = $accounts->get_list('accounts');
+
+		$GLOBALS['phpgw']->template->set_var('tts_account_lid', 'none' );
+		$GLOBALS['phpgw']->template->set_var('tts_account_name', lang('none'));
+		$GLOBALS['phpgw']->template->parse('tts_new_lstassigntos','tts_new_lstassignto',false);
+		
+		while (list($key,$entry) = each($account_list))
+		{
+			if ($entry['account_lid'])
+			{
+				$GLOBALS['phpgw']->template->set_var('tts_account_lid', $entry['account_lid']);
+				$GLOBALS['phpgw']->template->set_var('tts_account_name', $entry['account_lid']);
+				$GLOBALS['phpgw']->template->set_var('tts_assignedtoselected', $account_selected[$entry['account_lid']]);
+			}
+			$GLOBALS['phpgw']->template->parse('tts_new_lstassigntos','tts_new_lstassignto',true);
+		}
+
+		// Choose the correct priority to display
+		// $prority_selected[$GLOBALS['phpgw']->db->f("t_priority")] = " selected";
+		$priority_comment[1]=' - '.lang('Lowest');
+		$priority_comment[5]=' - '.lang('Medium');
+		$priority_comment[10]=' - '.lang('Highest');
+		for ($i=1; $i<=10; $i++)
+		{
+			$GLOBALS['phpgw']->template->set_var('tts_optionname', $i.$priority_comment[$i]);
+			$GLOBALS['phpgw']->template->set_var('tts_optionvalue', $i);
+			$GLOBALS['phpgw']->template->set_var('tts_optionselected', $priority_selected[$i]);
+			$GLOBALS['phpgw']->template->parse('tts_priority_options','tts_select_options',true);
+		}
+
+		$GLOBALS['phpgw']->template->set_var('tts_select_options','');
+		$GLOBALS['phpgw']->template->set_var('tts_new_lstcategory','');
+		$GLOBALS['phpgw']->template->set_var('tts_new_lstassignto','');
 	
-	$phpgw->common->phpgw_footer();
+		$GLOBALS['phpgw']->template->pparse('out', 'newticket');
+		$GLOBALS['phpgw']->common->phpgw_footer();
 	}
 	else
 	{
 		//$current_date = date("ymdHi");		//set timestamp
-
-		$txtDetail .= $phpgw_info["user"]["userid"] . " - " . $phpgw->common->show_date($phpgw->db->f(6)) . "<BR>\n";
-		$txtDetail .= $txtAdditional . "<br><hr>";
+		$txtDetail .= $GLOBALS['phpgw_info']['user']['userid'] . ' - ' . $GLOBALS['phpgw']->common->show_date($GLOBALS['phpgw']->db->f(6)) . "<BR>\n";
+		$txtDetail .= $txtAdditional . '<br><hr>';
 		$txtDetail = addslashes(nl2br($txtDetail));
 
-		$phpgw->db->query("INSERT INTO ticket (t_category,t_detail,t_priority,t_user,t_assignedto, "
+		$GLOBALS['phpgw']->db->query("INSERT INTO ticket (t_category,t_detail,t_priority,t_user,t_assignedto, "
 			. " t_timestamp_opened,t_timestamp_closed,t_subject) VALUES ('$lstCategory','$txtDetail',"
-			. "'$optPriority','" . $phpgw_info["user"]["userid"] . "','$assignto','"
+			. "'$optPriority','" . $GLOBALS['phpgw_info']["user"]["userid"] . "','$assignto','"
 			. time() . "',0,'$subject');");
-		$phpgw->db->query("SELECT t_id FROM ticket WHERE t_subject='$subject' AND t_user='".$phpgw_info["user"]["userid"]."'");
-		$phpgw->db->next_record();
-		if($phpgw_info['server']['tts_mailticket'])
+		$GLOBALS['phpgw']->db->query("SELECT t_id FROM ticket WHERE t_subject='$subject' AND t_user='".$GLOBALS['phpgw_info']['user']['userid']."'");
+		$GLOBALS['phpgw']->db->next_record();
+		if($GLOBALS['phpgw_info']['server']['tts_mailticket'])
 		{
-			mail_ticket($phpgw->db->f("t_id"));
+			mail_ticket($GLOBALS['phpgw']->db->f('t_id'));
 		}
 
-		Header("Location: " . $phpgw->link("/tts/index.php"));
+		Header('Location: ' . $GLOBALS['phpgw']->link('/tts/index.php'));
 	}
 ?>
--- tts/viewticket_details.php
+++ tts/viewticket_details.php
@@ -11,174 +11,180 @@
 
   /* $Id$ */
 
-	$phpgw_info["flags"] = array("currentapp" => "tts");
+	$GLOBALS['phpgw_info']['flags'] = array('currentapp' => 'tts');
+	$submit = $HTTP_POST_VARS['submit'];
 
 	if ($submit)
 	{
-		$phpgw_info["flags"]["noheader"] = True;
-		$phpgw_info["flags"]["nonavbar"] = True;
-		$phpgw_info["flags"]["enable_config_class"] = True;
+		$GLOBALS['phpgw_info']['flags']['noheader'] = True;
+		$GLOBALS['phpgw_info']['flags']['nonavbar'] = True;
+		$GLOBALS['phpgw_info']['flags']['enable_config_class'] = True;
 	}
+	include('../header.inc.php');
 
-	include("../header.inc.php");
-
-	if (! $submit)
+	if (!$submit)
 	{
-
 		// select the ticket that you selected
-		$phpgw->db->query("select t_id,t_category,t_detail,t_priority,t_user,t_assignedto,"
+		$GLOBALS['phpgw']->db->query("select t_id,t_category,t_detail,t_priority,t_user,t_assignedto,"
 			. "t_timestamp_opened, t_timestamp_closed, t_subject, t_watchers from ticket where t_id='$ticketid'");
-		$phpgw->db->next_record();
+		$GLOBALS['phpgw']->db->next_record();
 
-		$lstAssignedto=$phpgw->db->f("t_assignedto");
-		$lstCategory=$phpgw->db->f("t_category");
+		$lstAssignedto = $GLOBALS['phpgw']->db->f("t_assignedto");
+		$lstCategory   = $GLOBALS['phpgw']->db->f("t_category");
 
 		// mark as read.
-		$temp_watchers=explode(":",$phpgw->db->f("t_watchers"));
-		if (!(in_array( $phpgw_info["user"]["userid"], $temp_watchers))) {
-	    	    $temp_watchers[]=$phpgw_info["user"]["userid"];
-	    	    $t_watchers=implode(":",$temp_watchers);
-	    	    // var_dump($t_watchers);
-	    	    $phpgw->db->query("UPDATE ticket set t_watchers='".$t_watchers."' where t_id=$ticketid");
+		$temp_watchers=explode(':',$GLOBALS['phpgw']->db->f('t_watchers'));
+		if (!(in_array( $GLOBALS['phpgw_info']['user']['userid'], $temp_watchers)))
+		{
+			$temp_watchers[]=$GLOBALS['phpgw_info']['user']['userid'];
+			$t_watchers=implode(":",$temp_watchers);
+			// var_dump($t_watchers);
+			$GLOBALS['phpgw']->db->query("UPDATE ticket set t_watchers='".$t_watchers."' where t_id=$ticketid");
 		} 
 
-
 		// Print the table
-		$p = CreateObject('phpgwapi.Template',PHPGW_APP_TPL);
-		//  echo PHPGW_APP_TPL;
-		$p->set_file(array(
-		    'viewticket'   => 'viewticket_details.tpl'
-    		));
-	
-		$p->set_block('viewticket', 'tts_select_options','tts_select_options');
-	
-		$p->set_unknowns('remove');
+		$GLOBALS['phpgw']->template->set_file(array(
+			'viewticket' => 'viewticket_details.tpl'
+		));
 
+		$GLOBALS['phpgw']->template->set_block('viewticket', 'tts_select_options','tts_select_options');
 
-                if ($phpgw->db->f("t_timestamp_closed") > 0) {
-		    $p->set_var('tts_t_status', $phpgw->common->show_date($phpgw->db->f("t_timestamp_closed")));
-                } else {
-		    $p->set_var('tts_t_status', lang("in progress"));
-                }
-	
-		// Choose the correct priority to display
-		$priority_selected[$phpgw->db->f("t_priority")] = " selected";
-		$priority_comment[1]=" - ".lang("Lowest"); 
-		$priority_comment[5]=" - ".lang("Medium"); 
-		$priority_comment[10]=" - ".lang("Highest"); 
+		$GLOBALS['phpgw']->template->set_unknowns('remove');
 
-        	for ($i=1; $i<=10; $i++) {
-		    $p->set_var('tts_optionname', $i.$priority_comment[$i]);
-		    $p->set_var('tts_optionvalue', $i);
-		    $p->set_var('tts_optionselected', $priority_selected[$i]);
-		    $p->parse('tts_priority_options','tts_select_options',true);
+		if ($GLOBALS['phpgw']->db->f('t_timestamp_closed') > 0)
+		{
+			$GLOBALS['phpgw']->template->set_var('tts_t_status', $GLOBALS['phpgw']->common->show_date($GLOBALS['phpgw']->db->f('t_timestamp_closed')));
 		}
-		
+		else
+		{
+			$GLOBALS['phpgw']->template->set_var('tts_t_status', lang('in progress'));
+		}
+
+		// Choose the correct priority to display
+		$priority_selected[$GLOBALS['phpgw']->db->f('t_priority')] = ' selected';
+		$priority_comment[1]=' - '.lang('Lowest'); 
+		$priority_comment[5]=' - '.lang('Medium'); 
+		$priority_comment[10]=' - '.lang('Highest'); 
+
+		for ($i=1; $i<=10; $i++)
+		{
+			$GLOBALS['phpgw']->template->set_var('tts_optionname', $i.$priority_comment[$i]);
+			$GLOBALS['phpgw']->template->set_var('tts_optionvalue', $i);
+			$GLOBALS['phpgw']->template->set_var('tts_optionselected', $priority_selected[$i]);
+			$GLOBALS['phpgw']->template->parse('tts_priority_options','tts_select_options',true);
+		}
+
 		// assigned to
 		$accounts = CreateObject('phpgwapi.accounts',$group_id);
 		$account_list = $accounts->get_list('accounts');
-		$p->set_var('tts_optionname', lang("none"));
-		$p->set_var('tts_optionvalue', "none" );
-		$p->set_var('tts_optionselected', "");
-		$p->parse('tts_assignedto_options','tts_select_options',true);
+		$GLOBALS['phpgw']->template->set_var('tts_optionname', lang('none'));
+		$GLOBALS['phpgw']->template->set_var('tts_optionvalue', 'none' );
+		$GLOBALS['phpgw']->template->set_var('tts_optionselected', '');
+		$GLOBALS['phpgw']->template->parse('tts_assignedto_options','tts_select_options',true);
 		while (list($key,$entry) = each($account_list))
 		{
-		    $tag="";
-		    if ($entry['account_lid'] == "$lstAssignedto") { $tag = "selected"; }
-		    $p->set_var('tts_optionname', $entry['account_lid']);
-		    $p->set_var('tts_optionvalue', $entry['account_lid']);
-		    $p->set_var('tts_optionselected', $tag);
-		    $p->parse('tts_assignedto_options','tts_select_options',true);
+			$tag="";
+			if ($entry['account_lid'] == "$lstAssignedto")
+			{
+				$tag = "selected";
+			}
+			$GLOBALS['phpgw']->template->set_var('tts_optionname', $entry['account_lid']);
+			$GLOBALS['phpgw']->template->set_var('tts_optionvalue', $entry['account_lid']);
+			$GLOBALS['phpgw']->template->set_var('tts_optionselected', $tag);
+			$GLOBALS['phpgw']->template->parse('tts_assignedto_options','tts_select_options',true);
 		}
-		
+
 		// group
 		$groups = CreateObject('phpgwapi.accounts');
 		$group_list = $groups->get_list('groups');
 		while (list($key,$entry) = each($group_list))
 		{
-		    $tag="";
-		    if ($entry['account_lid'] == "$lstCategory") { $tag = "selected"; }
-		    $p->set_var('tts_optionname', $entry['account_lid']);
-		    $p->set_var('tts_optionvalue', $entry['account_lid']);
-		    $p->set_var('tts_optionselected', $tag);
-		    $p->parse('tts_group_options','tts_select_options',true);
+			$tag="";
+			if ($entry['account_lid'] == "$lstCategory")
+			{
+				$tag = "selected";
+			}
+			$GLOBALS['phpgw']->template->set_var('tts_optionname', $entry['account_lid']);
+			$GLOBALS['phpgw']->template->set_var('tts_optionvalue', $entry['account_lid']);
+			$GLOBALS['phpgw']->template->set_var('tts_optionselected', $tag);
+			$GLOBALS['phpgw']->template->parse('tts_group_options','tts_select_options',true);
 		}
-	    
-	        $details_string = stripslashes($phpgw->db->f("t_detail"));
 
-		$p->set_var('tts_viewticketdetails_link', $phpgw->link("/tts/viewticket_details.php"));
-		$p->set_var('tts_t_id', $phpgw->db->f("t_id"));
-		$p->set_var('tts_t_user', $phpgw->db->f("t_user"));
-		$p->set_var('tts_th_bg', $phpgw_info["theme"][th_bg]);
-		$p->set_var('tts_lang_viewjobdetails', lang("View Job Detail"));
-		$p->set_var('tts_lang_assignedfrom', lang("Assigned from"));
-		$p->set_var('tts_lang_opendate', lang("Open Date"));
-		$p->set_var('tts_t_opendate', $phpgw->common->show_date($phpgw->db->f("t_timestamp_opened")));
-		$p->set_var('tts_t_status', $phpgw->db->f("t_timestamp_closed")?$phpgw->common->show_date($phpgw->db->f("t_timestamp_closed")):lang("In progress"));
-		$p->set_var('tts_lang_closedate', lang("Close Date"));
-		$p->set_var('tts_lang_priority', lang("Priority"));
-		$p->set_var('tts_lang_group', lang("Group"));
-		$p->set_var('tts_lang_assignedto', lang("Assigned to"));
-		$p->set_var('tts_hidden_detailstring', $phpgw->strip_html($details_string));
-		$p->set_var('tts_lang_subject', lang("Subject"));
-		$p->set_var('tts_lang_details', lang("Details"));
-		$p->set_var('tts_t_subject', stripslashes($phpgw->db->f("t_subject")));
-		$p->set_var('tts_detailstring', stripslashes($details_string));
-		$p->set_var('tts_lang_additionalnotes', lang("Additional notes"));
-	        $p->set_var('tts_lang_ok', lang("OK"));
+		$details_string = stripslashes($GLOBALS['phpgw']->db->f('t_detail'));
+
+		$GLOBALS['phpgw']->template->set_var('tts_viewticketdetails_link', $GLOBALS['phpgw']->link('/tts/viewticket_details.php'));
+		$GLOBALS['phpgw']->template->set_var('tts_t_id', $GLOBALS['phpgw']->db->f('t_id'));
+		$GLOBALS['phpgw']->template->set_var('tts_t_user', $GLOBALS['phpgw']->db->f('t_user'));
+		$GLOBALS['phpgw']->template->set_var('tts_th_bg', $GLOBALS['phpgw_info']['theme']['th_bg']);
+		$GLOBALS['phpgw']->template->set_var('tts_lang_viewjobdetails', lang('View Job Detail'));
+		$GLOBALS['phpgw']->template->set_var('tts_lang_assignedfrom', lang('Assigned from'));
+		$GLOBALS['phpgw']->template->set_var('tts_lang_opendate', lang('Open Date'));
+		$GLOBALS['phpgw']->template->set_var('tts_t_opendate', $GLOBALS['phpgw']->common->show_date($GLOBALS['phpgw']->db->f('t_timestamp_opened')));
+		$GLOBALS['phpgw']->template->set_var('tts_t_status', $GLOBALS['phpgw']->db->f('t_timestamp_closed')?$GLOBALS['phpgw']->common->show_date($GLOBALS['phpgw']->db->f('t_timestamp_closed')):lang('In progress'));
+		$GLOBALS['phpgw']->template->set_var('tts_lang_closedate', lang('Close Date'));
+		$GLOBALS['phpgw']->template->set_var('tts_lang_priority', lang('Priority'));
+		$GLOBALS['phpgw']->template->set_var('tts_lang_group', lang('Group'));
+		$GLOBALS['phpgw']->template->set_var('tts_lang_assignedto', lang('Assigned to'));
+		$GLOBALS['phpgw']->template->set_var('tts_hidden_detailstring', $GLOBALS['phpgw']->strip_html($details_string));
+		$GLOBALS['phpgw']->template->set_var('tts_lang_subject', lang('Subject'));
+		$GLOBALS['phpgw']->template->set_var('tts_lang_details', lang('Details'));
+		$GLOBALS['phpgw']->template->set_var('tts_t_subject', stripslashes($GLOBALS['phpgw']->db->f('t_subject')));
+		$GLOBALS['phpgw']->template->set_var('tts_detailstring', stripslashes($details_string));
+		$GLOBALS['phpgw']->template->set_var('tts_lang_additionalnotes', lang('Additional notes'));
+		$GLOBALS['phpgw']->template->set_var('tts_lang_ok', lang('OK'));
 
 		// change buttons from update/close to close/reopen if ticket is already closed
-		if ($phpgw->db->f(7) > 0)
+		if ($GLOBALS['phpgw']->db->f(7) > 0)
 		{
-		        $p->set_var('tts_leftradio', lang("Closed"));
-		        $p->set_var('tts_rightradio', lang("ReOpen"));
-		        $p->set_var('tts_leftradiovalue', "letclosed");
-		        $p->set_var('tts_rightradiovalue', "reopen");
+			$GLOBALS['phpgw']->template->set_var('tts_leftradio', lang('Closed'));
+			$GLOBALS['phpgw']->template->set_var('tts_rightradio', lang('ReOpen'));
+			$GLOBALS['phpgw']->template->set_var('tts_leftradiovalue', 'letclosed');
+			$GLOBALS['phpgw']->template->set_var('tts_rightradiovalue', 'reopen');
 		}
 		else
 		{
-		        $p->set_var('tts_leftradio', lang("Update"));
-		        $p->set_var('tts_rightradio', lang("Close"));
-		        $p->set_var('tts_leftradiovalue', "update");
-		        $p->set_var('tts_rightradiovalue', "close");
+			$GLOBALS['phpgw']->template->set_var('tts_leftradio', lang('Update'));
+			$GLOBALS['phpgw']->template->set_var('tts_rightradio', lang('Close'));
+			$GLOBALS['phpgw']->template->set_var('tts_leftradiovalue', 'update');
+			$GLOBALS['phpgw']->template->set_var('tts_rightradiovalue', 'close');
 		}
-		
-		$p->set_var('tts_select_options',"");
-	
-		$p->pfp('out','viewticket');
 
-		$phpgw->common->phpgw_footer();
+		$GLOBALS['phpgw']->template->set_var('tts_select_options','');
+
+		$GLOBALS['phpgw']->template->pfp('out','viewticket');
+		$GLOBALS['phpgw']->common->phpgw_footer();
 	}
 	else
 	{
-
 		// DB Content is fresher than http posted value.
-		$phpgw->db->query("select t_detail, t_assignedto, t_category, t_priority from ticket where t_id='".$t_id."'");
-		$phpgw->db->next_record();
-		$txtDetail = $phpgw->db->f("t_detail");
-		$oldassigned = $phpgw->db->f("t_assignedto");
-		$oldpriority = $phpgw->db->f("t_priority");
-		$oldcategory = $phpgw->db->f("t_category");
-		
-		if ($optUpdateclose == "letclosed" )
+		$GLOBALS['phpgw']->db->query("select t_detail, t_assignedto, t_category, t_priority from ticket where t_id='".$t_id."'");
+		$GLOBALS['phpgw']->db->next_record();
+		$txtDetail = $GLOBALS['phpgw']->db->f('t_detail');
+		$oldassigned = $GLOBALS['phpgw']->db->f('t_assignedto');
+		$oldpriority = $GLOBALS['phpgw']->db->f('t_priority');
+		$oldcategory = $GLOBALS['phpgw']->db->f('t_category');
+
+		if ($optUpdateclose == 'letclosed')
 		{
 			# let ticket be closed
 			# don't do any changes, ppl will have to reopen tickets to
 			# submit additional infos
-		} else {
-			if ($optUpdateclose == "reopen")
+		}
+		else
+		{
+			if ($optUpdateclose == 'reopen')
 			{
 				# reopen the ticket
-				$phpgw->db->query("UPDATE ticket set t_timestamp_closed='0' WHERE t_id=$t_id");
-				$txtReopen = "<b>".lang("Ticket reopened")."</b><br>";
+				$GLOBALS['phpgw']->db->query("UPDATE ticket set t_timestamp_closed='0' WHERE t_id=$t_id");
+				$txtReopen = '<b>'.lang('Ticket reopened').'</b><br>';
 			}
 
-			if ( $optUpdateclose == "close" )
+			if ( $optUpdateclose == 'close' )
 			{
-				$txtClose = "<br /><b>".lang("Ticket closed")."</b>";
-				$phpgw->db->query("UPDATE ticket set t_timestamp_closed='" . time() . "' WHERE t_id=$t_id");
+				$txtClose = '<br /><b>'.lang('Ticket closed').'</b>';
+				$GLOBALS['phpgw']->db->query("UPDATE ticket set t_timestamp_closed='" . time() . "' WHERE t_id=$t_id");
 			}
-	
+
 			if ($oldassigned != $lstAssignedto)
 			{
 				$txtAssignTo = "<br /><b>".lang("Ticket assigned to x",$lstAssignedto)."</b>";
@@ -186,36 +192,34 @@
 
 			if ($oldpriority != $optPriority)
 			{
-				$txtPriority = "<br /><b>".lang("Priority changed to x",$optPriority)."</b>";
+				$txtPriority = '<br /><b>'.lang('Priority changed to x',$optPriority).'</b>';
 			}
 
 			if ($oldcategory != $lstCategory)
 			{
-				$txtCategory = "<br /><b>".lang("Category changed to x",$lstCategory)."</b>";
+				$txtCategory = '<br /><b>'.lang('Category changed to x',$lstCategory).'</b>';
 			}
 
 			$txtAdditional = $txtReopen.$txtAdditional.$txtAssignTo.$txtCategory.$txtPriority.$txtClose;
 
 			if (! empty($txtAdditional))
 			{
-		    	
-				$UserInfo = "<BR><i>\n" . $phpgw_info["user"]["userid"] . " - "
-		    				. $phpgw->common->show_date(time()) . "</i><BR>\n";
+				$UserInfo = "<BR><i>\n" . $GLOBALS['phpgw_info']['user']['userid'] . ' - '
+					. $GLOBALS['phpgw']->common->show_date(time()) . "</i><BR>\n";
 
 				$txtDetail .= $UserInfo;
 				$txtDetail .= nl2br($txtAdditional);
-				$txtDetail .= "<hr>";
-	
+				$txtDetail .= '<hr>';
+
 				# update the database if ticket content changed
-				$phpgw->db->query("UPDATE ticket set t_category='$lstCategory',t_detail='".addslashes($txtDetail)."',t_priority='$optPriority',t_user='$lstAssignedfrom',t_assignedto='$lstAssignedto',t_watchers='".$phpgw_info["user"]["userid"]."' WHERE t_id=$t_id");
-		
-				if ($phpgw_info['server']['tts_mailticket']) {
-				    mail_ticket($t_id);
+				$GLOBALS['phpgw']->db->query("UPDATE ticket set t_category='$lstCategory',t_detail='".addslashes($txtDetail)."',t_priority='$optPriority',t_user='$lstAssignedfrom',t_assignedto='$lstAssignedto',t_watchers='".$GLOBALS['phpgw_info']["user"]["userid"]."' WHERE t_id=$t_id");
+
+				if ($GLOBALS['phpgw_info']['server']['tts_mailticket'])
+				{
+					mail_ticket($t_id);
 				}
 			}
-
 		}
-		Header("Location: " . $phpgw->link("/tts/index.php"));
+		Header('Location: ' . $GLOBALS['phpgw']->link('/tts/index.php'));
 	}
-
 ?>
