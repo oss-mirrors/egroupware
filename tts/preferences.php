263eb883d473e7e6fcdd578a154f2e2b76b0bcc6
milosch <milosch@923e3c2e-ae13-0410-ba18-ced7211e33de>
2001-09-24 15:54:04 +0000

    Fix mainscreen pref setting, GLOBALS
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@6056 923e3c2e-ae13-0410-ba18-ced7211e33de

--- tts/preferences.php
+++ tts/preferences.php
@@ -1,22 +1,22 @@
 <?php
-/**************************************************************************\
-* phpGroupWare - TTS		                                           *
-* http://www.phpgroupware.org                                              *
-* --------------------------------------------                             *
-*  This program is free software; you can redistribute it and/or modify it *
-*  under the terms of the GNU General Public License as published by the   *
-*  Free Software Foundation; either version 2 of the License, or (at your  *
-*  option) any later version.                                              *
-\**************************************************************************/
+  /**************************************************************************\
+  * phpGroupWare - TTS                                                       *
+  * http://www.phpgroupware.org                                              *
+  * --------------------------------------------                             *
+  *	This program is free software; you can redistribute it and/or modify it  *
+  *	under the terms of the GNU General Public License as published by the    *
+  *	Free Software Foundation; either version 2 of the License, or (at your   *
+  *	option) any later version.                                               *
+  \**************************************************************************/
 
-  /* $Id$ */
+	/* $Id$ */
 
-	$phpgw_info['flags'] = array(
-		'currentapp'              => 'tts', 
-		'noheader'                => True, 
-		'nonavbar'                => True, 
-		'noappheader'             => True,
-		'noappfooter'             => True,
+	$GLOBALS['phpgw_info']['flags'] = array(
+		'currentapp'  => 'tts', 
+		'noheader'    => True, 
+		'nonavbar'    => True, 
+		'noappheader' => True,
+		'noappfooter' => True,
 		'enable_contacts_class'   => True,
 		'enable_nextmatchs_class' => True
 	);
@@ -26,157 +26,168 @@
 	$account_selected = array();
 	$entry_selected = array();
 	$priority_selected = array();
-	
-	$phpgw->preferences->read_repository();
 
-	if ($submit)
+	$GLOBALS['phpgw']->preferences->read_repository();
+
+	if ($HTTP_POST_VARS['submit'])
 	{
 		$totalerrors = 0;
 		if (! $totalerrors)
 		{
-
- 			if ($mainscreen_show_new_updated)
+			if ($HTTP_POST_VARS['mainscreen_show_new_updated'])
 			{
-				$phpgw->preferences->delete('tts','mainscreen_show_new_updated');
-				$phpgw->preferences->add('tts','mainscreen_show_new_updated');
+				$GLOBALS['phpgw']->preferences->delete('tts','mainscreen_show_new_updated');
+				$GLOBALS['phpgw']->preferences->add('tts','mainscreen_show_new_updated',True);
 			}
 			else
 			{
-				$phpgw->preferences->delete('tts','mainscreen_show_new_updated');
+				$GLOBALS['phpgw']->preferences->delete('tts','mainscreen_show_new_updated');
 			}
 
- 			if ($groupdefault)
+			if ($HTTP_POST_VARS['groupdefault'])
 			{
-				$phpgw->preferences->delete('tts','groupdefault');
-				$phpgw->preferences->add('tts','groupdefault',$groupdefault);
+				$GLOBALS['phpgw']->preferences->delete('tts','groupdefault');
+				$GLOBALS['phpgw']->preferences->add('tts','groupdefault',$HTTP_POST_VARS['groupdefault']);
 			}
 			else
 			{
-				$phpgw->preferences->delete('tts','groupdefault');
+				$GLOBALS['phpgw']->preferences->delete('tts','groupdefault');
 			}
 
- 			if ($assigntodefault)
+			if ($HTTP_POST_VARS['assigntodefault'])
 			{
-				$phpgw->preferences->delete('tts','assigntodefault');
-				$phpgw->preferences->add('tts','assigntodefault',$assigntodefault);
+				$GLOBALS['phpgw']->preferences->delete('tts','assigntodefault');
+				$GLOBALS['phpgw']->preferences->add('tts','assigntodefault',$HTTP_POST_VARS['assigntodefault']);
 			}
 			else
 			{
-				$phpgw->preferences->delete('tts','assigntodefault');
+				$GLOBALS['phpgw']->preferences->delete('tts','assigntodefault');
 			}
 
- 			if ($prioritydefault)
+			if ($HTTP_POST_VARS['prioritydefault'])
 			{
-				$phpgw->preferences->delete('tts','prioritydefault');
-				$phpgw->preferences->add('tts','prioritydefault',$prioritydefault);
+				$GLOBALS['phpgw']->preferences->delete('tts','prioritydefault');
+				$GLOBALS['phpgw']->preferences->add('tts','prioritydefault',$HTTP_POST_VARS['prioritydefault']);
 			}
 			else
 			{
-				$phpgw->preferences->delete('tts','prioritydefault');
+				$GLOBALS['phpgw']->preferences->delete('tts','prioritydefault');
 			}
 
- 			if ($refreshinterval)
+			if ($HTTP_POST_VARS['refreshinterval'])
 			{
-				$phpgw->preferences->delete('tts','refreshinterval');
-				$phpgw->preferences->add('tts','refreshinterval',$refreshinterval);
+				$GLOBALS['phpgw']->preferences->delete('tts','refreshinterval');
+				$GLOBALS['phpgw']->preferences->add('tts','refreshinterval',$HTTP_POST_VARS['refreshinterval']);
 			}
 			else
 			{
-				$phpgw->preferences->delete('tts','refreshinterval');
+				$GLOBALS['phpgw']->preferences->delete('tts','refreshinterval');
 			}
 
-			$phpgw->preferences->save_repository(True);
-			Header('Location: ' . $phpgw->link('/preferences/index.php'));
+			$GLOBALS['phpgw']->preferences->save_repository(True);
+			Header('Location: ' . $GLOBALS['phpgw']->link('/preferences/index.php'));
 		}
 	}
 
-	$phpgw->common->phpgw_header();
+	$GLOBALS['phpgw']->common->phpgw_header();
 	echo parse_navbar();
 
 	if ($totalerrors)
 	{
-		echo '<p><center>' . $phpgw->common->error_list($errors) . '</center>';
+		echo '<p><center>' . $GLOBALS['phpgw']->common->error_list($errors) . '</center>';
 	}
 
-	$t = new Template(PHPGW_APP_TPL);
-	$t->set_file(array(
+	$GLOBALS['phpgw']->template->set_file(array(
 		'preferences' => 'preferences.tpl'
 	));
-	$t->set_block('preferences', 'tts_select_options','tts_select_options');
+	$GLOBALS['phpgw']->template->set_block('preferences', 'tts_select_options','tts_select_options');
 
-	$t->set_var(action_url,$phpgw->link('/tts/preferences.php'));
+	$GLOBALS['phpgw']->template->set_var(action_url,$GLOBALS['phpgw']->link('/tts/preferences.php'));
 
-	$tr_color = $phpgw->nextmatchs->alternate_row_color($tr_color);
-	$t->set_var(tr_color,$tr_color);
-	$t->set_var(lang_show_new_updated,lang('show new/updated tickets on main screen'));
+	$tr_color = $GLOBALS['phpgw']->nextmatchs->alternate_row_color($tr_color);
+	$GLOBALS['phpgw']->template->set_var('tr_color',$tr_color);
+	$GLOBALS['phpgw']->template->set_var('lang_show_new_updated',lang('show new/updated tickets on main screen'));
 
-	if ($phpgw_info['user']['preferences']['tts']['mainscreen_show_new_updated'])
+	if ($GLOBALS['phpgw_info']['user']['preferences']['tts']['mainscreen_show_new_updated'])
 	{
-		$t->set_var(show_new_updated,' checked');
+		$GLOBALS['phpgw']->template->set_var('show_new_updated',' checked');
 	}
 	else
 	{
-		$t->set_var(show_new_updated,'');
+		$GLOBALS['phpgw']->template->set_var('show_new_updated','');
 	}
 
-	if ($phpgw_info['user']['preferences']['tts']['groupdefault']) { $entry_selected[$phpgw_info['user']['preferences']['tts']['groupdefault']]=" selected"; };
-	if ($phpgw_info['user']['preferences']['tts']['assigntodefault']) { $account_selected[$phpgw_info['user']['preferences']['tts']['assigntodefault']]=" selected"; };
-	if ($phpgw_info['user']['preferences']['tts']['prioritydefault']) { $priority_selected[$phpgw_info['user']['preferences']['tts']['prioritydefault']]=" selected"; };
+	if ($GLOBALS['phpgw_info']['user']['preferences']['tts']['groupdefault'])
+	{
+		$entry_selected[$GLOBALS['phpgw_info']['user']['preferences']['tts']['groupdefault']]=' selected';
+	}
+	if ($GLOBALS['phpgw_info']['user']['preferences']['tts']['assigntodefault'])
+	{
+		$account_selected[$GLOBALS['phpgw_info']['user']['preferences']['tts']['assigntodefault']]=' selected';
+	}
+	if ($GLOBALS['phpgw_info']['user']['preferences']['tts']['prioritydefault'])
+	{
+		$priority_selected[$GLOBALS['phpgw_info']['user']['preferences']['tts']['prioritydefault']]=' selected';
+	}
 
-	if ($phpgw_info['user']['preferences']['tts']['refreshinterval']) {
-		$t->set_var(refreshinterval,$phpgw_info['user']['preferences']['tts']['refreshinterval']);
-	} else {
-		$t->set_var(refreshinterval,"");
+	if ($GLOBALS['phpgw_info']['user']['preferences']['tts']['refreshinterval'])
+	{
+		$GLOBALS['phpgw']->template->set_var('refreshinterval',$GLOBALS['phpgw_info']['user']['preferences']['tts']['refreshinterval']);
+	}
+	else
+	{
+		$GLOBALS['phpgw']->template->set_var('refreshinterval','');
 	}
 
 	$groups = CreateObject('phpgwapi.accounts');
 	$group_list = $groups->get_list('groups');
 	while (list($key,$entry) = each($group_list))
 	{
-		    $t->set_var('tts_optionvalue', $entry['account_lid']);
-    		    $t->set_var('tts_optionname', $entry['account_lid']);
-		    $t->set_var('tts_optionselected', $entry_selected[$entry['account_lid']]);
-		    $t->parse('tts_groupoptions','tts_select_options',true);
+		$GLOBALS['phpgw']->template->set_var('tts_optionvalue', $entry['account_lid']);
+		$GLOBALS['phpgw']->template->set_var('tts_optionname', $entry['account_lid']);
+		$GLOBALS['phpgw']->template->set_var('tts_optionselected', $entry_selected[$entry['account_lid']]);
+		$GLOBALS['phpgw']->template->parse('tts_groupoptions','tts_select_options',true);
 	}
 
-        $t->set_var('tts_lang_assignto', lang("assign to"));
+	$GLOBALS['phpgw']->template->set_var('tts_lang_assignto', lang('assign to'));
 	$accounts = CreateObject('phpgwapi.accounts',$group_id);
 	$account_list = $accounts->get_list('accounts');
-	$t->set_var('tts_account_lid', "none" );
-	$t->set_var('tts_account_name', lang("none"));
-	$t->parse('tts_assignoptions','tts_select_options',false);
+	$GLOBALS['phpgw']->template->set_var('tts_account_lid', 'none' );
+	$GLOBALS['phpgw']->template->set_var('tts_account_name', lang('none'));
+	$GLOBALS['phpgw']->template->parse('tts_assignoptions','tts_select_options',false);
 	while (list($key,$entry) = each($account_list))
 	{
 		if ($entry['account_lid'])
 		{
-			$t->set_var('tts_optionvalue', $entry['account_lid']);
-    			$t->set_var('tts_optionname', $entry['account_lid']);
-			$t->set_var('tts_optionselected', $account_selected[$entry['account_lid']]);
+			$GLOBALS['phpgw']->template->set_var('tts_optionvalue', $entry['account_lid']);
+			$GLOBALS['phpgw']->template->set_var('tts_optionname', $entry['account_lid']);
+			$GLOBALS['phpgw']->template->set_var('tts_optionselected', $account_selected[$entry['account_lid']]);
 		}
-		$t->parse('tts_assigntooptions','tts_select_options',true);
+		$GLOBALS['phpgw']->template->parse('tts_assigntooptions','tts_select_options',true);
 	}
 
 	// Choose the correct priority to display
-	$prority_selected[$phpgw->db->f("t_priority")] = " selected";
-	$priority_comment[1]=" - ".lang("Lowest"); 
-	$priority_comment[5]=" - ".lang("Medium"); 
-	$priority_comment[10]=" - ".lang("Highest"); 
+	$prority_selected[$GLOBALS['phpgw']->db->f('t_priority')] = ' selected';
+	$priority_comment[1]  = ' - ' . lang('Lowest'); 
+	$priority_comment[5]  = ' - ' . lang('Medium'); 
+	$priority_comment[10] = ' - ' . lang('Highest'); 
 
-    	for ($i=1; $i<=10; $i++) {
-	    $t->set_var('tts_optionname', $i.$priority_comment[$i]);
-	    $t->set_var('tts_optionvalue', $i);
-	    $t->set_var('tts_optionselected', $priority_selected[$i]);
-	    $t->parse('tts_priorityoptions','tts_select_options',true);
+	for ($i=1; $i<=10; $i++)
+	{
+		$GLOBALS['phpgw']->template->set_var('tts_optionname', $i.$priority_comment[$i]);
+		$GLOBALS['phpgw']->template->set_var('tts_optionvalue', $i);
+		$GLOBALS['phpgw']->template->set_var('tts_optionselected', $priority_selected[$i]);
+		$GLOBALS['phpgw']->template->parse('tts_priorityoptions','tts_select_options',true);
 	}
 
-	$t->set_var(lang_refreshinterval,lang('Refresh every (seconds)'));
-	$t->set_var(lang_ttsprefs,lang('tts preferences'));
-	$t->set_var(lang_defaultgroup,lang('Default group'));
-	$t->set_var(lang_defaultassignto,lang('Default assign to'));
-	$t->set_var(lang_defaultpriority,lang('Default Priority'));
-	$t->set_var(lang_submit,lang('submit'));
-	$t->set_var(tts_select_options,'');
+	$GLOBALS['phpgw']->template->set_var('lang_refreshinterval',lang('Refresh every (seconds)'));
+	$GLOBALS['phpgw']->template->set_var('lang_ttsprefs',lang('tts preferences'));
+	$GLOBALS['phpgw']->template->set_var('lang_defaultgroup',lang('Default group'));
+	$GLOBALS['phpgw']->template->set_var('lang_defaultassignto',lang('Default assign to'));
+	$GLOBALS['phpgw']->template->set_var('lang_defaultpriority',lang('Default Priority'));
+	$GLOBALS['phpgw']->template->set_var('lang_submit',lang('submit'));
+	$GLOBALS['phpgw']->template->set_var('tts_select_options','');
 
-	$t->pparse('out','preferences');
-	$phpgw->common->phpgw_footer();
+	$GLOBALS['phpgw']->template->pparse('out','preferences');
+	$GLOBALS['phpgw']->common->phpgw_footer();
 ?>
