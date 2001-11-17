eb1dc712d08a9d513e4cf90e60587d0499c60535
milosch <milosch@923e3c2e-ae13-0410-ba18-ced7211e33de>
2001-11-17 02:08:02 +0000

    trying to get view access to work, again
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@6400 923e3c2e-ae13-0410-ba18-ced7211e33de

--- bookmarks/tree.php
+++ bookmarks/tree.php
@@ -137,7 +137,9 @@
 			{
 				$maintain_url  = $GLOBALS['phpgw']->link('/bookmarks/maintain.php','bm_id=' . $db2->f('bm_id'));
 				$maintain_link = sprintf('<a href="%s"><img src="%s/edit.gif" align="top" border="0" alt="%s"></a>', $maintain_url,PHPGW_IMAGES,lang('Edit this bookmark'));
-
+			}
+			if (($GLOBALS['phpgw']->bookmarks->grants[$db2->f('bm_owner')] & PHPGW_ACL_READ) || ($db2->f('bm_owner') == $GLOBALS['phpgw_info']['user']['account_id']))
+			{
 				$view_url      = $GLOBALS['phpgw']->link('/bookmarks/view.php','bm_id=' . $db2->f('bm_id'));
 				$view_link     = sprintf('<a href="%s"><img src="%s/document.gif" align="top" border="0" alt="%s"></a>', $view_url,PHPGW_IMAGES,lang('View this bookmark'));
 
