821b6a64e0e37da2b31d1f5d9d52a8f68d6d84aa
milosch <milosch@923e3c2e-ae13-0410-ba18-ced7211e33de>
2001-12-16 23:35:11 +0000

    Add banners, english lang file, fix one speling eroorr
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@6688 923e3c2e-ae13-0410-ba18-ced7211e33de

--- phpsysinfo/index.php
+++ phpsysinfo/index.php
@@ -1,78 +1,84 @@
 <?php
-   /**
-    **  index.php
-    **
-    **  $Id$
-    **/
+  /**************************************************************************\
+  * phpGroupWare - PHPSysInfo                                                *
+  * http://www.phpgroupware.org                                              *
+  * --------------------------------------------                             *
+  *  This program is free software; you can redistribute it and/or modify it *
+  *  under the terms of the GNU General Public License as published by the   *
+  *  Free Software Foundation; either version 2 of the License, or (at your  *
+  *  option) any later version.                                              *
+  \**************************************************************************/
 
-	$phpgw_info["flags"] = array
+  /* $Id$ */
+
+	$GLOBALS['phpgw_info']['flags'] = array
 		(
-			"currentapp" => "phpsysinfo"
+			'currentapp' => 'phpsysinfo'
 		);
-	include("../header.inc.php");
+	include('../header.inc.php');
 
-	$lng = $phpgw_info["user"]["preferences"]["common"]["lang"];
-	$template = $phpgw_info["user"]["preferences"]["common"]["theme"];
+	$lng = $GLOBALS['phpgw_info']['user']['preferences']['common']['lang'];
+	$template = $GLOBALS['phpgw_info']['user']['preferences']['common']['theme'];
 
-// default to english, but this is negotiable.
-if (!(isset($lng) && file_exists('includes/lang/' . $lng . '.php'))) {
-    $lng = 'en';
-}
-require('includes/lang/' . $lng . '.php');  // get our language include
-require('includes/color_scheme.php');        // our default color scheme
-#require('includes/class.Template.inc.php');  // template library
-require('includes/system_functions.php');    // finish defining all of our global functions and variables
+	// default to english, but this is negotiable.
+	if (!(isset($lng) && file_exists('includes/lang/' . $lng . '.php')))
+	{
+		$lng = 'en';
+	}
+	require('includes/lang/' . $lng . '.php');  // get our language include
+	require('includes/color_scheme.php');        // our default color scheme
+	#require('includes/class.Template.inc.php');  // template library
+	require('includes/system_functions.php');    // finish defining all of our global functions and variables
 
+	// figure out if we got a template passed in the url
+	if (!(isset($template) && file_exists("templates/$template")))
+	{
+		// default template we should use if we don't get a argument.
+		define('TEMPLATE_SET', 'default');
+		$template = 'default';
+	}
+	else
+	{
+		define('TEMPLATE_SET', $template);
+	}
 
-// figure out if we got a template passed in the url
-if (!(isset($template) && file_exists("templates/$template"))) {
-    // default template we should use if we don't get a argument.
-    define('TEMPLATE_SET', 'default');
-    $template = 'default';
-} else {
-    define('TEMPLATE_SET', $template);
-}
+	// fire up the template engine
+	$tpl = new Template(dirname(__FILE__) . '/templates/' . TEMPLATE_SET);
+	$tpl->set_file(array(
+		'form' => 'form.tpl'
+	));
 
+	// print out a box of information
+	function makebox ($title, $content)
+	{
+		$t = new Template(dirname(__FILE__) . '/templates/' . TEMPLATE_SET);
 
-// fire up the template engine
-$tpl = new Template(dirname(__FILE__) . '/templates/' . TEMPLATE_SET);
-$tpl->set_file(array(
-    'form' => 'form.tpl'
-));
+		$t->set_file(array(
+			'box'  => 'box.tpl'
+		));
 
+		$t->set_var('title', $title);
+		$t->set_var('content', $content);
 
-// print out a box of information
-function makebox ($title, $content)
-{
-    $t = new Template(dirname(__FILE__) . '/templates/' . TEMPLATE_SET);
+		return $t->parse('out', 'box');
+	}  
 
-    $t->set_file(array(
-        'box'  => 'box.tpl'
-    ));
+	// let the page begin.
+	#require('includes/system_header.php');
 
-    $t->set_var('title', $title);
-    $t->set_var('content', $content);
+	#$tpl->set_var('title', $text['title'] . ': ' . sys_chostname() . ' (' . sys_ip_addr() . ')');
 
-    return $t->parse('out', 'box');
-}  
+	require('includes/table_vitals.php');
+	require('includes/table_network.php');    
+	require('includes/table_hardware.php');
+	require('includes/table_memory.php');
+	require('includes/table_filesystems.php');
 
+	// parse our the template
+	$tpl->pparse('out', 'form');
 
-// let the page begin.
-#require('includes/system_header.php');
+	// finally our print our footer
+	#require('includes/system_footer.php');
 
-#$tpl->set_var('title', $text['title'] . ': ' . sys_chostname() . ' (' . sys_ip_addr() . ')');
-
-require('includes/table_vitals.php');
-require('includes/table_network.php');    
-require('includes/table_hardware.php');
-require('includes/table_memory.php');
-require('includes/table_filesystems.php');
-
-// parse our the template
-$tpl->pparse('out', 'form');
-
-// finally our print our footer
-#require('includes/system_footer.php');
-
-	$phpgw->common->phpgw_footer();
+	$GLOBALS['phpgw']->common->phpgw_footer();
 ?>
