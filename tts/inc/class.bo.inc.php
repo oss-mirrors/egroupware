ca1e8c9ac8deea4377a9ce892bb1ba894df16fd8
skeeter <skeeter@923e3c2e-ae13-0410-ba18-ced7211e33de>
2001-12-23 22:41:18 +0000

    Added the mail routine in.  Still not activated.  Need for jengo to turn it on when he's ready.
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@6782 923e3c2e-ae13-0410-ba18-ced7211e33de

--- tts/inc/class.bo.inc.php
+++ tts/inc/class.bo.inc.php
@@ -392,7 +392,7 @@
 	
 				if ($GLOBALS['phpgw_info']['server']['tts_mailticket'])
 				{
-					//mail_ticket($ticket_id);
+					//$this->mail_ticket($ticket_id);
 				}
 			}
 			else
@@ -403,4 +403,92 @@
 			return True;
 		}
 
+		function mail_ticket($ticket_id)
+		{
+			// $GLOBALS['phpgw']->preferences->read_repository();
+			// $GLOBALS['phpgw_info']['user']['preferences']['tts']['mailnotification']
+
+			$GLOBALS['phpgw']->config->read_repository();
+
+			if ($GLOBALS['phpgw']->config->config_data['mailnotification'])
+			{
+				$GLOBALS['phpgw']->send = CreateObject('phpgwapi.send');
+	
+				$this->db->query('select t_id,t_category,t_detail,t_priority,t_user,t_assignedto,'
+					. "t_timestamp_opened, t_timestamp_closed, t_subject from phpgw_tts_tickets where t_id='".$ticket_id."'",__LINE__,__FILE__);
+				$this->db->next_record();
+    
+				$group = $this->db->f('t_category');
+			
+				// build subject
+				$subject = '[TTS #'.$ticket_id.' '.$group.'] '.(!$this->db->f('t_timestamp_closed')?'Updated':'Closed').': '.$this->db->f('t_subject');
+
+				// build body
+				$body  = '';
+				$body .= 'TTS #'.$ticket_id."\n\n";
+				$body .= 'Subject: '.$this->db->f('t_subject')."\n\n";
+				$body .= 'Assigned To: '.$this->db->f('t_assignedto')."\n\n";
+				$body .= 'Priority: ".$this->db->f('t_priority')."\n\n";
+				$body .= 'Group: ".$group."\n\n";
+				$body .= 'Opened By: ".$this->db->f('t_user')."\n";
+				$body .= 'Date Opened: '.$GLOBALS['phpgw']->common->show_date($this->db->f('t_timestamp_opened'))."\n\n";
+				if($this->db->f('t_timestamp_closed'))
+				{
+					$body .= 'Date Closed: '.$GLOBALS['phpgw']->common->show_date($this->db->f('t_timestamp_closed'))."\n\n";
+				}
+				$body .= stripslashes(strip_tags($this->db->f('t_detail')))."\n\n.";
+			
+				$members = array();
+				if ($GLOBALS['phpgw']->config->config_data['groupnotification']) 
+				{
+					// select group recipients
+					$group_id = $GLOBALS['phpgw']->accounts->name2id($group);
+					$members  = $GLOBALS['phpgw']->accounts->members($group_id);
+				}
+
+				if ($GLOBALS['phpgw']->config->config_data['ownernotification'])
+				{
+					// add owner to recipients
+					$members[] = array('account_id' => $GLOBALS['phpgw']->accounts->name2id($this->db->f('t_user')), 'account_name' => $this->db->f('t_user'));
+				}
+
+				if ($GLOBALS['phpgw']->config->config_data['assignednotification'])
+				{
+					// add assigned to recipients
+					$members[] = array('account_id' => $GLOBALS['phpgw']->accounts->name2id($this->db->f('t_assignedto')), 'account_name' => $this->db->f('t_assignedto'));
+				}
+
+				$toarray = Array();
+				$i=0;
+				for ($i=0;$i<count($members);$i++)
+				{
+					if ($members[$i]['account_id'])
+					{
+						$prefs = $GLOBALS['phpgw']->preferences->create_email_preferences($members[$i]['account_id']);
+						$toarray[] = $prefs['email']['address'];
+					}
+				}
+				if(count($toarray) > 1)
+				{
+					@reset($toarray);
+					$to = implode(',',$toarray);
+				}
+				else
+				{
+					$to = $toarray[0];
+				}
+
+				$rc = $GLOBALS['phpgw']->send->msg('email', $to, $subject, stripslashes($body), '', $cc, $bcc);
+				if (!$rc)
+				{
+					echo  'Your message could <B>not</B> be sent!<BR>'."\n"
+						. 'The mail server returned:<BR>'
+						. "err_code: '".$GLOBALS['phpgw']->send->err['code']."';<BR>"
+						. "err_msg: '".htmlspecialchars($GLOBALS['phpgw']->send->err['msg'])."';<BR>\n"
+						. "err_desc: '".$GLOBALS['phpgw']->err['desc']."'.<P>\n"
+						. 'To go back to the msg list, click <a href="'.$GLOBALS['phpgw']->link('/tts/index.php','cd=13').'">here</a>';
+					$GLOBALS['phpgw']->common->phpgw_exit();
+				}
+			}
+		}
 	}
--- tts/inc/functions.inc.php
+++ tts/inc/functions.inc.php
@@ -23,61 +23,59 @@
 
 	function mail_ticket($ticket_id)
 	{
-		global $phpgw;
-		global $phpgw_info;
 		$members = array();
 		
-		// $phpgw->preferences->read_repository();
-		// $phpgw_info['user']['preferences']['tts']['mailnotification']
+		// $GLOBALS['phpgw']->preferences->read_repository();
+		// $GLOBALS['phpgw_info']['user']['preferences']['tts']['mailnotification']
 
-		$phpgw->config->read_repository();
+		$GLOBALS['phpgw']->config->read_repository();
 
 		
-		if ($phpgw->config->config_data['mailnotification']) {
+		if ($GLOBALS['phpgw']->config->config_data['mailnotification']) {
 		
-			$phpgw->send = CreateObject('phpgwapi.send');
+			$GLOBALS['phpgw']->send = CreateObject('phpgwapi.send');
 	
-			$phpgw->db->query("select t_id,t_category,t_detail,t_priority,t_user,t_assignedto,"
+			$GLOBALS['phpgw']->db->query('select t_id,t_category,t_detail,t_priority,t_user,t_assignedto,'
 				. "t_timestamp_opened, t_timestamp_closed, t_subject from phpgw_tts_tickets where t_id='$ticket_id'");
-			$phpgw->db->next_record();
+			$GLOBALS['phpgw']->db->next_record();
     
-			$group = $phpgw->db->f("t_category");
+			$group = $GLOBALS['phpgw']->db->f('t_category');
 			
 			// build subject
-			$subject = "[TTS #".$ticket_id." ".$group."] ".(!$phpgw->db->f("t_timestamp_closed")?"Updated":"Closed").": ".$phpgw->db->f("t_subject");
+			$subject = '[TTS #'.$ticket_id.' '.$group.'] '.(!$GLOBALS['phpgw']->db->f('t_timestamp_closed')?'Updated':'Closed').': '.$GLOBALS['phpgw']->db->f('t_subject');
 
 			// build body
-			$body  = "";
-			$body .= "TTS #".$ticket_id."\n\n";
-			$body .= "Subject: ".$phpgw->db->f("t_subject")."\n\n";
-			$body .= "Assigned To: ".$phpgw->db->f("t_assignedto")."\n\n";
-			$body .= "Priority: ".$phpgw->db->f("t_priority")."\n\n";
-			$body .= "Group: ".$group."\n\n";
-			$body .= "Opened By: ".$phpgw->db->f("t_user")."\n";
-			$body .= "Date Opened: ".$phpgw->common->show_date($phpgw->db->f("t_timestamp_opened"))."\n\n";
-			if($phpgw->db->f("t_timestamp_closed"))
+			$body  = '';
+			$body .= 'TTS #'.$ticket_id."\n\n";
+			$body .= 'Subject: '.$GLOBALS['phpgw']->db->f('t_subject')."\n\n";
+			$body .= 'Assigned To: '.$GLOBALS['phpgw']->db->f('t_assignedto')."\n\n";
+			$body .= 'Priority: ".$GLOBALS['phpgw']->db->f('t_priority')."\n\n";
+			$body .= 'Group: ".$group."\n\n";
+			$body .= 'Opened By: ".$GLOBALS['phpgw']->db->f('t_user')."\n";
+			$body .= 'Date Opened: '.$GLOBALS['phpgw']->common->show_date($GLOBALS['phpgw']->db->f('t_timestamp_opened'))."\n\n";
+			if($GLOBALS['phpgw']->db->f('t_timestamp_closed'))
 			{
-				$body .= "Date Closed: ".$phpgw->common->show_date($phpgw->db->f("t_timestamp_closed"))."\n\n";
+				$body .= 'Date Closed: '.$GLOBALS['phpgw']->common->show_date($GLOBALS['phpgw']->db->f('t_timestamp_closed'))."\n\n";
 			}
-			$body .= stripslashes(strip_tags($phpgw->db->f("t_detail")))."\n\n.";
+			$body .= stripslashes(strip_tags($GLOBALS['phpgw']->db->f('t_detail')))."\n\n.";
 			
-			if ($phpgw->config->config_data['groupnotification']) 
+			if ($GLOBALS['phpgw']->config->config_data['groupnotification']) 
 			{
-			// select group recipients
-			$group_id = $phpgw->accounts->name2id($group);
-			$members  = $phpgw->accounts->members($group_id);
+				// select group recipients
+				$group_id = $GLOBALS['phpgw']->accounts->name2id($group);
+				$members  = $GLOBALS['phpgw']->accounts->members($group_id);
 			}
 
-			if ($phpgw->config->config_data['ownernotification'])
+			if ($GLOBALS['phpgw']->config->config_data['ownernotification'])
 			{
-			// add owner to recipients
-			 $members[] = array("account_id" => $phpgw->accounts->name2id($phpgw->db->f("t_user")), "account_name" => $phpgw->db->f("t_user"));
+				// add owner to recipients
+				$members[] = array('account_id' => $GLOBALS['phpgw']->accounts->name2id($GLOBALS['phpgw']->db->f('t_user')), 'account_name' => $GLOBALS['phpgw']->db->f('t_user'));
 			}
 
-			if ($phpgw->config->config_data['assignednotification'])
+			if ($GLOBALS['phpgw']->config->config_data['assignednotification'])
 			{
-			// add assigned to recipients
-			 $members[] = array("account_id" => $phpgw->accounts->name2id($phpgw->db->f("t_assignedto")), "account_name" => $phpgw->db->f("t_assignedto"));
+				// add assigned to recipients
+				$members[] = array('account_id' => $GLOBALS['phpgw']->accounts->name2id($GLOBALS['phpgw']->db->f('t_assignedto')), 'account_name' => $GLOBALS['phpgw']->db->f('t_assignedto'));
 			}
 
 			$toarray = Array();
@@ -86,33 +84,34 @@
 			{
 				if ($members[$i]['account_id'])
 				{
-					$pref = CreateObject('phpgwapi.preferences',$members[$i]['account_id']);
-					$prefs = $pref->read_repository();
-					$prefs = $phpgw->common->create_emailpreferences($prefs,$members[$i]['account_id']);
-					$toarray[] = $prefs["email"]["address"];
+					$prefs = $GLOBALS['phpgw']->preferences->create_email_preferences($members[$i]['account_id']);
+//					$pref = CreateObject('phpgwapi.preferences',$members[$i]['account_id']);
+//					$prefs = $pref->read_repository();
+//					$prefs = $phpgw->common->create_emailpreferences($prefs,$members[$i]['account_id']);
+					$toarray[] = $prefs['email']['address'];
 					// echo '<br>'.$toarray[$i];
-					unset($pref);
+//					unset($pref);
 				}
 			}
 			if(count($toarray) > 1)
 			{
-				$to = implode(",",$toarray);
+				$to = implode(',',$toarray);
 			}
 			else
 			{
 				$to = $toarray[0];
 			}
     
-			$rc = $phpgw->send->msg("email", $to, $subject, stripslashes($body), "", $cc, $bcc);
+			$rc = $GLOBALS['phpgw']->send->msg('email', $to, $subject, stripslashes($body), '', $cc, $bcc);
 			if (!$rc)
 			{
-				echo  "Your message could <B>not</B> be sent!<BR>\n"
-					. "The mail server returned:<BR>"
-					. "err_code: '".$phpgw->send->err["code"]."';<BR>"
-					. "err_msg: '".htmlspecialchars($phpgw->send->err[msg])."';<BR>\n"
-					. "err_desc: '".$phpgw->err[desc]."'.<P>\n"
-					. "To go back to the msg list, click <A HRef=\"".$phpgw->link("/tts/index.php","cd=13")."\">here</a>";
-				$phpgw->common->phpgw_exit();
+				echo  'Your message could <B>not</B> be sent!<BR>'."\n"
+					. 'The mail server returned:<BR>'
+					. "err_code: '".$GLOBALS['phpgw']->send->err['code']."';<BR>"
+					. "err_msg: '".htmlspecialchars($GLOBALS['phpgw']->send->err['msg'])."';<BR>\n"
+					. "err_desc: '".$GLOBALS['phpgw']->err['desc']."'.<P>\n"
+					. 'To go back to the msg list, click <a href="'.$GLOBALS['phpgw']->link('/tts/index.php','cd=13').'">here</a>';
+				$GLOBALS['phpgw']->common->phpgw_exit();
 			}
 		}
 	}
