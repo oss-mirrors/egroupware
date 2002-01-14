6101dbe3a0ab04f01ab64b46207d2692f5cd20a7
ceb <ceb@923e3c2e-ae13-0410-ba18-ced7211e33de>
2002-01-14 01:16:37 +0000

    removed cats_level
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@7159 923e3c2e-ae13-0410-ba18-ced7211e33de

--- bookmarks/inc/hook_preferences.inc.php
+++ bookmarks/inc/hook_preferences.inc.php
@@ -8,13 +8,12 @@
 	*  Free Software Foundation; either version 2 of the License, or (at your  *
 	*  option) any later version.                                              *
 	\**************************************************************************/
-
 	/* $Id$ */
 
 	$file = array(
 		'Import Bookmarks' => $GLOBALS['phpgw']->link('/bookmarks/import.php'),
 		'Grant Access'  => $GLOBALS['phpgw']->link('/index.php','menuaction=preferences.uiaclprefs.index&acl_app='.$appname),
-		'Edit Categories' => $GLOBALS['phpgw']->link('/index.php','menuaction=preferences.uicategories.index&cats_app='.$appname . '&cats_level=True&global_cats=True')
+		'Edit Categories' => $GLOBALS['phpgw']->link('/index.php','menuaction=preferences.uicategories.index&cats_app=' . $appname . '&global_cats=True')
 	);
 	display_section('bookmarks','Bookmarks',$file);
 ?>
