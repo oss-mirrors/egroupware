346d61f3c138629b3af8f89b251c56a1a297289c
ceb <ceb@923e3c2e-ae13-0410-ba18-ced7211e33de>
2002-01-28 00:29:11 +0000

    added hook about
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@7345 923e3c2e-ae13-0410-ba18-ced7211e33de

--- stocks/inc/hook_about.inc.php
+++ stocks/inc/hook_about.inc.php
@@ -12,12 +12,12 @@
 
 	function about_app($tpl,$handle)
 	{
-		$t = CreateObject('phpgwapi.Template',$GLOBALS['phpgw']->common->get_tpl_dir('stocks'));
-		$s = $t->set_file(array('about' => 'about.tpl'));
+		$t = CreateObject('phpgwapi.Template',$GLOBALS['phpgw']->common->get_tpl_dir('phpgwapi'));
+		$s = $t->set_file(array('about' => 'about_app.tpl'));
 		$s .= $t->set_var('app_title',lang('Stock Quotes'));
 		$s .= $t->set_var('based_on',lang('based on'));
 		$s .= $t->set_var('written_by',lang('written by'));
-		$s .= $t->set_var('source','PStocks v.0.1&nbsp;/&nbsp;Dan Steinman (dan@dansteinman.com)');
+		$s .= $t->set_var('source','PStocks v.0.1&nbsp;/&nbsp;Dan Steinman (dan@dansteinman.com)<br>http://www.dansteinman.com/php/pstocks/');
 		$s .= $t->set_var('developers','Bettina Gille&nbsp;&nbsp;[ceb@phpgroupware.org]<br>Joseph Engo&nbsp;&nbsp;&lt;jengo@phpgroupware.org&gt;');
 		$s .= $t->fp('out','about');
 		return $s;
