986902d35279a96039843280d2666a8742b86293
ceb <ceb@923e3c2e-ae13-0410-ba18-ced7211e33de>
2002-01-28 02:21:32 +0000

    update hook_about
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@7351 923e3c2e-ae13-0410-ba18-ced7211e33de

--- backup/inc/hook_about.inc.php
+++ backup/inc/hook_about.inc.php
@@ -12,11 +12,14 @@
 
 	function about_app($tpl,$handle)
 	{
-		$t = CreateObject('phpgwapi.Template',$GLOBALS['phpgw']->common->get_tpl_dir('backup'));
-		$s = $t->set_file(array('about' => 'about.tpl'));
+		$t = CreateObject('phpgwapi.Template',$GLOBALS['phpgw']->common->get_tpl_dir('phpgwapi'));
+		$s = $t->set_file(array('about' => 'about_app.tpl'));
 		$s .= $t->set_var('app_title',lang('Backup'));
+		$s .= $t->set_var('lang_version',lang('Version'));
+		$s .= $t->set_var('app_version',$GLOBALS['phpgw_info']['apps']['backup']['version']);
 		$s .= $t->set_var('written_by',lang('written by'));
 		$s .= $t->set_var('developers','Bettina Gille&nbsp;&nbsp;[ceb@phpgroupware.org]');
+		$s .= $t->set_var('description',lang('phpGroupWare data backup for sql,ldap and email'));
 		$s .= $t->fp('out','about');
 		return $s;
 	}
