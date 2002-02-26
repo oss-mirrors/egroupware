b7672871f5998b1def6bc5910aadeca06f90b6d6
ceb <ceb@923e3c2e-ae13-0410-ba18-ced7211e33de>
2002-02-26 02:16:05 +0000

    update
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@7628 923e3c2e-ae13-0410-ba18-ced7211e33de

--- felamimail/setup/setup.inc.php
+++ felamimail/setup/setup.inc.php
@@ -8,25 +8,33 @@
 	*  Free Software Foundation; either version 2 of the License, or (at your  *
 	*  option) any later version.                                              *
 	\**************************************************************************/
-
 	/* $Id$ */
 
-	$setup_info['felamimail']['name']      = 'felamimail';
-	$setup_info['felamimail']['title']     = 'FeLaMiMail';
+	$setup_info['felamimail']['name']		= 'felamimail';
+	$setup_info['felamimail']['title']		= 'FeLaMiMail';
 	$setup_info['felamimail']['version']	= '0.8.4';
-	$setup_info['felamimail']['app_order'] = 2;
-	$setup_info['felamimail']['tables']    = array(
+	$setup_info['felamimail']['app_order']	= 2;
+	$setup_info['felamimail']['tables']		= array
+	(
 		'phpgw_felamimail_cache',
 		'phpgw_felamimail_folderstatus'
 	);
-	$setup_info['felamimail']['enable']    = 1;
+
+	$setup_info['felamimail']['enable']		= 1;
 
 	/* The hooks this app includes, needed for hooks registration */
-	$setup_info['felamimail']['hooks'][] = 'preferences';
+	$setup_info['felamimail']['hooks']		= array
+	(
+		'preferences',
+		'home',
+		'about',
+		'admin'
+	);
 
 	/* Dependacies for this app to work */
-	$setup_info['felamimail']['depends'][] = array(
+	$setup_info['felamimail']['depends'][]	= array
+	(
 		'appname'  => 'phpgwapi',
-		'versions' => Array('0.9.11','0.9.12','0.9.13','0.9.14')
+		'versions' => Array('0.9.13','0.9.14','0.9.15')
 	);
 ?>
