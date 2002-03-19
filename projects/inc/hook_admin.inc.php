18f483d6907c38b98c1044e4670e9b4ecd12ccbd
ceb <ceb@923e3c2e-ae13-0410-ba18-ced7211e33de>
2002-03-19 02:35:38 +0000

    update global apps cats to turn on or off showing of global systemwide cats
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@7786 923e3c2e-ae13-0410-ba18-ced7211e33de

--- projects/inc/hook_admin.inc.php
+++ projects/inc/hook_admin.inc.php
@@ -16,7 +16,7 @@
 		(
 			'Administration'	=> $GLOBALS['phpgw']->link('/index.php','menuaction=projects.uiprojects.list_admins&action=pad'),
 			'Accountancy'		=> $GLOBALS['phpgw']->link('/index.php','menuaction=projects.uiprojects.list_admins&action=pbo'),
-			'Global Categories'	=> $GLOBALS['phpgw']->link('/index.php','menuaction=admin.uicategories.index&appname=' . $appname)
+			'Global Categories'	=> $GLOBALS['phpgw']->link('/index.php','menuaction=admin.uicategories.index&appname=' . $appname . '&global_cats=True')
 		);
 //Do not modify below this line
 		display_section($appname,$appname,$file);
