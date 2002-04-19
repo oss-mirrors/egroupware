205c6995b717da4616c7c470261d2bb49b164ae6
skeeter <skeeter@923e3c2e-ae13-0410-ba18-ced7211e33de>
2002-04-19 01:15:14 +0000

    Fix for the reading of the comic list.
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@8029 923e3c2e-ae13-0410-ba18-ced7211e33de

--- comic/inc/comic_data.inc.php
+++ comic/inc/comic_data.inc.php
@@ -12,281 +12,253 @@
 
   /* $Id$ */
 
-function comic_table($order, $sort, $filter, $start, $query, $qfield, &$table_c)
-{
-    global $phpgw, $phpgw_info, $g_censor_level;
-    
-    $edit_label   = lang("Edit");
-    $delete_label = lang("Delete");
-    $searchobj = array(array("data_title",     "Title"),
-                       array("data_class",     "Genre"),
-                       array("data_censorlvl", "Rated"),
-                       array("data_parser",    "Parser"),
-                       array("data_resolve",   "Resolve"));
-    if ($order)
-    {
-        $ordermethod = "order by $order $sort ";
-    }
-    else
-    {
-        $ordermethod = "order by data_title asc ";
-    }
-    
-    if (! $sort)
-    {
-        $sort = "desc";
-    }
-    
-    if (! $start)
-    {
-        $start = 0;
-    }
-    
-    if (! $filter)
-    {
-        $filter = "none";
-    }
-    
-    $limit = $phpgw->db->limit($start);
+	function get_db_var($field)
+	{
+		$new_var = $GLOBALS['phpgw']->db->f($field);
+		if(!$new_var)
+		{
+			$new_var = '&nbsp;';
+		}
+		return $new_var;
+	}
 
-    $likeness = "like";
-    $myquery  = "%$query%";
+	function comic_table($order, $sort, $filter, $start, $query, $qfield, &$table_c)
+	{
+		$edit_label   = lang('Edit');
+		$delete_label = lang('Delete');
+		$searchobj = Array(
+			Array(
+				'data_title',
+				'Title'
+			),
+			Array(
+				'data_class',
+				'Genre'
+			),
+			Array(
+				'data_censorlvl',
+				'Rated'
+			),
+			Array(
+				'data_parser',
+				'Parser'
+			),
+			Array(
+				'data_resolve',
+				'Resolve'
+			)
+		);
+                       
+		if(!$sort)
+		{
+			$sort = 'desc';
+		}
     
-    if ($qfield == "data_censorlvl")
-    {
-        while(list($key,$value) = each($g_censor_level))
-        {
-            if (ucwords($query) == $value)
-            {
-                $myquery = $key;
-                $likeness = "=";
-                break;
-            }
-        }
-    }
+		if($order)
+		{
+			$ordermethod = "order by $order $sort ";
+		}
+		else
+		{
+			$ordermethod = "order by data_title asc ";
+		}
     
-    if (!$query)
-    {
-        $phpgw->db->query("select count(*) from phpgw_comic_data "
-                          .$ordermethod);
-    }
-    else
-    {
-        $phpgw->db->query("select count(*) from phpgw_comic_data "
-                          ."WHERE $qfield $likeness '$myquery' "
-                          .$ordermethod);
-    }
+		if(!$start)
+		{
+			$start = 0;
+		}
     
-    $phpgw->db->next_record();
+		if(!$filter)
+		{
+			$filter = 'none';
+		}
+    
+		$likeness = 'like';
+		$myquery  = '%'.$query.'%';
+    
+		if ($qfield == 'data_censorlvl')
+		{
+			while(list($key,$value) = each($GLOBALS['g_censor_level']))
+			{
+				if(ucwords($query) == $value)
+				{
+					$myquery = $key;
+					$likeness = '=';
+					break;
+				}
+			}
+		}
+    
+		$sql_query = 'select count(*) from phpgw_comic_data';
 
-    if ($phpgw->db->f(0) >
-        $phpgw_info["user"]["preferences"]["common"]["maxmatchs"])
-    {
-        $match_comment = 
-            lang("showing x - x of x",($start + 1),
-                 ($start +
-                  $phpgw_info["user"]["preferences"]["common"]["maxmatchs"]),
-                 $phpgw->db->f(0));
-    }
-    else
-    {
-        $match_comment = lang("showing x",$phpgw->db->f(0));
-    }
+		$GLOBALS['phpgw']->db->query($sql_query,__LINE__,__FILE__);
     
-    $match_bar =
-        $phpgw->nextmatchs->show_tpl("/comic/admin_comics.php",
-                                     $start,$phpgw->db->f(0), "",
-                                     "85%", $phpgw_info["theme"]["th_bg"],
-                                     $searchobj,0);
-    $comic_label = 
-        $phpgw->nextmatchs->show_sort_order($sort,"data_title",$order,
-                                            "/comic/admin_comics.php",
-                                            lang("Title"));
-    $comic_parser_label = 
-        $phpgw->nextmatchs->show_sort_order($sort,"data_parser",$order,
-                                            "/comic/admin_comics.php",
-                                            lang("Parser"));
-    $comic_resolve_label = 
-        $phpgw->nextmatchs->show_sort_order($sort,"data_resolve",$order,
-                                            "/comic/admin_comics.php",
-                                            lang("Resolve"));
-    $comic_class_label = 
-        $phpgw->nextmatchs->show_sort_order($sort,"data_class",$order,
-                                            "/comic/admin_comics.php",
-                                            lang("Genre"));
+		$GLOBALS['phpgw']->db->next_record();
 
-    $comic_censor_label = 
-        $phpgw->nextmatchs->show_sort_order($sort,"data_censorlvl",$order,
-                                            "/comic/admin_comics.php",
-                                            lang("Rated"));
-     
-    if (! $query)
-    {
-        $phpgw->db->query("select * from phpgw_comic_data "
-                          .$ordermethod
-                          .$limit);
-    }
-    else
-    {
-        $phpgw->db->query("select * from phpgw_comic_data "
-                          ."WHERE $qfield $likeness '$myquery' "
-                          .$ordermethod
-                          .$limit);
-    }
+		$total_records = $GLOBALS['phpgw']->db->f(0);
+
+		if ($total_records > $GLOBALS['phpgw_info']['user']['preferences']['common']['maxmatchs'])
+		{
+			if($start + $GLOBALS['phpgw_info']['user']['preferences']['common']['maxmatchs'] > $total_records)
+			{
+				$max_turn = $total_records;
+			}
+			else
+			{
+				$max_turn = $start + $GLOBALS['phpgw_info']['user']['preferences']['common']['maxmatchs'];
+			}
+			$match_comment = lang('showing x - x of x',($start + 1),$max_turn,$total_records);
+		}
+		else
+		{
+			$match_comment = lang('showing x',$total_records);
+		}
     
-    $table_tpl =
-        CreateObject('phpgwapi.Template',
-                     $phpgw->common->get_tpl_dir('comic'));
-    $table_tpl->set_unknowns("remove");
-    $table_tpl->set_file(array(table => "table.comics.tpl",
-                               row   => "row.comics.tpl"));
+		if(!$query)
+		{
+			$sql_query = 'select * from phpgw_comic_data '.$ordermethod;
+		}
+		else
+		{
+			$sql_query = 'select * from phpgw_comic_data WHERE '.$qfield.' '.$likeness." '$myquery' ".$ordermethod;
+		}
+
+		$GLOBALS['phpgw']->db->limit_query($sql_query,intval($start),__LINE__,__FILE__);
     
-    while ($phpgw->db->next_record()) 
-    {
-        $tr_color = $phpgw->nextmatchs->alternate_row_color($tr_color);
+		$table_tpl = CreateObject('phpgwapi.Template',$GLOBALS['phpgw']->common->get_tpl_dir('comic'));
+		$table_tpl->set_unknowns('remove');
+		$table_tpl->set_file(
+			Array(
+				'table' => 'table.comics.tpl',
+				'row'   => 'row.comics.tpl'
+			)
+		);
+    
+		while ($GLOBALS['phpgw']->db->next_record()) 
+		{
+			$tr_color = $GLOBALS['phpgw']->nextmatchs->alternate_row_color($tr_color);
         
-        $comic_id = $phpgw->db->f("data_id");
-        $comic_encoded = urlencode($comic_id);
+			$comic_id = $GLOBALS['phpgw']->db->f('data_id');
+			$comic_encoded = urlencode($comic_id);
         
-        $comic_title = $phpgw->db->f("data_title");
-        if (! $comic_title)
-        {
-            $comic_title = "&nbsp;";
-        }
-
-        $comic_parser = $phpgw->db->f("data_parser");
-        if (! $comic_parser)
-        {
-            $comic_parser = "&nbsp;";
-        }
-
-        $comic_resolve = $phpgw->db->f("data_resolve");
-        if (! $comic_resolve)
-        {
-            $comic_resolve = "&nbsp;";
-        }
-
-        $comic_class = $phpgw->db->f("data_class");
-        if (! $comic_class)
-        {
-            $comic_class = "&nbsp;";
-        }
-
-        $comic_censor = $g_censor_level[$phpgw->db->f("data_censorlvl")];
+			$comic_censor = $GLOBALS['g_censor_level'][$GLOBALS['phpgw']->db->f('data_censorlvl')];
         
-        $table_tpl->
-            set_var(
-                array(row_color    => $tr_color,
-                      comic_name   => $comic_title,
-                      comic_parser => $comic_parser,
-                      comic_resolve=> $comic_resolve,
-                      comic_class  => $comic_class,
-                      comic_censor => $comic_censor,
-                      edit_url     => $phpgw->link("/comic/admin_comics.php",
-                                                   "con=".$comic_encoded
-                                                   ."&act=edit"
-                                                   ."&start=$start"
-                                                   ."&order=$order"
-                                                   ."&filter=$filter"
-                                                   ."&sort=$sort"
-                                                   ."&query="
-                                                   .urlencode($query) 
-                                                   ."&qfield=$qfield"),
-                      edit_label   => $edit_label,
-                      delete_url   => $phpgw->link("/comic/admin_comics.php",
-                                                   "con=".$comic_encoded
-                                                   ."&act=delete"
-                                                   ."&start=$start"
-                                                   ."&order=$order"
-                                                   ."&filter=$filter"
-                                                   ."&sort=$sort"
-                                                   ."&query="
-                                                   .urlencode($query) 
-                                                   ."&qfield=$qfield"),
-                      delete_label => $delete_label));
-        $table_tpl->parse(comic_rows, "row", True);
-    }
+			$table_tpl->set_var(
+				Array(
+					'row_color'    => $tr_color,
+					'comic_name'   => get_db_var('data_title'),
+					'comic_parser' => get_db_var('data_parser'),
+					'comic_resolve'=> get_db_var('data_resolve'),
+					'comic_class'  => get_db_var('data_class'),
+					'comic_censor' => $comic_censor,
+					'edit_url'     => $GLOBALS['phpgw']->link('/comic/admin_comics.php',
+							Array(
+								'con'    => $comic_encoded,
+								'act'    => 'edit',
+								'start'  => $start,
+								'order'  => $order,
+								'filter' => $filter,
+								'sort'   => $sort,
+								'query'  => urlencode($query),
+								'qfield' => $qfield
+							)
+						),
+					'edit_label'   => $edit_label,
+					'delete_url'   => $GLOBALS['phpgw']->link('/comic/admin_comics.php',
+							Array(
+								'con'    => $comic_encoded,
+								'act'    => 'delete',
+								'start'  => $start,
+								'order'  => $order,
+								'filter' => $filter,
+								'sort'   => $sort,
+								'query'  => urlencode($query),
+								'qfield' => $qfield
+							)
+						),
+					'delete_label' => $delete_label
+				)
+			);
+			$table_tpl->parse(comic_rows, 'row', True);
+		}
     
-    $table_tpl->
-        set_var(array
-                (th_bg                => $phpgw_info["theme"]["th_bg"],
+		$table_tpl->set_var(
+			Array(
+				'th_bg'                => $GLOBALS['phpgw_info']['theme']['th_bg'],
+				'total_matchs'         => $match_comment,
+				'next_matchs'          => $GLOBALS['phpgw']->nextmatchs->show_tpl('/comic/admin_comics.php',$start,$total_records, '','85%', $GLOBALS['phpgw_info']['theme']['th_bg'],$searchobj,0),
+				'comic_label'          => $GLOBALS['phpgw']->nextmatchs->show_sort_order($sort,'data_title',$order,'/comic/admin_comics.php',lang('Title')),
+				'comic_parser_label'   => $GLOBALS['phpgw']->nextmatchs->show_sort_order($sort,'data_parser',$order,'/comic/admin_comics.php',lang('Parser')),
+				'comic_resolve_label'  => $GLOBALS['phpgw']->nextmatchs->show_sort_order($sort,'data_resolve',$order,'/comic/admin_comics.php',lang('Resolve')),
+				'comic_class_label'    => $GLOBALS['phpgw']->nextmatchs->show_sort_order($sort,'data_class',$order,'/comic/admin_comics.php',lang('Genre')),
+				'comic_censor_label'   => $GLOBALS['phpgw']->nextmatchs->show_sort_order($sort,'data_censorlvl',$order,'/comic/admin_comics.php',lang('Rated')),
+				'edit_label'           => $edit_label,
+				'delete_label'         => $delete_label,
+				'action_url'           => $action_url,
+				'action_label'         => lang($act),
+				'reset_label'          => lang('Reset')
+			)
+		);
 
-                 total_matchs         => $match_comment,
-                 next_matchs          => $match_bar,
-                 
-                 comic_label          => $comic_label,
-                 comic_parser_label   => $comic_parser_label,
-                 comic_resolve_label  => $comic_resolve_label,
-                 comic_class_label    => $comic_class_label,
-                 comic_censor_label   => $comic_censor_label,
-                 
-                 edit_label           => $edit_label,
-                 delete_label         => $delete_label,
-                 
-                 action_url           => $action_url,
-                 action_label         => lang($act),
-                 reset_label          => lang("Reset")));
+		$table_tpl->parse(table_part, 'table');
+		$table_c = $table_tpl->get('table_part');
+	}
+
+	function comic_entry($con, $act, $order, $sort, $filter, $start, $query, $qfield, &$form_c)
+	{
+		$action_url   = $GLOBALS['phpgw']->link('/comic/admin_comics.php',
+			Array(
+				'act'     => $act,
+				'start'   => $start,
+				'order'   => $order,
+				'filter'  => $filter,
+				'sort'    => $sort,
+				'query'   => urlencode($query),
+				'qfield'  => $qfield
+			)
+		);
     
-    $table_tpl->parse(table_part, "table");
-    $table_c = $table_tpl->get("table_part");
+		switch($act)
+		{
+			case 'add':
+				$bg_color = $GLOBALS['phpgw_info']['theme']['th_bg'];
+				break;
+			case 'delete':
+				$bg_color = $GLOBALS['phpgw_info']['theme']['bg07'];
+				break;
+			default:
+				$bg_color = $GLOBALS['phpgw_info']['theme']['table_bg'];
+				break;
+		}
+
+		$comic_name = '';
+    
+		if($con!='')
+		{
+			$GLOBALS['phpgw']->db->query('select * from phpgw_comic_data where data_id='.$con);
+
+			$GLOBALS['phpgw']->db->next_record();
+
+			$comic_name = $GLOBALS['phpgw']->db->f('data_title');
+		}
+        
+		$modify_tpl = CreateObject('phpgwapi.Template',$GLOBALS['phpgw']->common->get_tpl_dir('comic'));
+		$modify_tpl->set_unknowns('remove');
+		$modify_tpl->set_file(form, 'form.comics.tpl');
+    
+		$modify_tpl->set_var(
+			Array(
+				'bg_color'        => $bg_color,
+				'comic_id'        => $con,
+				'comic_label'     => lang('Title'),
+				'comic_name'      => $comic_name,
+				'action_url'      => $action_url,
+				'action_label'    => lang($act),
+				'reset_label'     => lang('Reset')
+			)
+		);
+    
+		$modify_tpl->parse(form_part, 'form');
+		$form_c = $modify_tpl->get('form_part');
 }
-
-
-function comic_entry($con, $act, $order, $sort, $filter, $start, $query, $qfield, &$form_c)
-{
-    global $phpgw, $phpgw_info;
-
-    $action_url   = $phpgw->link("/comic/admin_comics.php",
-                                 "act=$act"
-                                 ."&start=$start&order=$order&filter=$filter"
-                                 ."&sort=$sort"
-                                 ."&query=".urlencode($query) 
-                                 ."&qfield=$qfield");
-    
-    switch($act)
-    {
-      case "add":
-        $bg_color = $phpgw_info["theme"]["th_bg"];
-        break;
-      case "delete":
-        $bg_color = $phpgw_info["theme"]["bg07"];
-        break;
-      default:
-        $bg_color = $phpgw_info["theme"]["table_bg"];
-        break;
-    }
-
-    $comic_name = "";
-    
-    if ($con != "")
-    {
-        $phpgw->db->query("select * from phpgw_comic_data where data_id=$con");
-
-        $phpgw->db->next_record();
-
-        $comic_name = $phpgw->db->f("data_title");
-    }
-        
-    $modify_tpl =
-        CreateObject('phpgwapi.Template',
-                     $phpgw->common->get_tpl_dir('comic'));
-    $modify_tpl->set_unknowns("remove");
-    $modify_tpl->set_file(form, "form.comics.tpl");
-    
-    $modify_tpl->
-        set_var(array
-                (bg_color         => $bg_color,
-                 
-                 comic_id        => $con,
-                 comic_label     => lang("Title"),
-                 comic_name      => $comic_name,
-                 
-                 action_url       => $action_url,
-                 action_label     => lang($act),
-                 reset_label      => lang("Reset")));
-    
-    $modify_tpl->parse(form_part, "form");
-    $form_c = $modify_tpl->get("form_part");
-}
-
 ?>
