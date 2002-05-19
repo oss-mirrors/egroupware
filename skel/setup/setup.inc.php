ccf8838e858f37097a996ef31c4d53c2b8f5b613
milosch <milosch@923e3c2e-ae13-0410-ba18-ced7211e33de>
2002-05-19 19:20:21 +0000

    formatting
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@8262 923e3c2e-ae13-0410-ba18-ced7211e33de

--- skel/setup/setup.inc.php
+++ skel/setup/setup.inc.php
@@ -1,15 +1,15 @@
 <?php
-	/**************************************************************************\
-	* phpGroupWare                                                             *
-	* http://www.phpgroupware.org                                              *
-	* --------------------------------------------                             *
-	*  This program is free software; you can redistribute it and/or modify it *
-	*  under the terms of the GNU General Public License as published by the   *
-	*  Free Software Foundation; either version 2 of the License, or (at your  *
-	*  option) any later version.                                              *
-	\**************************************************************************/
+  /**************************************************************************\
+  * phpGroupWare                                                             *
+  * http://www.phpgroupware.org                                              *
+  * --------------------------------------------                             *
+  *  This program is free software; you can redistribute it and/or modify it *
+  *  under the terms of the GNU General Public License as published by the   *
+  *  Free Software Foundation; either version 2 of the License, or (at your  *
+  *  option) any later version.                                              *
+  \**************************************************************************/
 
-	/* $Id$ */
+  /* $Id$ */
 
 	/* Basic information about this app */
 	$setup_info['skel']['name']      = 'skel';
@@ -30,9 +30,9 @@
 
 	/* Dependencies for this app to work */
 	$setup_info['skel']['depends'][] = array(
-			 'appname' => 'phpgwapi',
-			 'versions' => Array('0.9.13', '0.9.14','0.9.15')
-		);
+		'appname' => 'phpgwapi',
+		'versions' => Array('0.9.14', '0.9.15','0.9.16')
+	);
 	$setup_info['skel']['depends'][] = array(
 		'appname' => 'email',
 		'versions' => Array('0.9.13', '0.9.14' , '0.9.15', '0.9.16')
