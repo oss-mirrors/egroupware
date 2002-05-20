dff4ccbbdbaaab608189b668227280ae6116416b
milosch <milosch@923e3c2e-ae13-0410-ba18-ced7211e33de>
2002-05-20 01:25:21 +0000

    fix link to old config.php
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@8273 923e3c2e-ae13-0410-ba18-ced7211e33de

--- felamimail/inc/hook_admin.inc.php
+++ felamimail/inc/hook_admin.inc.php
@@ -15,7 +15,7 @@
 // Only Modify the $file and $title variables.....
 	$title = $appname;
 	$file = Array(
-		'Site Configuration'	=> $GLOBALS['phpgw']->link('/admin/config.php','appname=email')
+		'Site Configuration' => $GLOBALS['phpgw']->link('/index.php','menuaction=admin.uiconfig.index&appname=email')
 	);
 //Do not modify below this line
 	display_section($appname,$title,$file);
