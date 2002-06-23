87037b70598ef1b3cd5c129452b306fdae3e4a9f
skeeter <skeeter@923e3c2e-ae13-0410-ba18-ced7211e33de>
2002-06-23 17:03:27 +0000

    GNU Patch #323 - modified.
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@8447 923e3c2e-ae13-0410-ba18-ced7211e33de

--- tts/inc/hook_home.inc.php
+++ tts/inc/hook_home.inc.php
@@ -1,6 +1,6 @@
 <?php
   /**************************************************************************\
-  * phpGroupWare - E-Mail                                                    *
+  * phpGroupWare - TTS                                                       *
   * http://www.phpgroupware.org                                              *
   * --------------------------------------------                             *
   *  This program is free software; you can redistribute it and/or modify it *
@@ -14,56 +14,64 @@
 	$d1 = strtolower(substr($GLOBALS['phpgw_info']['server']['app_inc'],0,3));
 	if($d1 == 'htt' || $d1 == 'ftp' )
 	{
-		echo "Failed attempt to break in via an old Security Hole!<br>\n";
+		echo 'Failed attempt to break in via an old Security Hole!<br>'."\n";
 		$GLOBALS['phpgw']->common->phpgw_exit();
 	}
 	unset($d1);
 
-	$tmp_app_tpl = $GLOBALS['phpgw']->common->get_tpl_dir('tts');
-
-	if ($GLOBALS['phpgw_info']["user"]["apps"]["tts"]
-		&& $GLOBALS['phpgw_info']["user"]["preferences"]["tts"]["mainscreen_show_new_updated"])
+	if ($GLOBALS['phpgw_info']['user']['apps']['tts']
+		&& $GLOBALS['phpgw_info']['user']['preferences']['tts']['mainscreen_show_new_updated'])
 	{
-		echo "\n<!-- Begin TTS New/Updated -->\n";
+		$GLOBALS['phpgw']->translation->add_app('tts');
+
+		$db2 = $GLOBALS['phpgw']->db;
+		$GLOBALS['phpgw']->historylog = createobject('phpgwapi.historylog','tts');
 
 		// this will be an user option
 		$filtermethod="where ticket_status='O' and ticket_assignedto='".$GLOBALS['phpgw_info']['user']['account_id']."'";
-		$sortmethod="order by ticket_priority desc";
+		$sortmethod='order by ticket_priority desc';
 
-		$GLOBALS['phpgw']->db->query("select ticket_id,ticket_category,ticket_priority,ticket_assignedto,"
-			. "ticket_owner,ticket_subject from phpgw_tts_tickets $filtermethod $sortmethod",__LINE__,__FILE__);
+		$GLOBALS['phpgw']->db->query('select ticket_id,ticket_category,ticket_priority,ticket_assignedto,ticket_owner,ticket_subject '
+			. 'from phpgw_tts_tickets ' . $filtermethod . ' ' . $sortmethod,__LINE__,__FILE__);
 
+		$tmp_app_tpl = $GLOBALS['phpgw']->common->get_tpl_dir('tts');
 		$p = CreateObject('phpgwapi.Template',$tmp_app_tpl);
-		// echo PHPGW_APP_TPL;
 		$p->set_file('index','hook_home.tpl');
 
 		$p->set_block('index', 'tts_list', 'tts_list');
 		$p->set_block('index', 'tts_row', 'tts_row');
 		$p->set_block('index', 'tts_ticket_id_read', 'tts_ticket_id_read');
 		$p->set_block('index', 'tts_ticket_id_unread', 'tts_ticket_id_unread');
-
+		$p->set_var(
+			Array(
+				'tts_head_id'			=> lang('Ticket').' #',
+				'tts_head_openedby'		=> lang('Opened by'),
+				'tts_head_dateopened'	=> lang('Date opened'),
+				'tts_head_subject'		=> lang('Subject')
+			)
+		);
 		while ($GLOBALS['phpgw']->db->next_record())
 		{
 
-			$p->set_var('tts_col_status',"");
+			$p->set_var('tts_col_status','');
 			$priority=$GLOBALS['phpgw']->db->f('ticket_priority');
-			switch ($priority)
+			if($priority >= 1 && $priority <= 9)
 			{
-				case 1:  $tr_color = $GLOBALS['phpgw_info']["theme"]["bg01"]; break;
-				case 2:  $tr_color = $GLOBALS['phpgw_info']["theme"]["bg02"]; break;
-				case 3:  $tr_color = $GLOBALS['phpgw_info']["theme"]["bg03"]; break;
-				case 4:  $tr_color = $GLOBALS['phpgw_info']["theme"]["bg04"]; break;
-				case 5:  $tr_color = $GLOBALS['phpgw_info']["theme"]["bg05"]; break;
-				case 6:  $tr_color = $GLOBALS['phpgw_info']["theme"]["bg06"]; break;
-				case 7:  $tr_color = $GLOBALS['phpgw_info']["theme"]["bg07"]; break;
-				case 8:  $tr_color = $GLOBALS['phpgw_info']["theme"]["bg08"]; break;
-				case 9:  $tr_color = $GLOBALS['phpgw_info']["theme"]["bg09"]; break;
-				case 10: $tr_color = $GLOBALS['phpgw_info']["theme"]["bg10"]; break;
-				default: $tr_color = $GLOBALS['phpgw_info']["theme"]["bg_color"];
+				$tr_color = $GLOBALS['phpgw_info']['theme']['bg0'.$priority];
+			}
+			elseif($priority==10)
+			{
+				$tr_color = $GLOBALS['phpgw_info']['theme']['bg10'];
+			}
+			else
+			{
+				$tr_color = $GLOBALS['phpgw_info']['theme']['bg_color'];
 			}
 
-			$t_watchers=explode(":",$GLOBALS['phpgw']->db->f("t_watchers"));
-			if (in_array( $GLOBALS['phpgw_info']["user"]["userid"], $t_watchers))
+			$db2->query("select count(*) from phpgw_tts_views where view_id='" . $GLOBALS['phpgw']->db->f('ticket_id')
+				. "' and view_account_id='" . $GLOBALS['phpgw_info']['user']['account_id'] . "'",__LINE__,__FILE__);
+			$db2->next_record();
+			if($db2->f(0))
 			{
 				$t_read=1;
 			}
@@ -72,7 +80,7 @@
 				$t_read=0;
 			}
 			$p->set_var('tts_row_color', $tr_color );
-			$p->set_var('tts_ticketdetails_link', $GLOBALS['phpgw']->link("/tts/viewticket_details.php","ticketid=" . $GLOBALS['phpgw']->db->f('ticket_id')));
+			$p->set_var('tts_ticketdetails_link', $GLOBALS['phpgw']->link('/tts/viewticket_details.php','ticket_id=' . $GLOBALS['phpgw']->db->f('ticket_id')));
 
 			$p->set_var('tts_t_id',$GLOBALS['phpgw']->db->f('ticket_id') );
 
@@ -85,12 +93,15 @@
 				$p->fp('tts_ticket_id', 'tts_ticket_id_read', false );
 			}
 
-			$p->set_var('tts_t_user', $GLOBALS['phpgw']->db->f('ticket_user'));
-			$p->set_var('tts_t_timestampopened', $GLOBALS['phpgw']->common->show_date($GLOBALS['phpgw']->db->f("t_timestamp_opened")));
+			$p->set_var('tts_t_user', $GLOBALS['phpgw']->accounts->id2name($GLOBALS['phpgw']->db->f('ticket_owner')));
+			$history_values = $GLOBALS['phpgw']->historylog->return_array(array(),array('O'),'history_timestamp','ASC',$GLOBALS['phpgw']->db->f('ticket_id'));
+			$p->set_var('tts_t_timestampopened',$GLOBALS['phpgw']->common->show_date($history_values[0]['datetime'] - ((60*60) * $GLOBALS['phpgw_info']['user']['preferences']['common']['tz_offset'])));
 			$p->set_var('tts_t_subject', $GLOBALS['phpgw']->db->f('ticket_subject'));
 
 			$p->fp('rows','tts_row',true);
 		}
+		
+		$extra_data = '<td>'."\n".$p->fp('out','tts_list').'</td>'."\n";
 		
 		$portalbox = CreateObject('phpgwapi.listbox',
 			array(
@@ -100,7 +111,7 @@
 				'tertiary'  => $GLOBALS['phpgw_info']['theme']['navbar_bg'],
 				'width'     => '100%',
 				'outerborderwidth' => '0',
-				'header_background_image' => $GLOBALS['phpgw']->common->image('phpgwapi/templates/phpgw_website','bg_filler.gif')
+				'header_background_image' => $GLOBALS['phpgw']->common->image('phpgwapi/templates/phpgw_website','bg_filler')
 			)
 		);
 
@@ -119,10 +130,6 @@
 			$portalbox->set_controls($key,$value);
 		}
 		$portalbox->data = array();
-		$portalbox->draw($p->fp('out','tts_list'));
-
-echo 'TEST -&gt;&gt;' . $p->fp('out','tts_list') . '&lt;&lt;-';
-//		$p->pfp('out','tts_list');
-		echo "\n<!-- End TTS New/Updated -->\n";
+		echo "\n".'<!-- Begin TTS New/Updated -->'."\n".$portalbox->draw($extra_data)."\n".'<!-- End TTS New/Updated -->'."\n";
 	}
 ?>
--- tts/templates/default/hook_home.tpl
+++ tts/templates/default/hook_home.tpl
@@ -1,23 +1,23 @@
 <!-- $Id$ -->
 <!-- BEGIN tts_list -->
-<center><br />
-  <table CELLSPACING=1 CELLPADDING=1 BORDER=0>
+  <table width="100%" cellspacing="1" cellpadding="1" border="0">
    <tr bgcolor="{tts_head_bgcolor}">
-    <td align=center>{tts_head_openedby}</td>
-    <td align=center>{tts_head_dateopened}</td>
-    <td align=center>{tts_head_subject}</td>
+    <td align="center">{tts_head_id}</td>
+    <td align="center">{tts_head_openedby}</td>
+    <td align="center">{tts_head_dateopened}</td>
+    <td align="center">{tts_head_subject}</td>
    </tr>
    {rows}
   </table>
-</center>
 <!-- END tts_list -->
 
 <!-- BEGIN tts_row -->
    <tr bgcolor="{tts_row_color}">
-     <td align=center>{tts_t_user}</td>
-     <td align=center>{tts_t_timestampopened}</td>
+     <td align="center">{tts_ticket_id}</td>
+     <td align="center">{tts_t_user}</td>
+     <td align="center">{tts_t_timestampopened}</td>
      {tts_col_status}
-     <td align=center><a href="{tts_ticketdetails_link}">{tts_t_subject}</a></td>
+     <td align="center"><a href="{tts_ticketdetails_link}">{tts_t_subject}</a></td>
    </tr>
 <!-- END tts_row -->
 
