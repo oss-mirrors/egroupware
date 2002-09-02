28f6c982ae59249a842050754deb6ba7dc29f435
ceb <ceb@923e3c2e-ae13-0410-ba18-ced7211e33de>
2002-09-02 22:02:04 +0000

    update cats to use class.categories
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@8796 923e3c2e-ae13-0410-ba18-ced7211e33de

--- sitemgr/inc/class.Categories_SO.inc.php
+++ sitemgr/inc/class.Categories_SO.inc.php
@@ -1,60 +1,83 @@
 <?php
 	class Categories_SO
 	{
-		var $db;
-		
+		var $cats;
+
 		function Categories_SO()
 		{
-			$this->db = $GLOBALS['phpgw']->db;
+			$this->cats = CreateObject('phpgwapi.categories',-1,'sitemgr');
+		}
+
+		function getFullChildrenIDList($parent = '')
+		{
+			if (!$parent)
+			{
+				$parent = 0;
+			}
+
+			$cats = $this->cats->return_array('all','',False,'','cat_data','',False, $parent)
+
+			while (is_array($cats) && list(,$subs) = each($cats))
+			{
+				$subs_id_list[] = $subs['cat_id'];
+			}
+			return $subs_id_list;
 		}
 
 		function getFullCategoryIDList()
 		{
-			$sql = 'SELECT cat_id FROM phpgw_sitemgr_categories ORDER BY sort_order';
-			$this->db->query($sql, __LINE__, __FILE__);
-			while ($this->db->next_record())
+			$cats = $this->cats->return_array('all','',False,'','cat_data','',False)
+
+			while (is_array($cats) && list(,$cat) = each($cats))
 			{
-				$cat_id_list[] = $this->db->f('cat_id');
+				$cat_id_list[] = $cat['cat_id'];
 			}
 			return $cat_id_list;
 		}
 
-		function addCategory($name, $description)
+		function addCategory($name, $description, $parent = '')
 		{
-			//Create a section for categoriy and return the newly added category id.
-			$sql = 'INSERT INTO phpgw_sitemgr_categories (name, description) VALUES ("'
-				. $name . '","' . $description . '")';
-			$this->db->query($sql, __LINE__, __FILE__);
-			return $this->db->get_last_insert_id('phpgw_sitemgr_categories','cat_id');
+			$data = array
+			(
+				'name'		=> $name,
+				'descr'		=> $description,
+				'access'	=> 'public',
+				'parent'	=> $parent
+			);
+
+			return $this->cats->add($data);
 		}
 
-		function removeCategory($cat_id)
+		function removeCategory($cat_id, $subs = False)
 		{
-			$sql = 'DELETE FROM phpgw_sitemgr_categories WHERE cat_id="' . $cat_id . '"';
-			$this->db->query($sql, __LINE__, __FILE__);
+			$this->cats->delete($cat_id,$subs);
 			return true;
 		}
 
 		function saveCategory($cat_info)
 		{
-			$sql = 'UPDATE phpgw_sitemgr_categories SET name="' .
-				$cat_info->name . '", description="' . $cat_info->description .
-				'", sort_order="'. (int) $cat_info->sort_order .
-				'" WHERE cat_id="' . $cat_info->id . '"';
-			$this->db->query($sql, __LINE__, __FILE__);
+			$data = array
+			(
+				'name'	=> $cat_info->name,
+				'descr'	=> $cat_info->description,
+				'data'	=> (int) $cat_info->sort_order,
+				'id'	=> $cat_info->id
+			);
+
+			$this->cats->edit($data);
 		}
 
 		function getCategory($cat_id)
 		{
-			$sql = 'SELECT * FROM phpgw_sitemgr_categories WHERE cat_id="' . $cat_id . '"';
-			$this->db->query($sql, __LINE__, __FILE__);
-			if ($this->db->next_record())
+			$cat = $this->cats->return_single($cat_id);
+
+			if (is_array($cat))
 			{
-				$cat_info = CreateObject('sitemgr.Category_SO', True);
-				$cat_info->id = $cat_id;
-				$cat_info->name = $this->db->f('name');
-				$cat_info->sort_order = $this->db->f('sort_order');
-				$cat_info->description = $this->db->f('description');
+				$cat_info				= CreateObject('sitemgr.Category_SO', True);
+				$cat_info->id			= $cat[0]['id'];
+				$cat_info->name			= $cat[0]['name'];
+				$cat_info->sort_order	= $cat[0]['data'];
+				$cat_info->description	= $cat[0]['descr'];
 				return $cat_info;
 			}
 			else
