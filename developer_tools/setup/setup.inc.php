1a2f48949c5b21a32059addcc73f41956fba7e46
ceb <ceb@923e3c2e-ae13-0410-ba18-ced7211e33de>
2002-11-15 22:31:51 +0000

    update
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@9398 923e3c2e-ae13-0410-ba18-ced7211e33de

--- developer_tools/setup/setup.inc.php
+++ developer_tools/setup/setup.inc.php
@@ -8,37 +8,40 @@
 	*  Free Software Foundation; either version 2 of the License, or (at your  *
 	*  option) any later version.                                              *
 	\**************************************************************************/
-
 	/* $Id$ */
 
 	/* Basic information about this app */
-	$setup_info['developer_tools']['name']      = 'developer_tools';
-	$setup_info['developer_tools']['version']   = '0.8.1';
-	$setup_info['developer_tools']['app_order'] = 8;
-	$setup_info['developer_tools']['enable']    = 1;
+	$setup_info['developer_tools']['name']				= 'developer_tools';
+	$setup_info['developer_tools']['version']			= '0.8.1';
+	$setup_info['developer_tools']['app_order']			= 8;
+	$setup_info['developer_tools']['enable']			= 1;
 
-	$setup_info['developer_tools']['author'] = 'Miles Lott';
-	$setup_info['developer_tools']['note'] = '';
-	$setup_info['developer_tools']['license']  = 'GPL';
-	$setup_info['developer_tools']['description'] =
-		'Contains the language management system.';
-	$setup_info['developer_tools']['maintainer'] = 'Miles Lott';
-	$setup_info['developer_tools']['maintainer_email']    = 'milosch@phpgroupware.org';
+	$setup_info['developer_tools']['author']			= 'Joseph Engo';
+	$setup_info['developer_tools']['note']				= '';
+	$setup_info['developer_tools']['license']			= 'GPL';
+	$setup_info['developer_tools']['description']		= 'Contains the language management system.';
+	$setup_info['developer_tools']['maintainer']		= 'Joseph Engo';
+	$setup_info['developer_tools']['maintainer_email']	= 'jengo@phpgroupware.org';
 
 	/* The tables this app creates */
-	$setup_info['developer_tools']['tables']    = array(
+	$setup_info['developer_tools']['tables'] = array
+	(
 		'phpgw_devtools_diary',
 		'phpgw_devtools_sf_cache',
 		'phpgw_devtools_changelogs'
 	);
 
 	/* The hooks this app includes, needed for hooks registration */
-	$setup_info['developer_tools']['hooks'][] = 'admin';
-	$setup_info['developer_tools']['hooks'][] = 'preferences';
+	$setup_info['developer_tools']['hooks'][] = array
+	(
+		'admin',
+		'preferences'
+	);
 
 	/* Dependencies for this app to work */
-	$setup_info['developer_tools']['depends'][] = array(
+	$setup_info['developer_tools']['depends'][] = array
+	(
 		 'appname' => 'phpgwapi',
-		 'versions' => Array('0.9.13', '0.9.14','0.9.15')
+		 'versions' => Array('0.9.15')
 	);
 ?>
