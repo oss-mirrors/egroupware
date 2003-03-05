4bd4a15d64994b20c3538c637712ca183b189757
ralfbecker <ralfbecker@923e3c2e-ae13-0410-ba18-ced7211e33de>
2003-03-05 12:17:22 +0000

    update for pgsql
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@9869 923e3c2e-ae13-0410-ba18-ced7211e33de

--- wiki/setup/default_records.inc.php
+++ wiki/setup/default_records.inc.php
@@ -13,18 +13,18 @@
 
 
 	$time = time();
-	$oProc->query("INSERT INTO phpgw_wiki_pages (title,version,time,supercede,username,author,body) VALUES ('RecentChanges',1,$time,$time,'setup','localhost','[[! *]]\n')");
-	$oProc->query("INSERT INTO phpgw_wiki_pages (title,version,time,supercede,username,author,body) VALUES ('PhpGroupWare',1,$time,$time,'setup','localhost','= PhpGroupWare =
+	$oProc->query("INSERT INTO phpgw_wiki_pages (title,version,time,supercede,username,author,body,comment) VALUES ('RecentChanges',1,$time,$time,'setup','localhost','[[! *]]\n','')");
+	$oProc->query("INSERT INTO phpgw_wiki_pages (title,version,time,supercede,username,author,body,comment) VALUES ('PhpGroupWare',1,$time,$time,'setup','localhost','= PhpGroupWare =
 	
 Welcome to \\'\\'\\'Wiki\\'\\'\\' - the PhpGroupWare Version of \\'\\'\\'WikkiTikkiTavi\\'\\'\\'. Wikis are a revolutionary new form of collaboration and online community.
 
-\\'\\'\\'PhpGroupWare\\'\\'\\' is the groupware suite you are useing right now. For further information see http://www.phpgroupware.org')");
+\\'\\'\\'PhpGroupWare\\'\\'\\' is the groupware suite you are useing right now. For further information see http://www.phpgroupware.org','')");
 	
-	$oProc->query("INSERT INTO phpgw_wiki_pages (title,version,time,supercede,username,author,body) VALUES ('WikkiTikkiTavi',1,$time,$time,'setup','localhost','= WikkiTikkiTavi =
+	$oProc->query("INSERT INTO phpgw_wiki_pages (title,version,time,supercede,username,author,body,comment) VALUES ('WikkiTikkiTavi',1,$time,$time,'setup','localhost','= WikkiTikkiTavi =
 
 WikkiTikkiTavi is the original version this documentation system.
 Their documentation is usable for the PhpGroupWare \\'\\'\\'Wiki\\'\\'\\' too.
 
 The documentation of WikkiTikkiTavi is online availible at: http://tavi.sourceforge.net
-You can learn about Wiki formatting at http://tavi.sourceforge.net/FormattingRules\n')");
+You can learn about Wiki formatting at http://tavi.sourceforge.net/FormattingRules\n','')");
 	
