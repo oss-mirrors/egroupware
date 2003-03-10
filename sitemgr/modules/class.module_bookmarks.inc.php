764375e0cc8c6bd1a89868dcf2f9918ffd4386f9
totschnig <totschnig@923e3c2e-ae13-0410-ba18-ced7211e33de>
2003-03-10 05:30:14 +0000

    new module for showing bookmarks on public website
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@9906 923e3c2e-ae13-0410-ba18-ced7211e33de

---
+++ sitemgr/modules/class.module_bookmarks.inc.php
@@ -0,0 +1,60 @@
+<?php
+
+class module_bookmarks extends Module 
+{
+	var $template;
+	var $startlevel;
+
+	function module_bookmarks()
+	{
+		$this->title = "Bookmarks";
+		$this->description = "This module displays bookmarks in a javascript based tree";
+		$this->cookie = array('expanded');
+		$this->arguments = array(
+			'category' => array(
+				'type' => 'select', 
+				'label' => 'Choose the categories to display', 
+				'options' => array(),
+				'multiple' => True
+			)
+		);
+	}
+
+	function get_user_interface()
+	{
+		$cat = createobject('phpgwapi.categories','','bookmarks');
+		$cats = $cat->return_array('mains',0,False,'','cat_name','',True);
+		$cat_ids = array();
+		while (list(,$category) = @each($cats))
+		{
+			$cat_ids[$category['id']] = $category['name'];
+		}
+		$this->arguments['category']['options'] = $cat_ids;
+		return parent::get_user_interface();
+	}
+
+	function set_block($block,$produce=False)
+	{
+		parent::set_block($block,$produce);
+
+		if ($produce)
+		{
+			require_once(PHPGW_INCLUDE_ROOT . SEP . 'sitemgr' . SEP . 'inc' . SEP . 'class.xslt_transform.inc.php');
+			$this->add_transformer(new xslt_transform($this->find_template_dir() . SEP . 'xbel.xsl'));
+		}
+	}
+
+	function get_content(&$arguments,$properties)
+	{
+		if ($arguments['expanded'])
+		{
+			$expandedcats = array_keys($arguments['expanded']);
+		}
+		else
+		{
+			$expandedcats = Array();
+		}
+		$bo = createobject('bookmarks.exportbo');
+		return $bo->export($arguments['category'],'xbel',$expandedcats);
+	}
+}
