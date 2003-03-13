400d78325865e5809c7becfc60052969c4e0ba67
ralfbecker <ralfbecker@923e3c2e-ae13-0410-ba18-ced7211e33de>
2003-03-13 21:16:07 +0000

    final comit to make all changes from the import HEAD
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@9961 923e3c2e-ae13-0410-ba18-ced7211e33de

---
+++ email/inc/hook_manual.inc.php
@@ -0,0 +1,23 @@
+<?php
+  /**************************************************************************\
+  * phpGroupWare - Calendar Holidays                                         *
+  * http://www.phpgroupware.org                                              *
+  * Written by Mark Peters <skeeter@phpgroupware.org>                        *
+  * --------------------------------------------                             *
+  *  This program is free software; you can redistribute it and/or modify it *
+  *  under the terms of the GNU General Public License as published by the   *
+  *  Free Software Foundation; either version 2 of the License, or (at your  *
+  *  option) any later version.                                              *
+  \**************************************************************************/
+
+	/* $Id$ */
+
+	$file = Array(
+		'Viewing'	=> 'viewing.php',
+		'Replying'	=> 'replying.php',
+		'Composing/Saving/Deleting'	=> 'other.php',
+		'Notes'		=> 'notes.php'
+	);
+//Do not modify below this line
+	display_manual_section($appname,$file);
+?>
