894eb3cd684161b856572c0ab9434bc10836f3cb
ralfbecker <ralfbecker@923e3c2e-ae13-0410-ba18-ced7211e33de>
2003-03-21 23:43:56 +0000

    fix for case-sensitive titles in pgSql
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@10080 923e3c2e-ae13-0410-ba18-ced7211e33de

--- wiki/setup/default_records.inc.php
+++ wiki/setup/default_records.inc.php
@@ -14,16 +14,14 @@
 
 	$time = time();
 	$oProc->query("INSERT INTO phpgw_wiki_pages (title,version,time,supercede,username,author,body,comment) VALUES ('RecentChanges',1,$time,$time,'setup','localhost','[[! *]]\n','')");
-	$oProc->query("INSERT INTO phpgw_wiki_pages (title,version,time,supercede,username,author,body,comment) VALUES ('PhpGroupWare',1,$time,$time,'setup','localhost','= PhpGroupWare =
-	
-Welcome to \\'\\'\\'Wiki\\'\\'\\' - the PhpGroupWare Version of \\'\\'\\'WikkiTikkiTavi\\'\\'\\'. Wikis are a revolutionary new form of collaboration and online community.
+	$oProc->query("INSERT INTO phpgw_wiki_pages (title,version,time,supercede,username,author,body,comment) VALUES ('phpGroupWare',1,$time,$time,'setup','localhost','Welcome to \\'\\'\\'Wiki\\'\\'\\' - the phpGroupWare Version of \\'\\'\\'WikkiTikkiTavi\\'\\'\\'. Wikis are a revolutionary new form of collaboration and online community.
 
-\\'\\'\\'PhpGroupWare\\'\\'\\' is the groupware suite you are useing right now. For further information see http://www.phpgroupware.org','')");
+\\'\\'\\'phpGroupWare\\'\\'\\' is the groupware suite you are useing right now. For further information see http://www.phpgroupware.org','')");
 	
 	$oProc->query("INSERT INTO phpgw_wiki_pages (title,version,time,supercede,username,author,body,comment) VALUES ('WikkiTikkiTavi',1,$time,$time,'setup','localhost','= WikkiTikkiTavi =
 
 WikkiTikkiTavi is the original version this documentation system.
-Their documentation is usable for the PhpGroupWare \\'\\'\\'Wiki\\'\\'\\' too.
+Their documentation is usable for the ((phpGroupWare)) \\'\\'\\'Wiki\\'\\'\\' too.
 
 The documentation of WikkiTikkiTavi is online availible at: http://tavi.sourceforge.net
 You can learn about Wiki formatting at http://tavi.sourceforge.net/FormattingRules\n','')");
