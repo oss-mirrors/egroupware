5b79544aace2e0068aa13d1e75bdcd6eb01dbdfa
ralfbecker <ralfbecker@923e3c2e-ae13-0410-ba18-ced7211e33de>
2003-04-07 00:30:29 +0000

    new english lang-file and hook_settings
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@10295 923e3c2e-ae13-0410-ba18-ced7211e33de

--- email/inc/hook_settings.inc.php
+++ email/inc/hook_settings.inc.php
@@ -32,42 +32,52 @@
 		
 	$lang_oldest = lang('oldest');
 	$lang_newest = lang('newest');
-	create_select_box('Default sorting order','default_sorting',array(
+	$options = array(
 		'old_new' => $lang_oldest.' -> '.$lang_newest,
 		'new_old' => $lang_newest.' -> '.$lang_oldest
-	),'In the email index page, the page which lists all the mails in a folder, mail may be sorted by date, author, size, or subject, HOWEVER all of these need to be ordered from first to last, this options controlls what is first and last. For example, if sorting by date, the newest to oldest displays the most recent emails first, in decending order down to the oldest emails last.');
-	unset($lang_oldest);
-	unset($lang_newest);
+	);
+	create_select_box('Default sorting order','default_sorting',$options,
+		'In the email index page, the page which lists all the mails in a folder, mail may be sorted by date, author, size, or subject, HOWEVER all of these need to be ordered from first to last, this options controlls what is first and last. For example, if sorting by date, the newest to oldest displays the most recent emails first, in decending order down to the oldest emails last.');
 	
-	create_select_box('Message List Layout','layout',array(
+	$options = array(
 		'1' => lang('Layout 1'),
 		'2' => lang('Layout 2')
-	),'The email application offers 2 different layouts for the index page, that is the page that lists the emails in a folder. This page may be the page the user looks at the most and so different layouts, or looks, are offered.');
+	);
+	create_select_box('Message List Layout','layout',$options,
+		'The email application offers 2 different layouts for the index page, that is the page that lists the emails in a folder. This page may be the page the user looks at the most and so different layouts, or looks, are offered.');
 		
-	create_select_box('Icon Theme','icon_theme',array(
+	$options = array(
 		'evo' => lang('Evolution Style'),
 		'moz' => lang('Mozilla Modern Style'),
 		'noia' => lang('Noia &#64; Carlitus Style')
-	),'The email application offers different icon image themes, groups of images of a similar style which are used in this email application. Currently the available themes are images based on Evolution by Ximian and the Netscape6 / Mozilla browser buttons. Additional themes are anticipated and welcome.');
+	);
+	create_select_box('Icon Theme','icon_theme',$options,
+		'The email application offers different icon image themes, groups of images of a similar style which are used in this email application. Currently the available themes are images based on Evolution by Ximian and the Netscape6 / Mozilla browser buttons. Additional themes are anticipated and welcome.');
 		
-	create_select_box('Icon Size','icon_size',array(
+	$options = array(
 		'16' => lang('Small'),
 		'24' => lang('Big')
-	),'The email application offers different icon image themes, these icons can be big or small.');
+	);
+	create_select_box('Icon Size','icon_size',$options,
+		'The email application offers different icon image themes, these icons can be big or small.');
 		
-	create_select_box('Button Type','button_type',array(
+	$options = array(
 		'text' => lang('Text'),
 		'image' => lang('Image'),
 		'both' => lang('Both')
-	),'The email application offers different button displays, these buttons can be text, images, or both.');
+	);
+	create_select_box('Button Type','button_type',$options,
+		'The email application offers different button displays, these buttons can be text, images, or both.');
 		
 	if (!$acctnum)
 	{
-		create_select_box('Show sender\'s email address with name','show_addresses',array(
+		$options = array(
 			'none' => lang('none'),
 			'From' => lang('From'),
 			'ReplyTo' => lang('ReplyTo')
-		),'This confusing and often misunderstood option is left over from this email apps origins as Aeromail by Mark Cushman. When viewing a list of emails in a folder, the FROM column may show you<br>a) the senders name only, if a name was provided,<br>b) the senders From email address, in addition to the senders name, or<br>c) the senders reply to address if it is different from the senders<br>from address, in addition to the senders name if it was provided. Typically users set this to none, which will show only the senders name. If no name was supplied by the sender, then the senders FROM email address will be shown, whether a seperate reply to address is provided has no effect on this, the FROM address is always used if the senders name is not provided.');
+		);
+		create_select_box('Show sender\'s email address with name','show_addresses',$options,
+			'This confusing and often misunderstood option is left over from this email apps origins as Aeromail by Mark Cushman. When viewing a list of emails in a folder, the FROM column may show you<br>a) the senders name only, if a name was provided,<br>b) the senders From email address, in addition to the senders name, or<br>c) the senders reply to address if it is different from the senders<br>from address, in addition to the senders name if it was provided. Typically users set this to none, which will show only the senders name. If no name was supplied by the sender, then the senders FROM email address will be shown, whether a seperate reply to address is provided has no effect on this, the FROM address is always used if the senders name is not provided.');
 		
 		create_check_box('show new messages on main screen','mainscreen_showmail',
 			'Each user has a summary page which can display a variety of information. This option will show a small list of email messages in the INBOX of the users default email account onthe users summary home page.');
@@ -88,21 +98,22 @@
 		'Most US and European users do not need to enable this. If this option is checked then your email server can handle folder names with non US-ASCII charactors in them / default is disabled, not checked. Only use if you are really sure you need it. Only works with IMAP servers, POP servers do not have folders.');
 		
 	create_check_box('Send forwarded mail as quoted attachment','fwd_inline_text',
-		'Select this box if you want the text body of the message you are forwarding to appear
-		inline in the body of your sent message.');
+		'Select this box if you want the text body of the message you are forwarding to appear inline in the body of your sent message.');
 		
+	$options = array(
+		'orig'=> lang('Simple'),
+		'lex' => lang('Javascript')
+	);
 	create_select_box('Select your style for the addressbook. The traditional, simple style. Or the new javascript enabled complex addressbook',
-		'addressbook_choice',array(
-			'orig'=>'Simple',
-			'lex' => 'Javascript'
-		),'We have recently added this new addressbook so that users can choose to have a more complex addressbook that features<br>a) Easy, point and click searching,<br>b) Best suited for organizations with large central addressbooks with many categories.<br>You can choose here which addressbook do you prefer.');
+		'addressbook_choice',$options,'We have recently added this new addressbook so that users can choose to have a more complex addressbook that features<br>a) Easy, point and click searching,<br>b) Best suited for organizations with large central addressbooks with many categories.<br>You can choose here which addressbook do you prefer.');
 
+	$options = array(
+		'900' =>'1200x1600',
+		'800' => '1024x768',
+		'700' => '800x600'
+	);
 	create_select_box('Select your screensize for propper showing of the Javascript addressbook',
-		'js_addressbook_screensize',array(
-			'900'=>'1200x1600',
-			'800' => '1024x768',
-			'700' => '800x600'
-		),'We have three sizes that tell us how to better render the addressbook for you: 800x600 (addressbook will popout in a 700 pixel wide box), 1024x768 (it will be a 800 box), 1200x1600 (will be a 900 box). The fonts for all html stuff will be, respectively set to xx-small, x-small and normal (no font setting).');
+		'js_addressbook_screensize',$options,'We have three sizes that tell us how to better render the addressbook for you: 800x600 (addressbook will popout in a 700 pixel wide box), 1024x768 (it will be a 800 box), 1200x1600 (will be a 900 box). The fonts for all html stuff will be, respectively set to xx-small, x-small and normal (no font setting).');
 
 	create_check_box('Show New Messages in ComboBox','newmsg_combobox',
 		'This specifies whether or not to show the number of new message in the folders combo box on the index screen.');
@@ -134,19 +145,21 @@
 		'Name of the mail server you want to access. Should be a name like &quot;mail.example.com&quot;. If you leave this box blank then the default value will be used.',
 		$GLOBALS["phpgw_info"]["server"]["mail_server"]);
 
-	create_select_box('Mail Server type','mail_server_type',array(
-		'imap'		=> 'IMAP',
-		'pop3'		=> 'POP-3',
-		'imaps'		=> 'IMAPS',
-		'pop3s'		=> 'POP-3S'
-	),'The type of mail server you want to access. IMAP mail servers have folders, such as the Sent and Trash folders. POP servers do not have folders. POP, POP-3, and POP3 are the same thing. You can have the server connection encrypted by using IMAPS or POPS, only if the mailserver supports it and if your phpGroupWare installation has a &quot;SSL&quot; capabable version of PHP.',
+	$options = array(
+		'imap'	=> 'IMAP',
+		'pop3'	=> 'POP-3',
+		'imaps'	=> 'IMAPS',
+		'pop3s'	=> 'POP-3S'
+	);
+	create_select_box('Mail Server type','mail_server_type',$options,'The type of mail server you want to access. IMAP mail servers have folders, such as the Sent and Trash folders. POP servers do not have folders. POP, POP-3, and POP3 are the same thing. You can have the server connection encrypted by using IMAPS or POPS, only if the mailserver supports it and if your phpGroupWare installation has a &quot;SSL&quot; capabable version of PHP.',
 		$GLOBALS["phpgw_info"]["server"]["mail_server_type"]);
 
-	create_select_box('IMAP Server Type - If Applicable','imap_server_type',array(
+	$options = array(
 		'Cyrus'		=> 'Cyrus '.lang('or').' Courier',
 		'UWash'		=> 'UWash',
-		'UW-Maildir'	=> 'UW-Maildir'
-	),'If using an IMAP server, what kind is it, most often this option can safely be set to &quot;Cyrus or Courier&quot;. Technically, this means the server uses a dot between the different parts of the folder names, such as &quot;INBOX.Sent&quot;. The other major kind of IMAP server is the University of Washington &quot;UWash&quot; IMAP server. It uses slashes instead of the dots the other servers use, and although it has a folder called &quot;INBOX&quot;, it is not considered the &quot;Namespace&quot; for the other folder names. The &quot;UW-Maildir&quot; is a rare combination of the two above types. This is the least used kind of IMAP server. If you are unsure, ask your IT administrator. Only applies to IMAP servers.',
+		'UW-Maildir'=> 'UW-Maildir'
+	);
+	create_select_box('IMAP Server Type - If Applicable','imap_server_type',$options,'If using an IMAP server, what kind is it, most often this option can safely be set to &quot;Cyrus or Courier&quot;. Technically, this means the server uses a dot between the different parts of the folder names, such as &quot;INBOX.Sent&quot;. The other major kind of IMAP server is the University of Washington &quot;UWash&quot; IMAP server. It uses slashes instead of the dots the other servers use, and although it has a folder called &quot;INBOX&quot;, it is not considered the &quot;Namespace&quot; for the other folder names. The &quot;UW-Maildir&quot; is a rare combination of the two above types. This is the least used kind of IMAP server. If you are unsure, ask your IT administrator. Only applies to IMAP servers.',
 		$GLOBALS["phpgw_info"]["server"]["imap_server_type"]);
 
 	create_input_box('U-Wash Mail Folder - If Applicable','mail_folder',
