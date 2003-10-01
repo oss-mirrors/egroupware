e381b598ca7799b3f9980d9d12e52d6667ccfc55
ralfbecker <ralfbecker@923e3c2e-ae13-0410-ba18-ced7211e33de>
2003-10-01 09:30:04 +0000

    removed emailadmin from the navbar, as there's no functionality and it gives only the set_root error of the template
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@11984 923e3c2e-ae13-0410-ba18-ced7211e33de

--- emailadmin/setup/setup.inc.php
+++ emailadmin/setup/setup.inc.php
@@ -16,7 +16,7 @@
 	$setup_info['emailadmin']['title']     = 'EMailAdmin';
 	$setup_info['emailadmin']['version']   = '0.0.6';
 	$setup_info['emailadmin']['app_order'] = 10;
-	$setup_info['emailadmin']['enable']    = 1;
+	$setup_info['emailadmin']['enable']    = 2;
 
 	$setup_info['emailadmin']['author'] = 'Lars Kneschke';
 	$setup_info['emailadmin']['license']  = 'GPL';
