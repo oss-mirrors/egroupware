dd3dfcb3671a092b315d785a0f25b140cffb9f0b
ralfbecker <ralfbecker@923e3c2e-ae13-0410-ba18-ced7211e33de>
2003-10-19 10:19:34 +0000

    added a 'site configuration' link for fudforum in our admin-page
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@12449 923e3c2e-ae13-0410-ba18-ced7211e33de

---
+++ fudforum/inc/hook_admin.inc.php
@@ -0,0 +1,22 @@
+<?php
+	/**************************************************************************\
+	* phpGroupWare - FUDforum administration                                   *
+	* http://www.phpgroupware.org                                              *
+	* --------------------------------------------                             *
+	*  This program is free software; you can redistribute it and/or modify it *
+	*  under the terms of the GNU General Public License as published by the   *
+	*  Free Software Foundation; either version 2 of the License, or (at your  *
+	*  option) any later version.                                              *
+	\**************************************************************************/
+	/* $Id$ */
+
+	{
+		$file = Array
+		(
+			'Site configuration' => $GLOBALS['phpgw']->link('/fudforum/adm/admglobal.php'),
+		);
+
+//Do not modify below this line
+		display_section($appname,$file);
+	}
+?>
