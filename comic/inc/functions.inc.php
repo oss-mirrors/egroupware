4885cf7bcc15e4e3ba10f2714e2ebfedf21005d6
uid135481 <uid135481@923e3c2e-ae13-0410-ba18-ced7211e33de>
2003-10-23 18:31:56 +0000

    added quotes to array keys
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@12547 923e3c2e-ae13-0410-ba18-ced7211e33de

--- comic/inc/functions.inc.php
+++ comic/inc/functions.inc.php
@@ -33,13 +33,13 @@ $g_censor_level = array(0 => 'G',
 
 $g_image_source = array(0 => 'Remote',
                         1 => 'Local');
-$g_dayofweek    = array(Mon => 1,
-                        Tue => 2,
-                        Wed => 3,
-                        Thu => 4,
-                        Fri => 5,
-                        Sat => 6,
-                        Sun => 7);
+$g_dayofweek    = array('Mon' => 1,
+                        'Tue' => 2,
+                        'Wed' => 3,
+                        'Thu' => 4,
+                        'Fri' => 5,
+                        'Sat' => 6,
+                        'Sun' => 7);
 
 function comic_admin_data(&$image, &$censor, &$override, &$remote, &$filesize)
 {
