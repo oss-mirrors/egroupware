a8161d269a00eff04088a09316ad1e02f36a25f5
iliaa <iliaa@923e3c2e-ae13-0410-ba18-ced7211e33de>
2003-10-30 18:56:36 +0000

    Missing file.
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@12648 923e3c2e-ae13-0410-ba18-ced7211e33de

---
+++ fudforum/index.php
@@ -0,0 +1,24 @@
+<?php
+/***************************************************************************
+* copyright            : (C) 2001-2003 Advanced Internet Designs Inc.
+* email                : forum@prohost.org
+* $Id$
+*
+* This program is free software; you can redistribute it and/or modify it 
+* under the terms of the GNU General Public License as published by the 
+* Free Software Foundation; either version 2 of the License, or 
+* (at your option) any later version.
+***************************************************************************/
+
+	if (!empty($_GET['domain'])) {
+		$dom = stripslashes($_GET['domain']);
+	} else {
+		$d = opendir('.');
+		readdir($d); readdir($d);
+		$dom = readdir($d);
+		closedir($d);
+	}
+
+	$path = dirname($_SERVER["SCRIPT_NAME"]) . "/" . sprintf("%u", crc32($dom)) . "/index.php?" . $_SERVER["QUERY_STRING"];
+	header("Location: ".$path);
+?>
