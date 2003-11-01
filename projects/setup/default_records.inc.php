a266dff2c5dc88e721befbbdc684217d425466e1
ralfbecker <ralfbecker@923e3c2e-ae13-0410-ba18-ced7211e33de>
2003-11-01 11:52:38 +0000

    added a default_records file, to add the Admin Group as project-admin and -bookkeeper and add one example activity, hope that solves the repeating support-request about that issue
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@12658 923e3c2e-ae13-0410-ba18-ced7211e33de

---
+++ projects/setup/default_records.inc.php
@@ -0,0 +1,16 @@
+<?php
+  /**************************************************************************\
+  * phpGroupWare - Setup                                                     *
+  * http://www.eGroupWare.org                                                *
+  * --------------------------------------------                             *
+  *  This program is free software; you can redistribute it and/or modify it *
+  *  under the terms of the GNU General Public License as published by the   *
+  *  Free Software Foundation; either version 2 of the License, or (at your  *
+  *  option) any later version.                                              *
+  \**************************************************************************/
+
+  /* $Id$ */
+
+	$oProc->query ("INSERT INTO phpgw_p_projectmembers (project_id,account_id,type) VALUES (0,2,'ag')");
+	$oProc->query ("INSERT INTO phpgw_p_projectmembers (project_id,account_id,type) VALUES (0,2,'bg')");
+	$oProc->query ("INSERT INTO phpgw_p_activities (num,descr,remarkreq,minperae,billperae,category) VALUES ('0815','Exampleactivity','Y',0,'10.0',0)");
