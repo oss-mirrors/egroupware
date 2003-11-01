088fa5f678abc043621232e57acc517c8f5f3a2d
shrykedude <shrykedude@923e3c2e-ae13-0410-ba18-ced7211e33de>
2003-11-01 06:22:11 +0000

    better empty var check
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@12656 923e3c2e-ae13-0410-ba18-ced7211e33de

--- comic/inc/functions.inc.php
+++ comic/inc/functions.inc.php
@@ -415,7 +415,7 @@ function comic_parser($baseurl, $fetch_url, $parse_expr, &$comic_url)
                 break;
             }
 
-            if (isset($parse_expr) && isset($file[$index]) && ereg($parse_expr, $file[$index], $elements))
+            if (!empty($parse_expr) && !empty($file[$index]) && ereg($parse_expr, $file[$index], $elements))
             {
                 $comic_url = $baseurl . $elements[0];
                 break;
