7e9bf46d73031112557c6c9c4a6328221fc84d89
alpeb <alpeb@923e3c2e-ae13-0410-ba18-ced7211e33de>
2004-01-23 16:13:44 +0000

    fixed number of days to show in RecentChanges
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@13424 923e3c2e-ae13-0410-ba18-ced7211e33de

--- wiki/parse/macros.php
+++ wiki/parse/macros.php
@@ -37,9 +37,7 @@ function view_macro_category($args)
  
   for($i = 0; $i < count($list); $i++)
   {
-    $editTime = mktime(substr($list[$i][0], 8, 2),  substr($list[$i][0], 10, 2),
-                       substr($list[$i][0], 12, 2), substr($list[$i][0], 4, 2),
-                       substr($list[$i][0], 6, 2),  substr($list[$i][0], 0, 4));
+    $editTime = $list[$i][0];
     if($DayLimit && $i >= $MinEntries
        && !$full && ($now - $editTime) > $DayLimit * 24 * 60 * 60)
       { break; }
