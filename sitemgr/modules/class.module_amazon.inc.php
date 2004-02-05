66df7dcd84ccda127ffbd0d9c8bef6d500ef8a62
ralfbecker <ralfbecker@923e3c2e-ae13-0410-ba18-ced7211e33de>
2004-02-05 18:59:43 +0000

    1) changed amazon module to read images from the amazon-server 2) added a serach-field 3) make the following parameters configurable: amazon_id, amazon-server, ASIN's
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@13655 923e3c2e-ae13-0410-ba18-ced7211e33de

--- sitemgr/modules/class.module_amazon.inc.php
+++ sitemgr/modules/class.module_amazon.inc.php
@@ -1,26 +1,41 @@
-<?php 
-/**************************************************************\
-* this was originally a phpNuke block.  the amazon_id has been *
-* changed.  feel free to change it yourself, of course.  the   *
-* below directions still apply.                                *
-\**************************************************************/
+<?php
+	/**************************************************************************\
+	* eGroupWare - SiteMgr Amazon module                                       *
+	* http://www.eGroupWare.org                                                *
+	* -------------------------------------------------                        *
+	* This was originaly a phpNuke block, the amazon_id has been changed       *
+	* Copyright (C) 2004 RalfBecker@outdoor-training.de                        *
+	* This program is free software; you can redistribute it and/or modify it  *
+	* under the terms of the GNU General Public License as published by the    *
+	* Free Software Foundation; either version 2 of the License, or (at your   *
+	* option) any later version.                                               *
+	\**************************************************************************/
 
-/***************************************************************/
-/* To use this block you only need to download .jpg or .gif    */
-/* images from amazon.com and copy them to the /images/amazon  */
-/* directory, then edit the $amazon_id variable to fit your ID */
-/* of the Associates program. If you don't change the ID, all  */
-/* the comissions ($) will go to my account! You're advised.   */
-/*                                                             */
-/* You need to know that any image in the amazon's directory   */
-/* has the same ASIN name as its filename given by Amazon. If  */
-/* you don't know what this is, leave it as is or disable it.  */
-/***************************************************************/
 class module_amazon extends Module
 {
 	function module_amazon()
 	{
-		$this->arguments = array();
+		$this->arguments = array(
+			'amazon_id' => array(
+				'type' => 'textfield',
+				'label' => lang('Own Amazon partner-id or empty to donate to the eGroupWare project')
+			),
+			'amazon_server' => array(
+				'type' => 'textfield',
+				'label' => lang('Amazon server (without www), eg. amazon.com')
+			),
+			'search' => array(
+				'type' => 'checkbox',
+				'label' => lang('Show searchbox')
+			),
+			'asins' => array(
+				'type' => 'textarea',
+				'params' => array('rows' => 10,'cols' => 20),
+				'label' => lang("ASIN[=title] pairs (title is optional)")
+			),
+		);
+		//$this->post = array('asin' => array('type' => 'textfield'));
+		//$this->session = array('asin');
 		$this->properties = array();
 		$this->title = lang('Amazon');
 		$this->description = lang('Use this module for displaying book ads for the amazon web site');
@@ -28,22 +43,33 @@ class module_amazon extends Module
 
 	function get_content(&$arguments,$properties)
 	{
-		$amazon_id = "phpgwsitemgr-20";
+		$asins = split("[; \n]+",$arguments['asins']);
+		if (!count($asins) || empty($asins[0]))
+		{
+			$asins = array('1565926102=Programming PHP');
+		}
+		//echo "<p>module_amazon('$arguments[asins]') = ".print_r($asins,True)."</p>";
 
 		mt_srand((double)microtime()*1000000);
-		$imgs = dir('images/amazon');
-		while ($file = $imgs->read()) {
-			if (eregi("gif", $file) || eregi("jpg", $file)) {
-			$imglist .= "$file ";
-			}
+		$asin = $asins[mt_rand(0, count($asins)-1)];
+		list($asin,$title) = explode('=',$asin);
+
+		$amazon_id = $arguments['amazon_id'] ? $arguments['amazon_id'] : "egroupware-21";
+		$server = $arguments['amazon_server'] ? $arguments['amazon_server'] : 'amazon.com';
+		$buy_at = ($title ? ': ' : '') . lang('Buy at %1',$server);
+		$base_url = ($_SERVER['HTTPS'] ? 'https://ssl-' : 'http://')."images.amazon.com/images/P";
+		$content = "<div align=\"center\"><a href=\"http://www.$server/exec/obidos/ASIN/$asin/$amazon_id\" target=\"_blank\">" .
+			"<img src=\"$base_url/$asin.03.MZZZZZZZ.jpg\" border=\"0\" title=\"$title$buy_at\"><br>$title</a>\n";
+
+		if ($arguments['search'])
+		{
+			$content .= "<p><form method=\"get\" action=\"http://www.$server/exec/obidos/external-search?tag=$amazon_id\" target=\"_blank\">".
+				'<INPUT type="text" name="keyword" size="10" value=""><input type="submit" value="'.lang('Search').'"></form>';
 		}
-		closedir($imgs->handle);
-		$imglist = explode(" ", $imglist);
-		$a = sizeof($imglist)-2;
-		$random = mt_rand(0, $a);
-		$image = $imglist[$random];
-		$asin = explode(".", $image);
-		return "<br><center><a href=\"http://www.amazon.com/exec/obidos/ASIN/$asin[0]/$amazon_id\" target=\"_blank\">" .
-			"<img src=\"images/amazon/$image\" border=\"0\" alt=\"\"><br><br></center>";
+		else
+		{
+			$content .= '<br>';
+		}
+		return $content;
 	}
 }
