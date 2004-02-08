b6bd7d2fc4471c31721e400f754018a382e40eaa
mipmip <mipmip@923e3c2e-ae13-0410-ba18-ced7211e33de>
2004-02-08 20:19:28 +0000

    finally: - fixed limited images handeling - fixed resizing - enabled user preferences - enabled preview in form - cleaned up de form display a bit
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@13688 923e3c2e-ae13-0410-ba18-ced7211e33de

--- jinn/plugins/plugin.imagepath.php
+++ jinn/plugins/plugin.imagepath.php
@@ -30,11 +30,17 @@
 
 	$this->plugins['imagepath']['name']				= 'imagepath';
 	$this->plugins['imagepath']['title']			= 'ImagePath plugin';
-	$this->plugins['imagepath']['version']			= '0.8.7';
+	$this->plugins['imagepath']['version']			= '0.8.8';
 	$this->plugins['imagepath']['description']		= 'plugin for uploading/resizing images and storing their imagepaths in to database, using default uploadpath for site or object';
 	$this->plugins['imagepath']['enable']			= 1;
 	$this->plugins['imagepath']['changelog']		= 
 	'
+	0.8.8
+	- fixed limited images handeling
+	- fixed resizing
+	- enabled user preferences
+	- enabled preview in form
+	- cleaned up de form display a bit
 	0.8.7
 	- added changelog
 	- changed varchar to string so it actually binds to varchars ;)
@@ -44,7 +50,6 @@
 	(
 		'text',
 		'string',
-		'blob'
 	);
 
 	/* ATTENTION: spaces and special character are not allowed in config array 
@@ -57,17 +62,17 @@
 		'Max_image_height' => array('','text','maxlength=4 size=4'),
 		'Image_filetype' => array(array('png','gif','jpg'),'select','maxlength=3 size=3'),
 		'Generate_thumbnail' => array( array('False','True') /* 1st is default the rest are all possibilities */ ,'select',''),
-		'Store_thumbnail_in_thumb_pathfield_for_backwards_compatibility' => array( array('False', 'True') ,'select',''),
+//		'Store_thumbnail_in_thumb_pathfield_for_backwards_compatibility' => array( array('False', 'True') ,'select',''),
 		'Max_thumbnail_width' => array('100','text','maxlength=3 size=3'),
 		'Max_thumbnail_height'=> array('100','text','maxlength=3 size=3'),
 		'Allow_other_images_sizes'=> array( array('False','True') /* 1st is default the rest are all possibilities */ ,'select',''),
-		'Show_image_in_form' => array( array('False','True') ,'select','')
+//		'Show_image_in_form' => array( array('False','True') ,'select','')
 	);
 
 	function plg_fi_imagepath($field_name,$value,$config)
 	{	
-
 		global $local_bo;
+
 		$field_name=substr($field_name,3);	
 
 		if($local_bo->common->so->config[server_type]=='dev')
@@ -91,39 +96,72 @@
 			$upload_path=false;
 		}
 
-		//if ($local_bo->site_object['upload_url']) $upload_url=$local_bo->site_object['upload_url'].'/';
-		//elseif($local_bo->site['upload_url']) $upload_url=$local_bo->site['upload_url'].'/';
-		//else $upload_url=false;
-
-		/* if value is set, show existing images */	
-		if($value)
+		$input.='<table style="" cellpadding="3" width="100%">';
+		/****************************************
+		* if value is set, show existing images *
+		****************************************/	
+		if(trim($value))// FIXME or rather TESTME
 		{
-			$input='<input type="hidden" name="IMG_ORG'.$field_name.'" value="'.$value.'">';
+			$input.='<input type="hidden" name="IMG_ORG'.$field_name.'" value="'.$value.'">';
 
 			$value=explode(';',$value);
 
-			/* there are more images */
-			if (is_array($value))
+			if (is_array($value) && count($value)>0)
 			{
 				$i=0;
+
+				$max_prev=$local_bo->read_preferences('max_prev');
+				
 				foreach($value as $img_path)
 				{
 					$i++;
 
-					$input.=$i.'. ';
-					if($upload_url) $input.='<b><a href="'.$upload_url.$img_path.'" target="_blank">'.$img_path.'</a></b>';
-					else $input.='<b>'.$img_path.'</b>';
-					$input.=' <input type="checkbox" value="'.$img_path.'" name="IMG_DEL'.$field_name.$i.'"> '.lang('remove').'<br>';
+					$input.='<tr><td style="border-width:1px;border-style:solid;border-color:grey" valign="top">'.$i.'.</td><td style="border-width:1px;border-style:solid;border-color:grey" >';
+
+					// if URL exists show link or if set show image in form
+					if($local_bo->read_preferences('prev_img')!='no' &&  ($max_prev>=$i || $max_prev==-1) && $upload_url && is_file($upload_path . SEP . $img_path)) 
+					{	
+						if($local_bo->read_preferences('prev_img')=='yes')
+						{
+							if(is_file($upload_path . SEP . str_replace('normal_size','thumb',$img_path)))
+							{
+								$input.='<a href="'.$upload_url.$img_path.'" target="_blank"><img src="'.$upload_url.str_replace('normal_size','thumb',$img_path).'" alt="preview" style="border-style:solid;border-width:1px;border-color:#000000" /></a>';
+							}
+							else
+							{
+								$input.='<img src="'.$upload_url.$img_path.'" alt="preview" style="border-style:solid;border-width:1px;border-color:#000000" />';
+							}
+						}
+						elseif($local_bo->read_preferences('prev_img')=='only_tn' && is_file($upload_path . SEP . str_replace('normal_size','thumb',$img_path)))
+						{
+								$input.='<a href="'.$upload_url.$img_path.'" target="_blank"><img src="'.$upload_url.str_replace('normal_size','thumb',$img_path).'" alt="preview" style="border-style:solid;border-width:1px;border-color:#000000" /></a>';
+						}
+						else
+						{
+							$input.='<b><a href="'.$upload_url.$img_path.'" target="_blank">'.$img_path.'</a></b>';
+						}
+					}
+					else // just show a link 
+					{
+						if($upload_url && is_file($upload_path . SEP . $img_path))
+						{
+							$input.='<b><a href="'.$upload_url.$img_path.'" target="_blank">'.$img_path.'</a></b>';
+						}
+						else
+						{
+							$input.='<b>'.$img_path.'</b>';
+						}
+					}
+
+
+					$input.='</td><td style="border-width:1px;border-style:solid;border-color:grey" valign="top"><input type="checkbox" value="'.$img_path.'" name="IMG_DEL'.$field_name.$i.'"> '.lang('remove').'</td></tr>';
 				}
 			}
-			/* there's just one image */
-			else
-			{
-				$input=$img_path.'<input type="checkbox" value="'.$img_path.'" name="IMG_DEL'.$fieldname.'"> '.lang('remove').'<br>';
-			}
 		}
-
-		/* get max images, set max 5 filefields */
+		
+		/***************************************
+		* get max images, set max 5 filefields *
+		***************************************/
 		if (is_numeric($config[Max_files])) 
 		{
 			if ($config[Max_files]>30) $num_input=30;
@@ -131,31 +169,36 @@
 		}
 		else 
 		{
-			$num_input=100;
+			$num_input=10;
 		}
 
 		for($i=1;$i<=$num_input;$i++)
 		{
-			if($num_input==1) $input .=lang('add image').'<input type="file" name="IMG_SRC'.$field_name.$i.'">';
-			
+			$input.='<tr><td colspan="3" style="border-width:1px;border-style:solid;border-color:grey">';
+			if($num_input==1) 
+			{
+				$input .=lang('add image').'<input type="file" name="IMG_SRC'.$field_name.$i.'">';
+			}
 			else
 			{
-				$input.='<br>';
-				$input.=lang('add image %1', $i).
-				' <input type="file" name="IMG_SRC'.$field_name.$i.'">';
+				$input.=lang('add image %1', $i).' <input type="file" name="IMG_SRC'.$field_name.$i.'">';
 			}
-			
+
 			/* when user is allowed to give own image sizes */
 			if($config['Allow_other_images_sizes']=='True')
 			{
-				$input.= ' '.lang('New image size limits.') . ' '. lang('Height: ').'<input type="text" size="3" name="IMG_HEI'.$field_name.$i.'">'.
-				lang('Width: ').'<input type="text" size="3" name="IMG_WID'.$field_name.$i.'"> '.lang('default:').' '.$config['Max_image_height'].'x'.$config['Max_image_width'].'px';
+				$input.= '<table>';
+				$input.='<tr><td>'.lang('Optional max. height').'('.lang('default').':'.$config['Max_image_height'].')</td><td><input type="text" size="3" name="IMG_HEI'.$field_name.$i.'"></td></tr>';
+				$input.='<tr><td>'.lang('Optional max. width').'('.lang('default').':'.$config['Max_image_width'].')</td><td><input type="text" size="3" name="IMG_WID'.$field_name.$i.'"></td></tr>';
+				$input.='</table>';
 			}
 
+
+			$input.='</td></tr>';			
+
 		}
-
+		$input.='</table>';
 		$input.='<input type="hidden" name="FLD'.$field_name.'" value="">';
-
 		return $input;
 	}
 
@@ -165,6 +208,8 @@
 	\****************************************************************************/
 	{
 		global $local_bo;
+
+		// FIXME implement gdlib class
 		$magick=CreateObject('jinn.boimagemagick');
 
 		if($local_bo->common->so->config[server_type]=='dev')
@@ -187,10 +232,6 @@
 			$upload_url=false;
 			$upload_path=false;
 		}
-
-		//if ($local_bo->site_object['upload_path']) $upload_path=$local_bo->site_object['upload_path'].'/';
-		//elseif($local_bo->site['upload_path']) $upload_path=$local_bo->site['upload_path'].'/';
-		//else $upload_path=false;
 
 		$images_to_delete=$local_bo->common->filter_array_with_prefix($HTTP_POST_VARS,'IMG_DEL');
 
@@ -228,19 +269,16 @@
 		/* finally adding new image and if neccesary a new thumb */
 		$images_to_add=$local_bo->common->filter_array_with_prefix($HTTP_POST_FILES,'IMG_SRC'.substr($field_name,3));
 
-		
+
 		$images_to_add_hei=$local_bo->common->filter_array_with_prefix($HTTP_POST_VARS,'IMG_HEI'.substr($field_name,3));
 		$images_to_add_wid=$local_bo->common->filter_array_with_prefix($HTTP_POST_VARS,'IMG_WID'.substr($field_name,3));
-
-		
-		
-		
 
 		// quick check for new images
 		if(is_array($images_to_add))
 		foreach($images_to_add as $imagecheck)
 		{
 			if($imagecheck['name']) $num_img_to_add++;
+			
 		}
 
 		if ($num_img_to_add)
@@ -294,12 +332,13 @@
 
 			}
 
-			$imgposition=0;
+
+			
+			$img_position=0;
 			foreach($images_to_add as $add_image)
 			{
 				if($add_image['name'])
 				{
-
 
 					if($images_to_add_hei[$img_position] || $images_to_add_wid[$img_position])
 					{
@@ -314,12 +353,7 @@
 						{
 							$new_img_height=$images_to_add_hei[$img_position];
 						}
-//						var_dump($fieldname);
-//						var_dump($images_to_add_hei);
-//						var_dump($images_to_add_wid);
-//						die();
-
-
+			
 					}
 					else
 					{
@@ -337,8 +371,7 @@
 						}
 
 					}
-					
-					
+
 					/* get original type */
 					$filetype=$magick->Get_Imagetype($add_image['tmp_name']);	
 					if(!$filetype)
@@ -354,6 +387,9 @@
 					}
 					elseif($new_img_width || $new_img_height)
 					{
+/*						echo $new_img_width;
+						echo $new_img_height;
+						die();*/
 						$target_image_name.='.'.$filetype;
 						$new_temp_file=$magick->Resize($new_img_width,$new_img_height,$add_image['tmp_name'],$filetype);
 						if(!$new_temp_file) die(lang('the resize process failed, please contact the administrator'));
@@ -368,7 +404,7 @@
 					{
 						//generate thumb
 						$new_temp_thumb=$magick->Resize($config['Max_thumbnail_width'],
-							$config['Max_thumbnail_height'],$add_image['tmp_name'],$new_filetype);
+						$config['Max_thumbnail_height'],$add_image['tmp_name'],$new_filetype);
 
 						//put thumbpath in db for backwards compatibility
 						if($config['Store_thumbnail_in_thumb_pathfield_for_backwards_compatibility']=='True')
@@ -377,15 +413,21 @@
 						}
 
 					}
-				
 
-					$target_image_name = time().ereg_replace("[^a-zA-Z0-9_.]", '_', $add_image['name']);
+					
+					$target_image_name = time() . ereg_replace("[^a-zA-Z0-9_.]", '_', $add_image['name']);
 
 					if(substr(substr($target_image_name,-4),0,1) =='.') 
 					{
-						$target_image_name = substr($target_image_name,0,(strlen($target_image_name)-3)).$filetype;					
+						$target_image_name = substr($target_image_name,0,(strlen($target_image_name)-3)).$filetype;	
 					}
-					else $target_image_name .='.'.$filetype;	
+					else $target_image_name .='.'.$filetype;
+					
+					if(is_file($upload_path . SEP . 'normal_size' . SEP .$target_image_name))
+					{
+						$target_image_name='another_'.$target_image_name;
+					}
+
 
 					if (copy($new_temp_file, $upload_path."/normal_size/".$target_image_name))
 					{
@@ -408,9 +450,14 @@
 
 		if(is_array($images_array))
 		{
+			//check max images
+			if( count($images_array) > $config[Max_files] )
+			{
+				$images_array=array_slice($images_array, 0, $config[Max_files]);
+			}	
+			
 			foreach ($images_array as $image_string)
 			{
-
 				if($image_path_new) $image_path_new .= ';';
 				$image_path_new.=$image_string;
 			}						
@@ -418,9 +465,7 @@
 
 		}
 
-		//die(var_dump($image_path_new));		
-
-		//// make return array for storage
+		// make return array for storage
 		if($image_path_new)
 		{
 			return $image_path_new;
@@ -459,9 +504,6 @@
 			$upload_url=false;
 			$upload_path=false;
 		}
-//		if ($local_bo->site_object['upload_url']) $upload_url=$local_bo->site_object['upload_url'].'/';
-//		elseif($local_bo->site['upload_url']) $upload_url=$local_bo->site['upload_url'].'/';
-//		else $upload_url=false;
 
 		/* if value is set, show existing images */	
 		if($value)
@@ -479,7 +521,7 @@
 					if($upload_url) $display.='<a href="'.$upload_url.$img_path.'" target="_blank">'.$i.'</a>';
 					else $display.=' '.$i;
 					$display.=' ';
-					
+
 				}
 			}
 		}
@@ -489,4 +531,4 @@
 
 	}
 
-?>
+	?>
