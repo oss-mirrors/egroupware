99b964f45251a6834719e902930c419e78003e7f
ralfbecker <ralfbecker@923e3c2e-ae13-0410-ba18-ced7211e33de>
2004-02-10 14:56:00 +0000

    bugfix: cats did not get saved (without error-msg) if the description was empty
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@13728 923e3c2e-ae13-0410-ba18-ced7211e33de

--- sitemgr/inc/class.Categories_UI.inc.php
+++ sitemgr/inc/class.Categories_UI.inc.php
@@ -1,13 +1,15 @@
 <?php
-	/***************************************************************************\
-	* phpGroupWare - Web Content Manager                                        *
-	* http://www.phpgroupware.org                                               *
-	* -------------------------------------------------                         *
-	* This program is free software; you can redistribute it and/or modify it   *
-	* under the terms of the GNU General Public License as published by the     *
-	* Free Software Foundation; either version 2 of the License, or (at your    *
-	* option) any later version.                                                *
-	\***************************************************************************/
+	/**************************************************************************\
+	* eGroupWare SiteMgr - Web Content Management                              *
+	* http://www.egroupware.org                                                *
+	* --------------------------------------------                             *
+	*  This program is free software; you can redistribute it and/or modify it *
+	*  under the terms of the GNU General Public License as published by the   *
+	*  Free Software Foundation; either version 2 of the License, or (at your  *
+	*  option) any later version.                                              *
+	\**************************************************************************/
+
+	/* $Id$ */
 
 	class Categories_UI
    	{
@@ -54,7 +56,7 @@
 			global $savelanguage, $inputgetparentpermissions,$inputapplypermissionstosubs;
 			$cat_id = $inputcatid ? $inputcatid : $_GET['cat_id'];
 
-			if ($btnSave && $inputcatname && $inputcatdesc)
+			if ($btnSave)
 			{
 				if ($inputcatname == '' || $inputcatdesc == '')
 				{
