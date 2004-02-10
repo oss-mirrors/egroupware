6ee503a14e2245a84ba9fdc73719598c5a0252c3
ralfbecker <ralfbecker@923e3c2e-ae13-0410-ba18-ced7211e33de>
2004-02-10 11:19:24 +0000

    added a logout link and updated the description
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@13723 923e3c2e-ae13-0410-ba18-ced7211e33de

--- sitemgr/modules/class.module_administration.inc.php
+++ sitemgr/modules/class.module_administration.inc.php
@@ -7,24 +7,26 @@ class module_administration extends Module
 		$this->arguments = array();
 		$this->properties = array();
 		$this->title = lang('Administration');
-		$this->description = lang('This module presents a link back to the sitemgr\'s administration menu. It is meant for registered users');
+		$this->description = lang('This module is a selectbox to change the mode (production, draft or edit) plus a link back to SiteMgr and to log out. It is meant for registered users only');
 	}
 
 	function get_content(&$arguments,$properties)
 	{
-		$content = '<form name="modeselect" method="post">' .
-			'<select onChange="location.href=this.value" name="mode">';
+		$content = '<form name="modeselect" method="post">' . "\n".
+			'<select onChange="location.href=this.value" name="mode">'."\n";
 		foreach(array(
 			'Production' => lang('Production mode'),
 			'Draft'      => lang('Draft mode'),
 			'Edit'       => lang('Edit mode')) as $mode => $label)
 		{
 			$selected = ($GLOBALS['sitemgr_info']['mode'] == $mode) ? ' selected="selected"' : '';
-			$content .=	'<option value="' .$this->link(array(),array('mode'=>$mode)) .'"' . $selected  . '>' . $label . '</option>';
+			$content .=	'<option value="' .$this->link(array(),array('mode'=>$mode)) .'"' . $selected  . '>' . $label . "</option>\n";
 		}
-		$content .= '</select></form>' .
-			'&nbsp;&nbsp;<strong><big>&middot;</big></strong><a href="' . phpgw_link('/sitemgr/index.php') .
-			'">' . lang('Content Manager') . '</a>';
+		$content .= "</select>\n</form>\n" .
+			'<p>&nbsp;&nbsp;<strong><big>&middot;</big></strong><a href="' . phpgw_link('/sitemgr/') .
+			'">' . lang('Content Manager') . "</a><br />\n".
+			'&nbsp;&nbsp;<strong><big>&middot;</big></strong><a href="' . phpgw_link('/logout.php') .
+			'">' . lang('Logout') . "</a></p>\n";
 		return $content;
 	}
 
