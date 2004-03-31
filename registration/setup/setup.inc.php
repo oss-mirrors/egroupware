17835b3ec43b4fb3022e21ab8ce4d136c5288ea2
reinerj <reinerj@923e3c2e-ae13-0410-ba18-ced7211e33de>
2004-03-31 08:08:46 +0000

    change the app order for administration tools
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@14756 923e3c2e-ae13-0410-ba18-ced7211e33de

--- registration/setup/setup.inc.php
+++ registration/setup/setup.inc.php
@@ -18,7 +18,7 @@
 	$setup_info['registration']['name']      = 'registration';
 	$setup_info['registration']['title']     = 'Registration';
 	$setup_info['registration']['version']   = '0.8.2';
-	$setup_info['registration']['app_order'] = '30';
+	$setup_info['registration']['app_order'] = '40';
 	$setup_info['registration']['enable']    = 1;
 
 	/* The tables this app creates */
