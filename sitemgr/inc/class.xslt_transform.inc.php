c27f07c7b509beedce5d5bc3eab18a9815b2e58c
ralfbecker <ralfbecker@923e3c2e-ae13-0410-ba18-ced7211e33de>
2004-05-02 18:13:30 +0000

    added error-message if no xslt extension is availible
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@15064 923e3c2e-ae13-0410-ba18-ced7211e33de

--- sitemgr/inc/class.xslt_transform.inc.php
+++ sitemgr/inc/class.xslt_transform.inc.php
@@ -11,6 +11,16 @@
 
 	/* $Id$ */
 
+// some constanst for pre php4.3
+if (!defined('PHP_SHLIB_SUFFIX'))
+{
+	define('PHP_SHLIB_SUFFIX',strtoupper(substr(PHP_OS, 0,3)) == 'WIN' ? 'dll' : 'so');
+}
+if (!defined('PHP_SHLIB_PREFIX'))
+{
+	define('PHP_SHLIB_PREFIX',PHP_SHLIB_SUFFIX == 'dll' ? 'php_' : '');
+}
+
 class xslt_transform
 {
 	var $arguments;
@@ -19,10 +29,16 @@ class xslt_transform
 	{
 		$this->xsltfile = $xsltfile;
 		$this->xsltparameters = $xsltparameters;
+
+		$this->xslt_extension_availible = extension_loaded('xslt') || @dl(PHP_SHLIB_PREFIX.'xslt.'.PHP_SHLIB_SUFFIX);
 	}
 
 	function apply_transform($title,$content)
 	{
+		if (!$this->xslt_extension_availible)
+		{
+			return 'The xslt_transformation used, needs the "xslt" extension of php !!!';
+		}
 		$xh = xslt_create();
 		$xsltarguments = array('/_xml' => $content);
 		$result = xslt_process($xh, 'arg:/_xml', $this->xsltfile, NULL, $xsltarguments,$this->xsltparameters);
