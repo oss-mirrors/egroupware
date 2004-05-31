4b5a4be3447601ca229f6d21359d27388db2b083
ralfbecker <ralfbecker@923e3c2e-ae13-0410-ba18-ced7211e33de>
2004-05-31 09:05:59 +0000

    completly reworked the so-layer to get everything correctly quoted
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@15338 923e3c2e-ae13-0410-ba18-ced7211e33de

--- bookmarks/inc/class.so.inc.php
+++ bookmarks/inc/class.so.inc.php
@@ -6,6 +6,7 @@
 	*                     http://www.renaghan.com/bookmarker                   *
 	* Ported to phpgroupware by Joseph Engo                                    *
 	* Ported to three-layered design by Michael Totschnig                      *
+	* SQL reworked by RalfBecker@outdoor-training.de to get everything quoted  *
 	* --------------------------------------------                             *
 	*  This program is free software; you can redistribute it and/or modify it *
 	*  under the terms of the GNU General Public License as published by the   *
@@ -22,34 +23,31 @@
 
 		function so()
 		{
-			$this->db          = $GLOBALS['phpgw']->db;
+			$this->db = $GLOBALS['phpgw']->db;
+			$this->table = 'phpgw_bookmarks';
+			$table_def = $this->db->get_table_definitions('bookmarks',$this->table);
+			$this->db->set_column_definitions($table_def['fd']);
 		}
 
 		function _list($cat_list,$public_user_list,$start,$where_clause)
 		{
-			$filtermethod = '( bm_owner=' . $GLOBALS['phpgw_info']['user']['account_id'];
+			$query = "SELECT * FROM $this->table WHERE ( bm_owner=" . (int)$GLOBALS['phpgw_info']['user']['account_id'];
 			if ($public_user_list)
 			{
-				$filtermethod .= " OR (bm_access='public' AND bm_owner in(" . implode(',',$public_user_list) . ')))';
+				$filtermethod .= ' OR ('.$this->db->column_data_implode(' AND ',array(
+					'bm_access'=>'public',
+					'bm_owner' => $public_user_list,
+				));
 			}
-			else
-			{
-				$filtermethod .= ' )';
-			}
-			$query = sprintf('SELECT * FROM phpgw_bookmarks WHERE %s',$filtermethod);
+			$query .= ' )';
+
 			if ($cat_list)
 			{
-				$where_clause .= " bm_category IN (" . implode(',',$cat_list) .") ";
+				$where_clause .= ' '.$this->db->column_data_implode(' AND ',array(
+					'bm_category' => $cat_list,
+				));
 			}
-			if ($where_clause)
-			{
-				$where_clause_sql = ' AND ' . $where_clause;
-			}
-			else
-			{
-				$where_clause_sql = ' ';
-			}
-			$query .= $where_clause_sql . ' order by bm_category, bm_name';
+			$query .= ($where_clause ? ' AND '.$where_clause : '') . ' ORDER BY bm_category, bm_name';
 
 			$this->db->query($query,__LINE__,__FILE__);
 			$this->total_records = $this->db->num_rows();
@@ -61,56 +59,41 @@
 
 			while ($this->db->next_record())
 			{
-				$bookmark['name'] = $GLOBALS['phpgw']->strip_html($this->db->f('bm_name'));
-				$bookmark['url'] = $GLOBALS['phpgw']->strip_html($this->db->f('bm_url'));
-				$bookmark['desc'] = $GLOBALS['phpgw']->strip_html($this->db->f('bm_desc'));
-				$bookmark['keywords'] = $GLOBALS['phpgw']->strip_html($this->db->f('bm_keywords'));
-				$bookmark['owner'] = $this->db->f('bm_owner');
-				$bookmark['access'] = $this->db->f('bm_access');
-				$bookmark['category'] = $this->db->f('bm_category');
-				$bookmark['rating'] = $this->db->f('bm_rating');
-				$bookmark['visits'] = $this->db->f('bm_visits');
-				$bookmark['info'] = $this->db->f('bm_info');
-				$result[$this->db->f('bm_id')] = $bookmark;
+				$result[$this->db->f('bm_id')] = $this->_db2bookmark();
 			}
 			return $result;
 		}
 
+		function _db2bookmark($do_htmlspecialchars = True)
+		{
+			foreach(array('name','url','desc','keywords','owner','access','category','rating','visits','info') as $name)
+			{
+				$bookmark[$name] = $this->db->f('bm_'.$name);
+			}
+			if ($do_htmlspecialchars)
+			{
+				foreach(array('name','url','desc','keywords') as $name)
+				{
+					$bookmark[$name] = $GLOBALS['phpgw']->strip_html($bookmark[$name]);
+				}
+			}
+			return $bookmark;
+		}
+
 		function read($id,$do_htmlspecialchars=True)
 		{
-			$query = "SELECT * FROM phpgw_bookmarks WHERE bm_id=$id";
-			$this->db->query($query);
-			if ($this->db->next_record())
+			$query = "SELECT * FROM $this->table WHERE bm_id=".(int)$id;
+			$this->db->query($query,__LINE__,__FILE__);
+			if (!$this->db->next_record())
 			{
-				$bookmark['name'] = $this->db->f('bm_name');
-				$bookmark['url'] = $this->db->f('bm_url');
-				$bookmark['desc'] = $this->db->f('bm_desc');
-				$bookmark['keywords'] = $this->db->f('bm_keywords');
-				$bookmark['owner'] = $this->db->f('bm_owner');
-				$bookmark['access'] = $this->db->f('bm_access');
-				$bookmark['category'] = $this->db->f('bm_category');
-				$bookmark['rating'] = $this->db->f('bm_rating');
-				$bookmark['visits'] = $this->db->f('bm_visits');
-				$bookmark['info'] = $this->db->f('bm_info');
-
-				if ($do_htmlspecialchars)
-				{
-					foreach(array('name','url','desc','keywords') as $name)
-					{
-						$bookmark[$name] = $GLOBALS['phpgw']->strip_html($bookmark[$name]);
-					}
-				}
-				return $bookmark;
+				return False;
 			}
-			else
-			{
-				return false;
-			}
+			return $this->_db2bookmark($do_htmlspecialchars);
 		}
 
 		function exists($url)
 		{
-			$query = sprintf("select count(*) from phpgw_bookmarks where bm_url='%s' and bm_owner='%s'",$url, $GLOBALS['phpgw_info']['user']['account_id']);
+			$query = "SELECT count(*) FROM $this->table WHERE bm_url=".$this->db->quote($url).' AND bm_owner='.(int)$GLOBALS['phpgw_info']['user']['account_id'];
 			$this->db->query($query,__LINE__,__FILE__);
 			$this->db->next_record();
 
@@ -119,80 +102,70 @@
 
 		function add($values)
 		{
-			if (! $values['access'])
-			{
-				$values['access'] = 'public';
-			}
+			$columns = $this->_bookmark2db($values,$values['timestamps'] ? $values['timestamps'] : time() . ',0,0');
+			$columns['bm_owner'] = (int) $GLOBALS['phpgw_info']['user']['account_id'];
+			$columns['bm_visits'] = 0;
 
-			if (! $values['timestamps'])
-			{
-				$values['timestamps'] = time() . ',0,0';
-			}
+			$query = "INSERT INTO $this->table (".implode(',',array_keys($columns)).") VALUES(".
+				$this->db->column_data_implode(',',$columns,False).')';
 
-			$query = sprintf("insert into phpgw_bookmarks (bm_url, bm_name, bm_desc, bm_keywords, bm_category,"
-				. "bm_rating, bm_owner, bm_access, bm_info, bm_visits) "
-				. "values ('%s','%s','%s','%s',%s,%s,'%s','%s','%s',0)",
-				$values['url'], addslashes($values['name']), addslashes($values['desc']), addslashes($values['keywords']),
-				$values['category'], $values['rating'], $GLOBALS['phpgw_info']['user']['account_id'], $values['access'],
-				$values['timestamps']);
-
-			if ($this->db->query($query,__LINE__,__FILE__))
+			if (!$this->db->query($query,__LINE__,__FILE__))
 			{
-				return $this->db->get_last_insert_id('phpgw_bookmarks','bm_id');
+				return False;
 			}
-			else
-			{
-				return false;
-			}
+			return $this->db->get_last_insert_id('phpgw_bookmarks','bm_id');
 		}
 
 		function update($id, $values)
 		{
 			#echo "so::update<pre>".htmlspecialchars(print_r($values,True))."</pre>\n";
-			if (! $values['access'])
+
+			$this->db->query("SELECT bm_info FROM $this->table WHERE bm_id=".(int)$id,__LINE__,__FILE__);
+			$this->db->next_record();
+			$ts = explode(',',$GLOBALS['phpgw']->db->f('bm_info'));
+			$ts[2] = time();
+
+			$columns = $this->_bookmark2db($values,implode(',',$ts));
+
+			// Update bookmark information.
+			$query = "UPDATE $this->table SET ".$this->db->column_data_implode(',',$columns).' WHERE bm_id='.(int)$id;
+
+			if (!$this->db->query($query,__LINE__,__FILE__))
+			{
+				return False;
+			}
+			return True;
+		}
+
+		function _bookmark2db($values,$timestamps)
+		{
+			if ($values['access'] != 'private')
 			{
 				$values['access'] = 'public';
 			}
-
-			$this->db->query("select bm_info from phpgw_bookmarks where bm_id='$id'",__LINE__,__FILE__);
-			$this->db->next_record();
-			$ts = explode(',',$GLOBALS['phpgw']->db->f('bm_info'));
-
-			$timestamps = sprintf('%s,%s,%s',$ts[0],$ts[1],time());
-
-			// Update bookmark information.
-			$query = sprintf("update phpgw_bookmarks set bm_url='%s', bm_name='%s', bm_desc='%s', "
-//				. "bm_keywords='%s', bm_category='%s', bm_subcategory='%s', bm_rating='%s',"
-				. "bm_keywords='%s', bm_category=%s, bm_rating='%s',"
-				. "bm_info='%s', bm_access='%s' where bm_id='%s'",
-					$values['url'], addslashes($values['name']), addslashes($values['desc']), addslashes($values['keywords']),
-//					$category, $subcategory, $values['rating'], $timestamps, $values['access'], $id);
-					$values['category'],$values['rating'], $timestamps, $values['access'], $id);
-
-			if ($this->db->query($query,__LINE__,__FILE__))
+			foreach(array('name','url','desc','keywords','access','category','rating') as $name)
 			{
-				return true;
+				$columns['bm_'.$name] = $values[$name];
 			}
-			else
-			{
-				return false;
-			}
+			$columns['bm_info'] = $timestamps;
+
+			return $columns;
 		}
 
 		function updatetimestamp($id,$timestamp)
 		{
-			$query = "UPDATE phpgw_bookmarks SET bm_info='$timestamp', bm_visits=bm_visits+1 WHERE bm_id=$id";
+			$query = "UPDATE $this->table SET bm_info=".$this->db->quote($timestamp).', bm_visits=bm_visits+1 WHERE bm_id='.(int)$id;
 			$this->db->query($query,__LINE__,__FILE__);
 		}
 
 		function delete($id)
 		{
-			$query = "delete from phpgw_bookmarks where bm_id=$id";
+			$query = "DELETE FROM $this->table WHERE bm_id=".(int)$id;
 			$this->db->query($query,__LINE__,__FILE__);
 			if ($this->db->Errno != 0)
 			{
 				return False;
 			}
-			return true;
+			return True;
 		}
 	}
