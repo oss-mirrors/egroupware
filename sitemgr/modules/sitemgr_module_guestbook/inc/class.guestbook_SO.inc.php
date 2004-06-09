92a03d152c7f86bfd9bee2876ea49c201314c262
ralfbecker <ralfbecker@923e3c2e-ae13-0410-ba18-ced7211e33de>
2004-06-09 09:01:01 +0000

    completly reworked the so-layer to get everything correctly quoted
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@15379 923e3c2e-ae13-0410-ba18-ced7211e33de

--- sitemgr/modules/sitemgr_module_guestbook/inc/class.guestbook_SO.inc.php
+++ sitemgr/modules/sitemgr_module_guestbook/inc/class.guestbook_SO.inc.php
@@ -7,25 +7,35 @@
 		function guestbook_SO()
 		{
 			$this->db = $GLOBALS['phpgw']->db;
+			$this->db->app = 'sitemgr_module_guestbook';	// as we run as sitemgr !
+			$this->books_table = 'phpgw_sitemgr_module_guestbook_books';
+			$this->entries_table = 'phpgw_sitemgr_module_guestbook_entries';
 		}
 
 
 		function create_book($title)
 		{
-			$this->db->query("INSERT INTO phpgw_sitemgr_module_guestbook_books (book_title) VALUES('$title')");
-			return $this->db->get_last_insert_id('phpgw_sitemgr_module_guestbook_books','book_id');
+			$this->db->insert($this->books_table,array('book_title' => $title),False,__LINE__,__FILE__);
+			return $this->db->get_last_insert_id($this->books_table,'book_id');
 		}
 
 
 		function add_entry($name,$comment,$book_id)
 		{
-			$timestamp = time();
-			$this->db->query("INSERT INTO phpgw_sitemgr_module_guestbook_entries (name,book_id,comment,timestamp) VALUES ('$name',$book_id,'$comment',$timestamp)");
+			$this->db->insert($this->entries_table,array(
+				'name' => $name,
+				'book_id' => $book_id,
+				'comment' => $comment,
+				'timestamp' => time()
+			),False,__LINE__,__FILE__);
 		}
 
 		function get_entries($book_id)
 		{
-			$this->db->query("SELECT name,comment,timestamp FROM phpgw_sitemgr_module_guestbook_entries WHERE book_id = $book_id ORDER BY timestamp DESC");
+			$this->db->select($this->entries_table,array('name','comment','timestamp'),
+				$this->db->expression($this->table,array('book_id' => $book_id),' ORDER BY timestamp DESC'),
+				__LINE__,__FILE__);
+
 			while($this->db->next_record())
 			{
 				foreach(array('name','comment','timestamp') as $field)
@@ -39,7 +49,7 @@
 
 		function get_books()
 		{
-			$this->db->query("SELECT * FROM phpgw_sitemgr_module_guestbook_books");
+			$this->db->select($this->books_table,'*',False,__LINE__,__FILE__);
 			while($this->db->next_record())
 			{
 				$result[$this->db->f('book_id')] = $this->db->f('book_title');
@@ -49,11 +59,12 @@
 
 		function delete_book($book_id)
 		{
-			$this->db->query("DELETE FROM phpgw_sitemgr_module_guestbook_entries WHERE book_id = $book_id");
-			$this->db->query("DELETE FROM phpgw_sitemgr_module_guestbook_books WHERE book_id = $book_id");
+			$this->db->delete($this->entries_table,array('book_id'=>$book_id),__LINE__,__FILE__);
+			$this->db->delete($this->books_table,array('book_id'=>$book_id),__LINE__,__FILE__);
 		}
+
 		function save_book($book_id,$title)
 		{
-			$this->db->query("UPDATE phpgw_sitemgr_module_guestbook_books SET book_title='$title' WHERE book_id = $book_id");
+			$this->db->update($this->books_table,array('book_title'=>$title),array('book_id'=>$book_id),__LINE__,__FILE__);
 		}
 	}
