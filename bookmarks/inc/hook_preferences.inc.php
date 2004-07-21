e5ebf942a76d4eb83f2562aeacab5ec2006c3100
alpeb <alpeb@923e3c2e-ae13-0410-ba18-ced7211e33de>
2004-07-21 14:13:57 +0000

    allow creation of more deeply nested categories
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@15828 923e3c2e-ae13-0410-ba18-ced7211e33de

--- bookmarks/inc/hook_preferences.inc.php
+++ bookmarks/inc/hook_preferences.inc.php
@@ -14,7 +14,7 @@
 		'Import Bookmarks' => $GLOBALS['phpgw']->link('/index.php','menuaction=bookmarks.ui.import'),
 		'Export Bookmarks' => $GLOBALS['phpgw']->link('/index.php','menuaction=bookmarks.ui.export'),
 		'Grant Access'  => $GLOBALS['phpgw']->link('/index.php','menuaction=preferences.uiaclprefs.index&acl_app='.$appname),
-		'Edit Categories' => $GLOBALS['phpgw']->link('/index.php','menuaction=preferences.uicategories.index&cats_app=' . $appname . '&global_cats=True')
+		'Edit Categories' => $GLOBALS['phpgw']->link('/index.php','menuaction=preferences.uicategories.index&cats_app=' . $appname . '&cats_level=True&global_cats=True')
 	);
 	display_section('bookmarks','Bookmarks',$file);
 ?>
