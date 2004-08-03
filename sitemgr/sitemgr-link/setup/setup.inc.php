354f1ecf80997c41027549cf708854983a3aaed6
ralfbecker <ralfbecker@923e3c2e-ae13-0410-ba18-ced7211e33de>
2004-08-03 08:03:04 +0000

    added 1.0.1 dependency for the api
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@16118 923e3c2e-ae13-0410-ba18-ced7211e33de

--- sitemgr/sitemgr-link/setup/setup.inc.php
+++ sitemgr/sitemgr-link/setup/setup.inc.php
@@ -21,7 +21,7 @@
 	/* Dependencies for this app to work */
 	$setup_info['sitemgr-link']['depends'][] = array(
 		'appname'  => 'phpgwapi',
-		'versions' => Array('0.9.14','0.9.15','1.0.0')
+		'versions' => Array('0.9.14','0.9.15','1.0.0','1.0.1')
 	);
 	$setup_info['sitemgr-link']['depends'][] = array(
 		'appname' => 'sitemgr',
