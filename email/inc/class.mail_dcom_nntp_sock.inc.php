c48defc565b6ac4e6eaf480c25c3d4f0ab16b25c
reinerj <reinerj@923e3c2e-ae13-0410-ba18-ced7211e33de>
2004-08-09 10:26:59 +0000

    replace the crap
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@16282 923e3c2e-ae13-0410-ba18-ced7211e33de

--- email/inc/class.mail_dcom_nntp_sock.inc.php
+++ email/inc/class.mail_dcom_nntp_sock.inc.php
@@ -1,12 +1,12 @@
 <?php
   /**************************************************************************\
-  * phpGroupWare API - NNTP                                                  *
+  * eGroupWare API - NNTP                                                    *
   * This file written by Mark Peters <skeeter@phpgroupware.org>              *
   * Handles specific operations in dealing with NNTP                         *
   * Copyright (C) 2001 Mark Peters                                           *
   * -------------------------------------------------------------------------*
-  * This library is part of the phpGroupWare API                             *
-  * http://www.phpgroupware.org/api                                          * 
+  * This library is part of the eGroupWare API                               *
+  * http://www.egroupware.org/                                               * 
   * ------------------------------------------------------------------------ *
   * This library is free software; you can redistribute it and/or modify it  *
   * under the terms of the GNU Lesser General Public License as published by *
