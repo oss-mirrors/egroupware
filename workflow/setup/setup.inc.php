a9d03e0a8ed8f1963579b7ab6b7e7b3a883a6862
alpeb <alpeb@923e3c2e-ae13-0410-ba18-ced7211e33de>
2004-09-17 17:55:08 +0000

    Fixed dependencies: workflow works only with api from 1.0.1 (the one in HEAD)
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@16958 923e3c2e-ae13-0410-ba18-ced7211e33de

--- workflow/setup/setup.inc.php
+++ workflow/setup/setup.inc.php
@@ -39,6 +39,6 @@
 	/* Dependencies for this app to work */
 	$setup_info['workflow']['depends'][] = array(
 		'appname' => 'phpgwapi',
-		'versions' => Array('0.9.13', '0.9.14', '0.9.15', '1.0.0', '1.0.1')
+		'versions' => Array('1.0.1')
 	);
 ?>
