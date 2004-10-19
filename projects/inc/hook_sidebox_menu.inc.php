864921934841761f4702803773337fa4b844de6c
ralfbecker <ralfbecker@923e3c2e-ae13-0410-ba18-ced7211e33de>
2004-10-19 17:04:20 +0000

    moved the project administrators menu-items from the (personal) projects preferences menubox to the projects admins menubox
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@17235 923e3c2e-ae13-0410-ba18-ced7211e33de

--- projects/inc/hook_sidebox_menu.inc.php
+++ projects/inc/hook_sidebox_menu.inc.php
@@ -40,7 +40,8 @@
 				$GLOBALS['phpgw']->link('/index.php','menuaction=projects.uiprojecthours.ttracker'),
 				
 				'Statistics' => 
-				$GLOBALS['phpgw']->link('/index.php','menuaction=projects.uistatistics.list_projects&action=mains'));
+				$GLOBALS['phpgw']->link('/index.php','menuaction=projects.uistatistics.list_projects&action=mains'),
+			);
 
 			if ($boprojects->isprojectadmin('pad') || $boprojects->isprojectadmin('pmanager'))
 			{
@@ -68,33 +69,35 @@
 					$GLOBALS['phpgw']->link('/index.php','menuaction=preferences.uiaclprefs.index&acl_app='.$appname);
 				$pref_file['Edit categories'] = 
 					$GLOBALS['phpgw']->link('/index.php','menuaction=preferences.uicategories.index&cats_app=projects&cats_level=True&global_cats=True');
-
-				if ($boprojects->isprojectadmin('pad') || $boprojects->isprojectadmin('pmanager'))
-				{
-					$pref_file['Roles'] =
-						$GLOBALS['phpgw']->link('/index.php','menuaction=projects.uiconfig.list_roles&action=role');
-					$pref_file['costs'] =
-						$GLOBALS['phpgw']->link('/index.php','menuaction=projects.uiconfig.list_roles&action=role&role_type=cost');
-					$pref_file['events'] =
-						$GLOBALS['phpgw']->link('/index.php','menuaction=projects.uiconfig.list_events');
-				}
 				display_sidebox($appname,$menu_title,$pref_file);
 			}
 
-			if ($GLOBALS['phpgw_info']['user']['apps']['admin'])
+			if ($GLOBALS['phpgw_info']['user']['apps']['admin'] || $boprojects->isprojectadmin('pad') || $boprojects->isprojectadmin('pmanager'))
 			{
 				$menu_title = $GLOBALS['phpgw_info']['apps'][$appname]['title'] . ' '. lang('Administration');
 
-				$admin_file['Site Configuration'] =
-					$GLOBALS['phpgw']->link('/index.php','menuaction=admin.uiconfig.index&appname=' . $appname);
-				$admin_file['managing committee'] =
-					$GLOBALS['phpgw']->link('/index.php','menuaction=projects.uiconfig.list_admins&action=pmanager');
-				$admin_file['project administrators'] =
-					$GLOBALS['phpgw']->link('/index.php','menuaction=projects.uiconfig.list_admins&action=pad');
-				$admin_file['sales department'] =
-					$GLOBALS['phpgw']->link('/index.php','menuaction=projects.uiconfig.list_admins&action=psale');
-				$admin_file['Global Categories'] =
-					$GLOBALS['phpgw']->link('/index.php','menuaction=admin.uicategories.index&appname=' . $appname);
+				if ($GLOBALS['phpgw_info']['user']['apps']['admin'] )
+				{
+					$admin_file['Site Configuration'] =
+						$GLOBALS['phpgw']->link('/index.php','menuaction=admin.uiconfig.index&appname=' . $appname);
+					/* $admin_file['managing committee'] =
+						$GLOBALS['phpgw']->link('/index.php','menuaction=projects.uiconfig.list_admins&action=pmanager'); */
+					$admin_file['project administrators'] =
+						$GLOBALS['phpgw']->link('/index.php','menuaction=projects.uiconfig.list_admins&action=pad');
+					/* $admin_file['sales department'] =
+						$GLOBALS['phpgw']->link('/index.php','menuaction=projects.uiconfig.list_admins&action=psale'); */
+					$admin_file['Global Categories'] =
+						$GLOBALS['phpgw']->link('/index.php','menuaction=admin.uicategories.index&appname=' . $appname);
+				}
+				if ($boprojects->isprojectadmin('pad') || $boprojects->isprojectadmin('pmanager'))
+				{
+					$admin_file['Roles'] =
+						$GLOBALS['phpgw']->link('/index.php','menuaction=projects.uiconfig.list_roles&action=role');
+					$admin_file['costs'] =
+						$GLOBALS['phpgw']->link('/index.php','menuaction=projects.uiconfig.list_roles&action=role&role_type=cost');
+					$admin_file['events'] =
+						$GLOBALS['phpgw']->link('/index.php','menuaction=projects.uiconfig.list_events');
+				}
 				display_sidebox($appname,$menu_title,$admin_file);
 			}
 			unset($boprojects);
