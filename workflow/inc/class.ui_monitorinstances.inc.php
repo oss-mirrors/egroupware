5b34fea894b45c7a660319cde89a91a5af8bcff4
mbartz <mbartz@923e3c2e-ae13-0410-ba18-ced7211e33de>
2004-12-07 02:21:15 +0000

    Changed GET parameter processing to correctly use the show_sort_order api call (passing an array). Changed error handling on the zero instances case. Removed extraneous code that was previously commented.
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@17503 923e3c2e-ae13-0410-ba18-ced7211e33de

--- workflow/inc/class.ui_monitorinstances.inc.php
+++ workflow/inc/class.ui_monitorinstances.inc.php
@@ -37,20 +37,20 @@
 			if ($filter_status) 
 			{
 				$this->wheres[] = "gi.`wf_status`='" . $filter_status . "'"; 
-				$this->extra[] = "filter_status=" .$filter_status;
+				$this->extra['filter_status'] = $filter_status;
 			}
 			if ($filter_process) {
 				$this->wheres[] = "ga.wf_p_id='" .$filter_process. "'"; 
-				$this->extra[] = "filter_process=" .$filter_process;
+				$this->extra['filter_process'] = $filter_process;
 			}		
 			if ($filter_activity) {
 				$this->wheres[] = "ga.wf_activity_id='" .$filter_activity. "'"; 
-				$this->extra[] = "filter_activity=" .$filter_act_status;
+				$this->extra['filter_activity'] = $filter_activity;
 			}
 			if ($filter_act_status)
 			{
-				$this->extra[] = "filter_act_status=" .$filter_act_status;
 				$this->wheres[] = "gia.`wf_status`='" . $filter_act_status . "'"; 
+				$this->extra['filter_act_status'] = $filter_act_status;
 			}
 
 			if( count($this->wheres) > 0 ) {
@@ -59,13 +59,11 @@
 			else {
 				$this->where = '';
 			}
-			if( count($this->extra) > 0 ) {
-		        $this->extra_params = implode('&', $this->extra);
-				$this->extra_params = '&'.$this->extra_params;
+
+			if( count($this->extra) == 0 ) {
+				$this->extra = '';
 			}
-			else {
-				$this->extra_params = '';
-			}
+
 			//echo "where: <pre>";print_r($this->where);echo "</pre>";
 			//echo "extra: <pre>";print_r($this->extra_params);echo "</pre>";
 
@@ -93,34 +91,38 @@
 		{
 			//_debug_array($instances_data);
 			$this->t->set_var(array(
-				'header_id'			=> $this->nextmatchs->show_sort_order($this->sort, 'wf_instance_id', $this->order, '', lang('Id'), $this->extra_params),
-				'header_process'	=> $this->nextmatchs->show_sort_order($this->sort, 'wf_p_id', $this->order, '', lang('Process'), $this->extra_param),
-				'header_activity'	=> $this->nextmatchs->show_sort_order($this->sort, 'wf_name', $this->order, '', lang('Activity'), $this->extra_param),
-				'header_status'		=> $this->nextmatchs->show_sort_order($this->sort, 'wf_status', $this->order, '', lang('Status'), $this->extra_param),
+				'header_id'			=> $this->nextmatchs->show_sort_order($this->sort, 'wf_instance_id', $this->order, '', lang('Id'), $this->extra),
+				'header_process'	=> $this->nextmatchs->show_sort_order($this->sort, 'wf_p_id', $this->order, '', lang('Process'), $this->extra),
+				'header_activity'	=> $this->nextmatchs->show_sort_order($this->sort, 'wf_name', $this->order, '', lang('Activity'), $this->extra),
+				'header_status'		=> $this->nextmatchs->show_sort_order($this->sort, 'wf_status', $this->order, '', lang('Status'), $this->extra),
 				//'header_act_status'		=> $this->nextmatchs->show_sort_order($this->sort, 'wf_act_status', $this->order, '', lang('Act. Status')),
 				'header_act_status'		=> lang('Act. Status'),
-				'header_user'		=> $this->nextmatchs->show_sort_order($this->sort, 'wf_user', $this->order, '', lang('User'), $this->extra_param),
+				'header_user'		=> $this->nextmatchs->show_sort_order($this->sort, 'wf_user', $this->order, '', lang('User'), $this->extra),
 			));
 
 			$this->t->set_block('monitor_instances', 'block_inst_table', 'inst_table');
 
-			foreach ($instances_data as $instance)
-			{
-				$this->t->set_var(array(
-					'inst_id_href'	=> $GLOBALS['phpgw']->link('/index.php', 'menuaction=workflow.ui_admininstance.form&iid='. $instance['wf_instance_id']),
-					'inst_id'		=> $instance['wf_instance_id'],
-					'inst_name'		=> $instance['wf_name'],
-					'inst_status'	=> $instance['wf_status'],
-					//'inst_user'		=> $instance['wf_user'],
-					'inst_user'		=> $GLOBALS['phpgw']->common->grab_owner_name($instance['wf_user']), 
-					'inst_procname'	=> $instance['wf_procname'],
-					'inst_version'	=> $instance['wf_version'],
-					'color_line'	=> $this->nextmatchs->alternate_row_color($tr_color),
-					'inst_act_status'	=>$instance['wf_act_status']
-				));
-				$this->t->parse('inst_table', 'block_inst_table', true);
+			if (!$instances_data) {
+				$this->t->set_var('inst_table', '<tr><td colspan="4" align="center">'. lang('There are no instances available') .'</td></tr>');
 			}
-			if (!$instances_data) $this->t->set_var('inst_table', '<tr><td colspan="4" align="center">'. lang('There are no instances available') .'</td></tr>');
+			else {
+				foreach ($instances_data as $instance)
+				{
+					$this->t->set_var(array(
+						'inst_id_href'	=> $GLOBALS['phpgw']->link('/index.php', 'menuaction=workflow.ui_admininstance.form&iid='. $instance['wf_instance_id']),
+						'inst_id'		=> $instance['wf_instance_id'],
+						'inst_name'		=> $instance['wf_name'],
+						'inst_status'	=> $instance['wf_status'],
+						//'inst_user'		=> $instance['wf_user'],
+						'inst_user'		=> $GLOBALS['phpgw']->common->grab_owner_name($instance['wf_user']), 
+						'inst_procname'	=> $instance['wf_procname'],
+						'inst_version'	=> $instance['wf_version'],
+						'color_line'	=> $this->nextmatchs->alternate_row_color($tr_color),
+						'inst_act_status'	=>$instance['wf_act_status']
+					));
+					$this->t->parse('inst_table', 'block_inst_table', true);
+				}
+			}
 		}
 
 		function show_filter_status($all_statuses, $filter_status)
@@ -157,7 +159,6 @@
 					'filter_user_selected'	=> ($user == $filter_user)? 'selected="selected"' : '',
 					'filter_user_value'		=> $user,
 					'filter_user_name'		=> $GLOBALS['phpgw']->common->grab_owner_name($user)
-//					'filter_user_name'		=> $user					
 				));
 				$this->t->parse('filter_user', 'block_filter_user', true);
 			}
