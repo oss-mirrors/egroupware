26810ff5f2ab3419134b206d9d417ee3cc679ef0
dragob <dragob@923e3c2e-ae13-0410-ba18-ced7211e33de>
2005-01-05 16:05:13 +0000

    Module for inserting frames into Sitemgr's pages.
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@17543 923e3c2e-ae13-0410-ba18-ced7211e33de

---
+++ sitemgr/modules/class.module_frame.inc.php
@@ -0,0 +1,41 @@
+<?php
+   /**************************************************************************\
+   * eGroupWare SiteMgr - Web Content Management                              *
+   * http://www.egroupware.org                                                *
+   * --------------------------------------------                             *
+   *  This program is free software; you can redistribute it and/or modify it *
+   *  under the terms of the GNU General Public License as published by the   *
+   *  Free Software Foundation; either version 2 of the License, or (at your  *
+   *  option) any later version.                                              *
+   \**************************************************************************/
+
+   /* $Id$ */
+
+class module_frame extends Module 
+{
+   function module_frame()
+   {
+      $this->arguments = array(
+         'URL' => array(
+            'type' => 'textfield',
+            'params' => array('size' => 100),
+            'label' => lang('The URL to display')
+         ),
+         'width' => array(
+            'type' => 'textfield',
+            'label' => lang('Width')
+         ),
+         'height' => array(
+            'type' => 'textfield',
+            'label' => lang('Height')
+         )
+      );
+      $this->title = lang('HTML Frame');
+      $this->description = lang('This module lets you show a given URL inside an IFRAME in the page.');
+   }
+
+   function get_content(&$arguments,$properties) 
+   {
+      return '<iframe width="'.$arguments['width'].'" height="'.$arguments['height'].'" src="'.$arguments['URL'].'"></iframe>';
+   }
+}
