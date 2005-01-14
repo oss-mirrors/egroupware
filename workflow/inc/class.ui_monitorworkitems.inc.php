53651515f614393a51a007b30d2a4d63678758a2
mbartz <mbartz@923e3c2e-ae13-0410-ba18-ced7211e33de>
2005-01-14 02:44:13 +0000

    Streamlined filter logic
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@17573 923e3c2e-ae13-0410-ba18-ced7211e33de

--- workflow/inc/class.ui_monitorworkitems.inc.php
+++ workflow/inc/class.ui_monitorworkitems.inc.php
@@ -19,51 +19,53 @@
 			$this->order			= get_var('order', 'any', 'wf_order_id');
 			$this->sort				= get_var('sort', 'any', 'asc');
 			$this->sort_mode		= $this->order . '__'. $this->sort;
-			$filter_instance		= get_var('filter_instance', 'any', '');
-			$filter_user			= get_var('filter_user', 'any', '');
+	        $this->search_str 		= get_var('search_str','any','');
+		    	
+			$filter_process 		= (int)get_var('filter_process', 'any', 0);
+			$filter_activity		= (int)get_var('filter_activity', 'any', 0);
+			$filter_user			= (int)get_var('filter_user', 'any', 0);
+			$filter_instance		= (int)get_var('filter_instance', 'any', 0);
 			
 				//echo "order: <pre>";print_r($this->order);echo "</pre>";
 				//echo "where: <pre>";print_r($where);echo "</pre>";
 				
-		        $where = '';
-		        $wheres = array();
-				
-			if (isset($_REQUEST['filter_process']) && $_REQUEST['filter_process']) {  
-		    	$filter_process = (int)get_var('filter_process', 'any', 0);
-		        $wheres[] = "gp.wf_p_id='" .$filter_process. "'";
+			$this->extra = array();
+			if( $filter_process ) {  
+		        $this->wheres[] = "ga.`wf_p_id`=" .$filter_process;
+				$this->extra['filter_process'] = $filter_process;
 		    }
-			if (isset($_REQUEST['filter_instance']) && $_REQUEST['filter_instance']) {  
-		    	$filter_instance = (int)get_var('filter_instance', 'any', 0);
-		        $wheres[] = "wf_instance_id=" .$filter_instance;
+			if( $filter_activity ) {  
+		        $this->wheres[] = "ga.`wf_activity_id`=" .$filter_activity;
+				$this->extra['filter_activity'] = $filter_activity;
 		    }
-			if (isset($_REQUEST['filter_activity']) && $_REQUEST['filter_activity']) {  
-		    	$filter_activity = (int)get_var('filter_activity', 'any', 0);
-		        $wheres[] = "wf_activity_id='" .$filter_activity. "'";
+			if( $filter_user ) {  
+		        $this->wheres[] = "wf_user =" .$filter_user;
+				$this->extra['filter_user'] = $filter_user;
 		    }
-			if( count($wheres) > 0 ) {
-		        $where = implode(' and ', $wheres);
-				//echo "where: <pre>";print_r($where);echo "</pre>";
+			if( $filter_instance != 0 ) {  
+		        $this->wheres[] = "wf_instance_id =" .$filter_instance;
+				//$this->extra['filter_instance'] = $filter_instance;
+		    }
+			else {
+				$filter_instance = '';
+			}
+			if( count($this->wheres) > 0 ) {
+		        $this->where = implode(' and ', $this->wheres);
 			}
 			else {
-				$where = '';
+				$this->where = '';
+			}
+			if( count($this->extra) == 0 ) {
+				$this->extra = '';
 			}
 
 			$wi_users	= $this->process_monitor->monitor_list_wi_users();
-			$workitems	= $this->process_monitor->monitor_list_workitems($this->start, -1, $this->sort_mode, $this->search_str, $where);
+			$workitems	= $this->process_monitor->monitor_list_workitems($this->start, -1, $this->sort_mode, $this->search_str, $this->where);
 		    $this->stats= $this->process_monitor->monitor_stats();
 
 			$this->show_filter_process();
-			//$this->show_filter_activities();
 			$this->show_filter_unique_activities();
-			/*
-			if (isset($_REQUEST['filter_process']) && $_REQUEST['filter_process']) {  
-		    	$filter_process = (int)get_var('filter_process', 'any', 0);
-				$this->show_filter_unique_activities("ga.wf_p_id=" .$filter_process);
-		    }
-			else {
-				$this->show_filter_unique_activities();
-			}
-			*/
+
 			$this->show_filter_user($wi_users, $filter_user);
 			$this->show_workitems_table($workitems['data']);
 
