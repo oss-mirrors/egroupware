d23410a44fd90dee11acd46105d9ed76e440ff50
ralfbecker <ralfbecker@923e3c2e-ae13-0410-ba18-ced7211e33de>
2005-02-15 08:06:46 +0000

    re-added the api dependency for 1.0.1, the last comit removed it
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@17780 923e3c2e-ae13-0410-ba18-ced7211e33de

--- tts/setup/setup.inc.php
+++ tts/setup/setup.inc.php
@@ -31,7 +31,7 @@
 	/* Dependencies for this app to work */
 	$setup_info['tts']['depends'][] = array(
 		 'appname' => 'phpgwapi',
-		 'versions' => Array('0.9.14','0.9.15','0.9.16','1.0.0')
+		 'versions' => Array('0.9.14','0.9.15','0.9.16','1.0.0','1.0.1')
 	);
 
 
