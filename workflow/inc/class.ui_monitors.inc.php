9797496bcb9cce92306af1f07d85c2aa4476b274
regis_glc <regis_glc@923e3c2e-ae13-0410-ba18-ced7211e33de>
2005-07-06 23:11:52 +0000

    add cleanup actions on the monitor form to remove aborted instances or all instances from a given process, clean workitems in the same actions
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@18735 923e3c2e-ae13-0410-ba18-ced7211e33de

--- workflow/inc/class.ui_monitors.inc.php
+++ workflow/inc/class.ui_monitors.inc.php
@@ -1,6 +1,6 @@
 <?php
 
-	include(dirname(__FILE__) . SEP . 'class.monitor.inc.php');
+	require_once(dirname(__FILE__) . SEP . 'class.monitor.inc.php');
 
 	class ui_monitors extends monitor
 	{
@@ -8,6 +8,8 @@
 		var $public_functions = array(
 			'form'	=> true,
 		);
+		var $filter_process_cleanup_aborted;
+		var $cleanup_aborted_instances;
 
 		function ui_monitors()
 		{
@@ -17,33 +19,113 @@
 
 		function form()
 		{
+			$this->filter_process_cleanup_aborted = (int)get_var('filter_process_cleanup_aborted', 'POST', '');
+			$this->filter_process_cleanup = (int)get_var('filter_process_cleanup', 'POST', '');
+			$this->cleanup_aborted_instances = get_var('cleanup_aborted_instances', 'POST', '');
+			$this->cleanup_process = get_var('cleanup_process', 'POST', '');
+			
+			$this->show_monitor_tabs($this->class_name);
+			$this->show_filter_process_cleanup_aborted();
+			$this->show_filter_process_cleanup();
+			
+			if (!($this->cleanup_aborted_instances==''))
+			{
+				//process removing of aborted instances
+				$this->perform_aborted_cleanup();
+			}
+			if (!($this->cleanup_process==''))
+			{
+				//process removing of all instances and workitems of the choosen process
+				$this->perform_process_cleanup();
+			}
+			
 			$this->fill_local_variables();
 			$this->fill_monitor_stats($this->stats);
-                        $this->t->set_var(array('message' => implode('<br>', $this->message)));			
+                        $this->t->set_var(array('message' => implode('<br>', $this->message)));
+                        $this->fill_general_variables();
 			$this->finish();
 		}
 		
-		//! fill all images and links
+		function perform_aborted_cleanup()
+		{
+			if (!($this->filter_process_cleanup_aborted))
+			{
+				$this->message[] = lang('removing instances and workitems for aborted instances on all processes');
+			}
+			else
+			{
+				$this->message[] = lang('removing instances and workitems for aborted instances on process #%1',$this->filter_process_cleanup_aborted);
+			}
+			if ($this->process_monitor->remove_aborted($this->filter_process_cleanup_aborted))
+			{
+				$this->message[] = lang('complete cleanup of aborted instances done');
+				// we need to reload the monitor stats, we just changed them, in fact
+				$this->stats =& $this->process_monitor->monitor_stats();
+			}
+			else
+			{
+				$this->message[] = lang('error: removing was not done');
+			}
+		}
+
+		function perform_process_cleanup()
+		{
+			$this->message[] = lang('removing instances and workitems for all instances on process #%1',$this->filter_process_cleanup);
+			if ($this->process_monitor->remove_all($this->filter_process_cleanup))
+			{
+				$this->message[] = lang('complete cleanup of all instances done');
+				// we need to reload the monitor stats, we just changed them, in fact
+				$this->stats =& $this->process_monitor->monitor_stats();
+			}
+			else
+			{
+				$this->message[] = lang('error: removing was not done');
+			}
+		}
+		
+		function show_filter_process_cleanup_aborted()
+		{
+			$this->t->set_var(array(
+				'filter_process_cleanup_aborted_selected_all' => (!$this->filter_process_cleanup_aborted)? 'selected="selected"' : ''
+			));
+			$this->t->set_block($this->template_name, 'block_filter_process_cleanup_aborted', 'filter_process_cleanup_aborted');
+			foreach ($this->all_processes['data'] as $process)
+			{
+				$this->t->set_var(array(
+					'filter_process_cleanup_aborted_selected'       => ($process['wf_p_id'] == $this->filter_process_cleanup_aborted)? 'selected':'',
+					'filter_process_cleanup_aborted_value'          => $process['wf_p_id'],
+					'filter_process_cleanup_aborted_name'           => $process['wf_name'],
+					'filter_process_cleanup_aborted_version'        => $process['wf_version'],
+				));
+				$this->t->parse('filter_process_cleanup_aborted', 'block_filter_process_cleanup_aborted', true);
+			} 
+		}
+
+		function show_filter_process_cleanup()
+		{
+			$this->t->set_block($this->template_name, 'block_filter_process_cleanup', 'filter_process_cleanup');
+			foreach ($this->all_processes['data'] as $process)
+			{
+				$this->t->set_var(array(
+					'filter_process_cleanup_selected'       => ($process['wf_p_id'] == $this->filter_process_cleanup)? 'selected':'',
+					'filter_process_cleanup_value'          => $process['wf_p_id'],
+					'filter_process_cleanup_name'           => $process['wf_name'],
+					'filter_process_cleanup_version'        => $process['wf_version'],
+				));
+				$this->t->parse('filter_process_cleanup', 'block_filter_process_cleanup', true);
+			} 
+		}
+		
+		//! fill help messages
 		function fill_local_variables()
 		{
-			$improc	= $GLOBALS['phpgw']->common->image('workflow', 'monitorprocess');
-			$imacti	= $GLOBALS['phpgw']->common->image('workflow', 'monitoractivity');
-			$iminst	= $GLOBALS['phpgw']->common->image('workflow', 'monitorinstance');
-			$imwork	= $GLOBALS['phpgw']->common->image('workflow', 'monitor');
-
 			$this->t->set_var(array(
-					'img_monitor_processes'		=> '<img src="'.$improc.'" alt="{lang_monitor_processes}" title="{lang_monitor_processes}">',
-					'img_monitor_activities'	=> '<img src="'.$imacti.'" alt="{lang_monitor_activities}" title="{lang_monitor_activities}">',
-					'img_monitor_instances'		=> '<img src="'.$iminst.'" alt="{lang_monitor_instances}" title="{lang_monitor_instances}">',
-					'img_monitor_workitems'		=> '<img src="'.$imwork.'" alt="{lang_monitor_workitems}" title="{lang_monitor_workitems}">',
-					'link_monitor_processes'	=> $GLOBALS['phpgw']->link('/index.php', array('menuaction' => 'workflow.ui_monitorprocesses.form')),
-					'link_monitor_activities'	=> $GLOBALS['phpgw']->link('/index.php', array('menuaction' => 'workflow.ui_monitoractivities.form')),
-					'link_monitor_instances'	=> $GLOBALS['phpgw']->link('/index.php', array('menuaction' => 'workflow.ui_monitorinstances.form')),
-					'link_monitor_workitems'	=> $GLOBALS['phpgw']->link('/index.php', array('menuaction' => 'workflow.ui_monitorworkitems.form')),
 					'help_monitor_processes'	=> lang('list of processes with status and validity and, for each, counters of instances by status'),
 					'help_monitor_activities'	=> lang('list of all activities with, for each,  counters of instances by status'),
 					'help_monitor_instances'	=> lang('list of all instances with info about current status and activities and link to administration of theses instances'),
 					'help_monitor_workitems'	=> lang('list of all history items made by instances while they travel in the workflow with information about duration and date'),
+					'help_cleanup_aborted'		=> lang('warning: by using this button you will definitively remove all aborted instances AND their history (workitems) for the selected process.'),
+					'help_cleanup'			=> lang('warning: by using this button you will definitively remove all instances AND their history (workitems) for the selected process, whatever the state they are.'),
 			));
 		}
 
--- workflow/templates/default/monitors.tpl
+++ workflow/templates/default/monitors.tpl
@@ -1,5 +1,5 @@
 <LINK href="{monitors_css_link}"  type="text/css" rel="StyleSheet">
-<div class="monitor_message">{message}</div>
+<div class="message">{message}</div>
 {monitor_tabs}
 <table class="monitor_table_header">
 	<tr class="th">
@@ -51,5 +51,47 @@
 			{help_monitor_workitems}
 		</td>
 	</tr>
+</table>
+<table class="monitor_table_header">
+<form action="{form_action}" method="post">
+	<tr class="th">
+		<td colspan="3" class="monitor_header_title">
+			{lang_cleanup_actions}
+		</td>
+	</tr>
+	<tr class="row_on">
+		<td>
+			{lang_Process:}
+			<select name="filter_process_cleanup_aborted">
+                        <option {filter_process_cleanup_aborted_selected_all} value="">{lang_All}</option>
+                        <!-- BEGIN block_filter_process_cleanup_aborted -->
+                        <option {filter_process_cleanup_aborted_selected} value="{filter_process_cleanup_aborted_value}">{filter_process_cleanup_aborted_name} {filter_process_cleanup_aborted_version}</option>
+                        <!-- END block_filter_process_cleanup_aborted -->
+                        </select>
+		</td>
+		<td>
+			<input type="submit" name="cleanup_aborted_instances" value="{lang_cleanup_aborted_instances}" />
+		</td>
+		<td>
+			{help_cleanup_aborted}
+		</td>
+	</tr>
+	<tr class="row_off">
+		<td>
+			{lang_Process:}
+			<select name="filter_process_cleanup">
+                        <!-- BEGIN block_filter_process_cleanup -->
+                        <option {filter_process_cleanup_selected} value="{filter_process_cleanup_value}">{filter_process_cleanup_name} {filter_process_cleanup_version}</option>
+                        <!-- END block_filter_process_cleanup -->
+                        </select>
+		</td>
+		<td>
+			<input type="submit" name="cleanup_process" value="{lang_remove_all_instances_from_this_process}" />
+		</td>
+		<td>
+			{help_cleanup}
+		</td>
+	</tr>
+</form>
 </table>
 {monitor_stats}
