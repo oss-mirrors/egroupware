ea3b58c6d11f920bfa9524e7d5c6f4970dd55823
regis_glc <regis_glc@923e3c2e-ae13-0410-ba18-ced7211e33de>
2005-07-07 23:05:11 +0000

    add some comments, some transactions, review syntax in list_mappings and add a list_mapped_users
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@18743 923e3c2e-ae13-0410-ba18-ced7211e33de

--- workflow/inc/engine/src/ProcessManager/RoleManager.php
+++ workflow/inc/engine/src/ProcessManager/RoleManager.php
@@ -1,7 +1,7 @@
 <?php
-include_once(GALAXIA_LIBRARY.'/src/ProcessManager/BaseManager.php');
+require_once(GALAXIA_LIBRARY.SEP.'src'.SEP.'ProcessManager'.SEP.'BaseManager.php');
 //!! RoleManager
-//! A class to maniplate roles.
+//! A class to manipulate roles.
 /*!
   This class is used to add,remove,modify and list
   roles used in the Workflow engine.
@@ -17,8 +17,8 @@ include_once(GALAXIA_LIBRARY.'/src/ProcessManager/BaseManager.php');
 class RoleManager extends BaseManager {
     
   /*!
-    Constructor takes a PEAR::Db object to be used
-    to manipulate roles in the database.
+  *  Constructor takes a PEAR::Db object to be used
+  *  to manipulate roles in the database.
   */
   function RoleManager($db) 
   {
@@ -35,7 +35,10 @@ class RoleManager extends BaseManager {
   }
   
   /*!
-    Gets a role fields are returned as an asociative array
+  * Gets a role
+  * @param $pId is the process Id
+  * @param $roleId is the role Id
+  * @return fields are returned as an associative array
   */
   function get_role($pId, $roleId)
   {
@@ -46,7 +49,10 @@ class RoleManager extends BaseManager {
   }
   
   /*!
-    Indicates if a role exists
+  * Indicates if a role exists
+  * @param $pid is the process Id
+  * @param $name is the name of the role
+  * @return the number of roles with this name on this process
   */
   function role_name_exists($pid,$name)
   {
@@ -61,7 +67,7 @@ class RoleManager extends BaseManager {
   {
   $query = "delete from `".GALAXIA_TABLE_PREFIX."user_roles` where wf_p_id=? AND wf_account_type=? and `wf_role_id`=? and `wf_user`=?";
   $this->query($query,array($pId, $account_type,$roleId, $user));
-  $query = "insert into ".GALAXIA_TABLE_PREFIX."user_roles (wf_p_id, wf_user, wf_role_id ,wf_account_type) 
+  $query = "insert into ".GALAXIA_TABLE_PREFIX."user_roles (wf_p_id, wf_user, wf_role_id ,wf_account_type)
   values(?,?,?,?)";
   $this->query($query,array($pId,$user,$roleId,$account_type));
   }
@@ -75,24 +81,46 @@ class RoleManager extends BaseManager {
   $this->query($query,array($user, $roleId));
   }
   
+  //!  List mappings
   /*!
-    List mappings
+  * get a list of roles/users mappings for a given process. Can expand groups to real users in the result and can restrict
+  * the mappings to a given subset of roles
+  * @param $pId Process Id. The mappings are returned for a complete process by default (see param roles_subset).
+  * @param $offset first record of the returned array
+  * @param $maxRecords maximum number of records for the returned array
+  * @param $sort_mode sort order for the query, like 'wf_name__ASC'
+  * @param $find searched string in role name, role description or user/group name
+  * @return an array containg for each row [wf_name] (role name),[wf_role_id],[wf_user] and [wf_account_type] ('u' user  or 'g' group)
+  * warning: you can have the same user or group several time if mapped to several roles.
   */
   function list_mappings($pId,$offset,$maxRecords,$sort_mode,$find)  {
     $sort_mode = $this->convert_sortmode($sort_mode);
-    if($find) {
+    $whereand = ' and gur.wf_p_id=? ';
+    $bindvars = Array($pId);
+    if($find) 
+    {
       // no more quoting here - this is done in bind vars already
       $findesc = '%'.$find.'%';
-      $query = "select `wf_name`,`gr`.`wf_role_id`,`wf_user` from `".GALAXIA_TABLE_PREFIX."roles` gr, `".GALAXIA_TABLE_PREFIX."user_roles` gur where `gr`.`wf_role_id`=`gur`.`wf_role_id` and `gur`.`wf_p_id`=? and ((`wf_name` like ?) or (`wf_user` like ?) or (`wf_description` like ?)) order by $sort_mode";
-      $result = $this->query($query,array($pId,$findesc,$findesc,$findesc), $maxRecords, $offset);
-      $query_cant = "select count(*) from `".GALAXIA_TABLE_PREFIX."roles` gr, `".GALAXIA_TABLE_PREFIX."user_roles` gur where `gr`.`wf_role_id`=`gur`.`wf_role_id` and `gur`.`wf_p_id`=? and ((`wf_name` like ?) or (`wf_user` like ?) or (`wf_description` like ?))";
-      $cant = $this->getOne($query_cant,array($pId,$findesc,$findesc,$findesc));
-    } else {
-      $query = "select `wf_name`,`gr`.`wf_role_id`,`wf_user` from `".GALAXIA_TABLE_PREFIX."roles` gr, `".GALAXIA_TABLE_PREFIX."user_roles` gur where `gr`.`wf_role_id`=`gur`.`wf_role_id` and `gur`.`wf_p_id`=? order by $sort_mode";
-      $result = $this->query($query,array($pId), $maxRecords, $offset);
-      $query_cant = "select count(*) from `".GALAXIA_TABLE_PREFIX."roles` gr, `".GALAXIA_TABLE_PREFIX."user_roles` gur where `gr`.`wf_role_id`=`gur`.`wf_role_id` and `gur`.`wf_p_id`=?";
-      $cant = $this->getOne($query_cant,array($pId));
+      $whereand .=  ' and ((wf_name like ?) or (wf_user like ?) or (wf_description like ?)) ';
+      $bindvars[] = $findesc;
+      $bindvars[] = $findesc;
+      $bindvars[] = $findesc;
     }
+    
+    $query = "select wf_name,gr.wf_role_id,wf_user,wf_account_type from
+                    ".GALAXIA_TABLE_PREFIX."roles gr,
+                    ".GALAXIA_TABLE_PREFIX."user_roles gur 
+                where gr.wf_role_id=gur.wf_role_id 
+                $whereand
+                order by $sort_mode";
+    $result = $this->query($query,$bindvars, $maxRecords, $offset);
+    $query_cant = "select count(*) from 
+                      ".GALAXIA_TABLE_PREFIX."roles gr, 
+                      ".GALAXIA_TABLE_PREFIX."user_roles gur 
+                  where gr.wf_role_id=gur.wf_role_id 
+                  $whereand";
+    $cant = $this->getOne($query_cant,$bindvars);
+
     $ret = Array();
     while($res = $result->fetchRow()) {
       $ret[] = $res;
@@ -101,6 +129,61 @@ class RoleManager extends BaseManager {
     $retval["data"] = $ret;
     $retval["cant"] = $cant;
     return $retval;
+  }
+
+  //!  List users/groups mapped on a process, this can be restricted to some activities
+  /*!
+  * Get a list of users/groups mapped for a given process. Can expand groups to real users in the result and can restrict
+  * the mappings to a given subset of roles and or activities
+  * @param $pId Process Id. The mappings are returned for a complete process by default (see param roles_subset or activities_subset).
+  * @param $expand_groups if true (false by default) we are not giving the group mappings but instead expand
+  *	theses groups to real users while avoiding repeating users twice.
+  * @param $roles_subset associative array containing a list of roles for which we want to restrict the list. 
+  * This array must contain the [wf_role_id] key with role id values. empty by default
+  * @param $activities_subset associative array containing a list of activities for which we want to restrict the list. 
+  * This array must contain the [wf_activity_id] key with activity id values. empty by default
+  * @return an array containg for each row the user or group id and an associated name
+  */
+
+  function list_mapped_users($pId,$expand_groups=false, $roles_subset=Array())  
+  {
+    $whereand = ' and gur.wf_p_id=? ';
+    $bindvars = Array($pId);
+    
+    if (!(count($roles_subset)==0))
+    {
+       $subset = Array();
+       foreach($roles_subset as $key => $value )
+       {
+         $subset[] = $value['wf_role_id'];
+       }
+       $whereand .= ' and ((gr.wf_role_id) in ('.implode($subset, ',').'))';
+    }
+    $query = "select distinct(wf_user),wf_account_type from
+                    ".GALAXIA_TABLE_PREFIX."roles gr,
+                    ".GALAXIA_TABLE_PREFIX."user_roles gur 
+                where gr.wf_role_id=gur.wf_role_id 
+                $whereand ";
+    $result = $this->query($query,$bindvars);
+    $ret = Array();
+    while($res = $result->fetchRow()) 
+    {
+      if (($expand_groups) && ($res['wf_account_type']=='g'))
+      {
+        //we have a group instead of a simple user and we want real users
+          //tmpres is a fake result
+        $real_users = galaxia_retrieve_group_users($res['wf_user'], true);
+        foreach ($real_users as $key => $value)
+        {
+          $ret[$key]=$value;
+        }
+      }
+      else
+      {
+        $ret[$res['wf_user']] = galaxia_retrieve_name($res['wf_user']);
+      }
+    }
+    return $ret;
   }
   
   /*!
@@ -134,30 +217,38 @@ class RoleManager extends BaseManager {
     $retval["cant"] = $cant;
     return $retval;
   }
-  
-  
-  
+   
   /*! 
-    Removes a role.
+  * Removes a role.
+  * @param $pId is the process Id
+  * @param $roleId is the role Id
+  * @return true if everything was ok, false in the other case
   */
   function remove_role($pId, $roleId)
   {
+    // start a transaction
+    $this->db->StartTrans();
     $query = "delete from `".GALAXIA_TABLE_PREFIX."roles` where `wf_p_id`=? and `wf_role_id`=?";
     $this->query($query,array($pId, $roleId));
     $query = "delete from `".GALAXIA_TABLE_PREFIX."activity_roles` where `wf_role_id`=?";
     $this->query($query,array($roleId));
     $query = "delete from `".GALAXIA_TABLE_PREFIX."user_roles` where `wf_role_id`=?";
     $this->query($query,array($roleId));
+    // perform commit (return true) or Rollback (return false)
+    return $this->db->CompleteTrans();
   }
   
   /*!
-    Updates or inserts a new role in the database, $vars is an asociative
-    array containing the fields to update or to insert as needed.
-    $pId is the processId
-    $roleId is the roleId  
+  * Updates or inserts a new role in the database, 
+  * @param $vars is an associative array containing the fields to update or to insert as needed.
+  * @param $pId is the processId
+  * @param $roleId is the roleId, 0 in insert mode
+  * @return the roleId (the new one if in insert mode) if everything was ok, false in the other case
   */
   function replace_role($pId, $roleId, $vars)
   {
+    // start a transaction
+    $this->db->StartTrans();
     $TABLE_NAME = GALAXIA_TABLE_PREFIX."roles";
     $now = date("U");
     $vars['wf_last_modif']=$now;
@@ -206,8 +297,16 @@ class RoleManager extends BaseManager {
       $this->query($query);
       $roleId = $this->getOne("select max(wf_role_id) from $TABLE_NAME where wf_p_id=$pId and wf_last_modif=$now"); 
     }
-    // Get the id
-    return $roleId;
+    // perform commit (return true) or Rollback (return false)
+    if ($this->db->CompleteTrans())
+    {
+      // Get the id
+      return $roleId;
+    }
+    else
+    {
+      return false;
+    }
   }
 }
 
