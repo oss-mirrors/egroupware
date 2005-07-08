f63cc5e408064d11112eb9d9e2f3913757d10cda
regis_glc <regis_glc@923e3c2e-ae13-0410-ba18-ced7211e33de>
2005-07-08 19:01:58 +0000

    add automatic recording of wf_next_user and wf_set_owner inputs after completion of activity form
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@18762 923e3c2e-ae13-0410-ba18-ced7211e33de

--- workflow/inc/engine/compiler/activity_pos.php
+++ workflow/inc/engine/compiler/activity_pos.php
@@ -1,14 +1,24 @@
 <?php
 //Code to be executed after an activity
-// we save name and activity only when the instance
+// we save name and others only when the instance
 // has been completed
 if ($GLOBALS['__activity_completed'])
 {
-  if(isset($_REQUEST['wf_name'])){
+  if(isset($_REQUEST['wf_name']))
+  {
     $instance->setName($_REQUEST['wf_name']);
   }
-  if(isset($_REQUEST['wf_priority'])){
+  if(isset($_REQUEST['wf_priority']))
+  {
     $instance->setPriority((int)$_REQUEST['wf_priority']);
+  }
+  if(isset($_REQUEST['wf_next_user']))
+  {
+    $instance->setNextUser((int)$_REQUEST['wf_next_user']);
+  }
+  if(isset($_REQUEST['wf_set_owner']))
+  {
+    $instance->setOwner((int)$_REQUEST['wf_set_owner']);
   }
 }
 ?>
