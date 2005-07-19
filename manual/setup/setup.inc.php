d7784b4c5e9025a87f62e04eeabb02092316af41
ralfbecker <ralfbecker@923e3c2e-ae13-0410-ba18-ced7211e33de>
2005-07-19 09:00:36 +0000

    wiki dependency allow 1.0.1
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@18831 923e3c2e-ae13-0410-ba18-ced7211e33de

--- manual/setup/setup.inc.php
+++ manual/setup/setup.inc.php
@@ -31,10 +31,10 @@
 	/* Dependencies for this app to work */
 	$setup_info['manual']['depends'][] = array(
 		 'appname' => 'phpgwapi',
-		 'versions' => Array('0.9.14','1.0.0','1.0.1')
+		 'versions' => Array('1.0.0','1.0.1')
 	);
 	$setup_info['manual']['depends'][] = array(
 		 'appname' => 'wiki',
-		 'versions' => Array('0.9.15.005','1.0.0')
+		 'versions' => Array('1.0.0','1.0.1')
 	);
 ?>
