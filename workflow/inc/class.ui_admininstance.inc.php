88d266a4aed4628eded7a17b43df962f16788b75
regis_glc <regis_glc@923e3c2e-ae13-0410-ba18-ced7211e33de>
2005-07-29 12:52:07 +0000

    use the new bo_uiinstance in edit mode for the instance administration template. Most of the code moved there.
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@18938 923e3c2e-ae13-0410-ba18-ced7211e33de

--- workflow/inc/class.ui_admininstance.inc.php
+++ workflow/inc/class.ui_admininstance.inc.php
@@ -15,6 +15,8 @@
 		var $process_manager;
 
 		var $activity_manager;
+		//egw category object to handle category in 'human' form
+		var $cat;
 
 		function ui_admininstance()
 		{
@@ -32,8 +34,9 @@
 			}
 
 			$this->instance_manager	=& CreateObject('workflow.workflow_instancemanager');
-			$this->process_manager	=& CreateObject('workflow.workflow_processmanager');
-			$this->activity_manager	=& CreateObject('workflow.workflow_activitymanager');
+			//$this->process_manager	=& CreateObject('workflow.workflow_processmanager');
+			//$this->activity_manager	=& CreateObject('workflow.workflow_activitymanager');
+			//$this->cat		=& CreateObject('phpgwapi.categories');
 		}
 
 		function form()
@@ -50,6 +53,7 @@
 			$instance_name          = get_var('instance_name', 'POST', '');
 			$instance_owner		= (int)get_var('owner', 'POST', 0);
 			$instance_priority      = (int)get_var('instance_priority', 'POST', 0);
+			$instance_category      = (int)get_var('instance_category', 'POST', 0);
 
 			// save changes
 			if (isset($_POST['save']))
@@ -59,6 +63,7 @@
 				$old_instance_name = get_var('instance_previous_name', 'POST', '');
 				$old_instance_owner	= (int)get_var('instance_previous_owner', 'POST', 0);
 				$old_instance_priority = (int)get_var('instance_previous_priority', 'POST', 0);
+				$old_instance_category = (int)get_var('instance_previous_category', 'POST', 0);
 				
 				if (!($instance_status == $old_instance_status))
 				{
@@ -75,6 +80,10 @@
 				if (!($instance_priority == $old_instance_priority))
 				{
 					$this->instance_manager->set_instance_priority($iid, $instance_priority);
+				}
+				if (!($instance_category == $old_instance_category))
+				{
+					$this->instance_manager->set_instance_category($iid, $instance_category);
 				}
 
 				// user reasignment
@@ -135,154 +144,31 @@
 				}
 			}
 
-			$instance		= $this->instance_manager->get_instance($iid);
-			$instance		= $this->instance_manager->get_instance($iid);
-			$process		= $this->process_manager->get_process($instance['wf_p_id']);
-			$proc_activities	= $this->activity_manager->list_activities($instance['wf_p_id'], 0, -1, 'wf_flow_num__asc', '', '');
-			$instance_acts		= $this->instance_manager->get_instance_activities($iid);
-			$properties		= $this->instance_manager->get_instance_properties($iid);
-
-
 			if (!$iid) die(lang('No instance indicated'));
-
-			$this->show_select_owner($instance);
-			$this->show_select_sendto($proc_activities['data']);
-			$this->show_instance_acts($iid, $instance_acts);
-			$this->show_properties($iid, $properties);
-
+			
+			//now we use bo_uiinstance to display the form
+			$instance =& CreateObject('workflow.workflow_instance');
+			$instance->getInstance($iid);
+			$inst_parser	=& CreateObject('workflow.bo_uiinstance', $this->t);
+			//this is necessary the CreateObject did not use ref parameters
+			$inst_parser->t =& $this->t;
+			//we use the bo_uiinstance parser in edit mode with the false parameter	
+			$inst_parser->parse_instance($instance, false);
+			$inst_parser->parse_instance_history($instance->workitems);
+				
 			// fill the general variables of the template
 			$this->t->set_var(array(
+				'instance'	=> $this->t->parse('output', 'instance_tpl'),
+				'history'	=> $this->t->parse('output', 'history_tpl'),
 				'message'		=> implode('<br />', $this->message),
 				'iid'			=> $iid,
 				'form_action'		=> $GLOBALS['phpgw']->link('/index.php', 'menuaction=workflow.ui_admininstance.form'),
-				'instance_process'	=> lang('Instance: %1 (Process: %2)', $instance['wf_instance_id'], $process['wf_name'] . ' ' . $process['wf_version']),
-				'inst_started'		=> $GLOBALS['phpgw']->common->show_date($instance['wf_started']),
-				'inst_ended' 		=> $GLOBALS['phpgw']->common->show_date($instance['wf_ended']),
-				'wi_href'		=> $GLOBALS['phpgw']->link('/index.php', 'menuaction=workflow.ui_monitorworkitems.form&filter_instance='. $instance['wf_instance_id']),
-				'wi_wi'			=> $instance['wf_workitems'],
-				'instance_name'		=> $instance['wf_name'],
-				'status'		=> $instance['wf_status'],
-				'owner'			=> $instance['wf_owner'],
-				'instance_priority'	=> $instance['wf_priority'],
-				'status_active'		=> ($instance['wf_status'] == 'active')? 'selected="selected"' : '',
-				'status_exception'	=> ($instance['wf_status'] == 'exception')? 'selected="selected"' : '',
-				'status_completed'	=> ($instance['wf_status'] == 'completed')? 'selected="selected"' : '',
-				'status_aborted'	=> ($instance['wf_status'] == 'aborted')? 'selected="selected"' : '',
-			));
-
+							));
 
 			$this->translate_template('admin_instance');
 			$this->t->pparse('output', 'admin_instance');
 			$GLOBALS['phpgw']->common->phpgw_footer();
 		}
-
-		function show_select_owner($instance)
-		{
-			$users = $GLOBALS['phpgw']->accounts->get_list('accounts');
-			//echo "users: <pre>";print_r($users);echo "</pre>";
-
-			$this->t->set_block('admin_instance', 'block_select_owner', 'select_owner');
-
-			foreach ($users as $user)
-			{
-				$this->t->set_var(array(
-					'select_owner_selected'	=> ($user['account_id'] == $instance['wf_owner'])? 'selected="selected"' : '',
-					'select_owner_value'	=> $user['account_id'],
-					'select_owner_name'		=> $user['account_firstname'] . ' ' . $user['account_lastname'],
-				));
-				$this->t->parse('select_owner', 'block_select_owner', true);
-			}
-		}
-		
-		function show_select_sendto($proc_activities_data)
-		{
-			$this->t->set_block('admin_instance', 'block_select_sendto', 'select_sendto');
-			foreach ($proc_activities_data as $activity)
-			{
-				$this->t->set_var(array(
-					'sendto_act_value'	=> $activity['wf_activity_id'],
-					'sendto_act_name'	=> $activity['wf_name'],
-				));
-				$this->t->parse('select_sendto', 'block_select_sendto', true);
-			}
-			if (!count($proc_activities_data)) $this->t->set_var('select_sendto', '');
-		}
-
-		function show_instance_acts($iid, $instance_acts)
-		{
-			$this->t->set_block('admin_instance', 'block_instance_acts_table_users', 'instance_acts_table_users');
-			$users = $GLOBALS['phpgw']->accounts->get_list('accounts');
-			
-			if ($instance_acts)
-			{
-				$this->t->set_block('admin_instance', 'block_instance_acts_table', 'instance_acts_table');
-				foreach ($instance_acts as $activity)
-				{
-				        foreach ($users as $user)
-				        {
-				                $this->t->set_var(array(
-							'inst_act_usr_value'	=> $user['account_id'],
-							'inst_act_usr_selected'	=> ($user['account_id'] == $activity['wf_user'])? 'selected="selected"' : '',
-							'inst_act_usr_name'	=> $user['account_firstname'] . ' ' . $user['account_lastname'],
-						 ));
-						$this->t->parse('instance_acts_table_users', 'block_instance_acts_table_users', true);
-					}
-
-					if ($activity['wf_is_interactive'] == 'y')
-					{
-						$this->t->set_var('inst_act_run', '<a href="'. $GLOBALS['phpgw']->link('/index.php', 'menuaction=workflow.run_activity.go&activity_id='. $activity['wf_activity_id']).'&iid='. $iid. '"><img src="'. $GLOBALS['phpgw']->common->image('workflow', 'next') .'" alt="'. lang('run') .'" title="'. lang('run') .'" /></a>');
-					}
-					else
-					{
-						$this->t->set_var('inst_act_run', '');
-					}
-
-					$this->t->set_var(array(
-						'activity_user'			=> $activity['wf_user'],
-						'inst_act_name'			=> $activity['wf_name'],
-						'inst_act_status'		=> $activity['wf_act_status'],
-						'inst_act_id'			=> $activity['wf_activity_id'],
-						'inst_act_star_selected'	=> ($activity['wf_user'] == '*')? 'selected="selected"' : '',
-					));
-
-
-					$this->t->parse('instance_acts_table', 'block_instance_acts_table', true);
-				}
-
-				$this->t->set_block('admin_instance', 'block_instance_acts', 'instance_acts');
-				$this->translate_template('block_instance_acts');
-				$this->t->parse('instance_acts', 'block_instance_acts');
-			}
-			else
-			{
-				$this->t->set_block('admin_instance', 'block_instance_acts', 'instance_acts');
-				$this->t->set_var('instance_acts', '');
-			}
-		}
-
-		function show_properties($iid, $props)
-		{
-			$this->t->set_block('admin_instance', 'block_properties', 'properties');
-			foreach ($props as $key=>$prop)
-			{
-				if (strlen($prop) > 80)
-				{
-					$this->t->set_var('prop_value', '<textarea name="props['. $key .']" cols="80" rows="5">'. $prop .'</textarea>');
-				}
-				else
-				{
-					$this->t->set_var('prop_value', '<input type="text" name="props['. $key .']" value="'. $prop .'" />');
-				}
-				$this->t->set_var(array(
-					'prop_href'		=> $GLOBALS['phpgw']->link('/index.php', 'menuaction=workflow.ui_admininstance.form&iid='. $iid .'&unsetprop='. $key),
-					'img_trash'		=> $GLOBALS['phpgw']->common->image('workflow', 'trash'),
-					'prop_key'		=> $key,
-					'color_line'	=> $this->nextmatchs->alternate_row_color($tr_color),
-				));
-				$this->translate_template('block_properties');
-				$this->t->parse('properties', 'block_properties', true);
-			}
-			if (!count($props)) $this->t->set_var('properties', '<tr><td colspan="2" align="center">'. lang('There are no properties available') .'</td></tr>');
-		}
+		    
 	}
 ?>
