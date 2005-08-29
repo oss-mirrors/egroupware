6c206d3865824a3abd3f492724fe4f63f4d6f77b
regis_glc <regis_glc@923e3c2e-ae13-0410-ba18-ced7211e33de>
2005-08-29 16:03:33 +0000

    comment some unnecessary includes
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@19034 923e3c2e-ae13-0410-ba18-ced7211e33de

--- workflow/inc/class.workflow_baseactivity.inc.php
+++ workflow/inc/class.workflow_baseactivity.inc.php
@@ -3,7 +3,7 @@
 	require_once('engine/config.egw.inc.php');
 
 	require_once(GALAXIA_LIBRARY . SEP . 'src' . SEP . 'API' . SEP . 'BaseActivity.php');
-	require_once(GALAXIA_LIBRARY . SEP . 'src' . SEP . 'API' . SEP . 'activities' . SEP . 'Activity.php');
+/*	require_once(GALAXIA_LIBRARY . SEP . 'src' . SEP . 'API' . SEP . 'activities' . SEP . 'Activity.php');
 	require_once(GALAXIA_LIBRARY . SEP . 'src' . SEP . 'API' . SEP . 'activities' . SEP . 'End.php');
 	require_once(GALAXIA_LIBRARY . SEP . 'src' . SEP . 'API' . SEP . 'activities' . SEP . 'Join.php');
 	require_once(GALAXIA_LIBRARY . SEP . 'src' . SEP . 'API' . SEP . 'activities' . SEP . 'Split.php');
@@ -12,7 +12,7 @@
 	require_once(GALAXIA_LIBRARY . SEP . 'src' . SEP . 'API' . SEP . 'activities' . SEP . 'Start.php');
 	require_once(GALAXIA_LIBRARY . SEP . 'src' . SEP . 'API' . SEP . 'activities' . SEP . 'SwitchActivity.php');
 	require_once(GALAXIA_LIBRARY . SEP . 'src' . SEP . 'common' . SEP . 'WfSecurity.php');
-
+*/
 	class workflow_baseactivity extends BaseActivity
 	{
 		function workflow_baseactivity()
