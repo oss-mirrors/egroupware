fc73930e17a6db3dfebf5982e09265da5b717310
regis_glc <regis_glc@923e3c2e-ae13-0410-ba18-ced7211e33de>
2005-09-08 07:45:36 +0000

    Runtime: handling the end of the runtime, not dying on errors on automatic activities, return info in auto mode, auto-release on leaving
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@19099 923e3c2e-ae13-0410-ba18-ced7211e33de

--- workflow/inc/engine/src/common/WfRuntime.php
+++ workflow/inc/engine/src/common/WfRuntime.php
@@ -10,8 +10,8 @@ class WfRuntime extends Base
 {
   
   // processes config values cached for this object life duration
-  // init is done at first use for each process
-  var $processesConfig= Array();
+  // init is done at first use for the only process associated with this runtime object
+  var $conf= Array();
   
   //instance and activity are the two most important object of the runtime
   var $activity = null;
@@ -26,8 +26,11 @@ class WfRuntime extends Base
   var $transaction_in_progress = false; 
   //boolean, wether or not we are in debug mode 
   var $debug=false;
+  //boolean, wether or not we are in automotic mode (i.e.: non-interactive), big impact on error handling
+  var $auto_mode=false;
   
   /*!
+  * @public
   * Constructor takes a PEAR::Db object
   */
   function WfRuntime(&$db) 
@@ -47,6 +50,7 @@ class WfRuntime extends Base
   }
 
   /*!
+  * @private
   * Collect errors from all linked objects which could have been used by this object
   * Each child class should instantiate this function with her linked objetcs, calling get_error(true)
   * for example if you had a $this->process_manager created in the constructor you shoudl call
@@ -63,18 +67,23 @@ class WfRuntime extends Base
   }
 
   /*!
+  * @public
   * Call this function to end-up dying and giving a last message
   * @param $last_message is your last sentence
   * @param $include_errors is a false boolean by default, if true we'll include error messages
   * @param $debug is false by default, if true you will obtain more messages, if false you could obtain theses
-  * messages as well if this object has been placed in debug maode with setDebug(true)
+  * @param $dying true by default, tells the engine to die or not
+  * messages as well if this object has been placed in debug mode with setDebug(true)
   * recorded by this runtme object.
   * @return nothing, it die!
   */
-  function fail($last_message, $include_errors = false, $debug=false)
+  function fail($last_message, $include_errors = false, $debug=false, $dying=true)
   {
     $the_end = '';
+    //see if local objects have been set to enforce it
     if ($this->debug) $debug = true;
+    if ($this->auto_mode) $dying = false;
+    
     if ($include_errors)
     {
       $the_end = $this->get_error(false, $debug).'<br />';
@@ -86,51 +95,119 @@ class WfRuntime extends Base
       $this->db->FailTrans();
       $this->db->CompleteTrans();
     }
-    //this will make the session die
-    galaxia_show_error($the_end);
+    if ($dying)
+    {
+      //this will make the session die
+      galaxia_show_error($the_end, $dying);
+    }
+    else
+    {
+      //this will NOT BREAK the session!
+      return $the_end;
+    }
+  }
+
+  /*! 
+  * @private
+  * load the config values for the process associated with the runtime
+  * config values are cached while this WfRuntime object stays alive
+  * @param $arrayconf is a config array with default value i.e.:
+  *	* key is the config option name
+  *	* value is the config default value
+  * @return a config array with values associated with the current process
+  * for the asked config options and as well for som WfRuntime internal
+  * config options
+  */
+  function &getConfigValues(&$arrayconf)
+  {
+    if (!(isset($this->process)))
+    {
+      $this->loadProcess();
+    }
+    $arrayconf['auto-release_on_leaving_activity'] = 1;  
+    $this->conf =  $this->process->getConfigValues($arrayconf);
+    return $this->conf;
   }
 
   /*!
+  * @public
+  * retrieve the process object associated with th eactivity
+  * @param $pId is the process id of the process you want, if you do not give it we will try to
+  * take it from the activity
+  * @return a Process Object of the right type or false
+  */
+  function &loadProcess($pId=0)
+  {
+    if ( (!(isset($this->process))) || ($this->process->getProcessId()==0))
+    {
+      if ( (empty($pId)) || (!($pId)) )
+      {
+        $pId = $this->activity->getProcessId();
+        if ( (empty($pId)) || (!($pId)) )
+        {
+          //fail can return in auto mode or die
+          $errors = $this->fail(tra('No Process indicated'),true, $this->debug, !($this->auto_mode));
+          $this->error[] = $errors;
+          return false;
+        }
+      }
+      if ($this->debug) $this->error[] = 'loading process '.$pId;
+      $this->process->getProcess($this->activity->getProcessId($pId));
+    }
+    return $this->process;
+  }
+
+  /*!
+  * @public
   * retrieve the activity of the right type from a baseActivity Object
   * @param $activity_id is the activity_id you want
   * @param $with_roles will load the roles links on the object
   * @param $with_agents will load the agents links on the object
   * @return an Activity Object of the right type or false
   */
-  function loadActivity($activity_id, $with_roles= true,$with_agents=false)
+  function &loadActivity($activity_id, $with_roles= true,$with_agents=false)
   {
     if ( (empty($activity_id)) || (!($activity_id)) )
     {
-      $this->fail(tra('No activity indicated'),true);
+      //fail can return in auto mode or die
+      $errors = $this->fail(tra('No activity indicated'),true, $this->debug, !($this->auto_mode));
+      $this->error[] = $errors;
+      return false;
     }
     $base_activity =& new BaseActivity($this->db);
     $this->activity =& $base_activity->getActivity($activity_id, $with_roles, $with_agents);
     $this->activity_id = $activity_id;
-    $this->process->getProcess($this->activity->getProcessId());
     $this->error[] =  $base_activity->get_error();
-    $this->error[] =  $this->process->get_error();
+    if ($this->debug) $this->error[] = 'loading activity '.$activity_id;
     return $this->activity;
   }
   
   /*!
+  * @public
   * retrieve the instance which could be an empty object
   * @param $instanceId is the instance id
-  * @return an Instance Object which can be empty
+  * @return an Instance Object which can be empty or or string if something was turning bad
   */
-  function loadInstance($instanceId)
+  function &loadInstance($instanceId)
   {
     $this->instance_id = $instanceId;
     $this->instance->getInstance($instanceId);
     if ( ($this->instance->getInstanceId()==0) 
       && (! (($this->activity->getType()=='standalone') || ($this->activity->getType()=='start') )) )
     {
-      $this->fail(tra('no instance avaible'), true);
+      //fail can return in auto mode or die
+      $errors = $this->fail(tra('no instance avaible'), true, $this->debug, !($this->auto_mode));
+      $this->error[] = $errors;
+      return false;
     }
+    if ($this->debug) $this->error[] = 'loading instance '.$instanceId;
     return $this->instance;
   }
   
   /*!
-  * Perform necessary security checks at runtime
+  * Perform necessary security checks at runtime before running an activity
+  * This will as well lock the tables via the security object.
+  * It should be launched in a transaction.
   * @param $user is the user id
   * @param return true if ok, false if the user has no runtime access
   * instance and activity are unsetted in case of false check
@@ -155,24 +232,48 @@ class WfRuntime extends Base
     }
     else
     {
-      unset($this->activity);
-      unset($this->instance);
       return false;
     }
   }
+
+  /*!
+  * Perform necessary security checks at runtime
+  * This will as well lock the tables via the security object.
+  * It should be launched in a transaction.
+  * @param $user is the user id
+  * @param return true if ok, false if the user has no runtime access
+  * instance and activity are unsetted in case of false check
+  */
+  function checkUserRelease()
+  {
+    //the first thing to scan if wether or not this process is configured for auto-release
+    if ( (isset($this->conf['auto-release_on_leaving_activity'])) && ($this->conf['auto-release_on_leaving_activity']))
+    {
+      //this will test the release rights and lock the necessary rows in tables in case of 'release'
+      $result = $this->security->checkUserAction($this->activity_id,$this->instance_id,'release');
+      $this->error[] =  $this->security->get_error();
+      if ($result)
+      {
+        //we are granted an access to release but there is a special bad case where
+        //we are a user authorized at releasing instances owned by others and where
+        //this instance is owned by another (for some quite complex reasons). 
+        //we should not release this new user!!
+        //Then this is auto-release, not a conscious act and so we will release only 
+        //if we can still grab this instance (avoiding the bad case)
+        
+        //test grab before release
+        if ($this->checkUserRun($GLOBALS['user']))
+        {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
   
   /*!
-  * This part of the runtime object will load the compiled activity code and include it
-  * this is in fact an 'execution' of the activity code
-  */
-/*  function executeCompiledActivity()
-  {
-    $source = GALAXIA_PROCESSES . SEP . $this->process->getNormalizedName(). SEP . 'compiled' . SEP . $this->activity->getNormalizedName(). '.php';
-    require_once ($source);
-  }
-*/
-  /*!
-  *
+  * This will set/unset the WfRuntime in debug mode
+  * @debug_mode is true by default, set it to false to disable debug mode
   */
   function setDebug($debug_mode=true)
   {
@@ -180,7 +281,18 @@ class WfRuntime extends Base
   }
   
   /*!
-  *
+  * This will set/unset the WfRuntime in automatic mode. i.e : executing
+  * non-interactive or interactive activities. Automatic mode have big impacts
+  * on error handling and on the way activities are executed
+  * @auto_mode is true by default, set it to false to disable automatic mode
+  */
+  function setAuto($auto_mode=true)
+  {
+    $this->auto_mode = $auto_mode;
+  }
+  
+  /*!
+  * This function will start a transaction, call it before setActivityUser()
   */
   function StartRun()
   {
@@ -189,7 +301,7 @@ class WfRuntime extends Base
   }
   
   /*!
-  *
+  * This function ends the transactions started in StartRun()
   */
   function EndStartRun()
   {
@@ -201,27 +313,135 @@ class WfRuntime extends Base
   }
   
   /*!
-  * For interactive activities this function
-  * will set the current user
+  * For interactive activities this function will set the current user on the instance-activities table. 
+  * This will prevent having several user using the same activity on the same intsance at the same time
+  * But if you want this function to behave well you should call it after a CheckUserRun or a CheckUserRelease
+  * and inside a transaction. Theses others function will ensure the table will be locked and the user
+  * is really granted the action
+  * @param $grab is true by default, if false the user will be set to '*', releasing the instance-activity record
   */
-  function setActivityUser()
+  function setActivityUser($grab=true)
   {
     if(isset($GLOBALS['user']) && !empty($this->instance->instanceId) && !empty($this->activity_id)) 
     {
       if ($this->activity->isInteractive())
-      {// activity is interactive and we want the form, we'll try to grab the ticket on this instance-activity
-        if (!$this->instance->setActivityUser($this->activity_id,$GLOBALS['user']))
+      {// activity is interactive and we want the form, we'll try to grab the ticket on this instance-activity (or release)
+        if ($grab)
         {
-           $this->fail(lang("You do not have the right to run this activity anymore, maybe a concurrent access problem, refresh your datas.", true));
+          $new_user = $GLOBALS['user'];
+        }
+        else
+        {
+          $new_user= '*';
+        }
+        if (!$this->instance->setActivityUser($this->activity_id,$new_user))
+        {
+           //fail can return in auto mode or die
+           $errors = $this->fail(lang("You do not have the right to run this activity anymore, maybe a concurrent access problem, refresh your datas.", true, $this->debug, !($this->auto_mode)));
+           $this->error[] = $errors;
+           return false;
         }
       }// if activity is not interactive there's no need to grab the token
     }
     else
     {
-      $this->fail(lang("We cannot run this activity, maybe this instance or this activity do not exists anymore.", true));
+      //fail can return in auto mode or die
+      $errors= $this->fail(lang("We cannot run this activity, maybe this instance or this activity do not exists anymore.", true, $this->debug, !($this->auto_mode)));
+      $this->error[] = $errors;
+      return false;
     }    
   }
   
+  /*!
+  * Try to give some usefull info about the current runtime
+  * @return an associative arrays with keys/values which could be usefull
+  */
+  function getRuntimeInfo()
+  {
+    $result = Array();
+    if (isset($this->instance))
+    {
+      $result['instance_name'] = $this->instance->getName();
+      $result['instance_owner'] = $this->instance->getOwner();
+    }
+    if (isset($this->activity))
+    {
+      $result['activity_name'] = $this->activity->getName();
+      $result['activity_id'] = $this->activity_id;
+      $result['activity_type'] = $this->activity->getType();
+    }
+    return $result;
+  }
+
+  /*!
+  * This part of the runtime will be runned just after the "require_once ($source);" which is the inclusion
+  * of compiled php file. We are in fact after all the "user code" part. We should decide what to do next
+  * @param $debug is false by default
+  * @return an array which must be analysed byr the application run class. It contains 2 keys
+  *	* 'action' : value is a string is the action the run class should do
+  *		* 'return' should return the result we just returned (in auto mode, to propagate infos)
+  *		* 'loop' should just loop on the form, i.e.: do nothing
+  *		* 'leaving' should show a page for the user leaving the activity (Cancel or Close without completing)
+  *		* 'completed' should show a page for the user having completed the activity
+  *	* 'engine_info' : value is an array is an array containing a lot of infos about what was done by the engine
+  *		especially when completing the instance or when executing an automatic activity
+  */
+  function handle_postUserCode($debug=false)
+  {
+    $result = Array();
+    
+     // re-retrieve instance data which could have been modified by a complete
+     $this->instance_id	= $this->instance->getInstanceId();
+
+    // for interactive activities in non-auto mode:
+    if (!($this->auto_mode) && $this->activity->isInteractive())
+    {
+      if ($GLOBALS['__activity_completed'])
+      {
+        // activity is interactive and completed, 
+        // we have to continue the workflow
+        // and send any autorouted activity which could be after this one
+        // this is not done in the $instance->complete() to let
+        // xxx_pos.php code be executed before sending the instance
+
+        $result['engine_info'] =& $this->instance->sendAutorouted($this->activity_id);
+        //_debug_array($engine_info);
+
+        // application should display completed page
+        $result['action']='completed';
+        return $result;
+      }
+      // it hasn't been completed
+      else
+      {
+        if ($GLOBALS['workflow']['__leave_activity'])
+        {
+          // activity is interactive and the activity source set the 
+          // $GLOBALS[workflow][__leave_activity] it's a 'cancel' mode.
+          // we redirect the user to the leave activity page
+          $result['action']='leaving';
+          return $result;
+        }
+        else
+        { 
+          //the activity is not completed and the user doesn't want to leave
+          // we loop on the form
+          $result['action']='loop';
+          return $result;
+        }
+      }
+    }
+    else
+    { 
+      // in auto mode or with non interactive activities we return engine info
+      // and we collect our errors, we do not let them for other objects
+      $this->collect_errors($debug);
+      $result['engine_info']['debug'] = implode('<br />',array_filter($this->error));
+      $result['engine_info']['info'] = $this->getRuntimeInfo();
+      $result['action'] = 'return';
+      return $result;
+    }
+  }
 }
 
 
