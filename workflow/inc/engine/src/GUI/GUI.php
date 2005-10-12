c5d47150a0ca5c442bbf5068aed55f1a3900e89f
regis_glc <regis_glc@923e3c2e-ae13-0410-ba18-ced7211e33de>
2005-10-12 10:29:21 +0000

    SQL fix: new GUi with readonly query should handle the add_property parameter in the GROUP BY
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@19367 923e3c2e-ae13-0410-ba18-ced7211e33de

--- workflow/inc/engine/src/GUI/GUI.php
+++ workflow/inc/engine/src/GUI/GUI.php
@@ -418,8 +418,9 @@ class GUI extends Base {
               $mid 
               GROUP BY gi.wf_instance_id, gi.wf_started, gi.wf_owner, gia.wf_user, gi.wf_status, gi.wf_category, 
               gia.wf_status, ga.wf_name, ga.wf_type, gp.wf_name, ga.wf_is_interactive, ga.wf_is_autorouted, 
-              ga.wf_activity_id, gp.wf_version, gp.wf_p_id, gi.wf_name, gi.wf_priority
-              order by $sort_mode";
+              ga.wf_activity_id, gp.wf_version, gp.wf_p_id, gi.wf_name, gi.wf_priority";
+    $query .= ($add_properties)? ', gi.wf_properties' : '';
+    $qsuery.= "          order by $sort_mode";
     // (regis) this count query as to count global -unlimited- (instances/activities) not just instances
     // as we can have multiple activities for one instance and we will show all of them and the problem is 
     // that a user having memberships in several groups having the rights is counted several times. 
