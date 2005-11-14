3847928a6a5a9d7ceab34f53b556ee70ac4ba119
regis_glc <regis_glc@923e3c2e-ae13-0410-ba18-ced7211e33de>
2005-11-14 19:30:44 +0000

    modernize
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@19829 923e3c2e-ae13-0410-ba18-ced7211e33de

--- workflow/index.php
+++ workflow/index.php
@@ -1,36 +1,36 @@
 <?php
 	$phpgw_info = array();
-	$GLOBALS['phpgw_info']['flags'] = array(
+	$GLOBALS['egw_info']['flags'] = array(
 		'currentapp' => 'workflow',
 		'noheader'   => True,
 		'nonavbar'   => True
 	);
 	require_once('../header.inc.php');
 	
-	$GLOBALS['phpgw']->preferences->read_repository();
-        $startscreen = $GLOBALS['phpgw_info']['user']['preferences']['workflow']['startpage'];
-        if (!isset($startscreen))
-        {
-        	$startscreen='workflow.ui_userprocesses';
-        	$form_args = Array();
-        }
-        else
-        {
-        	if ($startscreen == 'workflow.ui_useractivities2')
-        	{
-        		$startscreen = 'workflow.ui_useractivities';
-        		$form_args = 1;
-        	}
-        	else
-        	{
-        		$form_args = array();
-        	}
-        }
-        ExecMethod($startscreen.'.form',$form_args);
-        
-//	$obj = CreateObject($startscreen);
+	$GLOBALS['egw']->preferences->read_repository();
+	$startscreen = $GLOBALS['egw_info']['user']['preferences']['workflow']['startpage'];
+	if (!isset($startscreen))
+	{
+		$startscreen='workflow.ui_userprocesses';
+		$form_args = Array();
+	}
+	else
+	{
+		if ($startscreen == 'workflow.ui_useractivities2')
+		{
+			$startscreen = 'workflow.ui_useractivities';
+			$form_args = 1;
+		}
+		else
+		{
+			$form_args = array();
+		}
+	}
+	ExecMethod($startscreen.'.form',$form_args);
+				
+//	$obj =& CreateObject($startscreen);
 //	$obj->form();
 
-//	$GLOBALS['phpgw']->common->phpgw_footer();
+//	$GLOBALS['egw']->common->egw_footer();
 
 ?>
