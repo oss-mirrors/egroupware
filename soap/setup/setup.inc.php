e2ce2a7c24f34c36cf7ddbcd69012f4cb19e798d
milosch <milosch@923e3c2e-ae13-0410-ba18-ced7211e33de>
2005-11-24 11:16:06 +0000

    Update to new versions
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@19964 923e3c2e-ae13-0410-ba18-ced7211e33de

--- soap/setup/setup.inc.php
+++ soap/setup/setup.inc.php
@@ -1,7 +1,7 @@
 <?php
 	/**************************************************************************\
-	* phpGroupWare - Addressbook                                               *
-	* http://www.phpgroupware.org                                              *
+	* eGroupWare - SOAP test app                                               *
+	* http://www.egroupware.org                                                *
 	* --------------------------------------------                             *
 	*  This program is free software; you can redistribute it and/or modify it *
 	*  under the terms of the GNU General Public License as published by the   *
@@ -23,8 +23,8 @@
 	$setup_info['soap']['hooks'][] = 'admin';
 
 	/* Dependencies for this app to work */
-	$setup_info['soap']['depends'][] = array(
-		 'appname' => 'phpgwapi',
-		 'versions' => Array('0.9.13', '0.9.14')
+	$setup_info['xmlrpc']['depends'][] = array(
+		'appname' => 'phpgwapi',
+		'versions' => Array('0.9.13','0.9.14','1.0.0','1.0.1')
 	);
 ?>
