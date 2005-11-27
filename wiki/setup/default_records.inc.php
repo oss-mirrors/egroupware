15148975e8970f892205dbefd2d5e8afe29a4d6e
ralfbecker <ralfbecker@923e3c2e-ae13-0410-ba18-ced7211e33de>
2005-11-27 23:31:29 +0000

    updated default_records to the egw_wiki_pages name and added a formatting rules page
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@20026 923e3c2e-ae13-0410-ba18-ced7211e33de

--- wiki/setup/default_records.inc.php
+++ wiki/setup/default_records.inc.php
@@ -1,37 +1,167 @@
 <?php
-  /**************************************************************************\
-  * eGroupWare - Setup                                                       *
-  * http://www.eGroupWare.org                                                *
-  * --------------------------------------------                             *
-  *  This program is free software; you can redistribute it and/or modify it *
-  *  under the terms of the GNU General Public License as published by the   *
-  *  Free Software Foundation; either version 2 of the License, or (at your  *
-  *  option) any later version.                                              *
-  \**************************************************************************/
-
-  /* $Id$ */
-
+	/**************************************************************************\
+	* eGroupWare - Setup                                                       *
+	* http://www.eGroupWare.org                                                *
+	* --------------------------------------------                             *
+	*  This program is free software; you can redistribute it and/or modify it *
+	*  under the terms of the GNU General Public License as published by the   *
+	*  Free Software Foundation; either version 2 of the License, or (at your  *
+	*  option) any later version.                                              *
+	\**************************************************************************/
+	
+	/* $Id$ */
 
 	$time = time();
 	$oProc->query("DELETE FROM phpgw_wiki_pages");
-	$pages = array(
+	foreach(array(
 		'RecentChanges' => '[[! *]]',
 		'eGroupWare' => "Welcome to '''Wiki''' - the eGroupWare Version of '''WikkiTikkiTavi'''. Wikis are a revolutionary new form of collaboration and online community.
 
 '''eGroupWare''' is the groupware suite you are useing right now. For further information see http://www.eGroupWare.org",
 		'WikkiTikkiTavi' => "= WikkiTikkiTavi =
 
-WikkiTikkiTavi is the original version this documentation system.
-Their documentation is usable for the ((eGroupWare)) '''Wiki''' too.
+WikkiTikkiTavi is the original version of this documentation system. Their [http://tavi.sourceforge.net documentation] is usable for the eGroupWare '''Wiki''' too.
 
-The documentation of WikkiTikkiTavi is online availible at: http://tavi.sourceforge.net
-You can learn about Wiki formatting at http://tavi.sourceforge.net/FormattingRules",
-	);
-	foreach($pages as $name => $body)
+'''Learn about Wiki formatting:'''
+----
+SmashWordsTogether to create a page link.  Click on the ? to edit the new page.
+
+You can also create ((free links)) that aren't WordsSmashedTogether.  Type them like this: {{```((free links))```}}.  
+----
+{{```''italic text''```}} becomes ''italic text''
+----
+{{```'''bold text'''```}} becomes '''bold text'''
+----
+{{```{{monospace text}}```}} becomes {{monospace text}}
+----
+{{```----```}} becomes a horizontal rule:
+----
+Create a remote link simply by typing its URL: http://www.egroupware.org
+
+If you like, enclose it in brackets to create a numbered reference and avoid cluttering the page; {{```[http://www.php.net]```}} becomes [http://www.php.net].
+
+Or you can have a description instead of a numbered reference; {{```[http://www.php.net/manual/en/ PHP Manual]```}} becomes [http://www.php.net/manual/en/ PHP Manual]
+----
+You can put a picture in a page by typing the URL to the picture (it must end in gif, jpg, or png).  For example, {{```http://www.egroupware.org/egroupware/phpgwapi/templates/idots/images/logo.png```}} becomes
+http://www.egroupware.org/egroupware/phpgwapi/templates/idots/images/logo.png
+----
+There are 2 possibilities for '''code formatting''':
+{{'''{{\$is_admin = \$GLOBALS['phpgw_info']['user']['apps']['admin'];}}'''}}
+or
+{{<code>}}
+if (\$_POST['add'])
+{
+   do_something();
+}
+{{</code>}}
+becomes
+
+{{\$GLOBALS['phpgw_info']['user']['apps']['admin']}}
+or
+<code>
+if (\$_POST['add'])
+{
+   do_something();
+}
+</code>
+----
+You can indent by starting a paragraph with one or more colons.
+
+{{```:Indent me!```}}
+{{```::Me too!```}}
+becomes
+
+:Indent me
+::Me too!
+----
+You can create bullet lists by starting a paragraph with one or more asterisks.
+
+{{```*Bullet one```}}
+{{```**Sub-bullet```}} 
+becomes
+
+*Bullet one
+**Sub-bullet
+----
+You can create a description list by starting a paragraph with the following syntax 
+
+{{```*;Item 1: Something```}}
+{{```*;Item 2: Something else```}}
+
+which gives
+
+*;Item 1: Something
+*;Item 2: Something else
+----
+Similarly, you can create numbered lists by starting a paragraph with one or more hashes.
+
+{{```#Numero uno```}}
+{{```#Number two```}}
+{{```##Sub-item```}}
+becomes
+
+#Numero uno
+#Number two
+##Sub-item
+----
+You can mix and match list types:
+
+<code>
+#Number one
+#*Bullet
+#Number two
+</code>
+#Number one
+#*Bullet
+#Number two
+----
+You can make various levels of heading by putting = signs before and after the text =
+= Level 1 heading =
+== Level 2 heading ==
+=== Level 3 heading ===
+==== Level 4 heading ====
+===== Level 5 heading =====
+====== Level 6 heading ======
+<code>
+= Level 1 heading =
+== Level 2 heading ==
+=== Level 3 heading ===
+==== Level 4 heading ====
+===== Level 5 heading =====
+====== Level 6 heading ======
+</code>
+----
+You can create tables using pairs of vertical bars:
+
+||cell one || cell two ||
+|||| big ol' line ||
+|| cell four || cell five ||
+|| cell six || here's a very long cell ||
+
+<code>
+||cell one || cell two ||
+|||| big ol' line ||
+|| cell four || cell five ||
+|| cell six || here's a very long cell ||
+</code>
+",
+	) as $name => $body)
 	{
-		$name = $GLOBALS['phpgw_setup']->db->quote($name);
-		$body = $GLOBALS['phpgw_setup']->db->quote($body);
-		$oProc->query("INSERT INTO phpgw_wiki_pages (wiki_id,wiki_name,wiki_lang,wiki_version,wiki_time,wiki_supercede,wiki_readable,wiki_writable,wiki_username,wiki_hostname,wiki_title,wiki_body,wiki_comment) VALUES (0,$name,'en',1,$time,$time,0,0,'setup','localhost',$name,$body,'added by setup')");
+		$oProc->insert('egw_wiki_pages',array(
+			'wiki_id'        => 0,
+			'wiki_name'      => $name,
+			'wiki_lang'      => 'en',
+			'wiki_version'   => 1,
+			'wiki_time'      => $time,
+			'wiki_supercede' => $time,
+			'wiki_readable'  => 0,
+			'wiki_writable'  => 0,
+			'wiki_username'  => 'setup',
+			'wiki_hostname'  => 'localhost',
+			'wiki_title'     => $name,
+			'wiki_body'      => $body,
+			'wiki_comment'   => 'added by setup',
+		),false,__LINE__,__FILE__,'wiki');
 	}
 
 	
