2387576987b8a712bb228e6a5c72a4187b15d9bf
regis_glc <regis_glc@923e3c2e-ae13-0410-ba18-ced7211e33de>
2005-12-12 10:14:26 +0000

    fix untranslated please wait message
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@20115 923e3c2e-ae13-0410-ba18-ced7211e33de

--- workflow/inc/class.run_activity.inc.php
+++ workflow/inc/class.run_activity.inc.php
@@ -328,8 +328,9 @@
 			{
 				$GLOBALS['egw']->js =& CreateObject('phpgwapi.javascript');
 			}
+			$please_wait=lang('Please wait, task in progress ...');
 			$GLOBALS['egw_info']['flags']['java_script'] .= '<script type="text/javascript">
-				document.write(\'<DIV id="loading"><BR><BR>Please wait, task in progress ...*</DIV>\');
+				document.write(\'<DIV id="loading"><BR><BR>'.$please_wait.'</DIV>\');
 				function hide_loading()
 				{
 					document.getElementById("loading").style.display="none";
