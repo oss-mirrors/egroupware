1a4d7bcb15cd7fb5cd9a17d0627e46fcc55549b5
prito <prito@923e3c2e-ae13-0410-ba18-ced7211e33de>
2006-03-19 03:39:36 +0000

    0.9.08 intial start with setup stuff
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@20619 923e3c2e-ae13-0410-ba18-ced7211e33de

---
+++ icalsrv/setup/setup.inc.php
@@ -0,0 +1,32 @@
+<?php
+	/* @file EGroupware - IcalSrv setup file 
+	* @package IcalSrv 
+	* http://www.egroupware.org                                                *
+	* --------------------------------------------                             *
+	*  This program is free software; you can redistribute it and/or modify it *
+	*  under the terms of the GNU General Public License as published by the   *
+	*  Free Software Foundation; either version 2 of the License, or (at your  *
+	*  option) any later version.                                              *
+	* @version 0.0.1
+	* @author erics
+	**************************************************************************/
+
+    /* $Id$ */
+	/* Basic information about this app */
+	$setup_info['icalsrv']['name']      = 'icalsrv';
+	$setup_info['icalsrv']['title']     = 'Ical Server';
+	$setup_info['icalsrv']['version']   = '0.9.2';
+	$setup_info['icalsrv']['app_order'] = 3;
+	$setup_info['icalsrv']['enable']    = 1;
+
+	/* The hooks this app includes, needed for hooks registration */
+	$setup_info['icalsrv']['hooks'][] = 'preferences';
+	$setup_info['icalsrv']['hooks'][] = 'admin';
+	$setup_info['icalsrv']['hooks'][] = 'sidebox_menu';
+
+	/* Dependencies for this app to work */
+	$setup_info['icalsrv']['depends'][] = array(
+		'appname' => 'phpgwapi',
+		'versions' => Array('1.2')
+	);
+?>
