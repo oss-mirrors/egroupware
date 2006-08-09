00579283c14cd3effe0791cd41ddfb244a042e00
mipmip <mipmip@923e3c2e-ae13-0410-ba18-ced7211e33de>
2006-08-09 09:26:19 +0000

    improve indention
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@22198 923e3c2e-ae13-0410-ba18-ced7211e33de

--- jinn/templatesSavant2/default/plg_conf_widget_select.tpl.php
+++ jinn/templatesSavant2/default/plg_conf_widget_select.tpl.php
@@ -1,12 +1,13 @@
 <tr>
-<td valign="top" class="<?=$rowval?>"><?=$this->cval[label]?></td><td valign="top" class="<?=$rowval?>">
-<select name="<?=$this->cval[fname]?>">
-   <?php foreach($this->cval[select_arr] as $optkey => $optname):?>
-   <?php unset($selected);
-	  if($this->set_val==$optkey) $selected='selected="selected"';
-   ?>
-   <option value="<?=$optkey?>" <?=$selected?>><?=$optname?></option>
+   <td valign="top" class="<?=$rowval?>"><?=$this->cval[label]?></td>
+   <td valign="top" class="<?=$rowval?>">
+	  <select name="<?=$this->cval[fname]?>">
+		 <?php foreach($this->cval[select_arr] as $optkey => $optname):?>
+		 <?php unset($selected);
+			if($this->set_val==$optkey) $selected='selected="selected"';
+		 ?>
+		 <option value="<?=$optkey?>" <?=$selected?>><?=$optname?></option>
 
-   <?php endforeach?>
-</select>
+		 <?php endforeach?>
+	  </select>
 </td></tr>
