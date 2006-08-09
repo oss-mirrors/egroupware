755e861b0b915d1124736a220f39613fc2e2c2e1
mipmip <mipmip@923e3c2e-ae13-0410-ba18-ced7211e33de>
2006-08-09 09:26:45 +0000

    make radio widgets work
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@22199 923e3c2e-ae13-0410-ba18-ced7211e33de

--- jinn/templatesSavant2/default/plg_conf_widget_radio.tpl.php
+++ jinn/templatesSavant2/default/plg_conf_widget_radio.tpl.php
@@ -1,10 +1,12 @@
 <tr>
-<td valign="top" class="<?=$rowval?>"><?=$this->cval[label]?></td><td valign="top" class="<?=$rowval?>">
-<?php foreach($this->radio_arr as $radio):?>
-<?php 
-   unset($checked);
-   if($this->set_val==$radio) $checked='checked';
-?>
-<input name="<?=$this->cval[fname]?>" type="radio" <?=$checked?> value="<?=$radio?>"><?=$radio?><br/>
-<?php endforeach?>
+   <td valign="top" class="<?=$rowval?>"><?=$this->cval[label]?></td><td valign="top" class="<?=$rowval?>">
+	  <?php if(!trim($this->set_val)) $checkeddef='checked="checked"'?>
+	  <?php foreach($this->cval[radio_arr] as $radio_val=>$radio_label):?>
+	  <?php 
+		 unset($checked);
+		 if($this->set_val==$radio_val) $checked='checked="checked"';
+	  ?>
+	  <input name="<?=$this->cval[fname]?>" type="radio" <?=$checkeddef?> <?=$checked?> value="<?=$radio_val?>"><?=$radio_label?><br/>
+	  <?php unset($checkeddef);?>
+	  <?php endforeach?>
 </td></tr>
