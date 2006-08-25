d547a2efec1d08cecc0760f7803a4d7bd0de652b
ralfbecker <ralfbecker@923e3c2e-ae13-0410-ba18-ced7211e33de>
2006-08-25 12:01:51 +0000

    fixed / circumvent problems with the update and postgres
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@22351 923e3c2e-ae13-0410-ba18-ced7211e33de

--- syncml/setup/tables_update.inc.php
+++ syncml/setup/tables_update.inc.php
@@ -14,138 +14,13 @@
 	$test[] = '0.9.0';
 	function syncml_upgrade0_9_0()
 	{
-		$GLOBALS['egw_setup']->oProc->DropColumn('egw_syncmldevinfo',array(
-			'fd' => array(
-				'dev_dtdversion' => array('type' => 'varchar','precision' => '10','nullable' => False),
-				'dev_numberofchanges' => array('type' => 'bool','nullable' => False),
-				'dev_largeobjs' => array('type' => 'bool','nullable' => False),
-				'dev_swversion' => array('type' => 'varchar','precision' => '100','nullable' => False),
-				'dev_oem' => array('type' => 'varchar','precision' => '100','nullable' => False),
-				'dev_model' => array('type' => 'varchar','precision' => '100','nullable' => False),
-				'dev_manufacturer' => array('type' => 'varchar','precision' => '100','nullable' => False),
-				'dev_devicetype' => array('type' => 'varchar','precision' => '100','nullable' => False),
-				'dev_deviceid' => array('type' => 'varchar','precision' => '100','nullable' => False),
-				'dev_datastore' => array('type' => 'text','nullable' => False)
-			),
-			'pk' => array(),
-			'fk' => array(),
-			'ix' => array(),
-			'uc' => array()
-		),'dev_id');
-		$GLOBALS['egw_setup']->oProc->RefreshTable('egw_syncmldevinfo',array(
-			'fd' => array(
-				'dev_dtdversion' => array('type' => 'varchar','precision' => '10','nullable' => False),
-				'dev_numberofchanges' => array('type' => 'bool','nullable' => False),
-				'dev_largeobjs' => array('type' => 'bool','nullable' => False),
-				'dev_swversion' => array('type' => 'varchar','precision' => '100','nullable' => False),
-				'dev_oem' => array('type' => 'varchar','precision' => '100','nullable' => False),
-				'dev_model' => array('type' => 'varchar','precision' => '100','nullable' => False),
-				'dev_manufacturer' => array('type' => 'varchar','precision' => '100','nullable' => False),
-				'dev_devicetype' => array('type' => 'varchar','precision' => '100','nullable' => False),
-				'dev_deviceid' => array('type' => 'varchar','precision' => '100','nullable' => False),
-				'dev_datastore' => array('type' => 'text','nullable' => False)
-			),
-			'pk' => array(),
-			'fk' => array(),
-			'ix' => array(),
-			'uc' => array()
-		));
+		// We drop and create the table new, as the info's in it are queried 
+		// automatic from the device if not present in the table.
+		// This way we dont have to fight with DB's who can create an 
+		// autoincrement-index for an existing table.
+		$GLOBALS['egw_setup']->oProc->DropTable('egw_syncmldevinfo');
 
-		return $GLOBALS['setup_info']['syncml']['currentver'] = '0.9.1';
-	}
-
-
-	$test[] = '0.9.1';
-	function syncml_upgrade0_9_1()
-	{
-		/* done by RefreshTable() anyway
-		$GLOBALS['egw_setup']->oProc->AddColumn('egw_syncmldevinfo','dev_id',array(
-			'type' => 'auto',
-			'nullable' => False
-		));*/
-		$GLOBALS['egw_setup']->oProc->RefreshTable('egw_syncmldevinfo',array(
-			'fd' => array(
-				'dev_dtdversion' => array('type' => 'varchar','precision' => '10','nullable' => False),
-				'dev_numberofchanges' => array('type' => 'bool','nullable' => False),
-				'dev_largeobjs' => array('type' => 'bool','nullable' => False),
-				'dev_swversion' => array('type' => 'varchar','precision' => '100','nullable' => False),
-				'dev_oem' => array('type' => 'varchar','precision' => '100','nullable' => False),
-				'dev_model' => array('type' => 'varchar','precision' => '100','nullable' => False),
-				'dev_manufacturer' => array('type' => 'varchar','precision' => '100','nullable' => False),
-				'dev_devicetype' => array('type' => 'varchar','precision' => '100','nullable' => False),
-				'dev_deviceid' => array('type' => 'varchar','precision' => '100','nullable' => False),
-				'dev_datastore' => array('type' => 'text','nullable' => False),
-				'dev_id' => array('type' => 'auto','nullable' => False)
-			),
-			'pk' => array('dev_id'),
-			'fk' => array(),
-			'ix' => array('dev_id'),
-			'uc' => array('dev_id')
-		));
-
-		return $GLOBALS['setup_info']['syncml']['currentver'] = '0.9.2';
-	}
-
-
-	$test[] = '0.9.2';
-	function syncml_upgrade0_9_2()
-	{
-		/* done by RefreshTable() anyway
-		$GLOBALS['egw_setup']->oProc->AlterColumn('egw_syncmldevinfo','dev_swversion',array(
-			'type' => 'varchar',
-			'precision' => '100'
-		));*/
-		/* done by RefreshTable() anyway
-		$GLOBALS['egw_setup']->oProc->AlterColumn('egw_syncmldevinfo','dev_oem',array(
-			'type' => 'varchar',
-			'precision' => '100'
-		));*/
-		/* done by RefreshTable() anyway
-		$GLOBALS['egw_setup']->oProc->AddColumn('egw_syncmldevinfo','dev_fwversion',array(
-			'type' => 'varchar',
-			'precision' => '100'
-		));*/
-		/* done by RefreshTable() anyway
-		$GLOBALS['egw_setup']->oProc->AddColumn('egw_syncmldevinfo','dev_hwversion',array(
-			'type' => 'varchar',
-			'precision' => '100'
-		));*/
-		/* done by RefreshTable() anyway
-		$GLOBALS['egw_setup']->oProc->AddColumn('egw_syncmldevinfo','dev_utc',array(
-			'type' => 'bool',
-			'nullable' => False
-		));*/
-		$GLOBALS['egw_setup']->oProc->RefreshTable('egw_syncmldevinfo',array(
-			'fd' => array(
-				'dev_dtdversion' => array('type' => 'varchar','precision' => '10','nullable' => False),
-				'dev_numberofchanges' => array('type' => 'bool','nullable' => False),
-				'dev_largeobjs' => array('type' => 'bool','nullable' => False),
-				'dev_swversion' => array('type' => 'varchar','precision' => '100'),
-				'dev_oem' => array('type' => 'varchar','precision' => '100'),
-				'dev_model' => array('type' => 'varchar','precision' => '100','nullable' => False),
-				'dev_manufacturer' => array('type' => 'varchar','precision' => '100','nullable' => False),
-				'dev_devicetype' => array('type' => 'varchar','precision' => '100','nullable' => False),
-				'dev_deviceid' => array('type' => 'varchar','precision' => '100','nullable' => False),
-				'dev_datastore' => array('type' => 'text','nullable' => False),
-				'dev_id' => array('type' => 'auto','nullable' => False),
-				'dev_fwversion' => array('type' => 'varchar','precision' => '100'),
-				'dev_hwversion' => array('type' => 'varchar','precision' => '100'),
-				'dev_utc' => array('type' => 'bool','nullable' => False)
-			),
-			'pk' => array('dev_id'),
-			'fk' => array(),
-			'ix' => array('dev_id'),
-			'uc' => array('dev_id',array('dev_model','dev_manufacturer','dev_swversion'))
-		));
-
-		return $GLOBALS['setup_info']['syncml']['currentver'] = '0.9.3';
-	}
-
-
-	$test[] = '0.9.3';
-	function syncml_upgrade0_9_3()
-	{
-		$GLOBALS['egw_setup']->oProc->DropColumn('egw_syncmldevinfo',array(
+		$GLOBALS['egw_setup']->oProc->CreateTable('egw_syncmldevinfo',array(
 			'fd' => array(
 				'dev_dtdversion' => array('type' => 'varchar','precision' => '10','nullable' => False),
 				'dev_numberofchanges' => array('type' => 'bool','nullable' => False),
@@ -165,36 +40,15 @@
 			'fk' => array(),
 			'ix' => array('dev_id'),
 			'uc' => array('dev_id',array('dev_model','dev_manufacturer','dev_swversion'))
-		),'dev_deviceid');
+		));
 
 		return $GLOBALS['setup_info']['syncml']['currentver'] = '0.9.4';
 	}
-
 
 	$test[] = '0.9.4';
 	function syncml_upgrade0_9_4()
 	{
 		$GLOBALS['egw_setup']->oProc->CreateTable('egw_syncmldeviceowner',array(
-			'fd' => array(
-				'owner_accountid' => array('type' => 'varchar','precision' => '200','nullable' => False),
-				'owner_devid' => array('type' => 'int','precision' => '4','nullable' => False),
-				'owner_deviceid' => array('type' => 'varchar','precision' => '100','nullable' => False)
-			),
-			'pk' => array(),
-			'fk' => array(),
-			'ix' => array('owner_accountid','owner_deviceid'),
-			'uc' => array(array('owner_accountid','owner_devid','owner_deviceid'))
-		));
-
-		return $GLOBALS['setup_info']['syncml']['currentver'] = '0.9.5';
-	}
-
-
-	$test[] = '0.9.5';
-	function syncml_upgrade0_9_5()
-	{
-		$GLOBALS['egw_setup']->oProc->RenameColumn('egw_syncmldeviceowner','owner_accountid','owner_locname');
-		$GLOBALS['egw_setup']->oProc->RefreshTable('egw_syncmldeviceowner',array(
 			'fd' => array(
 				'owner_locname' => array('type' => 'varchar','precision' => '200','nullable' => False),
 				'owner_devid' => array('type' => 'int','precision' => '4','nullable' => False),
@@ -205,9 +59,6 @@
 			'ix' => array('owner_locname','owner_deviceid'),
 			'uc' => array(array('owner_locname','owner_devid','owner_deviceid'))
 		));
-		
-		$GLOBALS['egw_setup']->oProc->query('delete from egw_syncmldevinfo', __LINE__, __FILE__);
-		
 		return $GLOBALS['setup_info']['syncml']['currentver'] = '0.9.6';
 	}
 ?>
