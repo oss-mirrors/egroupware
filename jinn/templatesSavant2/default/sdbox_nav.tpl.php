ec30fbbe394fac2b5813a104939824c625d2872d
mipmip <mipmip@923e3c2e-ae13-0410-ba18-ced7211e33de>
2006-10-18 14:25:50 +0000

    always align left
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@22676 923e3c2e-ae13-0410-ba18-ced7211e33de

--- jinn/templatesSavant2/default/sdbox_nav.tpl.php
+++ jinn/templatesSavant2/default/sdbox_nav.tpl.php
@@ -10,9 +10,9 @@
 		 <?php echo $this->sites_options?>
 	  </select>
    </div>
-   <div style="border-top:solid 1px #aaaaaa;"></div>
+   <div style="border-top:solid 1px #aaaaaa;text-align:left"></div>
    <?php if($this->objects_arr):?>
-   <div style="margin-top:5px;padding:2px;">
+   <div style="margin-top:5px;padding:2px;text-align:left">
 	  <img src="<?=$GLOBALS[phpgw]->common->image('jinn','database_small')?>" alt="" style="margin-right:2px;"><a href="<?=$GLOBALS[egw]->link('/index.php','menuaction=jinn.uiuser.index&site_object_id=-1')?>"><?php echo $this->site_name?></a>
 	  <ul style="padding:0px;margin:0px;list-style-type: none;">
 		 <?php foreach($this->objects_arr as $obj):?>
