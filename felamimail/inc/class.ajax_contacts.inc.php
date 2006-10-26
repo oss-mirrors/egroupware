c2e9073b3958cd1f0cf033c8a121d28b05b48e5d
lkneschke <lkneschke@923e3c2e-ae13-0410-ba18-ced7211e33de>
2006-10-26 10:04:05 +0000

    fixed composing of the real name
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@22739 923e3c2e-ae13-0410-ba18-ced7211e33de

--- felamimail/inc/class.ajax_contacts.inc.php
+++ felamimail/inc/class.ajax_contacts.inc.php
@@ -28,17 +28,19 @@
 			if (method_exists($GLOBALS['egw']->contacts,'search')) {
 				// 1.3+
 				$contacts = $GLOBALS['egw']->contacts->search(array(
-					'n_fn'       => $_searchString,
-					'email'      => $_searchString,
-					'email_home' => $_searchString,
-				),array('n_fn','email','email_home'),'n_fn','','%',false,'OR',array(0,20));
+					'n_given'	=> $_searchString,
+					'n_family'	=> $_searchString,
+					'email'		=> $_searchString,
+					'email_home'	=> $_searchString,
+				),array('n_given', 'n_family', 'email', 'email_home'), 'n_family', '', '%', false, 'OR', array(0,20));
 			} else {
 				// < 1.3
 				$contacts = $GLOBALS['egw']->contacts->read(0,20,array(
-					'fn' => 1,
-					'email' => 1,
-					'email_home' => 1,
-				), $_searchString, 'tid=n', '', 'fn');
+					'n_given'	=> 1,
+					'n_family'	=> 1,
+					'email'		=> 1,
+					'email_home'	=> 1,
+				), $_searchString, 'tid=n', '', 'n_family');
 			}
 
 			$response =& new xajaxResponse();
@@ -52,10 +54,11 @@
 					foreach(array($contact['email'],$contact['email_home']) as $email) {
 						if(!empty($email) && !isset($jsArray[$email])) {
 							$i++;
-							$str = $GLOBALS['egw']->translation->convert(trim($contact['n_fn'] ? $contact['n_fn'] : $contact['fn']) .' <'. trim($email) .'>', $this->charset, 'utf-8');
+							$realName = trim($contact['n_given'] . (!empty($contact['n_given']) ? ' ' : '') . $contact['n_family']);
+							$str = $GLOBALS['egw']->translation->convert($realName .' <'. trim($email) .'>', $this->charset, 'utf-8');
 							$innerHTML .= '<div class="inactiveResultRow" onclick="selectSuggestion($i)">'.
 								htmlentities($str, ENT_QUOTES, 'utf-8') .'</div>';
-							$jsArray[$email] = addslashes(trim($contact['n_fn'] ? $contact['n_fn'] : $contact['fn']) .' <'. trim($email) .'>');
+							$jsArray[$email] = addslashes($realName .' <'. trim($email) .'>');
 						}
 						if ($i > 10) break;	// we check for # of results here, as we might have empty email addresses
 					}
