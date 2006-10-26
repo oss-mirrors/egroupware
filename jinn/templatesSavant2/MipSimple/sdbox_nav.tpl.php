94866b056ea2c3b6c88463dc42664f6790276343
mipmip <mipmip@923e3c2e-ae13-0410-ba18-ced7211e33de>
2006-10-26 12:29:14 +0000

    mods for PHP5
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@22746 923e3c2e-ae13-0410-ba18-ced7211e33de

--- jinn/templatesSavant2/MipSimple/sdbox_nav.tpl.php
+++ jinn/templatesSavant2/MipSimple/sdbox_nav.tpl.php
@@ -1,4 +1,3 @@
-
 <form method="POST" action="<?=$this->action?>">
    <div style="color:white;padding:5px;margin-bottom:5px;">
 	  <?php if($this->site_name):?>
@@ -16,19 +15,9 @@
    <div style="margin-top:5px;padding:2px;">
 	  <img src="<?=$GLOBALS[phpgw]->common->image('jinn','database_small')?>" alt="" style="margin-right:2px;"><a style="color:white;" href="<?=$GLOBALS[egw]->link('/index.php','menuaction=jinn.uiuser.index&site_object_id=-1')?>"><?php echo $this->site_name?></a>
 	  <ul style="padding:0px;margin:0;list-style-type: none;">
-	<?php foreach($this->objects_arr as $obj):?>
-	<li style="margin:2px;vertical-align:middle;padding:0px 2px 0px 7px;margin:0;width:162px;overflow:hidden;white-space:nowrap;"><img src="<?=$GLOBALS[phpgw]->common->image('jinn','mini_navbar')?>" alt="" style="margin-right:2px;"><a style="color:white;" href="<?=$GLOBALS[egw]->link('/index.php','menuaction=jinn.uiuser.index&site_object_id='.$obj[value])?>"><?php echo $obj[name]?></a><?php //echo _debug_array($obj,false)?></li>	
-	<?php endforeach?>
- </ul>
- <?php
-	/*
-	  
-	  <?php echo lang('select_object');?><br/>
-	  <select name="site_object_id" onChange="this.form.submit()">
-		 <?php #echo $this->object_options?>
-	  </select>
-   </div>
-   */
-   ?>
-   <?php endif?>
-</form>
+		 <?php foreach($this->objects_arr as $obj):?>
+		 <li style="margin:2px;vertical-align:middle;padding:0px 2px 0px 7px;margin:0;width:162px;overflow:hidden;white-space:nowrap;"><img src="<?=$GLOBALS[phpgw]->common->image('jinn','mini_navbar')?>" alt="" style="margin-right:2px;"><a style="color:white;" href="<?=$GLOBALS[egw]->link('/index.php','menuaction=jinn.uiuser.index&site_object_id='.$obj[value])?>"><?php echo $obj[name]?></a></li>	
+		 <?php endforeach?>
+	  </ul>
+	  <?php endif?>
+   </form>
