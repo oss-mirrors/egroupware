dea6484fd1f96831ff37539d4d49262ebfdec47b
mipmip <mipmip@923e3c2e-ae13-0410-ba18-ced7211e33de>
2007-01-14 19:49:08 +0000

    again change srtucture and initial plugin start
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@23282 923e3c2e-ae13-0410-ba18-ced7211e33de

---
+++ jinn/plugins/reports_engines/__pdmlreport/class.pdmlreport.php
@@ -0,0 +1,22 @@
+<?php
+   class pdmlreport
+   {
+	  function pdmlreport()
+	  {}
+
+	  function print_report()
+	  {}
+
+	  function save_report()
+	  {}
+	  
+	  function mail_report()
+	  {}
+
+	  function upload()
+	  {}
+
+	  function text_input()
+	  {}
+
+   }
