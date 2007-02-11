1f71b1ea28edd762482ef531b04c90a8b2279ded
milosch <milosch@923e3c2e-ae13-0410-ba18-ced7211e33de>
2007-02-11 16:13:40 +0000

    Updated link for Linux Today, and replaced broken site with Security Focus.  Only affects new installs.
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@23397 923e3c2e-ae13-0410-ba18-ced7211e33de

--- headlines/setup/default_records.inc.php
+++ headlines/setup/default_records.inc.php
@@ -11,8 +11,8 @@
 
 	/* $Id$ */
 
-	$oProc->query("INSERT INTO egw_headlines_sites (display,base_url,newsfile,lastread,newstype,cachetime,listings) VALUES ('Linux&nbsp;Today','http://linuxtoday.com','/backend/linuxtoday.xml',0,'lt',60,20)");
+	$oProc->query("INSERT INTO egw_headlines_sites (display,base_url,newsfile,lastread,newstype,cachetime,listings) VALUES ('Linux&nbsp;Today','http://www.linuxtoday.com','/backend/biglt.rss',0,'lt',60,20)");
 	$oProc->query("INSERT INTO egw_headlines_sites (display,base_url,newsfile,lastread,newstype,cachetime,listings) VALUES ('Linux&nbsp;Game&nbsp;Tome','http://happypenguin.org','/html/news.rdf',0,'rdf',60,20)");
 	$oProc->query("INSERT INTO egw_headlines_sites (display,base_url,newsfile,lastread,newstype,cachetime,listings) VALUES ('MozillaZine','http://www.mozillazine.org','/contents.rdf',0,'rdf',60,20)");
-	$oProc->query("INSERT INTO egw_headlines_sites (display,base_url,newsfile,lastread,newstype,cachetime,listings) VALUES ('Security Forums','http://www.security-forums.com','/forum/rdf.php',0,'rdf',60,20)");
+	$oProc->query("INSERT INTO egw_headlines_sites (display,base_url,newsfile,lastread,newstype,cachetime,listings) VALUES ('Security Focus','http://www.securityfocus.com','/rss/news.xml',0,'rdf',60,20)");
 ?>
