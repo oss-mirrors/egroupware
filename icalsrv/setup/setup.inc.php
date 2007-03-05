aecb0974b38df74bb9df31d211672129cb447aaf
milosch <milosch@923e3c2e-ae13-0410-ba18-ced7211e33de>
2007-03-05 21:33:31 +0000

    Change enabled status to 2 to prevent adding to the appbar.
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@23485 923e3c2e-ae13-0410-ba18-ced7211e33de

--- icalsrv/setup/setup.inc.php
+++ icalsrv/setup/setup.inc.php
@@ -14,25 +14,22 @@
    *  option) any later version.                                              *
    **************************************************************************/
 
-    /* Id$ */
+	/* Id$ */
 	/* Basic information about this app */
 	$setup_info['icalsrv']['name']      = 'icalsrv';
 	$setup_info['icalsrv']['title']     = 'Ical Server';
 	$setup_info['icalsrv']['version']   = '0.9.37';
 	$setup_info['icalsrv']['app_order'] = 8;
-    $setup_info['icalsrv']['enable']    = 1;
+	$setup_info['icalsrv']['enable']    = 2;
 	$setup_info['icalsrv']['author'] = 'Jan van Lieshout (JVL)';
-	$setup_info['icalsrv']['maintainer'] =
-	  array(
-			'name'  => 'Jan van Lieshout',
-			'email' => 'prito@users.sourceforge.net',
-			);
+	$setup_info['icalsrv']['maintainer'] = array(
+		'name'  => 'Jan van Lieshout',
+		'email' => 'prito@users.sourceforge.net'
+	);
 	$setup_info['icalsrv']['license']  = 'GPL';
-	$setup_info['icalsrv']['description'] = 
-    'A Service that provides access to  eGroupware Calendar- and Infolog data'
-	  . ' via the socalled "Icalendar-over-Http" protocol.';
-	$setup_info['icalsrv']['note'] = 
-	  'The developement of the Ical Service was sponsored by:<ul>
+	$setup_info['icalsrv']['description'] = 'A Service that provides access to  eGroupware Calendar- and Infolog data'
+		. ' via the socalled "Icalendar-over-Http" protocol.';
+	$setup_info['icalsrv']['note'] = 'The developement of the Ical Service was sponsored by:<ul>
 <li> <a href="http://www.wizwise.com" target="_blank">WizWise Technology</a></li>
 </ul>';
 
@@ -45,12 +42,5 @@
 	$setup_info['icalsrv']['depends'][] = array(
 		'appname' => 'phpgwapi',
 		'versions' => Array('1.2','1.3')
-		);
-//   sorry Egwical is not yet an application (and probably never will...)
-//   It should go into phpgwapi. However: IcalSRV DOES DEPEND ON EgwIcal!!!
-// 	$setup_info['icalsrv']['depends'][] = array(
-// 		'appname' => 'egwical',
-// 		'versions' => Array('0.9.30')
-// 		);
-
+	);
 ?>
