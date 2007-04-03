64b2d3e99fc38655debfd5880c17ecde1b3637b8
mipmip <mipmip@923e3c2e-ae13-0410-ba18-ced7211e33de>
2007-04-03 20:59:16 +0000

    complete new replacement object menu which uses the tree menu from the html class
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@23558 923e3c2e-ae13-0410-ba18-ced7211e33de

--- jinn/inc/class.uisidebox.inc.php
+++ jinn/inc/class.uisidebox.inc.php
@@ -24,7 +24,6 @@
    */
    class uisidebox extends uijinn
    {
-
 	  var $bo;
 	  var $ui;
 	  var $tplsav2;
@@ -35,7 +34,6 @@
 	  );
 
 	  /**
-	  /**
 	  * uisidebox 
 	  * 
 	  * @access public
@@ -45,6 +43,19 @@
 	  {
 		 $this->bo = &CreateObject('jinn.bojinn');
 		 parent::uijinn();
+
+		 $this->_set_select_treelink();
+	  }
+
+	  function _set_select_treelink()
+	  {
+		 if(!$_GET['link']) 
+		 {
+			$site_id=$this->bo->session['site_id'];
+			$obj_id=$this->bo->session['site_object_id'];
+
+			$this->selected_link='/s.'.$site_id.'.o.'.$obj_id;
+		 }
 	  }
 
 	  /**
@@ -55,11 +66,11 @@
 	  */
 	  function sidebox_menu()
 	  {
-		 $this->sidebox_menu_objectactions();
+		 //	 $this->sidebox_menu_objectactions();
 		 $this->sidebox_menu_object_dev();
 		 $this->sidebox_menu_site_dev();
-		 $this->sidebox_menu_simplenav();
-		 //		 $this->sidebox_menu_tree();
+		 //	 $this->sidebox_menu_simplenav();
+		 $this->sidebox_menu_tree();
 		 $this->sidebox_menu_sites_admin();
 		 $this->sidebox_menu_admin();
 	  }
@@ -71,7 +82,7 @@
 			$menu_title = lang('Actions %1',"<strong>".$this->bo->site_object['name']."</strong>");
 
 			//if list, if more then one record
-			if($this->bo->site_object[max_records]!=1)
+			if($this->bo->site_object['max_records']!=1)
 			{
 			   $file['Browse current object'] = array(
 				  'link'=>$GLOBALS['phpgw']->link('/index.php','menuaction=jinn.uiu_list_records.display'),
@@ -121,14 +132,11 @@
 			display_sidebox($appname,$menu_title,$file);
 			$file=array();
 		 }
-
 	  }
 
-	  function sidebox_menu_simplenav()
+	  function _site_selection()
 	  {
-		 // get sites for user and group and make options
-		 $appname='jinn';
-
+		 $this->tplsav2->assign('action',$GLOBALS['phpgw']->link('/index.php','menuaction=jinn.uiuser.index'));
 		 $sites=$this->bo->get_sites_allowed($GLOBALS['phpgw_info']['user']['account_id']);
 
 		 if(is_array($sites))
@@ -145,6 +153,13 @@
 		 $site_options=$this->select_options($site_arr,$this->bo->session['site_id'],true,'-1');
 		 $this->tplsav2->assign('sites_options',$site_options);
 		 $this->tplsav2->site_name=$this->bo->so->get_site_name($this->bo->session['site_id']);
+	  }
+
+	  function sidebox_menu_simplenav()
+	  {
+		 // get sites for user and group and make options
+		 $appname='jinn';
+		 $this->_site_selection();
 
 		 // get objects for user and group and make options
 		 if ($this->bo->session['site_id'])
@@ -172,8 +187,6 @@
 			unset($this->bo->session['site_object_id']);
 		 }
 
-		 $this->tplsav2->assign('action',$GLOBALS['phpgw']->link('/index.php','menuaction=jinn.uiuser.index'));
-
 		 $nav_form=$this->tplsav2->fetch('sdbox_nav.tpl.php');
 
 		 $menu_title = lang('JiNN Navigator');
@@ -199,6 +212,17 @@
 	  */
 	  function sidebox_menu_tree()
 	  {
+		 $this->_site_selection();
+		 $nav_form=$this->tplsav2->fetch('sdbox_nav.tpl.php');
+
+		 $content[] = array(
+			'text'=>$nav_form,
+			'icon'=>false,
+			'no_lang' => True,
+			'link' => False,
+		 );
+
+
 		 $folders=$this->setMenuTree();
 		 $folderImageDir = $GLOBALS['egw_info']['server']['webserver_url'].'/jinn/templates/default/images/';
 
@@ -208,14 +232,14 @@
 
 		 $select_link=$GLOBALS['egw']->link('/index.php','menuaction=jinn.uisidebox.openTreeLink&link=');
 
-		 unset($content);
+		 //unset($content);
 		 if(is_array($folders))	// show project-tree only if it's not empty
 		 {
-			$start_img = $GLOBALS['egw_info']['server']['webserver_url'].'/qproject/templates/default/images/dhtmlxtree/folderOpen.gif';
-			$this->tplsav2->assign('start_img',$start_img);
+//			$start_img = $GLOBALS['egw_info']['server']['webserver_url'].'/qproject/templates/default/images/dhtmlxtree/folderOpen.gif';
+//			$this->tplsav2->assign('start_img',$start_img);
 			$this->tplsav2->assign('select_link',$select_link);
 			$this->tplsav2->assign('tree',$GLOBALS['egw']->html->tree($folders,$this->selected_link,false,'load_project','foldertree','','folderClosed.gif',false,'/',$folderImageDir));
-			$content[0] = 
+			$content[] = 
 			array(
 			   'text' => $this->tplsav2->fetch('sidebox_treemenu.tpl.php'),
 			   'no_lang' => True,
@@ -226,6 +250,7 @@
 
 		 $menu_title = lang('JiNN Menu');
 
+		 $appname='jinn';
 		 display_sidebox($appname,$menu_title,$content);
 	  }
 
@@ -313,8 +338,8 @@
 			   $relation_link=$GLOBALS['phpgw']->link('/index.php','menuaction=jinn.uiadmin.edit_relation_widgets&object_id='.$this->bo->site_object['object_id']);
 			   $relation_js="void parent.window.open('$relation_link' , 'genobjoptions', 'width=980,height=600,location=no,menubar=no,directories=no,toolbar=no,scrollbars=yes,resizable=yes,status=no');";
 
-			   $devlinklist = $GLOBALS['phpgw']->link('/index.php','menuaction=jinn.uiu_list_records.display_dev&site_id='.$this->bo->site_object[parent_site_id].'&site_object_id='.$this->bo->session['site_object_id']);
-			   $devlinkform = $GLOBALS['phpgw']->link('/index.php','menuaction=jinn.uiu_edit_record.dev_edit_record&site_id='.$this->bo->site_object[parent_site_id].'&site_object_id='.$this->bo->session['site_object_id']);
+			   $devlinklist = $GLOBALS['phpgw']->link('/index.php','menuaction=jinn.uiu_list_records.display_dev&site_id='.$this->bo->site_object['parent_site_id'].'&site_object_id='.$this->bo->session['site_object_id']);
+			   $devlinkform = $GLOBALS['phpgw']->link('/index.php','menuaction=jinn.uiu_edit_record.dev_edit_record&site_id='.$this->bo->site_object['parent_site_id'].'&site_object_id='.$this->bo->session['site_object_id']);
 
 			   $file['Design Object Form'] = array(
 				  'link'=>$devlinkform,
@@ -347,19 +372,17 @@
 				  'no_lang' => True,
 				  'icon'=>'objectprop18'
 			   );
-/*			   $file['List Reports'] = array(
+			   /*			   $file['List Reports'] = array(
 				  'link'=>$GLOBALS['phpgw']->link('/index.php','menuaction=jinn.uireport.list_reports&site_id='.$this->bo->site_object['parent_site_id'].'&site_object_id='.$this->bo->session['site_object_id']),
 				  'text'=>lang('List Reports'),
 				  'no_lang' => True,
 				  'icon'=>'objectprop18'
 			   );
-*/
+			   */
 			   display_sidebox($appname,$menu_title,$file);
 			}
 
 		 }
-
-  
 	  }
 
 	  function sidebox_menu_site_dev()
@@ -383,7 +406,7 @@
 				  'icon'=>'import18.png'
 			   );
 			   $file['Save site conf to XML'] = array(
-				  'link'=>$GLOBALS['phpgw']->link('/index.php','menuaction=jinn.exportsite.save_site_to_xml&where_key=site_id&where_value='.$this->bo->site[site_id]),
+				  'link'=>$GLOBALS['phpgw']->link('/index.php','menuaction=jinn.exportsite.save_site_to_xml&where_key=site_id&where_value='.$this->bo->site['site_id']),
 				  'text'=>lang('Save site conf to XML'),
 				  'no_lang' => True,
 				  'icon'=>'siteexport18'
@@ -392,12 +415,65 @@
 			   display_sidebox($appname,$menu_title,$file);
 			   $file=array();
 			}
-
 		 }
-
 	  }
 
 	  function setMenuTree($parent_id=0,$ppath='')
+	  {
+		 //FIXME strange that this needs to be done again from here bad timing in bo
+		 $this->bo->set_activated_object_elements();
+		 $this->sites_allowed=$this->bo->get_sites_allowed($GLOBALS['phpgw_info']['user']['account_id']);
+
+		 if ($this->bo->session['site_id'])
+		 {
+			$site_id=$this->bo->session['site_id'];
+			$objects=$this->bo->get_objects_allowed($site_id, $GLOBALS['phpgw_info']['user']['account_id']);
+
+			if (is_array($objects) && $this->bo->session['site_id']==$site_id)
+			{
+			   foreach ( $objects as $object_id) 
+			   {
+				  $menustruct['/s.'.$site_id.'.o.'.$object_id]['label']=$this->bo->so->get_object_name($object_id); // FIXME PERFORMANCE!
+				  $menustruct['/s.'.$site_id.'.o.'.$object_id]['image']='object18.png';
+
+				  if($object_id==$this->bo->session['site_object_id'])
+				  {
+					 if($this->bo->site_object['max_records']!=1)
+					 {
+						$menustruct['/s.'.$site_id.'.o.'.$object_id.'/s.'.$site_id.'.o.'.$object_id.'.listrec']['label']='List Records';
+						$menustruct['/s.'.$site_id.'.o.'.$object_id.'/s.'.$site_id.'.o.'.$object_id.'.listrec']['image']='listview18.png';
+					 }
+					 //if create 
+					 if($this->bo->objectelements['enable_create_rec'] && $this->bo->session['site_id'] 
+					 && $this->bo->site_object['object_id'] && $this->bo->site_object['max_records']!=1)
+					 {
+						$menustruct['/s.'.$site_id.'.o.'.$object_id.'/s.'.$site_id.'.o.'.$object_id.'.addrec']['label']='Add Records';
+						$menustruct['/s.'.$site_id.'.o.'.$object_id.'/s.'.$site_id.'.o.'.$object_id.'.addrec']['image']='addrec18.png';
+					 }
+					 //if import
+					 if($this->bo->objectelements['enable_import']) 
+					 {
+						$menustruct['/s.'.$site_id.'.o.'.$object_id.'/s.'.$site_id.'.o.'.$object_id.'.import']['label']='Import';
+						$menustruct['/s.'.$site_id.'.o.'.$object_id.'/s.'.$site_id.'.o.'.$object_id.'.import']['image']='import18.png';
+					 }
+					 //if export
+					 if($this->bo->objectelements['enable_export']) 
+					 {
+						$menustruct['/s.'.$site_id.'.o.'.$object_id.'/s.'.$site_id.'.o.'.$object_id.'.export']['label']='Export';
+						$menustruct['/s.'.$site_id.'.o.'.$object_id.'/s.'.$site_id.'.o.'.$object_id.'.export']['image']='export18.png';
+					 }
+				  }
+			   }
+			}
+		 }
+		 else
+		 {
+			unset($this->bo->session['site_object_id']);
+		 }
+		 return $menustruct;
+	  }
+
+	  function setMenuTreeIncludingSites($parent_id=0,$ppath='')
 	  {
 		 $menustruct['/s']['label']='JiNN-sites';
 		 $menustruct['/s']['image']='jinn18.png';
@@ -433,36 +509,36 @@
 					 $menustruct['/s/s.'.$site_id.'/s.'.$site_id.'.o/s.'.$site_id.'.o.'.$object_id.'/s.'.$site_id.'.o.'.$object_id.'.export']['image']='export18.png';
 
 					 //admin or owners options
-//					 $menustruct['/s/s.'.$site_id.'/s.'.$site_id.'.o/s.'.$site_id.'.o.'.$object_id.'/s.'.$site_id.'.o.'.$object_id.'.objproperties']['label']='Object Properties';
-//					 $menustruct['/s/s.'.$site_id.'/s.'.$site_id.'.o/s.'.$site_id.'.o.'.$object_id.'/s.'.$site_id.'.o.'.$object_id.'.objproperties']['image']='objectprop18.png';
-//					 $menustruct['/s/s.'.$site_id.'/s.'.$site_id.'.o/s.'.$site_id.'.o.'.$object_id.'/s.'.$site_id.'.o.'.$object_id.'.designform']['label']='Design Form';
-//					 $menustruct['/s/s.'.$site_id.'/s.'.$site_id.'.o/s.'.$site_id.'.o.'.$object_id.'/s.'.$site_id.'.o.'.$object_id.'.designform']['image']='formprop18.png';
-//					 $menustruct['/s/s.'.$site_id.'/s.'.$site_id.'.o/s.'.$site_id.'.o.'.$object_id.'/s.'.$site_id.'.o.'.$object_id.'.designlist']['label']='Design List';
-//					 $menustruct['/s/s.'.$site_id.'/s.'.$site_id.'.o/s.'.$site_id.'.o.'.$object_id.'/s.'.$site_id.'.o.'.$object_id.'.designlist']['image']='listviewprop18.png';
-//					 $menustruct['/s/s.'.$site_id.'/s.'.$site_id.'.o/s.'.$site_id.'.o.'.$object_id.'/s.'.$site_id.'.o.'.$object_id.'.exportobject']['label']='Export Object';
-//					 $menustruct['/s/s.'.$site_id.'/s.'.$site_id.'.o/s.'.$site_id.'.o.'.$object_id.'/s.'.$site_id.'.o.'.$object_id.'.exportobject']['image']='objectexport18.png';
+					 //					 $menustruct['/s/s.'.$site_id.'/s.'.$site_id.'.o/s.'.$site_id.'.o.'.$object_id.'/s.'.$site_id.'.o.'.$object_id.'.objproperties']['label']='Object Properties';
+					 //					 $menustruct['/s/s.'.$site_id.'/s.'.$site_id.'.o/s.'.$site_id.'.o.'.$object_id.'/s.'.$site_id.'.o.'.$object_id.'.objproperties']['image']='objectprop18.png';
+					 //					 $menustruct['/s/s.'.$site_id.'/s.'.$site_id.'.o/s.'.$site_id.'.o.'.$object_id.'/s.'.$site_id.'.o.'.$object_id.'.designform']['label']='Design Form';
+					 //					 $menustruct['/s/s.'.$site_id.'/s.'.$site_id.'.o/s.'.$site_id.'.o.'.$object_id.'/s.'.$site_id.'.o.'.$object_id.'.designform']['image']='formprop18.png';
+					 //					 $menustruct['/s/s.'.$site_id.'/s.'.$site_id.'.o/s.'.$site_id.'.o.'.$object_id.'/s.'.$site_id.'.o.'.$object_id.'.designlist']['label']='Design List';
+					 //					 $menustruct['/s/s.'.$site_id.'/s.'.$site_id.'.o/s.'.$site_id.'.o.'.$object_id.'/s.'.$site_id.'.o.'.$object_id.'.designlist']['image']='listviewprop18.png';
+					 //					 $menustruct['/s/s.'.$site_id.'/s.'.$site_id.'.o/s.'.$site_id.'.o.'.$object_id.'/s.'.$site_id.'.o.'.$object_id.'.exportobject']['label']='Export Object';
+					 //					 $menustruct['/s/s.'.$site_id.'/s.'.$site_id.'.o/s.'.$site_id.'.o.'.$object_id.'/s.'.$site_id.'.o.'.$object_id.'.exportobject']['image']='objectexport18.png';
 				  }
 			   }
 
 			   //admin or owners options
-//			   $menustruct['/s/s.'.$site_id.'/s.'.$site_id.'.properties']['label']=lang('Site Properties');
-//			   $menustruct['/s/s.'.$site_id.'/s.'.$site_id.'.properties']['image']='siteprop18.png';
+			   //			   $menustruct['/s/s.'.$site_id.'/s.'.$site_id.'.properties']['label']=lang('Site Properties');
+			   //			   $menustruct['/s/s.'.$site_id.'/s.'.$site_id.'.properties']['image']='siteprop18.png';
 
-//			   $menustruct['/s/s.'.$site_id.'/s.'.$site_id.'.importobject']['label']=lang('Import Object');
-//			   $menustruct['/s/s.'.$site_id.'/s.'.$site_id.'.importobject']['image']='objectimport18.png';
+			   //			   $menustruct['/s/s.'.$site_id.'/s.'.$site_id.'.importobject']['label']=lang('Import Object');
+			   //			   $menustruct['/s/s.'.$site_id.'/s.'.$site_id.'.importobject']['image']='objectimport18.png';
 
-//			   $menustruct['/s/s.'.$site_id.'/s.'.$site_id.'.exportsite']['label']=lang('Export Site');
-//			   $menustruct['/s/s.'.$site_id.'/s.'.$site_id.'.exportsite']['image']='siteexport18.png';
+			   //			   $menustruct['/s/s.'.$site_id.'/s.'.$site_id.'.exportsite']['label']=lang('Export Site');
+			   //			   $menustruct['/s/s.'.$site_id.'/s.'.$site_id.'.exportsite']['image']='siteexport18.png';
 
-//			   $menustruct['/s/s.'.$site_id.'/s.'.$site_id.'createobject']['label']='Create Object';
-//			   $menustruct['/s/s.'.$site_id.'/s.'.$site_id.'createobject']['image']='createobject18.png';
+			   //			   $menustruct['/s/s.'.$site_id.'/s.'.$site_id.'createobject']['label']='Create Object';
+			   //			   $menustruct['/s/s.'.$site_id.'/s.'.$site_id.'createobject']['image']='createobject18.png';
 			}
 		 }
 
- 		 $menustruct['/addsite']['label']='Create JiNN-site';
+		 $menustruct['/addsite']['label']='Create JiNN-site';
 		 $menustruct['/addsite']['image']='createsite18.png';
-//		 $menustruct['/importsite']['label']='Import Site';
-//		 $menustruct['/importsite']['image']='siteimport18.png';
+		 //		 $menustruct['/importsite']['label']='Import Site';
+		 //		 $menustruct['/importsite']['image']='siteimport18.png';
 		 $menustruct['/acl']['label']='ACL';
 		 $menustruct['/acl']['image']='acl18.png';
 		 return $menustruct;
@@ -493,10 +569,90 @@
 		 }
 		 elseif(count($_link)==2)
 		 {
+			$this->bo->reset_site_and_object($_link[1]);
+			$this->selected_link='/s.'.$_link[1].'.o';
+			//_debug_array($this->bo->session);
+			ExecMethod('jinn.uiuser.index');
+		 }
+		 elseif(count($_link)==3)
+		 {
+			switch($_link[2])
+			{
+			   case 'importobject'://FIXME NOT WORKING
 			   $this->bo->reset_site_and_object($_link[1]);
-			   $this->selected_link='/s/s.'.$_link[1].'/s.'.$_link[1].'.o';
-			   //_debug_array($this->bo->session);
-			   ExecMethod('jinn.uiuser.index');
+			   ExecMethod('jinn.ui_importsite.import_object');
+			   break;
+			}
+		 }
+		 elseif(count($_link)==4)
+		 {
+			$this->bo->reset_site_and_object($_link[1],$_link[3]);
+			//$this->selected_link='/s.'.$_link[1].'.o.'.$_link[3].'/s.'.$_link[1].'.o.'.$_link[3].'.listrec';
+			$this->selected_link='/s.'.$_link[1].'.o.'.$_link[3];
+			ExecMethod('jinn.uiu_list_records.display');
+		 }
+		 elseif(count($_link)==5)
+		 {
+			$this->bo->reset_site_and_object($_link[1],$_link[3]);
+
+			switch($_link[4])
+			{
+			   case 'listrec':
+			   //$this->selected_link='/s.'.$_link[1].'.o.'.$_link[3].'/s.'.$_link[1].'.o.'.$_link[3].'.listrec';
+			   $this->selected_link='/s.'.$_link[1].'.o.'.$_link[3];
+			   ExecMethod('jinn.uiu_list_records.display');
+			   break;
+
+			   case 'addrec':
+			   //$this->selected_link='/s.'.$_link[1].'.o.'.$_link[3].'/s.'.$_link[1].'.o.'.$_link[3].'.addrec';
+			   $this->selected_link='/s.'.$_link[1].'.o.'.$_link[3];
+			   ExecMethod('jinn.uiu_edit_record.new_record');
+			   break;
+
+			   case 'import':
+			   //$this->selected_link='/s.'.$_link[1].'.o.'.$_link[3].'/s.'.$_link[1].'.o.'.$_link[3].'.import';
+			   $this->selected_link='/s.'.$_link[1].'.o.'.$_link[3];
+			   ExecMethod('jinn.uiu_import.import');
+			   break;
+
+			   case 'export':
+			   //$this->selected_link='/s.'.$_link[1].'.o.'.$_link[3].'/s.'.$_link[1].'.o.'.$_link[3].'.export';
+			   $this->selected_link='/s.'.$_link[1].'.o.'.$_link[3];
+			   ExecMethod('jinn.uiu_export.export');
+			   break;
+			}
+		 }
+	  }
+
+	  function openTreeLinkIncludingSites()
+	  {
+		 $_link=explode('.',$_GET['link']);
+		 if(count($_link)==1)
+		 {
+			$this->bo->reset_site_and_object();
+			$this->selected_link='/'.$_link[0];
+			switch($_GET['link'])
+			{
+			   case 's':
+			   ExecMethod('jinn.ui_listsites.browse_egw_jinn_sites');
+			   break;
+			   case 'addsite':
+			   ExecMethod('jinn.uiadmin.add_edit_site');
+			   break;
+			   case 'acl':
+			   ExecMethod('jinn.uiacl.main_screen');
+			   break;
+			   case 'importsite':
+			   ExecMethod('jinn.ui_importsite.import_egw_jinn_site');
+			   break;
+			}
+		 }
+		 elseif(count($_link)==2)
+		 {
+			$this->bo->reset_site_and_object($_link[1]);
+			$this->selected_link='/s/s.'.$_link[1].'/s.'.$_link[1].'.o';
+			//_debug_array($this->bo->session);
+			ExecMethod('jinn.uiuser.index');
 		 }
 		 elseif(count($_link)==3)
 		 {
@@ -530,7 +686,7 @@
 			   $this->selected_link='/s/s.'.$_link[1].'/s.'.$_link[1].'.o/s.'.$_link[1].'.o.'.$_link[3].'/s.'.$_link[1].'.o.'.$_link[3].'.listrec';
 			   ExecMethod('jinn.uiu_list_records.display');
 			   break;
-			   
+
 			   case 'addrec':
 			   $this->selected_link='/s/s.'.$_link[1].'/s.'.$_link[1].'.o/s.'.$_link[1].'.o.'.$_link[3].'/s.'.$_link[1].'.o.'.$_link[3].'.addrec';
 			   ExecMethod('jinn.uiu_edit_record.new_record');
@@ -546,12 +702,8 @@
 			   ExecMethod('jinn.uiu_export.export');
 			   break;
 			}
-			
+
 		 }
-
-
-
-		 
 	  }
    }
 ?>
--- jinn/templatesSavant2/default/sdbox_nav.tpl.php
+++ jinn/templatesSavant2/default/sdbox_nav.tpl.php
@@ -10,7 +10,7 @@
 		 <?php echo $this->sites_options?>
 	  </select>
    </div>
-   <div style="border-top:solid 1px #aaaaaa;text-align:left"></div>
+   <div style="border-top:solid 0px #aaaaaa;text-align:left"></div>
    <?php if($this->objects_arr):?>
    <div style="margin-top:5px;padding:2px;text-align:left">
 	  <img src="<?=$GLOBALS[phpgw]->common->image('jinn','database_small')?>" alt="" style="margin-right:2px;"><a href="<?=$GLOBALS[egw]->link('/index.php','menuaction=jinn.uiuser.index&site_object_id=-1')?>"><?php echo $this->site_name?></a>
