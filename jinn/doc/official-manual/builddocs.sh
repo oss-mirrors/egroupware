9c9c474fb8f3ed394cecc9d5ad6530a8a39bf6ad
mipmip <mipmip@923e3c2e-ae13-0410-ba18-ced7211e33de>
2007-04-13 14:46:12 +0000

    new documentation updates added tutorial in buildscript
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@23578 923e3c2e-ae13-0410-ba18-ced7211e33de

--- jinn/doc/official-manual/builddocs.sh
+++
@@ -1,8 +0,0 @@
-rm JiNN-User-Manual.pdf
-rm JiNN-User-Manual.html
-rm JiNN-User-Manual.ps
-rm *~
-lyx --export pdf JiNN-User-Manual.lyx
-lyx --export html JiNN-User-Manual.lyx
-lyx --export ps JiNN-User-Manual.lyx
-scp -r JiNN* pim@lingewoud.nl:/home/guide/project-jinn.org/htdomains/docs/
