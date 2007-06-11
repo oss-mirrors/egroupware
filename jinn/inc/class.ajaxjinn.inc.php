e1423f207b8b96bd8974677f7b471a1d005e2891
mipmip <mipmip@923e3c2e-ae13-0410-ba18-ced7211e33de>
2007-06-11 10:35:41 +0000

    fix type enableling fields
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@24065 923e3c2e-ae13-0410-ba18-ced7211e33de

--- jinn/inc/class.ajaxjinn.inc.php
+++ jinn/inc/class.ajaxjinn.inc.php
@@ -63,10 +63,10 @@
 		 }
 		 $response->addAssign('panel_fenabled', "innerHTML", $enabled);
 
-		 $where_string="field_parent_object='$object_id' AND  field_name='$field_name'";
+		 $where_string="field_parent_object='$object_id' AND  field_name='$field'";
 		 $status = $this->bo->so->update_phpgw_data('egw_jinn_obj_fields',$data,'','',$where_string,true);
 		 $this->bo->set_site_version_info($this->bo->site['site_id']);
-		 //	 return $response->addAlert($where_string);
+		 	 $response->addAlert($where_string);
 		 return $response->getXML();
 	  }
 
