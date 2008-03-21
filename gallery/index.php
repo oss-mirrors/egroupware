929a0cf20ebce8ef089c17532b60f319f735cd16
ralfbecker <ralfbecker@923e3c2e-ae13-0410-ba18-ced7211e33de>
2008-03-21 13:50:15 +0000

    using framework::render() and no CreateObject()
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@25132 923e3c2e-ae13-0410-ba18-ced7211e33de

--- gallery/index.php
+++ gallery/index.php
@@ -6,7 +6,7 @@
  * @link http://gallery.sourceforge.net/
  * @package gallery
  * @author Ralf Becker <RalfBecker-AT-outdoor-training.de>
- * @copyright 2006 by Ralf Becker <RalfBecker-AT-outdoor-training.de>
+ * @copyright 2006-8 by Ralf Becker <RalfBecker-AT-outdoor-training.de>
  * @license http://opensource.org/licenses/gpl-license.php GPL - GNU General Public License
  * @version $Id$
  */
@@ -21,19 +21,14 @@ $GLOBALS['egw_info'] = array(
 
 include('../header.inc.php');
 
-$GLOBALS['egw']->g2 =& CreateObject('gallery.g2_integration',false);	// no fullinit, we are going to call handleRequest
+require_once(EGW_INCLUDE_ROOT.'/gallery/inc/class.g2_integration.inc.php');
+$g2 = new g2_integration(false);	// no fullinit, we are going to call handleRequest
 
-if ($GLOBALS['egw']->g2->error)
+if ($g2->error)
 {
-	$content = $GLOBALS['egw']->g2->error;
+	$GLOBALS['egw']->framework->render($g2->error);
 }
 else
 {
-	$content = $GLOBALS['egw']->g2->handleRequest('core',$GLOBALS['egw_info']['flags']['app_header']);
+	$GLOBALS['egw']->framework->render($g2->handleRequest('core',$GLOBALS['egw_info']['flags']['app_header']));
 }
-
-$GLOBALS['egw']->common->egw_header();
-
-echo $content;
-
-$GLOBALS['egw']->common->egw_footer();
