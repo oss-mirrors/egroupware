e9bff8ca6242a92fbaed8b4ec5cacd3457720f54
ralfbecker <ralfbecker@923e3c2e-ae13-0410-ba18-ced7211e33de>
2008-04-16 07:04:30 +0000

    "using the new vfs"
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@25287 923e3c2e-ae13-0410-ba18-ced7211e33de

--- sitemgr/modules/class.module_download.inc.php
+++ sitemgr/modules/class.module_download.inc.php
@@ -1,58 +1,58 @@
 <?php
-/**************************************************************************\
-* eGroupWare SiteMgr - Web Content Management                              *
-* Module : download                                                        *
-* Author : Cornelius Weiss <egw@von-und-zu-weiss.de>                        *
-*          based on an old SiteMgr module                                  *
-* http://www.egroupware.org                                                *
-* --------------------------------------------                             *
-*  This program is free software; you can redistribute it and/or modify it *
-*  under the terms of the GNU General Public License as published by the   *
-*  Free Software Foundation; either version 2 of the License, or (at your  *
-*  option) any later version.                                              *
-\**************************************************************************/
+/**
+ * sitemgr - Download from VFS block
+ *
+ * @link http://www.egroupware.org
+ * @author Cornelius Weiss <egw@von-und-zu-weiss.de> based on old sitemgr module
+ * @author Ralf Becker <RalfBecker(at)outdoor-training.de> updated to new vfs
+ * @package sitemgr
+ * @subpackage modules
+ * @license http://opensource.org/licenses/gpl-license.php GPL - GNU General Public License
+ * @version $Id$
+ */
 
-/* $Id$ */
-
-class module_download extends Module 
+class module_download extends Module
 {
-	function module_download() 
+	function module_download()
 	{
 		$this->arguments = array (
 			'format' => array (
-				'type'  => 'select', 
-				'label' => lang('Choose a format'), 
+				'type'  => 'select',
+				'label' => lang('Choose a format'),
 				'options' => array (
-					'file' => lang('Single file download'), 
-					'dir' => lang('Show contents of a directory'), 
+					'file' => lang('Single file download'),
+					'dir' => lang('Show contents of a directory'),
 					'dirnsub' => lang('Show contents of a directory with subdirectories'),
+					'recursive' => lang('Show files including the ones from subdirectories'),
 				),
-			), 
+			),
 			'showpath' => array (
-				'type' => 'checkbox', 
+				'type' => 'checkbox',
 				'label' => lang('show path?'),
-			), 
+			),
 			'path' => array (
-				'type' => 'textfield', 
+				'type' => 'textfield',
 				'label' => lang('The path to the file to be downloaded'),
-			), 
+			),
 			'file' => array (
-				'type' => 'textfield', 
+				'type' => 'textfield',
 				'label' => lang('The file to be downloaded').' '.lang('(only used in case of single file)'),
-			), 
+			),
 			'text' => array (
-				'type' => 'textfield', 
+				'type' => 'textfield',
 				'label' => lang('The text for the link, if empty the module returns the raw URL (without a link)').' '.
 					lang('(only used in case of single file)'),
-			), 
+			),
+/*			disabled, because currently not working
 			'op' => array (
-				'type' => 'select', 
-				'label' => lang('Should the file be viewed in the browser or downloaded'), 
+				'type' => 'select',
+				'label' => lang('Should the file be viewed in the browser or downloaded'),
 				'options' => array (
-					1 => lang('viewed'), 
+					1 => lang('viewed'),
 					2 => lang('downloaded'),
 				),
 			),
+*/
 		);
 		$this->post = array (
 			'subdir' => array ('type' => 'textfield'),
@@ -62,20 +62,16 @@ class module_download extends Module
 		$this->description = lang('This module create a link for downloading a file(s) from the VFS');
 	}
 
-	function get_content(&$arguments, $properties) 
+	function get_content(&$arguments, $properties)
 	{
-		if ($arguments['op'] == 2) 
-		{
-			$linkdata['download'] = 1;
-		}
-		if (substr($arguments['path'],-1) == '/') 
+		$GLOBALS['egw']->translation->add_app('filemanager');
+
+		if (substr($arguments['path'],-1) == '/')
 		{
 			$arguments['path'] = substr($arguments['path'], 0, -1);
 		}
-		$linkdata['path'] = rawurlencode(base64_encode($arguments['path']));
-		$linkdata['menuaction'] = 'filemanager.uifilemanager.view';
-
-		switch ($arguments['format']) 
+		$out = '';
+		switch ($arguments['format'])
 		{
 			case 'dirnsub' :
 				if ($arguments['subdir']) {
@@ -84,54 +80,54 @@ class module_download extends Module
 				if ($arguments['showpath']) {
 					$out = lang('Path').': '.$arguments['path'].'<hr>';
 				}
-
+				// fall through
 			case 'dir' :
-				$this->vfs =& Createobject('phpgwapi.vfs');
-				$data = array (
-					'string' => $arguments['path'], 
-					'relatives' => array (RELATIVE_ROOT), 
-					'checksubdirs' => false,
-					//'mime'	=> ,
-					'nofiles' => false,
-				);
-				$ls_dir = $this->vfs->ls($data);
+			case 'recursive':
+				$ls_dir = egw_vfs::find($arguments['path'],array(
+					'need_mime' => true,
+					'maxdepth' => $arguments['format'] != 'recursive' ? 1 : null,
+					'type' => $arguments['format'] != 'dirnsub' ? 'f' : null,
+				),true);
 
 				$out .= '<table class="moduletable">
 						<tr>
 							<td width="1%">'./*mime png*/ ''.'</td>
 							<td>'.lang('Filename').'</td>
-							<td>'.lang('Comment').'</td>
-							<td>'.lang('Size').'</td>
+							<!--td>'.lang('Comment').'</td-->
+							<td align="right">'.lang('Size').'</td>
 							<td>'.lang('Date').'</td>
 							<td>'./*action*/ ''.'</td>
 						</tr>
 						<tr><td height="1px" colspan="6"><hr></td></tr>';
 
-				if ($arguments['subdir'] && $arguments['format'] == 'dirnsub') 
+				if ($arguments['subdir'] && $arguments['format'] == 'dirnsub')
 				{
 					$out .= '<tr>
 							<td>..</td>
-							<td><a href="'.$this->link(array ('subdir' => strrchr($arguments['subdir'], '/') ? 
+							<td><a href="'.$this->link(array ('subdir' => strrchr($arguments['subdir'], '/') ?
 								substr($arguments['subdir'], 0, strlen($arguments['subdir']) - strlen(strrchr($arguments['subdir'], '/'))) :
 								 false)).'">'.lang('parent directory').'</a>
-							</td><td></td><td></td><td></td><td></td>
+							</td><!--td></td--><td></td><td></td><td></td>
 						</tr>';
 				}
 
-				foreach ($ls_dir as $num => $file) 
+				$dateformat = $GLOBALS['egw_info']['user']['preferences']['common']['dateformat'].
+					($GLOBALS['egw_info']['user']['preferences']['common']['timeformat'] != 12 ? ' H:i' : 'h:ia');
+
+				foreach ($ls_dir as $num => $file)
 				{
-					if ($file['mime_type'] == 'Directory') 
+					if ($file['mime'] == egw_vfs::DIR_MIME_TYPE)
 					{
-						if ($arguments['format'] == 'dirnsub') 
+						if ($arguments['format'] == 'dirnsub' && $file['name'])
 						{
 							$out .= '<tr>
-									<td>'.$this->mime_icon($file['mime_type']).'</td>
-									<td><a href="'.$this->link(array ('subdir' => $arguments['subdir'] ? 
+									<td>'.egw_vfs::mime_icon($file['mime'],false).'</td>
+									<td><a href="'.$this->link(array ('subdir' => $arguments['subdir'] ?
 										$arguments['subdir'].'/'.$file['name'] : $file['name'])).'">'.$file['name'].'</a>
 									</td>
-									<td>'.$file['comment'].'</td>
-									<td>'.$file['size'].'</td>
-									<td>'. ($file['modified'] ? $file['modified'] : $file['created']).'</td>
+									<!--td>'.$file['comment'].'</td-->
+									<td align="right">'./*egw_vfs::hsize($file['size']).*/'</td>
+									<td>'. date($dateformat,$file['mtime'] ? $file['mtime'] : $file['ctime']).'</td>
 									<td></td>
 								</tr>';
 						}
@@ -139,15 +135,15 @@ class module_download extends Module
 					}
 				}
 
-				foreach ($ls_dir as $num => $file) 
+				foreach ($ls_dir as $num => $file)
 				{
-					$linkdata['file'] = rawurlencode(base64_encode($file['name']));
+					$link = $GLOBALS['egw']->link(egw_vfs::download_url($arguments['path'].'/'.$file['name'],$arguments['op'] == 2));
 					$out .= '<tr>
-							<td>'.$this->mime_icon($file['mime_type']).'</td>
-							<td><a href="'.phpgw_link('/index.php', $linkdata).'">'.$file['name'].'</a></td>
-							<td>'.$file['comment'].'</td>
-							<td>'.$file['size'].'</td>
-							<td>'. ($file['modified'] ? $file['modified'] : $file['created']).'</td>
+							<td>'.egw_vfs::mime_icon($file['mime'],false).'</td>
+							<td><a href="'.$link.'">'.$file['name'].'</a></td>
+							<!--td>'.$file['comment'].'</td-->
+							<td align="right">'.egw_vfs::hsize($file['size']).'</td>
+							<td>'. date($dateformat,$file['mtime'] ? $file['mtime'] : $file['ctime']).'</td>
 							<td></td>
 						</tr>';
 				}
@@ -156,21 +152,8 @@ class module_download extends Module
 
 			case 'file' :
 			default :
-				$linkdata['file'] = rawurlencode(base64_encode($arguments['file']));
-				return $arguments['text'] ? ('<a href="'.phpgw_link('/index.php', $linkdata).'">'.
-					$arguments['text'].'</a>') : phpgw_link('/index.php', $linkdata);
+				$link = $GLOBALS['egw']->link(egw_vfs::download_url($arguments['path'].'/'.$file['name'],$arguments['op'] == 2));
+				return $arguments['text'] ? ('<a href="'.$link.'">'.$arguments['text'].'</a>') : $link;
 		}
-	}
-
-	function mime_icon($mime_type, $size = 16) 
-	{
-		if (!$mime_type) $mime_type = 'unknown';
-		$mime_type = str_replace('/', '_', $mime_type);
-
-		$img = $GLOBALS['phpgw']->common->image('filemanager', 'mime'.$size.'_'.strtolower($mime_type));
-		if (!$img) $img = $GLOBALS['phpgw']->common->image('filemanager', 'mime'.$size.'_unknown');
-		$icon = '<img src="'.$img.' "alt="'.lang($mime_type).'" />';
-
-		return $icon;
 	}
 }
