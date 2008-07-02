32d679b81171693552409f5a9fba5c3a8a42903b
stefanbecker <stefanbecker@923e3c2e-ae13-0410-ba18-ced7211e33de>
2008-07-02 14:11:08 +0000

    changed some text and copyright information
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@25681 923e3c2e-ae13-0410-ba18-ced7211e33de

--- sitemgr/sitemgr-site/mos-compat/class.joomla.inc.php
+++ sitemgr/sitemgr-site/mos-compat/class.joomla.inc.php
@@ -1,16 +1,12 @@
 <?php
-	/**************************************************************************\
-	* eGroupWare SiteMgr - SiteMgr support for Mambo Open Source templates     *
-	* http://www.egroupware.org                                                *
-	* Written and (c) by StefanBecker@outdoor-training.de                        *
-	* --------------------------------------------                             *
-	*  This program is free software; you can redistribute it and/or modify it *
-	*  under the terms of the GNU General Public License as published by the   *
-	*  Free Software Foundation; either version 2 of the License, or (at your  *
-	*  option) any later version.                                              *
-	\**************************************************************************/
-
-	/* $Id: mosmenu.inc.php 13831 2004-02-22 01:46:27Z stefanbecker $ */
+	/**
+ * eGroupWare: sitemgr: Joomla Template handler
+ *
+ * @link http://www.egroupware.org
+ * @license http://opensource.org/licenses/gpl-license.php GPL - GNU General Public License
+ * @package sitemgr
+ * @author Stefan Becker <StefanBecker-AT-outdoor-training.de>
+ */
 
 class joomla {
 	//require that files in the joomla template to get the menu working
--- sitemgr/sitemgr-site/mos-compat/joomla_Legacy_function.inc.php
+++ sitemgr/sitemgr-site/mos-compat/joomla_Legacy_function.inc.php
@@ -1,15 +1,12 @@
 <?php
-	/**************************************************************************\
-	* eGroupWare SiteMgr - SiteMgr support for Mambo Open Source templates     *
-	* http://www.egroupware.org                                                *
-	* Written and (c) by StefanBecker@outdoor-training.de                        *
-	* --------------------------------------------                             *
-	*  This program is free software; you can redistribute it and/or modify it *
-	*  under the terms of the GNU General Public License as published by the   *
-	*  Free Software Foundation; either version 2 of the License, or (at your  *
-	*  option) any later version.                                              *
-	\**************************************************************************/
-
+	/**
+	* eGroupWare: sitemgr Jooma Template handler
+	*
+ 	* @link http://www.egroupware.org
+ 	* @license http://opensource.org/licenses/gpl-license.php GPL - GNU General Public License
+	* @package sitemgr
+	* Modified by Stefan Becker <StefanBecker-AT-outdoor-training.de>
+ 	*/
 	/* $Id: mosmenu.inc.php 13831 2004-02-22 01:46:27Z stefanbecker $ */
 
 	/**
