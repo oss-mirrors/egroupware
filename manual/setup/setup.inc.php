aba6e37a3844e465c2fb51e158dcfb5dcce8a81a
ralfbecker <ralfbecker@923e3c2e-ae13-0410-ba18-ced7211e33de>
2008-10-10 11:27:28 +0000

    "version and dependencies for 1.6"
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@26132 923e3c2e-ae13-0410-ba18-ced7211e33de

--- manual/setup/setup.inc.php
+++ manual/setup/setup.inc.php
@@ -1,42 +1,41 @@
 <?php
-	/**************************************************************************\
-	* eGroupWare - Online User Manual                                          *
-	* http://www.eGroupWare.org                                                *
-	* --------------------------------------------                             *
-	*  This program is free software; you can redistribute it and/or modify it *
-	*  under the terms of the GNU General Public License as published by the   *
-	*  Free Software Foundation; either version 2 of the License, or (at your  *
-	*  option) any later version.                                              *
-	\**************************************************************************/
+/**************************************************************************\
+* eGroupWare - Online User Manual                                          *
+* http://www.eGroupWare.org                                                *
+* --------------------------------------------                             *
+*  This program is free software; you can redistribute it and/or modify it *
+*  under the terms of the GNU General Public License as published by the   *
+*  Free Software Foundation; either version 2 of the License, or (at your  *
+*  option) any later version.                                              *
+\**************************************************************************/
 
-	/* $Id$ */
+/* $Id$ */
 
-	/* Basic information about this app */
-	$setup_info['manual']['name']      = 'manual';
-	$setup_info['manual']['title']     = 'User Manual';
-	$setup_info['manual']['version']   = '1.4';
-	$setup_info['manual']['app_order'] = 5;
-	$setup_info['manual']['enable']    = 4;	// popup
+/* Basic information about this app */
+$setup_info['manual']['name']      = 'manual';
+$setup_info['manual']['title']     = 'User Manual';
+$setup_info['manual']['version']   = '1.6';
+$setup_info['manual']['app_order'] = 5;
+$setup_info['manual']['enable']    = 4;	// popup
 
-	$setup_info['manual']['author']    =
-	$setup_info['manual']['maintainer'] = 'Ralf Becker';
-	$setup_info['manual']['maintainer_email'] = 'RalfBecker@outdoor-training.de';
-	$setup_info['manual']['license']   = 'GPL';
-	$setup_info['manual']['description'] =
-		'The new eGW Online User Manual uses the Wiki app.';
+$setup_info['manual']['author']    =
+$setup_info['manual']['maintainer'] = 'Ralf Becker';
+$setup_info['manual']['maintainer_email'] = 'RalfBecker@outdoor-training.de';
+$setup_info['manual']['license']   = 'GPL';
+$setup_info['manual']['description'] =
+	'The new eGW Online User Manual uses the Wiki app.';
 
-	/* The hooks this app includes, needed for hooks registration */
-	$setup_info['manual']['hooks']['admin'] = 'manual.uimanualadmin.menu';
-	$setup_info['manual']['hooks']['config'] = 'manual.uimanualadmin.config';
-	$setup_info['manual']['hooks']['config_validate'] = 'manual.uimanualadmin.config';
+/* The hooks this app includes, needed for hooks registration */
+$setup_info['manual']['hooks']['admin'] = 'manual.uimanualadmin.menu';
+$setup_info['manual']['hooks']['config'] = 'manual.uimanualadmin.config';
+$setup_info['manual']['hooks']['config_validate'] = 'manual.uimanualadmin.config';
 
-	/* Dependencies for this app to work */
-	$setup_info['manual']['depends'][] = array(
-		 'appname' => 'phpgwapi',
-		 'versions' => Array('1.3','1.4','1.5')
-	);
-	$setup_info['manual']['depends'][] = array(
-		 'appname' => 'wiki',
-		 'versions' => Array('1.3','1.4','1.5')
-	);
-?>
+/* Dependencies for this app to work */
+$setup_info['manual']['depends'][] = array(
+	 'appname' => 'phpgwapi',
+	 'versions' => Array('1.4','1.5','1.6','1.7')
+);
+$setup_info['manual']['depends'][] = array(
+	 'appname' => 'wiki',
+	 'versions' => Array('1.4','1.5','1.6','1.7')
+);
