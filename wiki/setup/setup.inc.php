7996ba8c7f39a97d87ecff0c895366f08e92f7e8
ralfbecker <ralfbecker@923e3c2e-ae13-0410-ba18-ced7211e33de>
2008-10-10 11:33:28 +0000

    version and dependencies for 1.6
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@26134 923e3c2e-ae13-0410-ba18-ced7211e33de

--- wiki/setup/setup.inc.php
+++ wiki/setup/setup.inc.php
@@ -1,64 +1,50 @@
 <?php
-	/**************************************************************************\
-	* eGroupWare - Setup                                                       *
-	* http://www.egroupware.org                                                *
-	* --------------------------------------------                             *
-	*  This program is free software; you can redistribute it and/or modify it *
-	*  under the terms of the GNU General Public License as published by the   *
-	*  Free Software Foundation; either version 2 of the License, or (at your  *
-	*  option) any later version.                                              *
-	\**************************************************************************/
-	/* $Id$ */
+/**
+ * eGroupware - Wiki
+ *
+ * @link http://www.egroupware.org
+ * @author Ralf Becker <RalfBecker-AT-outdoor-training.de>
+ * @license http://opensource.org/licenses/gpl-license.php GPL - GNU General Public License
+ * @package wiki
+ * @subpackage setup
+ * @version $Id$
+ */
 
-	$setup_info['wiki']['name']      = 'wiki';
-	$setup_info['wiki']['title']     = 'Wiki';
-	$setup_info['wiki']['version']   = '1.5.001';
-	$setup_info['wiki']['app_order'] = 11;
-	$setup_info['wiki']['enable']    = 1;
+$setup_info['wiki']['name']      = 'wiki';
+$setup_info['wiki']['title']     = 'Wiki';
+$setup_info['wiki']['version']   = '1.6';
+$setup_info['wiki']['app_order'] = 11;
+$setup_info['wiki']['enable']    = 1;
 
-	$setup_info['wiki']['author']    = 'Tavi Team';
-	$setup_info['wiki']['license']   = 'GPL';
-	$setup_info['wiki']['description'] =
-		'Wiki is a modified and enhanced version of <a href="http://tavi.sf.net" target="_new">WikkiTikkiTavi</a> for use with eGroupware.';
-	$setup_info['wiki']['maintainer'] = 'Ralf Becker';
-	$setup_info['wiki']['maintainer_email'] = 'RalfBecker@outdoor-training.de';
+$setup_info['wiki']['author']    = 'Tavi Team';
+$setup_info['wiki']['license']   = 'GPL';
+$setup_info['wiki']['description'] =
+	'Wiki is a modified and enhanced version of <a href="http://tavi.sf.net" target="_new">WikkiTikkiTavi</a> for use with eGroupware.';
+$setup_info['wiki']['maintainer'] = 'Ralf Becker';
+$setup_info['wiki']['maintainer_email'] = 'RalfBecker@outdoor-training.de';
 
-	$setup_info['wiki']['tables'][] = 'egw_wiki_links';
-	$setup_info['wiki']['tables'][] = 'egw_wiki_pages';
-	$setup_info['wiki']['tables'][] = 'egw_wiki_rate';
-	$setup_info['wiki']['tables'][] = 'egw_wiki_interwiki';
-	$setup_info['wiki']['tables'][] = 'egw_wiki_sisterwiki';
-	$setup_info['wiki']['tables'][] = 'egw_wiki_remote_pages';
-	
-	/* The hooks this app includes, needed for hooks registration */
-	$setup_info['wiki']['hooks'][] = 'admin';
-	$setup_info['wiki']['hooks'][] = 'sidebox_menu';
-	$setup_info['wiki']['hooks'][] = 'config_validate';
-	$setup_info['wiki']['hooks'][] = 'settings';
-	$setup_info['wiki']['hooks']['search_link'] = 'wiki.bowiki.search_link';
+$setup_info['wiki']['tables'][] = 'egw_wiki_links';
+$setup_info['wiki']['tables'][] = 'egw_wiki_pages';
+$setup_info['wiki']['tables'][] = 'egw_wiki_rate';
+$setup_info['wiki']['tables'][] = 'egw_wiki_interwiki';
+$setup_info['wiki']['tables'][] = 'egw_wiki_sisterwiki';
+$setup_info['wiki']['tables'][] = 'egw_wiki_remote_pages';
 
-	/* Dependencies for this app to work */
-	$setup_info['wiki']['depends'][] = array
-	(
-		'appname'  => 'phpgwapi',
-		'versions' => Array('1.3','1.4','1.5','1.6')
-	);
-	$setup_info['wiki']['depends'][] = array
-	(
-		'appname'  => 'etemplate',
-		'versions' => Array('1.3','1.4','1.5','1.6')
-	);
+/* The hooks this app includes, needed for hooks registration */
+$setup_info['wiki']['hooks'][] = 'admin';
+$setup_info['wiki']['hooks'][] = 'sidebox_menu';
+$setup_info['wiki']['hooks'][] = 'config_validate';
+$setup_info['wiki']['hooks'][] = 'settings';
+$setup_info['wiki']['hooks']['search_link'] = 'wiki.bowiki.search_link';
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+/* Dependencies for this app to work */
+$setup_info['wiki']['depends'][] = array
+(
+	'appname'  => 'phpgwapi',
+	'versions' => Array('1.4','1.5','1.6','1.7')
+);
+$setup_info['wiki']['depends'][] = array
+(
+	'appname'  => 'etemplate',
+	'versions' => Array('1.4','1.5','1.6','1.7')
+);
