6c698ee58dfee79322c74ba186251499aeedd298
ralfbecker <ralfbecker@923e3c2e-ae13-0410-ba18-ced7211e33de>
2008-10-10 11:59:20 +0000

    version and dependencies for 1.6
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@26148 923e3c2e-ae13-0410-ba18-ced7211e33de

--- sitemgr/setup/setup.inc.php
+++ sitemgr/setup/setup.inc.php
@@ -1,19 +1,17 @@
 <?php
-/**************************************************************************\
-* eGroupWare SiteMgr - Web Content Management                              *
-* http://www.egroupware.org                                                *
-* --------------------------------------------                             *
-*  This program is free software; you can redistribute it and/or modify it *
-*  under the terms of the GNU General Public License as published by the   *
-*  Free Software Foundation; either version 2 of the License, or (at your  *
-*  option) any later version.                                              *
-\**************************************************************************/
-
-/* $Id$ */
+/**
+ * eGroupWare - SiteMgr Web content Management
+ *
+ * @link http://www.egroupware.org
+ * @license http://opensource.org/licenses/gpl-license.php GPL - GNU General Public License
+ * @package sitemgr
+ * @subpackage setup
+ * @version $Id$
+ */
 
 $setup_info['sitemgr']['name']      = 'sitemgr';
 $setup_info['sitemgr']['title']     = 'SiteMgr Web Content Management';
-$setup_info['sitemgr']['version']   = '1.5.002';
+$setup_info['sitemgr']['version']   = '1.6';
 $setup_info['sitemgr']['app_order'] = 14;
 $setup_info['sitemgr']['tables']    = array('egw_sitemgr_pages','egw_sitemgr_pages_lang','egw_sitemgr_categories_state','egw_sitemgr_categories_lang','egw_sitemgr_modules','egw_sitemgr_blocks','egw_sitemgr_blocks_lang','egw_sitemgr_content','egw_sitemgr_content_lang','egw_sitemgr_active_modules','egw_sitemgr_properties','egw_sitemgr_sites','egw_sitemgr_notifications','egw_sitemgr_notify_messages');
 $setup_info['sitemgr']['enable']    = 1;
@@ -58,11 +56,11 @@ $setup_info['sitemgr']['hooks'][] = 'settings';
 /* Dependencies for this app to work */
 $setup_info['sitemgr']['depends'][] = array(
 	'appname'  => 'phpgwapi',
-	'versions' => Array('1.3','1.4','1.5','1.6','1.7')
+	'versions' => Array('1.4','1.5','1.6','1.7')
 );
 $setup_info['sitemgr']['depends'][] = array(
 	'appname'  => 'etemplate',
-	'versions' => Array('1.3','1.4','1.5','1.6','1.7')
+	'versions' => Array('1.4','1.5','1.6','1.7')
 );
 
 /**
