d06a4912b65096a7e6d1a389dcbe467f413f6e64
ralfbecker <ralfbecker@923e3c2e-ae13-0410-ba18-ced7211e33de>
2008-10-10 12:04:49 +0000

    version and dependencies for 1.6
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@26149 923e3c2e-ae13-0410-ba18-ced7211e33de

--- syncml/setup/setup.inc.php
+++ syncml/setup/setup.inc.php
@@ -1,52 +1,50 @@
 <?php
-	/**************************************************************************\
-	* eGroupWare - syncml setup                                                *
-	* http://www.egroupware.org                                                *
-	* --------------------------------------------                             *
-	*  This program is free software; you can redistribute it and/or modify it *
-	*  under the terms of the GNU General Public License as published by the   *
-	*  Free Software Foundation; version 2 of the License.                     *
-	\**************************************************************************/
+/**
+ * eGroupWare - SyncML
+ *
+ * @link http://www.egroupware.org
+ * @license http://opensource.org/licenses/gpl-license.php GPL - GNU General Public License
+ * @package syncml
+ * @subpackage setup
+ * @version $Id$
+ */
 
-	// $Id$
+/* Basic information about this app */
+$setup_info['syncml']['name']      = 'syncml';
+$setup_info['syncml']['title']     = 'SyncML';
+$setup_info['syncml']['version']   = '1.6';
+$setup_info['syncml']['enable']    = 3;
+$setup_info['syncml']['app_order'] = 99;
 
-	/* Basic information about this app */
-	$setup_info['syncml']['name']      = 'syncml';
-	$setup_info['syncml']['title']     = 'SyncML';
-	$setup_info['syncml']['version']   = '1.4';
-	$setup_info['syncml']['enable']    = 3;
-	$setup_info['syncml']['app_order'] = 99;
+$setup_info['syncml']['author'] = 'Lars Kneschke, Horde Project';
+$setup_info['syncml']['note']   = 'SyncML interface for eGroupWare';
+$setup_info['syncml']['license']  = 'GPL';
+$setup_info['syncml']['description'] =
+	'This module allows you to syncronize your SyncML enabled device.';
 
-	$setup_info['syncml']['author'] = 'Lars Kneschke';
-	$setup_info['syncml']['note']   = 'SyncML interface for eGroupWare';
-	$setup_info['syncml']['license']  = 'GPL';
-	$setup_info['syncml']['description'] =
-		'This module allows you to syncronize your SyncML enabled device.';
+$setup_info['syncml']['maintainer'] = 'eGroupware coretime';
+$setup_info['syncml']['maintainer_email'] = 'egroupware-developers@list.sf.net';
 
-	$setup_info['syncml']['maintainer'] = 'Lars Kneschke';
-	$setup_info['syncml']['maintainer_email'] = 'l.kneschke@metaways.de';
- 
 
-	/* The tables this app creates */
-	$setup_info['syncml']['tables'][]  = 'egw_contentmap';
-	$setup_info['syncml']['tables'][]  = 'egw_syncmldevinfo';
-	$setup_info['syncml']['tables'][]  = 'egw_syncmlsummary';
-	$setup_info['syncml']['tables'][]  = 'egw_syncmldeviceowner';
+/* The tables this app creates */
+$setup_info['syncml']['tables'][]  = 'egw_contentmap';
+$setup_info['syncml']['tables'][]  = 'egw_syncmldevinfo';
+$setup_info['syncml']['tables'][]  = 'egw_syncmlsummary';
+$setup_info['syncml']['tables'][]  = 'egw_syncmldeviceowner';
 
-	/* Dependencies for this app to work */
-	$setup_info['syncml']['depends'][] = array(
-		 'appname'  => 'phpgwapi',
-		 'versions' => Array('1.3','1.4','1.5')
-	);
-	// installation checks for SyncML
-	$setup_info['syncml']['check_install'] = array(
-		'' => array(
-			'func' => 'pear_check',
-			'from' => 'SyncML',
-		),
-		'Log' => array(
-			'func' => 'pear_check',
-			'from' => 'SyncML',
-		),
-	);
-
+/* Dependencies for this app to work */
+$setup_info['syncml']['depends'][] = array(
+	 'appname'  => 'phpgwapi',
+	 'versions' => Array('1.4','1.5','1.6','1.7')
+);
+// installation checks for SyncML
+$setup_info['syncml']['check_install'] = array(
+	'' => array(
+		'func' => 'pear_check',
+		'from' => 'SyncML',
+	),
+	'Log' => array(
+		'func' => 'pear_check',
+		'from' => 'SyncML',
+	),
+);
--- syncml/setup/tables_update.inc.php
+++ syncml/setup/tables_update.inc.php
@@ -1,118 +1,118 @@
 <?php
-	/**************************************************************************\
-	* eGroupWare - Setup                                                       *
-	* http://www.egroupware.org                                                *
-	* --------------------------------------------                             *
-	*  This program is free software; you can redistribute it and/or modify it *
-	*  under the terms of the GNU General Public License as published by the   *
-	*  Free Software Foundation; either version 2 of the License, or (at your  *
-	*  option) any later version.                                              *
-	\**************************************************************************/
+/**
+ * eGroupWare - SyncML
+ *
+ * @link http://www.egroupware.org
+ * @license http://opensource.org/licenses/gpl-license.php GPL - GNU General Public License
+ * @package syncml
+ * @subpackage setup
+ * @version $Id$
+ */
 
-	// $Id: tables_update.inc.php 22049 2006-07-08 22:18:36Z ralfbecker $
+function syncml_upgrade0_9_0()
+{
+	// We drop and create the table new, as the info's in it are queried
+	// automatic from the device if not present in the table.
+	// This way we dont have to fight with DB's who can create an
+	// autoincrement-index for an existing table.
+	$GLOBALS['egw_setup']->oProc->DropTable('egw_syncmldevinfo');
 
-	$test[] = '0.9.0';
-	function syncml_upgrade0_9_0()
-	{
-		// We drop and create the table new, as the info's in it are queried 
-		// automatic from the device if not present in the table.
-		// This way we dont have to fight with DB's who can create an 
-		// autoincrement-index for an existing table.
-		$GLOBALS['egw_setup']->oProc->DropTable('egw_syncmldevinfo');
+	$GLOBALS['egw_setup']->oProc->CreateTable('egw_syncmldevinfo',array(
+		'fd' => array(
+			'dev_dtdversion' => array('type' => 'varchar','precision' => '10','nullable' => False),
+			'dev_numberofchanges' => array('type' => 'bool','nullable' => False),
+			'dev_largeobjs' => array('type' => 'bool','nullable' => False),
+			'dev_swversion' => array('type' => 'varchar','precision' => '100'),
+			'dev_oem' => array('type' => 'varchar','precision' => '100'),
+			'dev_model' => array('type' => 'varchar','precision' => '100','nullable' => False),
+			'dev_manufacturer' => array('type' => 'varchar','precision' => '100','nullable' => False),
+			'dev_devicetype' => array('type' => 'varchar','precision' => '100','nullable' => False),
+			'dev_datastore' => array('type' => 'text','nullable' => False),
+			'dev_id' => array('type' => 'auto','nullable' => False),
+			'dev_fwversion' => array('type' => 'varchar','precision' => '100'),
+			'dev_hwversion' => array('type' => 'varchar','precision' => '100'),
+			'dev_utc' => array('type' => 'bool','nullable' => False)
+		),
+		'pk' => array('dev_id'),
+		'fk' => array(),
+		'ix' => array('dev_id'),
+		'uc' => array('dev_id',array('dev_model','dev_manufacturer','dev_swversion'))
+	));
 
-		$GLOBALS['egw_setup']->oProc->CreateTable('egw_syncmldevinfo',array(
-			'fd' => array(
-				'dev_dtdversion' => array('type' => 'varchar','precision' => '10','nullable' => False),
-				'dev_numberofchanges' => array('type' => 'bool','nullable' => False),
-				'dev_largeobjs' => array('type' => 'bool','nullable' => False),
-				'dev_swversion' => array('type' => 'varchar','precision' => '100'),
-				'dev_oem' => array('type' => 'varchar','precision' => '100'),
-				'dev_model' => array('type' => 'varchar','precision' => '100','nullable' => False),
-				'dev_manufacturer' => array('type' => 'varchar','precision' => '100','nullable' => False),
-				'dev_devicetype' => array('type' => 'varchar','precision' => '100','nullable' => False),
-				'dev_datastore' => array('type' => 'text','nullable' => False),
-				'dev_id' => array('type' => 'auto','nullable' => False),
-				'dev_fwversion' => array('type' => 'varchar','precision' => '100'),
-				'dev_hwversion' => array('type' => 'varchar','precision' => '100'),
-				'dev_utc' => array('type' => 'bool','nullable' => False)
-			),
-			'pk' => array('dev_id'),
-			'fk' => array(),
-			'ix' => array('dev_id'),
-			'uc' => array('dev_id',array('dev_model','dev_manufacturer','dev_swversion'))
-		));
-
-		return $GLOBALS['setup_info']['syncml']['currentver'] = '0.9.4';
-	}
-
-	$test[] = '0.9.4';
-	function syncml_upgrade0_9_4()
-	{
-		$GLOBALS['egw_setup']->oProc->CreateTable('egw_syncmldeviceowner',array(
-			'fd' => array(
-				'owner_locname' => array('type' => 'varchar','precision' => '200','nullable' => False),
-				'owner_devid' => array('type' => 'int','precision' => '4','nullable' => False),
-				'owner_deviceid' => array('type' => 'varchar','precision' => '100','nullable' => False)
-			),
-			'pk' => array('owner_devid'),
-			'fk' => array(),
-			'ix' => array('owner_locname','owner_deviceid'),
-			'uc' => array(array('owner_locname','owner_devid','owner_deviceid'))
-		));
-		return $GLOBALS['setup_info']['syncml']['currentver'] = '0.9.6';
-	}
-
-	$test[] = '0.9.6';
-	function syncml_upgrade0_9_6()
-	{
-		$GLOBALS['egw_setup']->oProc->RefreshTable('egw_syncmldeviceowner',array(
-			'fd' => array(
-				'owner_locname' => array('type' => 'varchar','precision' => '200','nullable' => False),
-				'owner_devid' => array('type' => 'int','precision' => '4','nullable' => False),
-				'owner_deviceid' => array('type' => 'varchar','precision' => '100','nullable' => False)
-			),
-			'pk' => array(),
-			'fk' => array(),
-			'ix' => array('owner_deviceid'),
-			'uc' => array(array('owner_locname','owner_devid','owner_deviceid'))
-		));
-
-		return $GLOBALS['setup_info']['syncml']['currentver'] = '0.9.007';
-	}
+	return $GLOBALS['setup_info']['syncml']['currentver'] = '0.9.4';
+}
 
 
-	$test[] = '0.9.007';
-	function syncml_upgrade0_9_007()
-	{
-		$GLOBALS['egw_setup']->oProc->RefreshTable('egw_syncmldevinfo',array(
-			'fd' => array(
-				'dev_dtdversion' => array('type' => 'varchar','precision' => '10','nullable' => False),
-				'dev_numberofchanges' => array('type' => 'bool','nullable' => False),
-				'dev_largeobjs' => array('type' => 'bool','nullable' => False),
-				'dev_swversion' => array('type' => 'varchar','precision' => '100'),
-				'dev_oem' => array('type' => 'varchar','precision' => '100'),
-				'dev_model' => array('type' => 'varchar','precision' => '100','nullable' => False),
-				'dev_manufacturer' => array('type' => 'varchar','precision' => '100','nullable' => False),
-				'dev_devicetype' => array('type' => 'varchar','precision' => '100','nullable' => False),
-				'dev_datastore' => array('type' => 'text','nullable' => False),
-				'dev_id' => array('type' => 'auto','nullable' => False),
-				'dev_fwversion' => array('type' => 'varchar','precision' => '100'),
-				'dev_hwversion' => array('type' => 'varchar','precision' => '100'),
-				'dev_utc' => array('type' => 'bool','nullable' => False)
-			),
-			'pk' => array('dev_id'),
-			'fk' => array(),
-			'ix' => array(array('dev_model','dev_manufacturer')),
-			'uc' => array()
-		));
-
-		return $GLOBALS['setup_info']['syncml']['currentver'] = '0.9.008';
-	}
+function syncml_upgrade0_9_4()
+{
+	$GLOBALS['egw_setup']->oProc->CreateTable('egw_syncmldeviceowner',array(
+		'fd' => array(
+			'owner_locname' => array('type' => 'varchar','precision' => '200','nullable' => False),
+			'owner_devid' => array('type' => 'int','precision' => '4','nullable' => False),
+			'owner_deviceid' => array('type' => 'varchar','precision' => '100','nullable' => False)
+		),
+		'pk' => array('owner_devid'),
+		'fk' => array(),
+		'ix' => array('owner_locname','owner_deviceid'),
+		'uc' => array(array('owner_locname','owner_devid','owner_deviceid'))
+	));
+	return $GLOBALS['setup_info']['syncml']['currentver'] = '0.9.6';
+}
 
 
-	$test[] = '0.9.008';
-	function syncml_upgrade0_9_008()
-	{
-		return $GLOBALS['setup_info']['syncml']['currentver'] = '1.4';
-	}
-?>
+function syncml_upgrade0_9_6()
+{
+	$GLOBALS['egw_setup']->oProc->RefreshTable('egw_syncmldeviceowner',array(
+		'fd' => array(
+			'owner_locname' => array('type' => 'varchar','precision' => '200','nullable' => False),
+			'owner_devid' => array('type' => 'int','precision' => '4','nullable' => False),
+			'owner_deviceid' => array('type' => 'varchar','precision' => '100','nullable' => False)
+		),
+		'pk' => array(),
+		'fk' => array(),
+		'ix' => array('owner_deviceid'),
+		'uc' => array(array('owner_locname','owner_devid','owner_deviceid'))
+	));
+
+	return $GLOBALS['setup_info']['syncml']['currentver'] = '0.9.007';
+}
+
+
+function syncml_upgrade0_9_007()
+{
+	$GLOBALS['egw_setup']->oProc->RefreshTable('egw_syncmldevinfo',array(
+		'fd' => array(
+			'dev_dtdversion' => array('type' => 'varchar','precision' => '10','nullable' => False),
+			'dev_numberofchanges' => array('type' => 'bool','nullable' => False),
+			'dev_largeobjs' => array('type' => 'bool','nullable' => False),
+			'dev_swversion' => array('type' => 'varchar','precision' => '100'),
+			'dev_oem' => array('type' => 'varchar','precision' => '100'),
+			'dev_model' => array('type' => 'varchar','precision' => '100','nullable' => False),
+			'dev_manufacturer' => array('type' => 'varchar','precision' => '100','nullable' => False),
+			'dev_devicetype' => array('type' => 'varchar','precision' => '100','nullable' => False),
+			'dev_datastore' => array('type' => 'text','nullable' => False),
+			'dev_id' => array('type' => 'auto','nullable' => False),
+			'dev_fwversion' => array('type' => 'varchar','precision' => '100'),
+			'dev_hwversion' => array('type' => 'varchar','precision' => '100'),
+			'dev_utc' => array('type' => 'bool','nullable' => False)
+		),
+		'pk' => array('dev_id'),
+		'fk' => array(),
+		'ix' => array(array('dev_model','dev_manufacturer')),
+		'uc' => array()
+	));
+
+	return $GLOBALS['setup_info']['syncml']['currentver'] = '0.9.008';
+}
+
+
+function syncml_upgrade0_9_008()
+{
+	return $GLOBALS['setup_info']['syncml']['currentver'] = '1.4';
+}
+
+
+function syncml_upgrade1_4()
+{
+	return $GLOBALS['setup_info']['syncml']['currentver'] = '1.6';
+}
