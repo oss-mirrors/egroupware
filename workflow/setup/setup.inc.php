1e58039a4ac2b836c433b82d85680f882510eb53
nathangray <nathangray@923e3c2e-ae13-0410-ba18-ced7211e33de>
2008-11-04 16:07:52 +0000

    Workflow seems to be working with 1.6, update dependancies
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@26312 923e3c2e-ae13-0410-ba18-ced7211e33de

--- workflow/setup/setup.inc.php
+++ workflow/setup/setup.inc.php
@@ -52,10 +52,10 @@
 	/* Dependencies for this app to work */ 
 	$setup_info['workflow']['depends'][] = array(
 		'appname' => 'phpgwapi',
-		'versions' => Array('1.0.1','1.2','1.3','1.4','1.5')
+		'versions' => Array('1.4','1.5','1.6')
 	);
 	$setup_info['workflow']['depends'][] = array(
 		'appname' => 'preferences',
-		'versions' => Array('1.2','1.3','1.4','1.5')
+		'versions' => Array('1.4','1.5','1.6')
 	);
 ?>
