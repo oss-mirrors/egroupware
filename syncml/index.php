58514b231caa292e7275b4c7a892a5387d2abe7b
jaytraxx <jaytraxx@923e3c2e-ae13-0410-ba18-ced7211e33de>
2009-10-06 15:12:39 +0000

    Adjusted SyncML index page (some typos, removed passages of the 1.6 patch). Added direct link to SyncML preferences
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@28044 923e3c2e-ae13-0410-ba18-ced7211e33de

--- syncml/index.php
+++ syncml/index.php
@@ -22,32 +22,32 @@ $GLOBALS['egw_info'] = array(
 include('../header.inc.php');
 
 print("
-<h1>The SyncML Application</h1>
-This application does not provide <i>frontend</i> functionality but implements synchronization of various content with extremal devices via the SyncML/HTTP protocol.<br/>
-This eGroupWare version does support SyncML 1.0, 1.1 and 1.2.<br/>
+<h1>The SyncML application</h1>
+This application does not provide <i>frontend</i> functionality but implements synchronization of various content with external devices via the SyncML/HTTP protocol.<br/>
+This EGroupware version does support SyncML 1.0, 1.1 and 1.2.<br/>
 You can synchronize your
 <ul>
 <li>Addressbook</li>
 <li>Calendar<li>
 <li>Task (InfoLog)</li>
 <li>Notes (InfoLog)</li></ul>
-The SyncML module does provide some customization parameters for conflict handling which you can find underneath <i>Preferences</i>.
+The SyncML module provides some customization parameters for conflict handling which you can find in your ".html::a_href('SyncML preferences',array('menuaction'=>'preferences.uisettings.index','appname'=>'syncml')).".
 
-<h2>Conflict Handling and Server R/O Options</h2>
-If items are change on both &mdash; server and client side &mdash; before the next synchronization, this is called a conflict situation. The original SyncML implementation of EGW left you no choice but the client changes always overwrote the server changes. This was maybe not your preferred behavior. With my latest extensions you can now configure the conflict handling of EGW for every target individually. There are three different ways now to handle colliding client changes:
+<h2>Conflict handling and server R/O options</h2>
+If items are changed on both &mdash; server and client side &mdash; before the next synchronization, this is called a conflict situation. You can configure the conflict handling of EGroupware for every target individually. There are three different ways to handle colliding client changes:
 <ol>
-<li>The client wins and overwrites the server data (original behavior).</li>
-<li>The server wins and overwrites the client data (new default). </li>
+<li>The client wins and overwrites the server data.</li>
+<li>The server wins and overwrites the client data (default). </li>
 <li>Duplicate entries are created from both versions.</li></ol>
-The last two options will put the server in the read only mode. If the client sends changes to the server, you can choose between:
+The next two options will put the server in read only mode. If the client sends changes to the server, you can choose between:
 <ol start='4'>
-<li>The client and server data are unchanged (split brain).</li>
-<li>The Sever reverts all client changes.</li></ol>
+<li>The client and server data is unchanged (split brain).</li>
+<li>The server reverts all client changes.</li></ol>
 The second option relies on the client's cooperation, though. If it gets a change, the server will send its own version of the data back to the client. The client is supposed to roll back the changes this way.
-<h2>Conflict Categories for eGroupWare Data Types</h2>
-If you select <b>duplicates</b> as conflict resolution, you may want to assign a certain <b>category</b> for these data items. If eGroupWare detects a collision, it will assign the old server item to the selected <i>conflict category</i> and replace the original item with the clients content.
-<h2>Client Configuration</h2>
-There are certain settings of the client which you have to adjust for eGroupWare synchronization:
+<h2>Conflict categories for EGroupware data types</h2>
+If you select <b>duplicates</b> as conflict resolution, you may want to assign a certain <b>category</b> for these data items. If EGroupware detects a collision, it will assign the old server item to the selected <i>conflict category</i> and replace the original item with the clients content.
+<h2>Client configuration</h2>
+There are certain settings of the client which you have to adjust for EGroupware synchronization:
 <dl>
 <dt><b>Server location/URL</b></dt>
 <dd>");
@@ -66,11 +66,11 @@ print($GLOBALS['egw_info']['user']['domain']);
 print("
 </dd>
 <dt><b>Password</b></dt>
-<dd>Your eGroupWare Password.<dd>
+<dd>Your EGroupware password<dd>
 </dl>
-<h4>Remote Database Names</h4>
+<h4>Remote database names</h4>
 <table border='1'>
-<tr><th>&nbsp;EGW application&nbsp;</th><th>&nbsp;datastore name&nbsp;</th><th>&nbsp;content type&nbsp;</th></tr>
+<tr><th>&nbsp;EGroupware application&nbsp;</th><th>&nbsp;datastore name&nbsp;</th><th>&nbsp;content type&nbsp;</th></tr>
 <tr><td><b>Addressbook</b></td><td>./contacts</td><td>text/vCard</td></tr>
 <tr><td></td><td>./sifcontacts</td><td>text/x-s4j-sifc</td></tr>
 <tr><td></td><td>./scard</td><td>text/x-s4j-sifc</td></tr>
