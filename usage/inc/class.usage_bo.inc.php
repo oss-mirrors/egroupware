b6036462be79bc333ed5ea0f0ffadaa1b43b2f99
ralfbecker <ralfbecker@923e3c2e-ae13-0410-ba18-ced7211e33de>
2010-03-06 15:40:39 +0000

    "simple textual statistic (incl. caching for 1h)"
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@29411 923e3c2e-ae13-0410-ba18-ced7211e33de

--- usage/inc/class.usage_bo.inc.php
+++ usage/inc/class.usage_bo.inc.php
@@ -5,7 +5,7 @@
  * @link http://www.egroupware.org
  * @author Ralf Becker <RalfBecker-AT-outdoor-training.de>
  * @package usage
- * @copyright (c) 2009 by Ralf Becker <RalfBecker-AT-outdoor-training.de>
+ * @copyright (c) 2009/10 by Ralf Becker <RalfBecker-AT-outdoor-training.de>
  * @license http://opensource.org/licenses/gpl-license.php GPL - GNU General Public License
  * @version $Id$
  */
@@ -63,7 +63,7 @@ class usage_bo extends so_sql
 
 		$this->data = array(
 			'usage_country' => $submitted['country'] ? $submitted['country'] : null,
-			'usage_type' => $submitted['usage_type'],
+			'usage_type' => $submitted['usage_type'] == 'comercial' ? 'commercial' : $submitted['usage_type'],
 			'usage_users' => $submitted['users'],
 			'usage_sessions' => $submitted['sessions'],
 			'usage_version' => $submitted['version'],
@@ -142,15 +142,141 @@ class usage_bo extends so_sql
 			throw new egw_exception_wrong_userinput(lang("This submit ID already submitted it's monthly report! --> report ignored"));
 		}
 	}
+	
+	/**
+	 * Time how often statistic get's calculated
+	 */
+	const CACHING_TIME = 600;
 
 	/**
-	 * Display statistic
+	 * Return statistic: either calculate it or use cached version
 	 *
 	 * @return string
 	 */
 	public function statistic()
 	{
-		return "<h2>Usage statistics get currently only collected, there's not yet any statistic to display.</h2>\n<h2>Come back soon ...</h2>\n".
-			"<h2>".$this->db->select(self::MAIN_TABLE,'COUNT(*)','',__LINE__,__FILE__,false,'',self::APP_NAME)->fetchColumn()." submissions to the statistic so far.</h2>\n";
+		return egw_cache::getInstance('usage','statistic-'.$GLOBALS['egw_info']['user']['preferences']['common']['lang'],array($this,'calc_statistic'),array(),self::CACHING_TIME);
+	}
+	
+	/**
+	 * Calculate statistic
+	 *
+	 * @return string
+	 */
+	public function calc_statistic()
+	{
+		translation::add_app('admin');	// contains translations of our labels
+
+		$sel_options['usage_type'] = array(
+			'commercial'   => lang('Commercial: all sorts of companies'),
+			'governmental' => lang('Governmental: incl. state or municipal authorities or services'),
+			'educational' => lang('Educational: Universities, Schools, ...'),
+			'non-profit'  => lang('Non profit: Clubs, associations, ...'),
+			'personal'    => lang('Personal: eg. within a family'),
+			'other'       => lang('Other'),
+		);
+		$sel_options['usage_install_type'] = array(
+			'archive' => lang('Archive: zip or tar'),
+			'package' => lang('RPM or Debian package'),
+			'svn'     => lang('Subversion checkout'),
+			'other'   => lang('Other'),
+		);
+		$all_time_total = $this->db->select(self::MAIN_TABLE,'COUNT(*)','',__LINE__,__FILE__,false,'',self::APP_NAME)->fetchColumn();
+
+		$stat_limit = time() - 30*24*60*60;	// 30 days back
+		$stat_limit_sql = 'usage_submitted >= '.$this->db->quote($stat_limit,'timestamp');
+		$total_30d = $this->db->select(self::MAIN_TABLE ,'COUNT(*)',$stat_limit_sql,__LINE__,__FILE__,false,'',self::APP_NAME)->fetchColumn();
+
+		$content .= "<h1>Evaluation of $total_30d submissions of last 30 days:</h1>\n";
+
+		$content .= '<style type="text/css">
+	.usageList {
+		margin: 0;
+		padding: 0;
+		max-height: 4em;
+		overflow-y: auto;
+		overflow-x: hidden;
+	}
+	.usageList li {
+		line-height: 133%;
+		white-space: nowrap;
+	}
+</style>
+';
+		$content .= "<table width='100%'>\n";
+		/*$content .= "<thead>\n\t<tr>\n";
+		$content .= "\t\t<th>".lang('Category')."</th>\n";
+		$content .= "\t\t<th>".lang('Answers')."</th>\n";
+		$content .= "\t</tr>\n</thead>\n";*/
+		$contetn .= "<tbody\n";
+		foreach(array(
+			'usage_country' => 'Country',
+			'usage_type' => 'Usage',
+			'usage_users' => 'Number of users',
+			'usage_sessions' => 'Sessions last 30 days',
+			'usage_version' => 'EGroupware Version',
+			'usage_os' => 'Operating System',
+			'usage_php' => 'PHP Version',
+			'usage_install_type' => 'Installation Type',
+		) as $col => $label)
+		{
+			$group_by = $col;
+			$group_by2 = $col2 = '';
+			if ($col == 'usage_users' || $col == 'usage_sessions')
+			{
+				$max = $this->db->select(self::MAIN_TABLE ,"MAX($col)",$stat_limit_sql,__LINE__,__FILE__,false,'',self::APP_NAME)->fetchColumn();
+				$max_fraction = ceil($max / 20);
+				$group_by = "TRUNCATE($col/$max_fraction,0)";
+				$col = "CONCAT($group_by*$max_fraction,' - ',($group_by+1)*$max_fraction)";
+			}
+			elseif($col == 'usage_php')
+			{
+				$group_by2 = $col2 = "SUBSTR($col,LOCATE(':',$col)+1,99)";
+				$group_by = $col = "LEFT($col,CASE WHEN LOCATE(':',$col) < LOCATE('-',$col) OR LOCATE('-',$col)=0 THEN LOCATE(':',$col) ELSE LOCATE('-',$col) END-1)";
+			}
+			elseif($col == 'usage_os')
+			{
+				$group_by2 = $col2 = $col;
+				$group_by = $col = "CASE WHEN LOCATE('Linux',$col) > 0 THEN 'Linux' ELSE $col END";
+			}
+			elseif($col == 'usage_version')
+			{
+				$group_by = $col = "CASE WHEN RIGHT($col,3)='EPL' THEN CONCAT('EPL ',SUBSTR($col,LOCATE(' ',$col),4)) ELSE $col END";
+			}
+			$colspan = empty($col2) ? 'colspan="2"' : ' width="200"';
+			$content .= "\t<tr valign='top'>\n\t\t<td><b>".lang($label)."</b></td>\n\t\t<td $colspan>\n\t\t\t<ol class='usageList'>\n";
+			foreach($this->db->select(self::MAIN_TABLE,$col.' AS value,COUNT(*) AS count',$stat_limit_sql,__LINE__,__FILE__,false,'GROUP BY '.$group_by.' ORDER BY COUNT(*) DESC',self::APP_NAME) as $row)
+			{
+				switch($col)
+				{
+					case 'usage_country':
+						if (!isset($country)) $country = new country();
+						$row['value'] = empty($row['value']) ? lang('International use') : $country->get_full_name($row['value']);
+						break;
+					case 'usage_type':
+					case 'usage_install_type':
+						$row['value'] = $sel_options[$col][$row['value']];
+						break;
+				}
+				$content .= "\t\t\t\t<li>".$row['count'].' = '.number_format(100.0*$row['count']/$total_30d,1).'% &nbsp; '.$row['value']."</li>\n";
+			}
+			$content .= "\t\t\t</ol>\n\t\t</td>\n";
+			
+			if (!empty($col2))
+			{
+				$content .= "\t\t<td>\n\t\t\t<ol class='usageList'>\n";
+				foreach($this->db->select(self::MAIN_TABLE,$col2.' AS value,COUNT(*) AS count',$stat_limit_sql,__LINE__,__FILE__,false,'GROUP BY '.$group_by2.' ORDER BY COUNT(*) DESC',self::APP_NAME) as $row)
+				{
+					$content .= "\t\t\t\t<li>".$row['count'].' = '.number_format(100.0*$row['count']/$total_30d,1).'%&nbsp; '.$row['value']."</li>\n";
+				}
+				$content .= "\t\t\t</ol>\n\t\t</td>\n";
+			}
+			$content .= "\t</tr>\n";
+		}
+		$content .= "</tbody>\n</table>\n";
+		
+		$content .= "<h2>$all_time_total submissions to the statistic since it's start.</h2>\n";
+
+		return $content;
 	}
 }
