5ecbf2b75bd02a9eda5509c001346073b693e42d
ralfbecker <ralfbecker@923e3c2e-ae13-0410-ba18-ced7211e33de>
2010-03-07 14:45:12 +0000

    Fixed r29141: "Add a checkbox to allow user to prevent notification of changes to a ticket", to NOT prevent history logging, if checked
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@29433 923e3c2e-ae13-0410-ba18-ced7211e33de

--- tracker/inc/class.tracker_bo.inc.php
+++ tracker/inc/class.tracker_bo.inc.php
@@ -511,7 +511,7 @@ class tracker_bo extends tracker_so
 				$this->data['customfields'] = implode("\n",$data_custom);
 				$old['customfields'] = implode("\n",$old_custom);
 			}
-			if (!$this->data['no_notifications'] && !$this->tracking->track($this->data,$old,$this->user,null,null,$autoreply))
+			if (!$this->tracking->track($this->data,$old,$this->user,null,null,$this->data['no_notifications']))
 			{
 				return implode(', ',$this->tracking->errors);
 			}
