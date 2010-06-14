dbee07f8e48daaa239f51632f976ef68416c12ea
nathangray <nathangray@923e3c2e-ae13-0410-ba18-ced7211e33de>
2010-06-14 12:48:31 +0000

    Fix tree view with a bookmark name the same as category name
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@30791 923e3c2e-ae13-0410-ba18-ced7211e33de

--- bookmarks/inc/class.bookmarks_ui.inc.php
+++ bookmarks/inc/class.bookmarks_ui.inc.php
@@ -396,9 +396,11 @@
 				$parent = $cat['parent'];
 				while ( $parent != 0) {
 					$categories[$key]['tree'] = $parent. '/'. $categories[$key]['tree'];
-					if($this->bo->categories->read($parent) && $this->bo->categories->check_perms(EGW_ACL_READ, $parent)) {
-						$categories[$key]['tree'] = $parent. '/'. $categories[$key]['tree'];
-					}
+
+					// Don't know what this does, but it can cause tree issues (like when cat name = bookmark name)
+//					if($this->bo->categories->read($parent) && $this->bo->categories->check_perms(EGW_ACL_READ, $parent)) {
+//						$categories[$key]['tree'] = $parent. '/'. $categories[$key]['tree'];
+//					}
 					// Select a nonexisting key, in case the referenced cat doesn't exist.
 					$parcatkey = count($categories) + 1;
 					foreach ( $categories as $ikey => $icat ) {
