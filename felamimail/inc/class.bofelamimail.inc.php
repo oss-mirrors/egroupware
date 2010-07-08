9c02d252fb7ee94a339dd04f65a4da4b3b8bc18c
leithoff <leithoff@923e3c2e-ae13-0410-ba18-ced7211e33de>
2010-07-08 09:13:56 +0000

    quitening warning for mb-strpos, when attachment->cid is not set
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@31337 923e3c2e-ae13-0410-ba18-ced7211e33de

--- felamimail/inc/class.bofelamimail.inc.php
+++ felamimail/inc/class.bofelamimail.inc.php
@@ -1155,8 +1155,8 @@
 			#error_log("getAttachmentByCID:$_uid, $_cid, $_part");
 			$attachments = $this->getMessageAttachments($_uid, $_part);
 			foreach($attachments as $attachment) {
-				#error_log(print_r($attachment,true));
-				if(strpos($attachment['cid'], $_cid) !== false || strpos($cid, $attachment['cid']) !== false) {
+				//error_log(print_r($attachment,true));
+				if(isset($attachment['cid']) && (strpos($attachment['cid'], $_cid) !== false || strpos($_cid, $attachment['cid']) !== false)) {
 					$partID = $attachment['partID'];
 					break;
 				}
