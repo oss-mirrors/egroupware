53b20296a968853d5fde0f52a993d401ee0b85cb
ralfbecker <ralfbecker@923e3c2e-ae13-0410-ba18-ced7211e33de>
2010-08-19 15:50:17 +0000

    moved sieve support from fmail to emailadmin cyrusimap plugin, to use emailadmin imap plugin for that purpose too
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@31730 923e3c2e-ae13-0410-ba18-ced7211e33de

--- emailadmin/inc/class.emailadmin_bo.inc.php
+++ emailadmin/inc/class.emailadmin_bo.inc.php
@@ -571,6 +571,7 @@
 				if ($imapAuthType == 'email' || $icServer->loginType == 'email') {
 					$icServer->username = $icServer->loginName = $GLOBALS['egw_info']['user']['account_email'];
 				}
+				if (method_exists($icServer,'init')) $icServer->init();
 				$eaPreferences->setIncomingServer($icServer);
 
 				// fetch the SMTP / outgoing server data
@@ -606,7 +607,7 @@
 						$ogServer->password     = $GLOBALS['egw_info']['user']['passwd'];
 					}
 				}
-
+				if (method_exists($ogServer,'init')) $ogServer->init();
 				$eaPreferences->setOutgoingServer($ogServer);
 
 				foreach($ogServer->getAccountEmailAddress($GLOBALS['egw_info']['user']['account_lid']) as $emailAddresses)
