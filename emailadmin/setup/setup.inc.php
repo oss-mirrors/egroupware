e361ec26d84e41b006d6b9b58812486ef9a2552a
leithoff <leithoff@923e3c2e-ae13-0410-ba18-ced7211e33de>
2010-09-28 11:54:44 +0000

    make sure tables upgrade has the same columns as tables current, by refreshing the table (obsolet columns will get dropped)
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@32351 923e3c2e-ae13-0410-ba18-ced7211e33de

--- emailadmin/setup/setup.inc.php
+++ emailadmin/setup/setup.inc.php
@@ -12,7 +12,7 @@
 
 $setup_info['emailadmin']['name']      = 'emailadmin';
 $setup_info['emailadmin']['title']     = 'EMailAdmin';
-$setup_info['emailadmin']['version']   = '1.9.001';
+$setup_info['emailadmin']['version']   = '1.9.002';
 $setup_info['emailadmin']['app_order'] = 10;
 $setup_info['emailadmin']['enable']    = 2;
 $setup_info['emailadmin']['index']     = 'emailadmin.emailadmin_ui.listProfiles';
