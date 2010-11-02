f0b7749087cffc249dc433e23f0e65f0c7edd333
ralfbecker <ralfbecker@923e3c2e-ae13-0410-ba18-ced7211e33de>
2010-11-02 16:43:34 +0000

    * improved filecontents module to use regular expression or CSS selector (requires Zend Framework) to extract part of a html page
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@32786 923e3c2e-ae13-0410-ba18-ced7211e33de

--- sitemgr/modules/class.module_filecontents.inc.php
+++ sitemgr/modules/class.module_filecontents.inc.php
@@ -1,31 +1,87 @@
 <?php
-	/**************************************************************************\
-	* eGroupWare SiteMgr - Web Content Management                              *
-	* http://www.egroupware.org                                                *
-	* --------------------------------------------                             *
-	*  This program is free software; you can redistribute it and/or modify it *
-	*  under the terms of the GNU General Public License as published by the   *
-	*  Free Software Foundation; either version 2 of the License, or (at your  *
-	*  option) any later version.                                              *
-	\**************************************************************************/
+/**
+ * EGroupware SiteMgr CMS - filecontents module
+ *
+ * @link http://www.egroupware.org
+ * @package sitemgr
+ * @subpackage modules
+ * @author Ralf Becker <RalfBecker-AT-outdoor-training.de>
+ * @license http://opensource.org/licenses/gpl-license.php GPL - GNU General Public License
+ * @version $Id$
+ */
 
-	/* $Id$ */
-
-class module_filecontents extends Module 
+/**
+ * Filecontents module: displays/includes content of files or urls
+ *
+ * Regular expressions:
+ * @see http://www.php.net/manual/en/reference.pcre.pattern.syntax.php
+ *
+ * To replace the read content with an in brackets enclosed part, you have to make sure the
+ * regular expression matches the whole text, eg: '/^.*<body[^>]*>(.*)<\/body>.*$/si'
+ * ('/<body[^>]*>(.*)<\/body>/si' will only remove the body tags, not the rest before and after!)
+ * The 's' modifier is neccessary as .* does not match newlines without!
+ *
+ * The 'e' modifier (eval) is NOT allowed for security reasons!
+ *
+ * CSS query:
+ * If Zend Framework is install, you can also use a css query to get a part of the included html.
+ * Eg. "div#id" or "div.class"
+ */
+class module_filecontents extends Module
 {
-	function module_filecontents()
+	/**
+	 * Pearl regular expression to replace html page with content between body tags (replace='$1')
+	 *
+	 * @var string
+	 */
+	const GRAB_BODY = '/^.*<body[^>]*>(.*)<\/body>.*$/si';
+
+	/**
+	 * Constructor
+	 */
+	function __construct()
 	{
 		$this->arguments = array(
 			'filepath' => array(
-				'type' => 'textfield', 
+				'type' => 'textfield',
 				'label' => lang('The complete URL or path to a file to be included'),
 				'params' => array('size' => 50),
-			)
+			),
+			'preg' => array(
+				'type' => 'textfield',
+				'label' => lang('Regular expression, if only parts should be used').'<br />'.
+					lang('default for html is "%1"',self::GRAB_BODY),
+				'params' => array('size' => 50),
+			),
+			'replace' => array(
+				'type' => 'textfield',
+				'label' => lang('Replace above regular expressions with').'<br />'.
+					lang('default %1 (content of first brackets)','$1'),
+				'params' => array('size' => 50),
+			),
 		);
+		// if Zend Framework is installed, allow css queries to get parts of html
+		if (@include_once('Zend/Dom/Query.php'))
+		{
+			$this->arguments['css_query'] = array(
+				'type' => 'textfield',
+				'label' => lang('CSS selector for part of html (does NOT use regular expression)').'<br />'.
+					lang('eg. %1 or %2','"div#id", "table.classname"','"div[attr=\'value\'] > h1"'),
+				'params' => array('size' => 50),
+			);
+		}
 		$this->title = lang('File contents');
 		$this->description = lang('This module includes the contents of an URL or file (readable by the webserver and in its docroot !)');
 	}
 
+	/**
+	 * Get module content
+	 *
+	 * @see Module::get_content()
+	 * @param array &$arguments
+	 * @param array $properties
+	 * @return string
+	 */
 	function get_content(&$arguments,$properties)
 	{
 		$url = parse_url($path = $arguments['filepath']);
@@ -39,13 +95,13 @@ class module_filecontents extends Module
 			return $this->validation_error;
 		}
 		$is_html = preg_match('/\.html?$/i',$path);
-		
+
 		if ($this->is_script($path) || @$url['scheme'])
 		{
 			if (!@$url['scheme'])
 			{
 				$path = ($_SERVER['HTTPS'] ? 'https://' : 'http://') .
-					($url['hostname'] ? $url['hostname'] : $_SERVER['HTTP_HOST']) .
+					($url['host'] ? $url['host'] : $_SERVER['HTTP_HOST']) .
 					str_replace($_SERVER['DOCUMENT_ROOT'],'',$path);
 			}
 			if ($fp = fopen($path,'rb'))
@@ -67,28 +123,56 @@ class module_filecontents extends Module
 		{
 			$ret = file_get_contents($path);
 		}
-		if ($is_html)
+		if ($is_html && preg_match('/<meta http-equiv="content-type" content="text\/html; ?charset=([^"]+)"/i',$ret,$parts))
 		{
-			$one_line = str_replace("\n",'\\n',$ret);
-			// only use what's between the body tags
-			if (preg_match('/<body[^>]*>(.*)<\/body>/i',$one_line,$parts))
+			$ret = translation::convert($ret,$parts[1]);
+		}
+
+		// for html use css query if given AND Zend Framework available
+		if ($is_html && $arguments['css_query'] && @include_once('Zend/Dom/Query.php'))
+		{
+			$dom = new Zend_Dom_Query($ret);
+			$ret = '';
+			foreach($dom->query($arguments['css_query']) as $element)
 			{
-				$ret = str_replace('\\n',"\n",$parts[1]);
-			}
-			if (preg_match('/<meta http-equiv="content-type" content="text\/html; ?charset=([^"]+)"/i',$one_line,$parts))
-			{
-				$ret = $GLOBALS['egw']->translation->convert($ret,$parts[1]);
+				$ret .= simplexml_import_dom($element)->asXML()."\n";
 			}
 		}
-		elseif(substr($path,-4) == '.txt')
+		else
+		{
+			// use given regular expression (or default) to use only part of the content
+			$preg = $arguments['preg'];
+			// html default: only use what's between the body tags
+			if ($is_html && empty($preg)) $preg = self::GRAB_BODY;
+
+			if (!empty($preg))
+			{
+				$ret = preg_replace($preg, empty($arguments['replace']) ? '$1' : $arguments['replace'], $ret);
+			}
+		}
+
+		// replace images and links with correct host
+		if ($is_html && ($url['scheme'] == 'http' || $url['scheme'] == 'https'))
+		{
+			$ret = strtr($ret,array(
+				'src="/' => 'src="'.$url['scheme'].'://'.$url['host'].'/',
+				'href="/' => 'href="'.$url['scheme'].'://'.$url['host'].'/',
+			));
+		}
+
+		if(substr($path,-4) == '.txt')
 		{
 			$ret = "<pre>\n$ret\n</pre>\n";
 		}
 		return $ret;
 	}
 
-	// test if $path lies within the webservers document-root
-	//
+	/**
+	 * test if $path lies within the webservers document-root
+	 *
+	 * @param string $path
+	 * @return boolean
+	 */
 	function in_docroot($path)
 	{
 		$docroots = array(EGW_SERVER_ROOT,$_SERVER['DOCUMENT_ROOT']);
@@ -111,6 +195,12 @@ class module_filecontents extends Module
 		return False;
 	}
 
+	/**
+	 * Check if url refers to a script
+	 *
+	 * @param string $url
+	 * @return boolean
+	 */
 	function is_script($url)
 	{
 		$url = parse_url($url);
@@ -118,8 +208,25 @@ class module_filecontents extends Module
 		return preg_match('/\.(php.?|pl|py)$/i',$url['path']);
 	}
 
+	/**
+	 * Validate given parameters: url or path and regular expression
+	 *
+	 * @see Module::validate()
+	 * @param array &$data arguments of block
+	 * @return boolean
+	 */
 	function validate(&$data)
 	{
+		// check if regular expression contains /e modifier, we can NOT allow that!
+		if (!empty($data['preg']))
+		{
+			$parts = explode($data['preg'][0],$data['preg']);
+			if (strpos(array_pop($parts),'e') !== false)
+			{
+				$this->validation_error = lang('Regular expression modifier "%1" in "%2" is NOT allowed!','e',htmlspecialchars($data['preg']));
+				return false;
+			}
+		}
 		$url = parse_url($data['filepath']);
 		$allow_url_fopen = ini_get('allow_url_fopen');
 
