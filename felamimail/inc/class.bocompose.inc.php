7a122f94d250085d261819da0f0ad0fdd0eb98ca
leithoff <leithoff@923e3c2e-ae13-0410-ba18-ced7211e33de>
2011-01-12 11:59:50 +0000

    * eMail: email to infolog -> fix for not decoded senderinformation
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@33593 923e3c2e-ae13-0410-ba18-ced7211e33de

--- felamimail/inc/class.bocompose.inc.php
+++ felamimail/inc/class.bocompose.inc.php
@@ -1121,8 +1121,8 @@ error_log(__METHOD__.__LINE__.$BCCmail.$mail->getMessageHeader().$mail->getMessa
 			}
 			if (is_array($this->sessionData['cc'])) $mailaddresses['cc'] = $this->sessionData['cc'];
 			if (is_array($this->sessionData['bcc'])) $mailaddresses['bcc'] = $this->sessionData['bcc'];
-			if (!empty($mailaddresses)) $mailaddresses['from'] = $fromAddress;
-			// attention: we dont return from infolog/trackere. You cannot check both. cleanups will be done there.
+			if (!empty($mailaddresses)) $mailaddresses['from'] = $GLOBALS['egw']->translation->decodeMailHeader($fromAddress);
+			// attention: we dont return from infolog/tracker. You cannot check both. cleanups will be done there.
 			if ($_formData['to_infolog'] == 'on') {
 				$uiinfolog =& CreateObject('infolog.infolog_ui');
 				$uiinfolog->import_mail(
