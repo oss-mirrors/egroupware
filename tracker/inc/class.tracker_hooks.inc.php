0c3ac70ed135ba6b98ade3753b2b098101a2315e
nathangray <nathangray@923e3c2e-ae13-0410-ba18-ced7211e33de>
2011-02-22 19:46:26 +0000

    Get list of extensions from parent class
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@33879 923e3c2e-ae13-0410-ba18-ced7211e33de

--- tracker/inc/class.tracker_hooks.inc.php
+++ tracker/inc/class.tracker_hooks.inc.php
@@ -201,7 +201,7 @@ class tracker_hooks
 				'name'   => 'document_dir',
 				'help'   => lang('If you specify a directory (full vfs path) here, eGroupWare displays an action for each document. That action allows to download the specified document with the %1 data inserted.', lang('tracker')).' '.
 					lang('The document can contain placeholder like {{tr_summary}}, to be replaced with the contact data (%1full list of placeholder names%2).','<a href="'.$link.'" target="_blank">','</a>').' '.
-					lang('At the moment the following document-types are supported:').'*.rtf, *.txt',
+					lang('At the moment the following document-types are supported:') . implode(',',bo_merge::get_file_extensions()),
 				'run_lang' => false,
 				'xmlrpc' => True,
 				'admin'  => False,
