a65319cc782479651ffd10414dc86be43aea77c0
leithoff <leithoff@923e3c2e-ae13-0410-ba18-ced7211e33de>
2011-03-17 13:57:34 +0000

    fix typo
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@34196 923e3c2e-ae13-0410-ba18-ced7211e33de

--- tracker/inc/class.tracker_import_csv.inc.php
+++ tracker/inc/class.tracker_import_csv.inc.php
@@ -147,7 +147,7 @@ class tracker_import_csv implements importexport_iface_import_plugin  {
 			'tr_tracker'    => $this->bo->trackers,
 			'tr_version'    => $this->bo->get_tracker_labels('version', null),
 			'tr_status'     => $this->bo->get_tracker_stati(null),
-			'tr_resolution' => $this->get_tracker_labels('resolution',null),
+			'tr_resolution' => $this->bo->get_tracker_labels('resolution',null),
 			'cat_id'	=> $this->bo->get_tracker_labels('cat', null)
 		);
 		foreach($lookups['tr_tracker'] as $id => $name) {
