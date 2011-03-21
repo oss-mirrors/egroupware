d3ba4593e1fc8ff710ec56024e4cea5c03dfad97
nathangray <nathangray@923e3c2e-ae13-0410-ba18-ced7211e33de>
2011-03-21 22:29:52 +0000

    Hide category action choice, replace is the only one that makes sense
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@34310 923e3c2e-ae13-0410-ba18-ced7211e33de

--- tracker/inc/class.tracker_wizard_import_csv.inc.php
+++ tracker/inc/class.tracker_wizard_import_csv.inc.php
@@ -79,6 +79,14 @@ class tracker_wizard_import_csv extends importexport_wizard_basic_import_csv
 		);
 	}
 
+	function wizard_step40(&$content, &$sel_options, &$readonlys, &$preserv)
+	{
+		$result = parent::wizard_step40($content, $sel_options, $readonlys, $preserv);
+		// Hide category choice, replace is the only one that makes sense
+		$content['no_cats'] = true;
+		return $result;
+	}
+
 	function wizard_step45(&$content, &$sel_options, &$readonlys, &$preserv)
 	{
 		if($this->debug) error_log(__METHOD__.'->$content '.print_r($content,true));
