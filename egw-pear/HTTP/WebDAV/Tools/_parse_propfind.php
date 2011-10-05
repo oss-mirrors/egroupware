d95e7a22e402de67c9110c7cc4b7482511b4508e
ralfbecker <ralfbecker@923e3c2e-ae13-0410-ba18-ced7211e33de>
2011-10-05 07:15:52 +0000

    added attributes to filter, required eg. for <filter test="(anyof|alloff)"> in CardDAV
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@36816 923e3c2e-ae13-0410-ba18-ced7211e33de

--- egw-pear/HTTP/WebDAV/Tools/_parse_propfind.php
+++ egw-pear/HTTP/WebDAV/Tools/_parse_propfind.php
@@ -206,6 +206,7 @@ class _parse_propfind
            			break;
             	case 'filter':
             		$this->use = 'filters';
+            		$this->filters['attrs'] = $attrs;	// need attrs eg. <filters test="(anyof|alloff)">
             		break;
             	default:
             		$this->use = 'other';
