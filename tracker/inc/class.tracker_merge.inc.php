50c328ac59bb03002d48c1b82fb98f8a07e5f5aa
nathangray <nathangray@923e3c2e-ae13-0410-ba18-ced7211e33de>
2011-10-26 16:45:38 +0000

    Add {{links}}, {{attachments}}, {{links_attachments}} placeholders
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@37063 923e3c2e-ae13-0410-ba18-ced7211e33de

--- tracker/inc/class.tracker_merge.inc.php
+++ tracker/inc/class.tracker_merge.inc.php
@@ -110,6 +110,11 @@ class tracker_merge extends bo_merge
 			if(!$array['#'.$name]) $array['#'.$name] = '';
 		}
 
+		// Links
+		$array['links'] = $this->get_links('tracker', $id, '!'.egw_link::VFS_APPNAME);
+ 		$array['attachments'] = $this->get_links('tracker', $id, egw_link::VFS_APPNAME);
+		$array['links_attachments'] = $this->get_links('tracker', $id);
+
 		// Add markers
 		foreach($array as $key => &$value)
 		{
@@ -206,6 +211,9 @@ class tracker_merge extends bo_merge
 
 		echo '<tr><td colspan="4"><h3>'.lang('General fields:')."</h3></td></tr>";
 		foreach(array(
+			'links' => lang('Titles of any entries linked to the current record, excluding attached files'),
+ 			'attachments' => lang('List of files linked to the current record'),
+			'links_attachments' => lang('Links and attached files'),
 			'date' => lang('Date'),
 			'user/n_fn' => lang('Name of current user, all other contact fields are valid too'),
 			'user/account_lid' => lang('Username'),
