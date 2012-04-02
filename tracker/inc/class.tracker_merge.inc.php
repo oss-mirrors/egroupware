c23dc2355f0b235f9b838b99ecd66a8d72f900a2
nathangray <nathangray@923e3c2e-ae13-0410-ba18-ced7211e33de>
2012-04-02 22:16:07 +0000

    Fix missing all queues signature
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@38750 923e3c2e-ae13-0410-ba18-ced7211e33de

--- tracker/inc/class.tracker_merge.inc.php
+++ tracker/inc/class.tracker_merge.inc.php
@@ -106,7 +106,7 @@ class tracker_merge extends bo_merge
 		$array = array();
 
 		// Signature
-		if($this->bo->notification[$record->tr_tracker]['use_signature'])
+		if($this->bo->notification[$record->tr_tracker]['use_signature'] || $this->bo->notification[0]['use_signature'])
 		{
 			if(trim(strip_tags($this->bo->notification[$record->tr_tracker]['signature'])))
 			{
