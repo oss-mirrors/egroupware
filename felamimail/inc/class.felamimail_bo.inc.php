6d9c3347811f32aecc4643a8517ad33f14961fa4
leithoff <leithoff@923e3c2e-ae13-0410-ba18-ced7211e33de>
2013-01-29 15:01:29 +0000

    fix Fatal Error for call of getTemplateFolder
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@41468 923e3c2e-ae13-0410-ba18-ced7211e33de

--- felamimail/inc/class.felamimail_bo.inc.php
+++ felamimail/inc/class.felamimail_bo.inc.php
@@ -2125,7 +2125,7 @@ class felamimail_bo
 									$createfolder=false;
 								break;
 							case 'Templates': //] => Vorlagen
-								$templateFolder = $this->bofelamimail->getTemplateFolder();
+								$templateFolder = $this->getTemplateFolder();
 								if ($templateFolder && $templateFolder=='none')
 									$createfolder=false;
 								break;
