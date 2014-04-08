dd35898028b37c963762430371b5ba208e50a814
ralfbecker <ralfbecker@923e3c2e-ae13-0410-ba18-ced7211e33de>
2014-04-08 14:26:50 +0000

    Move outdated app to old
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@46558 923e3c2e-ae13-0410-ba18-ced7211e33de

--- mydms/js/jscode/folderChooser.js
+++
@@ -1,7 +0,0 @@
-function onNodeSelect(_nodeID)
-{
-	targetName.value = tree.getSelectedItemText();
-	targetID.value = _nodeID;
-	window.close();
-	return true;
-}
--- mydms/runNotifyMailer.bat
+++
@@ -1,3 +0,0 @@
-cd E:\web\php\dms\app\op
-
-c:\programme\php4.2\php-cli.exe op.NotifyMailer.php > notifyMailer.log.html
