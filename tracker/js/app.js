22039203cfd19b9ff99e5a593c925a9f1d6d9178
hnategh <hnategh@923e3c2e-ae13-0410-ba18-ced7211e33de>
2014-05-26 14:01:18 +0000

    Remove _targetapp argument from observer and add _links. Additionally, make sure if the application, fired observer, is linked and needs to be refreshed
    git-svn-id: http://svn.egroupware.org/egroupware/trunk@47079 923e3c2e-ae13-0410-ba18-ced7211e33de

--- tracker/js/app.js
+++ tracker/js/app.js
@@ -80,16 +80,20 @@ app.classes.tracker = AppJS.extend(
 	 * - delete: just delete the given rows clientside (no server interaction neccessary)
 	 * - add: requires full reload for proper sorting
 	 * @param {string} _msg_type 'error', 'warning' or 'success' (default)
-	 * @param {string} _targetapp which app's window should be refreshed, default current
+	 * @param {object|null} _links app => array of ids of linked entries
+	 * or null, if not triggered on server-side, which adds that info
 	 */
-	observer: function(_msg, _app, _id, _type, _msg_type, _targetapp)
+	observer: function(_msg, _app, _id, _type, _msg_type, _links)
 	{
-		switch (_app)
-		{
-			case 'timesheet':
-				var nm = this.et2 ? this.et2.getWidgetById('nm') : null;
-				if (nm) nm.applyFilters();
-				break;
+		if (typeof _links['tracker'] != 'undefined')
+		{	
+			switch (_app)
+			{
+				case 'timesheet':
+					var nm = this.et2 ? this.et2.getWidgetById('nm') : null;
+					if (nm) nm.applyFilters();
+					break;
+			}
 		}
 	},
 	
